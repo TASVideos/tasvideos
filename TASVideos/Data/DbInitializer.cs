9fde90d248b9429441d2ed3994bf07784364d438
adelikat <adelikat@tasvideos.org>
2017-10-30 17:07:23 -0500

    Inititial commit, .net core2 mvc app with boilerplate, EF set up, some tinkering on master layout

---
+++ TASVideos/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Data
+{
+	public static class DbInitializer
+	{
+		public static void Initialize(ApplicationDbContext context)
+		{
+			context.Database.EnsureCreated();
+
+			if (context.Publications.Any())
+			{
+				return;   // DB has been seeded
+			}
+
+			var publications = new Publication[]
+			{
+
+				new Publication { DummyProperty = "dummy1" },
+				new Publication { DummyProperty = "dummy2" },
+				new Publication { DummyProperty = "dummy3" },
+			};
+
+			foreach (var p in publications)
+			{
+				context.Publications.Add(p);
+			}
+
+			context.SaveChanges();
+		}
+	}
+}
