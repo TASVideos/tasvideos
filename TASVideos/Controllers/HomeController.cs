d3b30eaaf069a3da2089ddc07cd1e709d120253e
adelikat <adelikat@tasvideos.org>
2017-10-30 17:07:23 -0500

    Inititial commit, .net core2 mvc app with boilerplate, EF set up, some tinkering on master layout

---
+++ TASVideos/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Models;
+using TASVideos.Data;
+
+namespace TASVideos.Controllers
+{
+	public class HomeController : Controller
+	{
+
+		private readonly ApplicationDbContext _db;
+
+		public HomeController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IActionResult Index()
+		{
+			var dummyData = _db.Publications.ToList();
+			return View(dummyData);
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
