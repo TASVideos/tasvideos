617d7fc900b68882dfa76d1670920bb0aa8415dd
adelikat <adelikat@tasvideos.org>
2017-11-03 19:31:11 -0500

    remove ApplicationUser

--- TASVideos/Controllers/AccountController.cs
+++ TASVideos/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TASVideos.Data.Entity;
 using TASVideos.Models;
 using TASVideos.Models.AccountViewModels;
 using TASVideos.Services;
@@ -20,14 +21,14 @@ namespace TASVideos.Controllers
 	[Route("[controller]/[action]")]
 	public class AccountController : Controller
 	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly SignInManager<ApplicationUser> _signInManager;
+		private readonly UserManager<User> _userManager;
+		private readonly SignInManager<User> _signInManager;
 		private readonly IEmailSender _emailSender;
 		private readonly ILogger _logger;
 
 		public AccountController(
-			UserManager<ApplicationUser> userManager,
-			SignInManager<ApplicationUser> signInManager,
+			UserManager<User> userManager,
+			SignInManager<User> signInManager,
 			IEmailSender emailSender,
 			ILogger<AccountController> logger)
 		{
@@ -220,14 +221,14 @@ namespace TASVideos.Controllers
 			ViewData["ReturnUrl"] = returnUrl;
 			if (ModelState.IsValid)
 			{
-				var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+				var user = new User { UserName = model.Email, Email = model.Email };
 				var result = await _userManager.CreateAsync(user, model.Password);
 				if (result.Succeeded)
 				{
 					_logger.LogInformation("User created a new account with password.");
 
 					var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-					var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
+					var callbackUrl = Url.EmailConfirmationLink(user.Id.ToString() /* TODO: this might be bad? We could add a random guid in the user table*/, code, Request.Scheme);
 					await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
 					await _signInManager.SignInAsync(user, isPersistent: false);
@@ -310,7 +311,7 @@ namespace TASVideos.Controllers
 				{
 					throw new ApplicationException("Error loading external login information during confirmation.");
 				}
-				var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+				var user = new User { UserName = model.Email, Email = model.Email };
 				var result = await _userManager.CreateAsync(user);
 				if (result.Succeeded)
 				{
@@ -370,7 +371,7 @@ namespace TASVideos.Controllers
 				// For more information on how to enable account confirmation and password reset please
 				// visit https://go.microsoft.com/fwlink/?LinkID=532713
 				var code = await _userManager.GeneratePasswordResetTokenAsync(user);
-				var callbackUrl = Url.ResetPasswordCallbackLink(user.Id, code, Request.Scheme);
+				var callbackUrl = Url.ResetPasswordCallbackLink(user.Id.ToString() /*TODO*/, code, Request.Scheme);
 				await _emailSender.SendEmailAsync(model.Email, "Reset Password",
 				   $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
 				return RedirectToAction(nameof(ForgotPasswordConfirmation));
