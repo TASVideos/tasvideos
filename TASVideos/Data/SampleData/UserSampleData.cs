bc7c386a41677f1ce5e511db06ba3be9ded338fe
adelikat <adelikat@tasvideos.org>
2017-11-04 12:00:27 -0500

    create sample users

---
+++ TASVideos/Data/SampleData/UserSampleData.cs
@@ -0,0 +1,20 @@
+﻿using TASVideos.Data.Entity;
+
+namespace TASVideos.Data.SampleData
+{
+	// User data is sample data not seed data because we do not want to go to production with known credentials
+	public class UserSampleData
+	{
+		public static User[] Users =
+		{
+			new User
+			{
+				UserName = "adelikat",
+				NormalizedUserName = "ADELIKAT",
+				Email = "adelikat@tasvideos.org"
+			}
+		};
+
+		public const string SamplePassword = "Password1234!@#$"; // Obviously no one should use this in production
+	}
+}
