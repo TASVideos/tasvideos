cf9e14bb732774ad7207401d32a6c0fc1ffa316c
adelikat <adelikat@tasvideos.org>
2017-11-04 21:45:13 -0500

    Rip out about and contact boiler plate pages since these will become wiki pages eventually

--- TASVideos/Controllers/HomeController.cs
+++ TASVideos/Controllers/HomeController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 using TASVideos.Models;
 using TASVideos.Data;
 
@@ -12,7 +11,6 @@ namespace TASVideos.Controllers
 {
 	public class HomeController : Controller
 	{
-
 		private readonly ApplicationDbContext _db;
 
 		public HomeController(ApplicationDbContext db)
@@ -28,20 +26,6 @@ namespace TASVideos.Controllers
 
 			ViewData["Users"] = _db.Users.ToList();
 			return View(allRoles);
-		}
-
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
-
-			return View();
-		}
-
-		public IActionResult Contact()
-		{
-			ViewData["Message"] = "Your contact page.";
-
-			return View();
 		}
 
 		public IActionResult Error()
