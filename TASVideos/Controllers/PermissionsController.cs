356c01ab72aeaaace6e08e633a1e0bccb8cd5e45
adelikat <adelikat@tasvideos.org>
2017-11-07 18:07:04 -0600

    Permission/Edit - ability to edit, fixups to Permission/Index code

--- TASVideos/Controllers/PermissionsController.cs
+++ TASVideos/Controllers/PermissionsController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TASVideos.Data.Entity;
 using TASVideos.Tasks;
 using TASVideos.Filter;
+using TASVideos.Models;
 
 namespace TASVideos.Controllers
 {
@@ -22,14 +25,32 @@ namespace TASVideos.Controllers
 		public IActionResult Index()
 		{
 			var model = _permissionTasks.GetAllPermissionsForDisplay();
-
 			return View(model);
 		}
 
 		[RequirePermission(PermissionTo.EditPermissionDetails)]
 		public IActionResult Edit()
 		{
-			var model = _permissionTasks.GetAllPermissionsForDisplay();
+			var model = _permissionTasks.GetAllPermissionsForEdit()
+				.ToList();
+			return View(model);
+		}
+
+		[RequirePermission(PermissionTo.EditPermissionDetails)]
+		[HttpPost]
+		public IActionResult Edit(IEnumerable<PermissionEditViewModel> model)
+		{
+			if (model == null)
+			{
+				ModelState.AddModelError("", "No permissions were supplied");
+			}
+
+			if (ModelState.IsValid)
+			{
+				_permissionTasks.UpdatePermissionDetails(model);
+				return RedirectToAction(nameof(Index));
+			}
+
 			return View(model);
 		}
     }
