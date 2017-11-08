515bb57ed8a7a347184b12d9da90c4ad7d0bf2a9
adelikat <adelikat@tasvideos.org>
2017-11-07 18:19:17 -0600

    Permissions edit - validation

--- TASVideos/Models/PermissionViewModels.cs
+++ TASVideos/Models/PermissionViewModels.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using TASVideos.Data.Entity;
@@ -24,12 +25,15 @@ namespace TASVideos.Models
 		public PermissionTo Id { get; set; }
 
 		[Required]
+		[StringLength(50, MinimumLength = 1)]
 		public string Name { get; set; }
 
 		[Required]
+		[StringLength(200, MinimumLength = 1)]
 		public string Description { get; set; }
 
 		[Required]
+		[StringLength(20, MinimumLength = 1)]
 		public string Group { get; set; }
 	}
 }
