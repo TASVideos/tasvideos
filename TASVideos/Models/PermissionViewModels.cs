c4c580ca7be340f7a44f7f10b99b81661ae9793f
adelikat <adelikat@tasvideos.org>
2017-11-07 18:07:04 -0600

    Permission/Edit - ability to edit, fixups to Permission/Index code

--- TASVideos/Models/PermissionViewModels.cs
+++ TASVideos/Models/PermissionViewModels.cs
@@ -1,15 +1,35 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TASVideos.Data.Entity;
 
 namespace TASVideos.Models
 {
 	/// <summary>
-	/// Presents a Permission entry for a permission display page
+	/// Represents a Permission entry for the purpose of display
 	/// </summary>
-	public class PermissionViewModel
+	public class PermissionDisplayViewModel
 	{
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public string Group { get; set; }
 		public IEnumerable<string> Roles { get; set; } = new List<string>();
+	}
+
+	/// <summary>
+	/// Represents a Permission entry for the purpose of editing
+	/// </summary>
+	public class PermissionEditViewModel
+	{
+		[Required]
+		public PermissionTo Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Description { get; set; }
+
+		[Required]
+		public string Group { get; set; }
 	}
 }
--- TASVideos/Tasks/PermissionTasks.cs
+++ TASVideos/Tasks/PermissionTasks.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using TASVideos.Data;
@@ -15,13 +16,15 @@ namespace TASVideos.Tasks
 			_db = db;
 		}
 
-		// TODO: document
-		public IEnumerable<PermissionViewModel> GetAllPermissionsForDisplay()
+		/// <summary>
+		/// Returns all of the <see cref="TASVideos.Data.Entity.Permission" /> records for the purpose of display
+		/// </summary>
+		public IEnumerable<PermissionDisplayViewModel> GetAllPermissionsForDisplay()
 		{
-			var permissions = _db.Permissions
+			return _db.Permissions
 				.Include(p => p.RolePermission)
-				.Include("RolePermission.Role") // TODO: figure out the lambda version of child of child in EF Core, EF 6 would allow this: .Include(p => p.RolePermission.Select(rp => rp.Role))
-				.Select(p => new PermissionViewModel
+				.ThenInclude(rp => rp.Role)
+				.Select(p => new PermissionDisplayViewModel
 				{
 					Name = p.Name,
 					Description = p.Description,
@@ -31,8 +34,52 @@ namespace TASVideos.Tasks
 				.OrderBy(p => p.Group)
 				.ThenBy(p => p.Name)
 				.ToList();
+		}
 
-			return permissions;
+		/// <summary>
+		/// Returns all of the <see cref="TASVideos.Data.Entity.Permission" /> records for the purpose of editing the metadata
+		/// </summary>
+		public IEnumerable<PermissionEditViewModel> GetAllPermissionsForEdit()
+		{
+			return _db.Permissions
+				.Select(p => new PermissionEditViewModel
+				{
+					Id = p.Id,
+					Name = p.Name,
+					Description = p.Description,
+					Group = p.Group,
+				})
+				.OrderBy(p => p.Group)
+				.ThenBy(p => p.Name)
+				.ToList();
+		}
+
+		/// <summary>
+		/// Updates all of the given <see cref="TASVideos.Data.Entity.Permission" /> records
+		/// </summary>
+		public void UpdatePermissionDetails(IEnumerable<PermissionEditViewModel> model)
+		{
+			if (model == null)
+			{
+				throw new ArgumentException($"{nameof(model)} can not be null");
+			}
+
+			var newPermisions = model.ToList();
+
+			var permissions = _db.Permissions
+				.Where(p => newPermisions.Select(np => np.Id).Contains(p.Id))
+				.ToList();
+
+			foreach (var permission in permissions)
+			{
+				var permModel = newPermisions.Single(p => p.Id == permission.Id);
+
+				permission.Name = permModel.Name;
+				permission.Description = permModel.Description;
+				permission.Group = permModel.Group;
+			}
+
+			_db.SaveChanges();
 		}
 	}
 }
