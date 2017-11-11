5d4143d62630aa8868b20967dc2ecfbaefc053b4
adelikat <adelikat@tasvideos.org>
2017-11-10 22:19:54 -0600

    wire up a taghelper

---
+++ TASVideos/TagHelpers/BootstrapTagHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers
+{
+    public class RowTagHelper : TagHelper
+    {
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "div";
+			output.AddCssClass("row");
+		}
+	}
+
+	public static class TagHelperOutputExtensions
+	{
+		public static void AddCssClass(this TagHelperOutput output, string className)
+		{
+			var existingClass = output.Attributes.FirstOrDefault(a => a.Name == "class");
+			var cssClass = existingClass?.Value.ToString() ?? "";
+			cssClass += $" {className}";
+			var ta = new TagHelperAttribute("class", cssClass);
+			if (existingClass != null)
+			{
+				output.Attributes.Remove(existingClass);
+			}
+
+			output.Attributes.Add(ta);
+		}
+	}
+}
