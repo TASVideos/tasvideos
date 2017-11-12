9bbef846b6fb973f7f06dce5b4f74b1f2000c89f
adelikat <adelikat@tasvideos.org>
2017-11-12 08:35:24 -0600

    remove prefix AspNet on identity tables

--- TASVideos/Data/Entity/User.cs
+++ TASVideos/Data/Entity/User.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
 namespace TASVideos.Data.Entity
 {
+	[Table(nameof(User))]
 	public class User : IdentityUser<int>
 	{
 	}
