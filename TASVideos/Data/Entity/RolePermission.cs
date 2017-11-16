c132443765686b174e77b7b72bf55ae26258d856
adelikat <adelikat@tasvideos.org>
2017-11-15 19:29:29 -0600

    put RolePermission in its own file

---
+++ TASVideos/Data/Entity/RolePermission.cs
@@ -0,0 +1,11 @@
+﻿namespace TASVideos.Data.Entity
+{
+	public class RolePermission
+	{
+		public int RoleId { get; set; }
+		public Role Role { get; set; }
+
+		public PermissionTo PermissionId { get; set; }
+		public Permission Permission { get; set; }
+	}
+}
