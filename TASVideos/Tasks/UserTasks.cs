891991b6cad0cfe12b0b33e0f62f3d699fd49a00
adelikat <adelikat@tasvideos.org>
2017-11-15 20:12:11 -0600

    User stuff

--- TASVideos/Tasks/UserTasks.cs
+++ TASVideos/Tasks/UserTasks.cs
@@ -36,20 +36,9 @@ namespace TASVideos.Tasks
 				.ToListAsync();
 		}
 
-		private IQueryable<PermissionTo> GetUserPermissionByIdQuery(int userId)
-		{
-			return _db.Users
-				.Include(u => u.UserRoles)
-				.ThenInclude(u => u.Role)
-				.ThenInclude(r => r.RolePermission)
-				.ThenInclude(rp => rp.Permission)
-				.Where(u => u.Id == userId)
-				.SelectMany(u => u.UserRoles)
-				.SelectMany(ur => ur.Role.RolePermission)
-				.Select(rp => rp.PermissionId)
-				.Distinct();
-		}
-
+		/// <summary>
+		/// Gets a list of <see cref="Role"/>s that the given user currently has
+		/// </summary>
 		public async Task<IEnumerable<RoleBasicDisplay>> GetUserRoles(int userId)
 		{
 			return await _db.Users
@@ -67,7 +56,9 @@ namespace TASVideos.Tasks
 				.ToListAsync();
 		}
 
-		// TODO: document
+		/// <summary>
+		/// Gets a list of <see cref="User"/>s for the purpose of a user list
+		/// </summary>
 		public PageOf<UserListViewModel> GetPageOfUsers(PagedModel paging)
 		{
 			var data = _db.Users
@@ -92,7 +83,11 @@ namespace TASVideos.Tasks
 			return paged;
 		}
 
-		// TODO: document, for the read-only user view page
+		/// <summary>
+		/// Gets a <see cref="User"/> for the purpose of viewing
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
 		public async Task<UserDetailsViewModel> GetUserDetails(int id)
 		{
 			return await _db.Users
@@ -105,41 +100,69 @@ namespace TASVideos.Tasks
 				.SingleAsync(u => u.Id == id);
 		}
 
-		// TODO: document, for the User/Edit screen
+		/// <summary>
+		/// Returns a <see cref="User"/>  with the given id for the purpose of editing
+		/// </summary>
 		public async Task<UserEditViewModel> GetUserForEdit(int id)
 		{
-			// TODO: BeginTransaction
-			var model = await _db.Users
-				.Select(u => new UserEditViewModel
-				{
-					Id = u.Id,
-					UserName = u.UserName,
-					Email = u.Email,
-					EmailConfirmed = u.EmailConfirmed,
-					IsLockedOut = u.LockoutEnabled && u.LockoutEnd.HasValue
-				})
-				.SingleAsync(u => u.Id == id);
+			using (await _db.Database.BeginTransactionAsync())
+			{
+				var model = await _db.Users
+					.Select(u => new UserEditViewModel
+					{
+						Id = u.Id,
+						UserName = u.UserName,
+						Email = u.Email,
+						EmailConfirmed = u.EmailConfirmed,
+						IsLockedOut = u.LockoutEnabled && u.LockoutEnd.HasValue,
+					})
+					.SingleAsync(u => u.Id == id);
 
-			model.AvailableRoles = await _db.Roles
-				.Select(r => new SelectListItem
-				{
-					Value = r.Id.ToString(),
-					Text = r.Name,
-					Selected = model.SelectedRoles.Contains(r.Id)
-				})
-				.ToListAsync();
+				model.SelectedRoles = await _db.UserRoles
+					.Where(ur => ur.UserId == id)
+					.Select(ur => ur.RoleId)
+					.ToListAsync();
 
-			return model;
+				model.AvailableRoles = await _db.Roles
+					.Select(r => new SelectListItem
+					{
+						Value = r.Id.ToString(),
+						Text = r.Name,
+						Selected = model.SelectedRoles.Contains(r.Id)
+					})
+					.ToListAsync();
+
+				return model;
+			}
 		}
 
-		// TODO: document
-		public async Task EditUser(UserEditViewModel model)
+		/// <summary>
+		/// Updates the given <see cref="User"/>
+		/// </summary>
+		public async Task EditUser(UserEditPostViewModel model)
 		{
 			var user = await _db.Users.SingleAsync(u => u.Id == model.Id);
-			// TODO edit and save
+			if (model.UserName != user.UserName)
+			{
+				user.UserName = model.UserName;
+			}
+			
+			_db.UserRoles.RemoveRange(_db.UserRoles.Where(ur => ur.User == user));
+			await _db.SaveChangesAsync();
+
+			_db.UserRoles.AddRange(model.SelectedRoles
+				.Select(r => new UserRole
+				{
+					User = user,
+					RoleId = r
+				}));
+
+			await _db.SaveChangesAsync();
 		}
 
-		// TODO: document
+		/// <summary>
+		/// Removes the lock out property on a <see cref="User"/>
+		/// </summary>
 		public async Task UnlockUser(int id)
 		{
 			var user = await _db.Users.SingleAsync(u => u.Id == id);
@@ -147,10 +170,27 @@ namespace TASVideos.Tasks
 			await _db.SaveChangesAsync();
 		}
 
+		/// <summary>
+		/// Checks if the given user name already exists in the database
+		/// </summary>
 		public async Task<bool> CheckUserNameExists(string userName)
 		{
 			return await _db.Users
 				.AnyAsync(u => u.UserName == userName);
+		}
+
+		private IQueryable<PermissionTo> GetUserPermissionByIdQuery(int userId)
+		{
+			return _db.Users
+				.Include(u => u.UserRoles)
+				.ThenInclude(u => u.Role)
+				.ThenInclude(r => r.RolePermission)
+				.ThenInclude(rp => rp.Permission)
+				.Where(u => u.Id == userId)
+				.SelectMany(u => u.UserRoles)
+				.SelectMany(ur => ur.Role.RolePermission)
+				.Select(rp => rp.PermissionId)
+				.Distinct();
 		}
 	}
 }
