961fc2dbfd3bb3c733209fdcf1506a4b425fb00d
adelikat <adelikat@tasvideos.org>
2017-11-27 20:24:59 -0600

    ITrackable with Create and LastUpdate timestamps, wire up to User and Role

--- TASVideos/Data/Entity/Role.cs
+++ TASVideos/Data/Entity/Role.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace TASVideos.Data.Entity
 {
-	public class Role : IdentityRole<int>
+	public class Role : IdentityRole<int>, ITrackable
 	{
 		[StringLength(200)]
 		public string Description { get; set; }
+
+		public DateTime CreateTimeStamp { get; set; }
+		public DateTime LastUpdateTimeStamp { get; set; }
 
 		public virtual ICollection<RolePermission> RolePermission { get; set; } = new HashSet<RolePermission>();
 		public virtual ICollection<UserRole> UserRole { get; set; } = new HashSet<UserRole>();
