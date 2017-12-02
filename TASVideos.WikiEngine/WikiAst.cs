2dfd8d3fd7b10c404287f67f1849082e701552bb
adelikat <adelikat@tasvideos.org>
2017-12-02 11:57:36 -0600

    set up wiki classes and test harness

---
+++ TASVideos.WikiEngine/WikiAst.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TASVideos.WikiEngine
+{
+	public class WikiAst
+	{
+		private readonly string _markup;
+
+		public WikiAst(string markup)
+		{
+			_markup = markup;
+			Parse(_markup);
+		}
+
+		public WikiNode WikiTree { get; private set; }
+
+		public string ToHtml()
+		{
+			return "TODO";
+		}
+
+		private void Parse(string markup)
+		{
+			WikiTree = new WikiNode(null, NodeType.OpenTag);
+		}
+	}
+
+	// TODO: separate file
+	public class WikiNode
+	{
+		public WikiNode(WikiNode parent, NodeType type)
+		{
+			Parent = parent;
+			Type = type;
+		}
+
+		public bool IsRoot => Parent == null;
+
+		public IEnumerable<WikiNode> ChildNodes { get; set; }
+
+		public WikiNode Parent { get; }
+		public NodeType Type { get; }
+	}
+
+	public enum NodeType
+	{
+		Text,
+		OpenTag,
+		CloseTag,
+		SelClosingTag
+	}
+}
---
+++ WikiTestHarness/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using TASVideos.WikiEngine;
+
+namespace WikiTestHarness
+{
+	public partial class Form1 : Form
+	{
+		private Config Config { get; set; }
+
+		public Form1()
+		{
+			InitializeComponent();
+			Closing += (o, e) =>
+			{
+				Config.WikiMarkup = MarkupBox.Text;
+				ConfigService.Save("./config.ini", Config);
+			};
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Config = ConfigService.Load<Config>("./config.ini");
+			MarkupBox.Text = Config.WikiMarkup;
+			RunBtn_Click(null, null);
+		}
+
+		private void RunBtn_Click(object sender, EventArgs e)
+		{
+			var wikiAst = new WikiAst(MarkupBox.Text);
+			AstBox.Text = wikiAst.ToString();
+			HtmlBox.Text = wikiAst.ToHtml();
+		}
+	}
+}
