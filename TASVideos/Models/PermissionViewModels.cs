ee5a3fcd1df6d13d4a064a7dcce8664864f0c0e8
adelikat <adelikat@tasvideos.org>
2017-12-05 18:17:01 -0600

    add a display attribute to permission enums and display them on the permisisons/index screen

--- TASVideos/Models/PermissionViewModels.cs
+++ TASVideos/Models/PermissionViewModels.cs
@@ -9,8 +9,11 @@ namespace TASVideos.Models
 	/// </summary>
 	public class PermissionDisplayViewModel
 	{
-		[DisplayName("Permission")]
 		public PermissionTo Id { get; set; }
+
+		[DisplayName("Permission")]
+		public string Name { get; set; }
+
 		public string Description { get; set; }
 		public string Group { get; set; }
 		public IEnumerable<string> Roles { get; set; } = new List<string>();
