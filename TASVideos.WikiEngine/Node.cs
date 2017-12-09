884f823c7faa5a3a345780c266b7f048a431cd04
nattthebear <goyuken@gmail.com>
2017-12-09 09:24:39 -0500

    Refactor some stuff and add support for SRC_EMBED

--- TASVideos.WikiEngine/Node.cs
+++ TASVideos.WikiEngine/Node.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using System.Collections.ObjectModel;
 
 namespace TASVideos.WikiEngine.AST
 {
@@ -32,13 +33,24 @@ namespace TASVideos.WikiEngine.AST
 	public class Element : INode
 	{
 		private static readonly IEnumerable<INode> EmptyChildren = new INode[0];
+		private static readonly ReadOnlyDictionary<string, string> EmptyAttributes = new ReadOnlyDictionary<string, string>(new Dictionary<string, string>());
 		public NodeType Type => NodeType.Element;
-		public IEnumerable<INode> Children { get; }
+		public IEnumerable<INode> Children { get; } = EmptyChildren;
+		public IReadOnlyDictionary<string, string> Attributes { get; } = EmptyAttributes;
 		public string Tag { get; }
-		public Element(string tag, IEnumerable<INode> children = null)
+		public Element(string tag)
 		{
 			Tag = tag;
-			Children = children?.ToList().AsReadOnly() ?? EmptyChildren;
+		}
+		public Element(string tag, IEnumerable<INode> children)
+			:this(tag)
+		{
+			Children = children.ToList().AsReadOnly();
+		}
+		public Element(string tag, IEnumerable<KeyValuePair<string, string>> attributes, IEnumerable<INode> children)
+			:this(tag, children)
+		{
+			Attributes = new ReadOnlyDictionary<string, string>(attributes.ToDictionary(kvp => kvp.Key, kvp => kvp.Value));
 		}
 	}
 
@@ -47,9 +59,9 @@ namespace TASVideos.WikiEngine.AST
 		public NodeType Type => NodeType.IfModule;
 		public IEnumerable<INode> Children { get; }
 		public string Condition { get; }
-		public IfModule(INode condition, IEnumerable<INode> children)
+		public IfModule(string condition, IEnumerable<INode> children)
 		{
-			Condition = ((Text)condition).Content;
+			Condition = condition;
 			Children = children.ToList().AsReadOnly();
 		}
 	}
@@ -58,9 +70,9 @@ namespace TASVideos.WikiEngine.AST
 	{
 		public NodeType Type => NodeType.Module;
 		public string Text { get; }
-		public Module(INode text)
+		public Module(string text)
 		{
-			Text = ((Text)text).Content;
+			Text = text;
 		}
 	}
 }
