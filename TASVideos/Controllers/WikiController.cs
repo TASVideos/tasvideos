3cea69d14ba85b9aedac42c909bd750e6b13b3f6
adelikat <adelikat@tasvideos.org>
2017-12-09 15:43:14 -0600

    start wiki edit page and preview

---
+++ TASVideos/Controllers/WikiController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Models;
+using TASVideos.Tasks;
+using TASVideos.WikiEngine;
+
+namespace TASVideos.Controllers
+{
+	// TODO: edit permissions
+	public class WikiController : BaseController
+    {
+		public WikiController(UserTasks userTasks)
+			: base(userTasks)
+		{
+		}
+
+		public IActionResult Edit(string path)
+		{
+			// TODO: grab page from db based on path
+			var model = new WikiEditModel
+			{
+				PageName = path ?? "/Dummy/Dummy",
+				Markup = "Hello __World__"
+			};
+
+			return View(model);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(WikiEditModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				// TODO
+				RedirectHome();
+			}
+
+			return View(model);
+		}
+
+		public ContentResult GeneratePreview(string markup)
+		{
+			// TODO:
+			//var input = new StreamReader(Request.Body, Encoding.UTF8).ReadToEnd();
+			//var output = Util.DebugParseWikiPage(input);
+			//var w = new StringWriter();
+			//Util.DebugWriteHtml(input, w);
+			//return Content(output + "\n" + w, "text/plain");
+			return Content("Hello <strong>World</strong>");
+		}
+    }
+}
---
+++ TASVideos/Models/WikiModels.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TASVideos.Models
+{
+    public class WikiEditModel
+    {
+		public string PageName { get; set; }
+
+		
+		public string Markup { get; set; }
+
+		[Display(Name = "Minor Edit")]
+		public bool MinorEdit { get; set; }
+
+		[Required] // Yeah, I did that
+		[Display(Name = "Edit Comments")]
+		public string EditComments { get; set; }
+    }
+}
