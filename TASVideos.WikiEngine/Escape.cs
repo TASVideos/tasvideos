65ac07f06aaa506875d3a1b718e536b051f2fc5e
nattthebear <goyuken@gmail.com>
2017-12-10 21:28:37 -0500

    Fix some injection vulnerabilities

---
+++ TASVideos.WikiEngine/Escape.cs
@@ -0,0 +1,28 @@
+using System.IO;
+
+namespace TASVideos.WikiEngine
+{
+	public static class Escape
+	{
+		public static void WriteCSharpString(TextWriter w, string s)
+		{
+			w.Write('"');
+			foreach (var c in s)
+			{
+				if (c < 0x20)
+				{
+					w.Write($"\\x{((int)c).ToString("x2")}");
+				}
+				else if (c == '"')
+				{
+					w.Write("\\\"");
+				}
+				else
+				{
+					w.Write(c);
+				}
+			}
+			w.Write('"');
+		}
+	}
+}
--- TASVideos.WikiEngine/Util.cs
+++ TASVideos.WikiEngine/Util.cs
@@ -47,7 +47,9 @@ namespace TASVideos.WikiEngine
 			var result = parser.GetMatch(content, parser.Document);
 			if (result.Success && result.NextIndex == content.Length)
 			{
-				w.Write($@"@{{Layout=""/Views/Shared/_WikiLayout.cshtml""; ViewData[""Title""] = ""{pageName}"";}}");
+				w.Write(@"@{ Layout = ""/Views/Shared/_WikiLayout.cshtml""; ViewData[""Title""] = ");
+				Escape.WriteCSharpString(w, pageName);
+				w.Write(";}");
 				foreach (var r in result.Results)
 					r.WriteHtml(w);
 			}
