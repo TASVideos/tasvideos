c66621b3b92337161f7d575a2f1598323283ed6b
adelikat <adelikat@tasvideos.org>
2017-12-11 20:28:03 -0600

    Make PageNotFound a real view and wire it up

--- TASVideos/Data/SeedData/WikiPageSeedData.cs
+++ TASVideos/Data/SeedData/WikiPageSeedData.cs
@@ -4,22 +4,9 @@ namespace TASVideos.Data.SeedData
 {
     public class WikiPageSeedData
 	{
-		public const string PageNotFound = "System/PageNotFound";
 		public static readonly WikiPage[] SeedPages =
 		{
-			new WikiPage
-			{
-				Markup = @"
-!!! This page does not yet exist.
-%%%
-Want to create it?
-
-TODO: finish this page when all the markup is supported
-",
-				PageName = PageNotFound,
-				MinorEdit = false,
-				RevisionMessage = "Initial Creation"
-			}
+			// TODO
 		};
 	}
 }
