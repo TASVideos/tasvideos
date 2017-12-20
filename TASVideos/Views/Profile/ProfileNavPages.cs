ced176b1ef9bd3356e2d7879deb522f621506e62
adelikat <adelikat@tasvideos.org>
2017-12-20 13:58:04 -0600

    remove some unused code

--- TASVideos/Views/Profile/ProfileNavPages.cs
+++ TASVideos/Views/Profile/ProfileNavPages.cs
@@ -13,13 +13,9 @@ namespace TASVideos.Views.Profile
 
         public static string ChangePassword => "ChangePassword";
 
-        public static string ExternalLogins => "ExternalLogins";
-
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
-
-        public static string ExternalLoginsNavClass(ViewContext viewContext) => PageNavClass(viewContext, ExternalLogins);
    
         public static string PageNavClass(ViewContext viewContext, string page)
         {
