c7813390ffdfadcf45d804030b86676224c2d24d
adelikat <adelikat@tasvideos.org>
2017-12-24 16:49:01 -0600

    stub in ActiveTab view component that does nothing, at least handles the module for now

---
+++ TASVideos/ViewComponent/ActiveTab.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.ViewComponents
+{
+	public class ActiveTab : ViewComponent
+	{
+		public IViewComponentResult Invoke(WikiPage pageData, string pp)
+		{
+			// TODO
+			return View();
+		}
+	}
+}
