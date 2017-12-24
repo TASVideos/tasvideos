f485ffe20b6cdc1d19bd0f4b8f6909314982c1b5
adelikat <adelikat@tasvideos.org>
2017-12-24 17:11:27 -0600

    wire up the ActiveTab module

--- TASVideos/ViewComponent/ActiveTab.cs
+++ TASVideos/ViewComponent/ActiveTab.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using TASVideos.Data.Entity;
 
 namespace TASVideos.ViewComponents
@@ -7,7 +8,60 @@ namespace TASVideos.ViewComponents
 	{
 		public IViewComponentResult Invoke(WikiPage pageData, string pp)
 		{
-			// TODO
+			// Support legacy markup: tab=b6 with a list of hardcoded translations
+			// These are the names of the tabs in the legacy system so assume those names are the intent here
+			// Whether we have those menus or not
+			pp = pp ?? "";
+			if (pp.StartsWith("tab=b"))
+			{
+				var val = pp.Split(new[] { "tab=b" }, StringSplitOptions.RemoveEmptyEntries);
+
+				if (val != null && val.Length > 0 && !string.IsNullOrWhiteSpace(val[0]))
+				{
+					var result = int.TryParse(val[0], out int tabIndex);
+					if (result)
+					{
+						switch(tabIndex)
+						{
+							case 0:
+								pp = "Home";
+								break;
+							case 1:
+								pp = "Movies";
+								break;
+							case 2:
+								pp = "Game Resources";
+								break;
+							case 3:
+								pp = "Articles";
+								break;
+							case 4:
+								pp = "Emulators";
+								break;
+							case 5:
+								pp = "Submissions";
+								break;
+							case 6:
+								pp = "News";
+								break;
+							case 7:
+								pp = "Forums";
+								break;
+							case 8:
+								pp = "Chat";
+								break;
+							case 9:
+								pp = "Staff";
+								break;
+							case 10:
+								pp = "About";
+								break;
+						}
+					}
+				}
+			}
+
+			TempData["ActiveTab"] = pp;
 			return View();
 		}
 	}
