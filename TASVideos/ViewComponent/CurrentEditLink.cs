a8a5d81d7d5edf2a6ef5bfbbb1b4b6b50595bc7b
adelikat <adelikat@tasvideos.org>
2017-12-25 16:09:03 -0600

    Implement module expr:WikiGetCurrentEditLink, note that it would need to be done in passes to do its intended functionlaity of embedding in a link such as: [expr:WikiGetCurrentEditLink]|Edit this page]

---
+++ TASVideos/ViewComponent/CurrentEditLink.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.ViewComponents
+{
+	public class CurrentEditLink : ViewComponent
+	{
+		public IViewComponentResult Invoke(WikiPage pageData, string pp)
+		{
+			return View(pageData);
+		}
+	}
+}
