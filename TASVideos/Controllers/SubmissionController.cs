ba7b33efba2a584c2efb677c4b95520ea585063c
adelikat <adelikat@tasvideos.org>
2017-12-29 12:56:24 -0600

    Plumbing for a submit movie page

---
+++ TASVideos/Controllers/SubmissionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TASVideos.Models;
+using TASVideos.Tasks;
+
+namespace TASVideos.Controllers
+{
+    public class SubmissionController : BaseController
+    {
+		public SubmissionController(UserTasks userTasks)
+			: base(userTasks)
+		{
+		}
+
+		// Submisison List
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Submit(SubmissionCreateViewModel model)
+		{
+			return View(model);
+		}
+	}
+}
---
+++ TASVideos/Models/SubmissionViewModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TASVideos.Models
+{
+	/// <summary>
+	/// Represents a movie to submit to the submission queue for the submit page
+	/// </summary>
+	public class SubmissionCreateViewModel
+    {
+		public string GameVersion { get; set; }
+		public string GameName { get; set; }
+		public string BranchName { get; set; }
+		// TODO: game id
+		public string RomName { get; set; }
+		public string Emulator { get; set; }
+		//TODO: authors
+		public string Markup { get; set; }
+		public byte[] MovieFile { get; set; }
+    }
+}
