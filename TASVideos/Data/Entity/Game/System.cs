70187dd0734b67070bb492fc0aecf6e7fe7569b0
adelikat <adelikat@tasvideos.org>
2017-12-29 12:21:07 -0600

    add GameSystem table (system in old tasvideos) and start the seed data (only first 3 entries for now)

---
+++ TASVideos/Data/Entity/Game/System.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TASVideos.Data.Entity.Game
+{
+	/// <summary>
+	/// Represents the system that a game runs on, such as NES, SNES, Commodore 64, PSX, etc
+	/// </summary>
+	public class GameSystem
+    {
+		public int Id { get; set; } // Note that this is Non-auto-incrementing, we need Ids to be identical across any database
+
+		[Required]
+		[StringLength(8)]
+		public string Code { get; set; }
+
+		[Required]
+		[StringLength(100)]
+		public string DisplayName { get; set; }
+	}
+}
