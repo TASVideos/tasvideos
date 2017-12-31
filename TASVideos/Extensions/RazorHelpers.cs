a6323260ef21d117674e1ac9bc3a38b1b1a554bf
adelikat <adelikat@tasvideos.org>
2017-12-31 08:50:04 -0600

    make an extension method for rendering a wiki page as a partial

--- TASVideos/Extensions/RazorHelpers.cs
+++ TASVideos/Extensions/RazorHelpers.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using TASVideos.Data.Entity;
+using TASVideos.ViewComponents;
 
 namespace TASVideos.Extensions
 {
@@ -26,6 +30,11 @@ namespace TASVideos.Extensions
 		public static string Action(this ViewContext viewContext)
 		{
 			return viewContext.ActionDescriptor.RouteValues["action"];
+		}
+
+		public static async Task<IHtmlContent> RenderWiki(this IViewComponentHelper component, string pageName)
+		{
+			return await component.InvokeAsync(nameof(RenderWikiPage), new {url = pageName});
 		}
 	}
 }
