57ec10e4a19cd2e3bafc2591fdc94880f7ea2f4f
adelikat <adelikat@tasvideos.org>
2018-01-01 08:58:50 -0600

    some more game systems

--- TASVideos/Data/SeedData/SystemSeedData.cs
+++ TASVideos/Data/SeedData/SystemSeedData.cs
@@ -4,6 +4,9 @@ namespace TASVideos.Data.SeedData
 {
     public class SystemSeedData
 	{
+		public const string NTSC = "NTSC";
+		public const string PAL = "PAL";
+
 		public static GameSystem[] Systems =
 		{
 			new GameSystem
@@ -23,6 +26,30 @@ namespace TASVideos.Data.SeedData
 				Id = 3,
 				Code = "Genesis",
 				DisplayName = "Sega Genesis"
+			},
+			new GameSystem
+			{
+				Id = 4,
+				Code = "GB",
+				DisplayName = "Game Boy"
+			},
+			new GameSystem
+			{
+				Id = 5,
+				Code = "SGB",
+				DisplayName = "Super Game Boy"
+			},
+			new GameSystem
+			{
+				Id = 6,
+				Code = "GBC",
+				DisplayName = "Game Boy Color"
+			},
+			new GameSystem
+			{
+				Id = 7,
+				Code = "GBA",
+				DisplayName = "Game Boy Advance"
 			}
 		};
 
@@ -31,39 +58,69 @@ namespace TASVideos.Data.SeedData
 			new GameSystemFrameRate
 			{
 				GameSystemId = 1,
-				RegionCode = "NTSC",
+				RegionCode = NTSC,
 				FrameRate = 60.0988138974405
 			},
 			new GameSystemFrameRate
 			{
 				GameSystemId = 1,
-				RegionCode = "PAL",
+				RegionCode = PAL,
 				FrameRate = 50.0069789081886
 			},
 			new GameSystemFrameRate
 			{
 				GameSystemId = 2,
-				RegionCode = "NTSC",
+				RegionCode = NTSC,
 				FrameRate = 60.0988138974405
 			},
 			new GameSystemFrameRate
 			{
 				GameSystemId = 2,
-				RegionCode = "PAL",
+				RegionCode = PAL,
 				FrameRate = 50.0069789081886
 			},
 			new GameSystemFrameRate
 			{
 				GameSystemId = 3,
-				RegionCode = "NTSC",
+				RegionCode = NTSC,
 				FrameRate = 59.922751013550524
 			},
 			new GameSystemFrameRate
 			{
 				GameSystemId = 3,
-				RegionCode = "PAL",
+				RegionCode = PAL,
 				FrameRate = 49.70146011994842
-			}
+			},
+			new GameSystemFrameRate
+			{
+				GameSystemId = 4,
+				RegionCode = NTSC,
+				FrameRate = 59.7275005696058
+			},
+			new GameSystemFrameRate
+			{
+				GameSystemId = 5,
+				RegionCode = NTSC,
+				FrameRate = 60.0988138974405
+			},
+			new GameSystemFrameRate
+			{
+				GameSystemId = 5,
+				RegionCode = PAL,
+				FrameRate = 50.0069789081886
+			},
+			new GameSystemFrameRate
+			{
+				GameSystemId = 6,
+				RegionCode = NTSC,
+				FrameRate = 59.7275005696058
+			},
+			new GameSystemFrameRate
+			{
+				GameSystemId = 7,
+				RegionCode = NTSC,
+				FrameRate = 59.7275005696058
+			},
 		};
 	}
 }
