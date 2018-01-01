df6fa4f65990ae71b167bdae448d0add32ebd549
adelikat <adelikat@tasvideos.org>
2017-12-31 18:12:17 -0600

    start bk2 parser - can parse rerecordcount and framecount

--- TASVideos.Parsers/Parsers/Bk2.cs
+++ TASVideos.Parsers/Parsers/Bk2.cs
@@ -12,16 +12,76 @@ namespace TASVideos.MovieParsers
 
 		public IParseResult Parse(Stream file)
 		{
-			var bk2Archive = new ZipArchive(file);
-			var inputLogEntry = bk2Archive.Entries.Single(e => e.Name == "Input Log.txt");
-			
-			return new ParseResult
+			var result = new ParseResult
 			{
 				Region = RegionType.Ntsc,
-				Frames = new Random().Next(10000, 250000),
 				SystemCode = "NES",
 				RerecordCount = new Random().Next(10000, 50000)
 			};
+
+
+
+			var bk2Archive = new ZipArchive(file);
+
+			var inputLogEntry = bk2Archive.Entries.Single(e => e.Name == "Input Log.txt");
+			using (var stream = inputLogEntry.Open())
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					string[] inputLog = reader.ReadToEnd().Split('\n');
+					result.Frames = inputLog.Count(i => i.StartsWith('|'));
+				}
+			}
+
+			var headerEntry = bk2Archive.Entries.Single(e => e.Name == "Header.txt");
+			using (var stream = headerEntry.Open())
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					string[] headerLines = reader.ReadToEnd().Split(new [] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+					int? rerecordVal = GetInt(GetValue(headerLines, "rerecordcount"));
+					if (rerecordVal.HasValue)
+					{
+						result.RerecordCount = rerecordVal.Value;
+					}
+					else
+					{
+						result.WarningList.Add("Could not determine the rerecord count, using 0 instead");
+					}
+				}
+			}
+
+			return result;
+		}
+
+		private static string GetValue(string[] lines, string header) // Case insensitive
+		{
+			if (lines == null || !lines.Any() || string.IsNullOrWhiteSpace(header))
+			{
+				return "";
+			}
+
+			var row = lines.FirstOrDefault(l => l.ToLower().StartsWith(header?.ToLower()))?.ToLower();
+			if (!string.IsNullOrWhiteSpace(row))
+			{
+				var valstr = row.Replace(header.ToLower(), "").Trim().Replace("\r", "").Replace("\n", "");
+				
+				return valstr;
+			}
+
+			return "";
+		}
+
+		private static int? GetInt(string val)
+		{
+			var result = int.TryParse(val, out int parsedVal);
+			if (result)
+			{
+				return parsedVal;
+			}
+
+			return null;
 		}
 	}
 }
