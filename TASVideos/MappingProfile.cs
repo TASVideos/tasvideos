64671ae9df5ee867a0b17e2d301a0004a5fee63b
adelikat <adelikat@tasvideos.org>
2018-01-05 13:23:07 -0600

    automapper proof of concept - wire up automapper and use it in a basic mapping and using the .ProjectTo() queryable extension

---
+++ TASVideos/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TASVideos.Data.Entity;
+using TASVideos.Models;
+
+namespace TASVideos
+{
+	// https://stackoverflow.com/questions/40275195/how-to-setup-automapper-in-asp-net-core
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<SubmissionCreateViewModel, Submission>()
+				.ForMember(dest => dest.EmulatorVersion, opt => opt.MapFrom(src => src.Emulator));
+
+			CreateMap<User, UserEditViewModel>()
+				.ForMember(dest => dest.IsLockedOut, opt => opt.MapFrom(src => src.LockoutEnabled && src.LockoutEnd.HasValue));
+		}
+	}
+}
