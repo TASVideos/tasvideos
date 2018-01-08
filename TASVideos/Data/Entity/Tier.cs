669ae9a587c94a4cf7cb1d87fb430ba5500447b1
adelikat <adelikat@tasvideos.org>
2018-01-07 18:07:49 -0600

    add tiers, ability to set tier on submission

---
+++ TASVideos/Data/Entity/Tier.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TASVideos.Data.Entity
+{
+    public class Tier
+    {
+		public int Id { get; set; }
+
+		[StringLength(20)]
+		public string Name { get; set; }
+		public double Weight { get; set; }
+		public string IconPath { get; set; }
+
+		[Required]
+		public string Link { get; set; }
+	}
+}
