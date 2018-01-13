986ba23a266bc40f39a21cc3f04909f48fd70fe2
adelikat <adelikat@tasvideos.org>
2018-01-13 15:20:25 -0600

    some cleanups in view files

--- TASVideos/Views/Profile/ProfileNavPages.cs
+++ TASVideos/Views/Profile/ProfileNavPages.cs
@@ -5,28 +5,28 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace TASVideos.Views.Profile
 {
-    public static class ProfileNavPages
-    {
-        public static string ActivePageKey => "ActivePage";
+	public static class ProfileNavPages
+	{
+		public static string ActivePageKey => "ActivePage";
 
-        public static string Index => "Index";
+		public static string Index => "Index";
 
-        public static string ChangePassword => "ChangePassword";
+		public static string ChangePassword => "ChangePassword";
 
 		public static string HomePage => "HomePage";
 
-        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+		public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
-        public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+		public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
 		public static string HomePageClass(ViewContext viewContext) => PageNavClass(viewContext, HomePage);
-   
-        public static string PageNavClass(ViewContext viewContext, string page)
-        {
-            var activePage = viewContext.ViewData["ActivePage"] as string;
-            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
-        }
 
-        public static void AddActivePage(this ViewDataDictionary viewData, string activePage) => viewData[ActivePageKey] = activePage;
-    }
+		public static string PageNavClass(ViewContext viewContext, string page)
+		{
+			var activePage = viewContext.ViewData["ActivePage"] as string;
+			return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+		}
+
+		public static void AddActivePage(this ViewDataDictionary viewData, string activePage) => viewData[ActivePageKey] = activePage;
+	}
 }
