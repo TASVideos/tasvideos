2897163de31bd46faaf97f18747d793399680e3c
adelikat <adelikat@tasvideos.org>
2018-01-14 15:05:29 -0600

    submission tasks - ducumentation, minor cleanups, and reorder methods

--- TASVideos/Tasks/SubmissionTasks.cs
+++ TASVideos/Tasks/SubmissionTasks.cs
@@ -2,9 +2,11 @@
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+
 using TASVideos.Constants;
 using TASVideos.Data;
 using TASVideos.Data.Entity;
@@ -20,6 +22,7 @@ namespace TASVideos.Tasks
 		private readonly MovieParser _parser;
 		private readonly WikiTasks _wikiTasks;
 		private readonly IMapper _mapper;
+
 		public SubmissionTasks(
 			ApplicationDbContext db,
 			MovieParser parser,
@@ -32,19 +35,37 @@ namespace TASVideos.Tasks
 			_mapper = mapper;
 		}
 
-		// TODO: document - for reverifying a status can be set
-		public async Task<SubmissionStatusValidationModel> GetStatusVerificationValues(int id, string userName)
+		/// <summary>
+		/// Gets a list of <see cref="Submission"/>s for the submission queue filtered on the given <see cref="criteria" />
+		/// </summary>
+		public async Task<IEnumerable<SubmissionListViewModel>> GetSubmissionList(SubmissionSearchCriteriaModel criteria)
 		{
-			return await _db.Submissions
-				.Where(s => s.Id == id)
-				.Select(s => new SubmissionStatusValidationModel
-				{
-					UserIsJudge = s.Judge != null && s.Judge.UserName == userName,
-					UserIsAuhtorOrSubmitter = s.Submitter.UserName == userName || s.SubmissionAuthors.Any(sa => sa.Author.UserName == userName),
-					CurrentStatus = s.Status,
-					CreateDate = s.CreateTimeStamp
-				})
-				.SingleAsync();
+			IQueryable<Submission> query = _db.Submissions;
+
+			if (!string.IsNullOrWhiteSpace(criteria.User))
+			{
+				query = query.Where(s => s.Submitter.UserName == criteria.User);
+			}
+
+			var iquery = query
+				.Include(s => s.Submitter)
+				.Include(s => s.System)
+				.Include(s => s.SystemFrameRate)
+				.Include(s => s.SubmissionAuthors)
+				.ThenInclude(sa => sa.Author);
+
+			var results = await iquery.ToListAsync();
+			return results.Select(s => new SubmissionListViewModel
+			{
+				Id = s.Id,
+				System = s.System.Code,
+				GameName = s.GameName,
+				Time = s.Time,
+				Branch = s.Branch,
+				Author = string.Join(" & ", s.SubmissionAuthors.Select(sa => sa.Author.UserName)),
+				Submitted = s.CreateTimeStamp,
+				Status = s.Status
+			});
 		}
 
 		/// <summary>
@@ -104,34 +125,18 @@ namespace TASVideos.Tasks
 			}
 		}
 
-		public async Task<IEnumerable<SubmissionListViewModel>> GetSubmissionList(SubmissionSearchCriteriaModel criteria)
+		/// <summary>
+		/// Returns the submission file as bytes with the given id
+		/// If no submission is found, an empty byte array is returned
+		/// </summary>
+		public async Task<byte[]> GetSubmissionFile(int id)
 		{
-			IQueryable<Submission> query = _db.Submissions;
+			var data = await _db.Submissions
+				.Where(s => s.Id == id)
+				.Select(s => s.MovieFile)
+				.SingleOrDefaultAsync();
 
-			if (!string.IsNullOrWhiteSpace(criteria.User))
-			{
-				query = query.Where(s => s.Submitter.UserName == criteria.User);
-			}
-
-			var iquery = query
-				.Include(s => s.Submitter)
-				.Include(s => s.System)
-				.Include(s => s.SystemFrameRate)
-				.Include(s => s.SubmissionAuthors)
-				.ThenInclude(sa => sa.Author);
-
-			var results = await iquery.ToListAsync();
-			return results.Select(s => new SubmissionListViewModel
-			{
-				Id = s.Id,
-				System = s.System.Code,
-				GameName = s.GameName,
-				Time = s.Time,
-				Branch = s.Branch,
-				Author = string.Join(" & ", s.SubmissionAuthors.Select(sa => sa.Author.UserName)),
-				Submitted = s.CreateTimeStamp,
-				Status = s.Status
-			});
+			return data ?? new byte[0];
 		}
 
 		/// <summary>
@@ -141,6 +146,98 @@ namespace TASVideos.Tasks
 		public async Task<string> GetTitle(int id)
 		{
 			return (await _db.Submissions.SingleOrDefaultAsync(s => s.Id == id))?.Title;
+		}
+
+		/// <summary>
+		/// Returns values necessary to determine which <see cref="SubmissionStatus" /> can be assigned to a <seealso cref="Submission"/>
+		/// </summary>
+		public async Task<SubmissionStatusValidationModel> GetStatusVerificationValues(int id, string userName)
+		{
+			return await _db.Submissions
+				.Where(s => s.Id == id)
+				.Select(s => new SubmissionStatusValidationModel
+				{
+					UserIsJudge = s.Judge != null && s.Judge.UserName == userName,
+					UserIsAuhtorOrSubmitter = s.Submitter.UserName == userName || s.SubmissionAuthors.Any(sa => sa.Author.UserName == userName),
+					CurrentStatus = s.Status,
+					CreateDate = s.CreateTimeStamp
+				})
+				.SingleAsync();
+		}
+
+		/// <summary>
+		/// Takes the given data and generates a movie submission
+		/// If successful, the Success flag will be true, and the Id field will be
+		/// greater than 0 and represent the id to the newly created submission
+		/// If unsucessful, the success flag will be false, Id will be 0 and the 
+		/// Errors property will be filled with any relevant error messages
+		/// </summary>
+		public async Task<SubmitResult> SubmitMovie(SubmissionCreateViewModel model, string userName)
+		{
+			var submission = _mapper.Map<Submission>(model);
+			submission.Submitter = await _db.Users.SingleAsync(u => u.UserName == userName);
+
+			// Parse movie file
+			// TODO: check warnings
+			var parseResult = _parser.Parse(model.MovieFile.OpenReadStream());
+			if (parseResult.Success)
+			{
+				submission.Frames = parseResult.Frames;
+				submission.RerecordCount = parseResult.RerecordCount;
+				submission.System = await _db.GameSystems.SingleOrDefaultAsync(g => g.Code == parseResult.SystemCode);
+				if (submission.System == null)
+				{
+					return new SubmitResult($"Unknown system type of {parseResult.SystemCode}");
+				}
+
+				submission.SystemFrameRate = await _db.GameSystemFrameRates
+					.SingleOrDefaultAsync(f => f.GameSystemId == submission.System.Id
+						&& f.RegionCode == parseResult.Region.ToString());
+			}
+			else
+			{
+				return new SubmitResult(parseResult.Errors);
+			}
+
+			using (var memoryStream = new MemoryStream())
+			{
+				await model.MovieFile.CopyToAsync(memoryStream);
+				submission.MovieFile = memoryStream.ToArray();
+			}
+
+			_db.Submissions.Add(submission);
+			await _db.SaveChangesAsync();
+
+			// Create a wiki page corresponding to this submission
+			var wikiPage = new WikiPage
+			{
+				RevisionMessage = $"Auto-generated from Submission #{submission.Id}",
+				PageName = LinkConstants.SubmissionWikiPage + submission.Id,
+				MinorEdit = false,
+				Markup = model.Markup
+			};
+
+			_db.WikiPages.Add(wikiPage);
+
+			submission.WikiContent = wikiPage;
+
+			// Add authors
+			var users = await _db.Users
+				.Where(u => model.Authors.Contains(u.UserName))
+				.ToListAsync();
+
+			var submissionAuthors = users.Select(u => new SubmissionAuthor
+			{
+				SubmissionId = submission.Id,
+				UserId = u.Id
+			});
+
+			_db.SubmissionAuthors.AddRange(submissionAuthors);
+
+			submission.GenerateTitle();
+			await _db.SaveChangesAsync();
+
+			return new SubmitResult(submission.Id);
 		}
 
 		/// <summary>
@@ -199,6 +296,9 @@ namespace TASVideos.Tasks
 			}
 		}
 
+		/// <summary>
+		/// Updates an existing <see cref="Submission"/> with the given values
+		/// </summary>
 		public async Task<SubmitResult> UpdateSubmission(SubmissionEditModel model, string userName)
 		{
 			var submission = await _db.Submissions
@@ -316,94 +416,9 @@ namespace TASVideos.Tasks
 		}
 
 		/// <summary>
-		/// Returns the submission file as bytes with the given id
-		/// If no submission is found, an empty byte array is returned
+		/// Returns whether or not the <see cref="Submission"/> wiht the given <see cref="id"/>
+		/// is ready to be published
 		/// </summary>
-		public async Task<byte[]> GetSubmissionFile(int id)
-		{
-			var data = await _db.Submissions
-				.Where(s => s.Id == id)
-				.Select(s => s.MovieFile)
-				.SingleOrDefaultAsync();
-
-			return data ?? new byte[0];
-		}
-
-		/// <summary>
-		/// Takes the given data and generates a movie submission
-		/// If successful, the Success flag will be true, and the Id field will be
-		/// greater than 0 and represent the id to the newly created submission
-		/// If unsucessful, the success flag will be false, Id will be 0 and the 
-		/// Errors property will be filled with any relevant error messages
-		/// </summary>
-		public async Task<SubmitResult> SubmitMovie(SubmissionCreateViewModel model, string userName)
-		{
-			var submission = _mapper.Map<Submission>(model);
-			submission.Submitter = await _db.Users.SingleAsync(u => u.UserName == userName);
-
-			// Parse movie file
-			// TODO: check warnings
-			var parseResult = _parser.Parse(model.MovieFile.OpenReadStream());
-			if (parseResult.Success)
-			{
-				submission.Frames = parseResult.Frames;
-				submission.RerecordCount = parseResult.RerecordCount;
-				submission.System = await _db.GameSystems.SingleOrDefaultAsync(g => g.Code == parseResult.SystemCode);
-				if (submission.System == null)
-				{
-					return new SubmitResult($"Unknown system type of {parseResult.SystemCode}");
-				}
-
-				submission.SystemFrameRate = await _db.GameSystemFrameRates
-					.SingleOrDefaultAsync(f => f.GameSystemId == submission.System.Id
-						&& f.RegionCode == parseResult.Region.ToString());
-			}
-			else
-			{
-				return new SubmitResult(parseResult.Errors);
-			}
-
-			using (var memoryStream = new MemoryStream())
-			{
-				await model.MovieFile.CopyToAsync(memoryStream);
-				submission.MovieFile = memoryStream.ToArray();
-			}
-
-			_db.Submissions.Add(submission);
-			await _db.SaveChangesAsync();
-
-			// Create a wiki page corresponding to this submission
-			var wikiPage = new WikiPage
-			{
-				RevisionMessage = $"Auto-generated from Submission #{submission.Id}",
-				PageName = LinkConstants.SubmissionWikiPage + submission.Id,
-				MinorEdit = false,
-				Markup = model.Markup
-			};
-
-			_db.WikiPages.Add(wikiPage);
-
-			submission.WikiContent = wikiPage;
-
-			// Add authors
-			var users = await _db.Users
-				.Where(u => model.Authors.Contains(u.UserName))
-				.ToListAsync();
-
-			var submissionAuthors = users.Select(u => new SubmissionAuthor
-			{
-				SubmissionId = submission.Id,
-				UserId = u.Id
-			});
-
-			_db.SubmissionAuthors.AddRange(submissionAuthors);
-
-			submission.GenerateTitle();
-			await _db.SaveChangesAsync();
-
-			return new SubmitResult(submission.Id);
-		}
-
 		public async Task<bool> CanPublish(int id)
 		{
 			return await _db.Submissions
@@ -416,6 +431,10 @@ namespace TASVideos.Tasks
 					&& s.Status == SubmissionStatus.PublicationUnderway);
 		}
 
+		/// <summary>
+		/// Returns the <see cref="Submission"/> with the given <see cref="id"/>
+		/// for the purpose of publishing
+		/// </summary>
 		public async Task<SubmissionPublishModel> GetSubmissionForPublish(int id)
 		{
 			using (_db.Database.BeginTransactionAsync())
@@ -435,8 +454,6 @@ namespace TASVideos.Tasks
 						Rom = s.Rom.Name,
 						RomId = s.RomId ?? 0,
 						Tier = s.IntendedTier.Name,
-
-
 						Branch = s.Branch,
 						EmulatorVersion = s.EmulatorVersion
 					})
@@ -444,57 +461,11 @@ namespace TASVideos.Tasks
 			}
 		}
 
-		// TODO: document, returns id of the published movie
-		public async Task<int> PublishSubmission(SubmissionPublishModel model)
-		{
-			var submission = await _db.Submissions
-				.Include(s => s.IntendedTier)
-				.Include(s => s.System)
-				.Include(s => s.SystemFrameRate)
-				.Include(s => s.Game)
-				.Include(s => s.Rom)
-				.Include(s => s.SubmissionAuthors)
-				.ThenInclude(sa => sa.Author)
-				.SingleAsync(s => s.Id == model.Id);
-
-			var publication = new Publication
-			{
-				TierId = submission.IntendedTier.Id,
-				SystemId = submission.System.Id,
-				SystemFrameRateId = submission.SystemFrameRate.Id,
-				GameId = submission.Game.Id,
-				RomId = submission.Rom.Id,
-				MovieFile = submission.MovieFile,
-				Branch = submission.Branch,
-				EmulatorVersion = model.EmulatorVersion,
-				OnlineWatchingUrl = model.OnlineWatchingUrl,
-				Frames = submission.Frames,
-				RerecordCount = submission.RerecordCount
-			};
-
-			var publicationAuthors = submission.SubmissionAuthors
-				.Select(sa => new PublicationAuthor
-				{
-					Pubmisison = publication,
-					Author = sa.Author
-				});
-
-			foreach (var author in publicationAuthors)
-			{
-				publication.Authors.Add(author);
-			}
-
-			publication.Submission = submission;
-			_db.Publications.Add(publication);
-
-
-			await _db.SaveChangesAsync(); // Need an Id for the Title
-			publication.GenerateTitle();
-			await _db.SaveChangesAsync();
-
-			return publication.Id; // TODO
-		}
-
+		/// <summary>
+		/// Returns the <see cref="Submission"/> with the given <see cref="id"/>
+		/// for the purpose of setting <see cref="TASVideos.Data.Entity.Game.Game"/> catalogging information.
+		/// If no submission is found, null is returned
+		/// </summary>
 		public async Task<SubmissionCatalogModel> Catalog(int id)
 		{
 			using (_db.Database.BeginTransactionAsync())
@@ -552,11 +523,68 @@ namespace TASVideos.Tasks
 			}
 		}
 
+		/// <summary>
+		/// Updates the given <see cref="Submission"/> with the given <see cref="TASVideos.Data.Entity.Game.Game"/> catalog information
+		/// </summary>
 		public async Task UpdateCatalog(SubmissionCatalogModel model)
 		{
 			var submission = await _db.Submissions.SingleAsync(s => s.Id == model.Id);
 			_mapper.Map(model, submission);
 			await _db.SaveChangesAsync();
+		}
+
+		/// <summary>
+		/// Takes a <see cref="Submission"/> and creates a new <see cref="Publication"/>
+		/// If successful, the id of the new publicaiton is returned
+		/// </summary>
+		/// <returns>The id of the new <see cref="Publication"/></returns>
+		public async Task<int> PublishSubmission(SubmissionPublishModel model)
+		{
+			var submission = await _db.Submissions
+				.Include(s => s.IntendedTier)
+				.Include(s => s.System)
+				.Include(s => s.SystemFrameRate)
+				.Include(s => s.Game)
+				.Include(s => s.Rom)
+				.Include(s => s.SubmissionAuthors)
+				.ThenInclude(sa => sa.Author)
+				.SingleAsync(s => s.Id == model.Id);
+
+			var publication = new Publication
+			{
+				TierId = submission.IntendedTier.Id,
+				SystemId = submission.System.Id,
+				SystemFrameRateId = submission.SystemFrameRate.Id,
+				GameId = submission.Game.Id,
+				RomId = submission.Rom.Id,
+				MovieFile = submission.MovieFile,
+				Branch = submission.Branch,
+				EmulatorVersion = model.EmulatorVersion,
+				OnlineWatchingUrl = model.OnlineWatchingUrl,
+				Frames = submission.Frames,
+				RerecordCount = submission.RerecordCount
+			};
+
+			var publicationAuthors = submission.SubmissionAuthors
+				.Select(sa => new PublicationAuthor
+				{
+					Pubmisison = publication,
+					Author = sa.Author
+				});
+
+			foreach (var author in publicationAuthors)
+			{
+				publication.Authors.Add(author);
+			}
+
+			publication.Submission = submission;
+			_db.Publications.Add(publication);
+
+			await _db.SaveChangesAsync(); // Need an Id for the Title
+			publication.GenerateTitle();
+			await _db.SaveChangesAsync();
+
+			return publication.Id;
 		}
 	}
 }
