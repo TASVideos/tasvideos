c6e4067cb2da5845b59b608c227e0c46ef66f58a
adelikat <adelikat@tasvideos.org>
2018-01-14 13:02:27 -0600

    routing for [id]M for movies

--- TASVideos/Startup.cs
+++ TASVideos/Startup.cs
@@ -86,6 +86,7 @@ namespace TASVideos
 				routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
 				routes.MapRoute("sub-list", "Sub-List", defaults: new { controller = "Submission", action = "List" });
 				routes.MapRoute("submission", "{id:int}S", defaults: new { controller = "Submission", action = "View" });
+				routes.MapRoute("movie", "{id:int}M", defaults: new { controller = "Publication", action = "View" });
 				routes.MapRoute("wiki", "{*url}", defaults: new { controller = "Wiki", action = "RenderWikiPage" });
 			});
 		}
