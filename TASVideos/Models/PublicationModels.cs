f926e3118765f9223d6fe975150d355634904f92
adelikat <adelikat@tasvideos.org>
2018-01-14 12:57:44 -0600

    plumbing for publication view

---
+++ TASVideos/Models/PublicationModels.cs
@@ -0,0 +1,8 @@
+﻿namespace TASVideos.Models
+{
+	public class PublicationViewModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+	}
+}
---
+++ TASVideos/Tasks/PublicationTasks.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TASVideos.Tasks
+{
+	using TASVideos.Data;
+	using TASVideos.Models;
+
+	public class PublicationTasks
+    {
+		private readonly ApplicationDbContext _db;
+
+		public PublicationTasks(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		/// <summary>
+		/// Gets a publication with the given <see cref="id" /> for the purpose of display
+		/// If no publication with the given id is found then null is returned
+		/// </summary>
+		public async Task<PublicationViewModel> GetPublicationForDisplay(int id)
+		{
+			return await _db.Publications
+				.Select(p => new PublicationViewModel
+				{
+					Id = p.Id,
+					Title = p.Title
+				})
+				.SingleOrDefaultAsync(p => p.Id == id);
+		}
+	}
+}
