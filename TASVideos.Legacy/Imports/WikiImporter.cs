38d8e11a5c7c0e7c8899c59aa1a60dfb8f3402dc
adelikat <adelikat@tasvideos.org>
2018-02-05 21:48:57 -0600

    wiki importer - handle cases where deleted pages were later created, speed up child referencing process

--- TASVideos.Legacy/Imports/WikiImporter.cs
+++ TASVideos.Legacy/Imports/WikiImporter.cs
@@ -13,7 +13,7 @@ namespace TASVideos.Legacy.Imports
 {
 	public static class WikiImporter
 	{
-		public static void ImportWikiPages(ApplicationDbContext context, NesVideosSiteContext legacyContext)
+		public static void Import(ApplicationDbContext context, NesVideosSiteContext legacyContext)
 		{
 			// TODO: page to keep ram down
 			// TODO: createdby username (look up by userid)
@@ -27,17 +27,55 @@ namespace TASVideos.Legacy.Imports
 			foreach (var legacyPage in siteTexts)
 			{
 				string markup = legacyPage.Description;
+				int revision = legacyPage.Revision;
+				string pageName = legacyPage.PageName;
+
+				if (legacyPage.PageName.StartsWith("System"))
+				{
+					pageName = legacyPage.PageName.Replace("System", "System/");
+				}
+
+				else if (legacyPage.PageName == "FrontPage")
+				{
+					pageName = "System/FrontPage";
+				}
+
+				// ******** Deleted pages that were recreated *************/
+
+				// Not worth preserving history here, revisions were mistakes and revision history is too large
+				else if (legacyPage.PageName == "DeletedPages/Bizhawk/ReleaseHistory")
+				{
+					continue;
+				}
+
+				else if (legacyPage.PageName == "GameResources/N64/Kirby64TheCrystalShards" || legacyPage.PageName == "DeletedPages/GameResources/N64/Kirby64TheCrystalShards")
+				{
+					revision = CrystalShardsLookup[(legacyPage.PageName, legacyPage.Revision)];
+				}
+
+				// This page had 2 deleted pages that came first, so we can just add to the revision number
+				else if (legacyPage.PageName == "GameResources/DS/MetroidPrimeHunters")
+				{
+					revision += 2;
+				}
+
+				// ******** END Deleted pages that were recreated *************/
 
 				// Shenanigans
-				if (legacyPage.PageName == "Phil" && legacyPage.Revision >= 7 && legacyPage.Revision <= 11)
+				else if (legacyPage.PageName == "Phil" && legacyPage.Revision >= 7 && legacyPage.Revision <= 11)
 				{
 					markup = markup.Replace(":[", ":|");
 				}
 
+				else if (legacyPage.PageName == "971S" && legacyPage.Revision == 3)
+				{
+					markup = markup.Replace("[Phi:", "[Phil]:");
+				}
+
+
 				var pubId = SubmissionHelper.IsPublicationLink(legacyPage.PageName);
 				var subId = SubmissionHelper.IsSubmissionLink(legacyPage.PageName);
 
-				string pageName = legacyPage.PageName;
 				bool isDeleted = false;
 				if (pubId.HasValue)
 				{
@@ -57,7 +95,7 @@ namespace TASVideos.Legacy.Imports
 				{
 					PageName = pageName,
 					Markup = markup,
-					Revision = legacyPage.Revision,
+					Revision = revision,
 					MinorEdit = legacyPage.MinorEdit == "Y",
 					RevisionMessage = legacyPage.WhyEdit,
 					IsDeleted = isDeleted,
@@ -65,7 +103,6 @@ namespace TASVideos.Legacy.Imports
 				};
 
 				context.WikiPages.Add(wikiPage);
-
 				var referrals = Util.GetAllWikiLinks(wikiPage.Markup);
 				foreach (var referral in referrals)
 				{
@@ -81,9 +118,10 @@ namespace TASVideos.Legacy.Imports
 			context.SaveChanges();
 
 			// Set child references
-			foreach (var wikiPage in context.WikiPages)
+			var wikiList = context.WikiPages.ToList();
+			foreach (var wikiPage in wikiList)
 			{
-				var nextWiki = context.WikiPages
+				var nextWiki = wikiList
 					.SingleOrDefault(wp => wp.Revision == wikiPage.Revision + 1
 						&& wp.PageName == wikiPage.PageName);
 
@@ -95,5 +133,52 @@ namespace TASVideos.Legacy.Imports
 
 			context.SaveChanges();
 		}
+
+		private static Dictionary<(string, int), int> CrystalShardsLookup = new Dictionary<(string, int), int>
+		{
+			[("GameResources/N64/Kirby64TheCrystalShards", 1)] = 1,
+			[("GameResources/N64/Kirby64TheCrystalShards", 2)] = 2,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 1)] = 3,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 2)] = 4,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 3)] = 5,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 4)] = 6,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 5)] = 7,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 6)] = 8,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 7)] = 9,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 8)] = 10,
+			[("GameResources/N64/Kirby64TheCrystalShards", 3)] = 11,
+			[("GameResources/N64/Kirby64TheCrystalShards", 4)] = 12,
+			[("GameResources/N64/Kirby64TheCrystalShards", 5)] = 13,
+			[("GameResources/N64/Kirby64TheCrystalShards", 6)] = 14,
+			[("GameResources/N64/Kirby64TheCrystalShards", 7)] = 15,
+			[("GameResources/N64/Kirby64TheCrystalShards", 8)] = 16,
+			[("GameResources/N64/Kirby64TheCrystalShards", 9)] = 17,
+			[("GameResources/N64/Kirby64TheCrystalShards", 10)] = 18,
+			[("GameResources/N64/Kirby64TheCrystalShards", 11)] = 19,
+			[("GameResources/N64/Kirby64TheCrystalShards", 12)] = 20,
+			[("GameResources/N64/Kirby64TheCrystalShards", 13)] = 21,
+			[("GameResources/N64/Kirby64TheCrystalShards", 14)] = 22,
+			[("GameResources/N64/Kirby64TheCrystalShards", 15)] = 23,
+			[("GameResources/N64/Kirby64TheCrystalShards", 16)] = 24,
+			[("GameResources/N64/Kirby64TheCrystalShards", 17)] = 25,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 9)] = 26,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 10)] = 27,
+			[("GameResources/N64/Kirby64TheCrystalShards", 18)] = 28,
+			[("GameResources/N64/Kirby64TheCrystalShards", 19)] = 29,
+			[("GameResources/N64/Kirby64TheCrystalShards", 20)] = 30,
+			[("GameResources/N64/Kirby64TheCrystalShards", 21)] = 31,
+			[("DeletedPages/GameResources/N64/Kirby64TheCrystalShards", 11)] = 32,
+			[("GameResources/N64/Kirby64TheCrystalShards", 22)] = 33,
+			[("GameResources/N64/Kirby64TheCrystalShards", 23)] = 34,
+		};
+
+		private static Dictionary<(string, int), int> MetroidPrimeHuntersLookup = new Dictionary<(string, int), int>
+		{
+			[("DeletedPages/GameResources/DS/MetroidPrimeHunters", 1)] = 1,
+			[("DeletedPages/GameResources/DS/MetroidPrimeHunters", 2)] = 2,
+			[("GameResources/DS/MetroidPrimeHunters", 1)] = 3,
+			[("GameResources/DS/MetroidPrimeHunters", 2)] = 4,
+			[("GameResources/DS/MetroidPrimeHunters", 3)] = 5
+		};
 	}
 }
