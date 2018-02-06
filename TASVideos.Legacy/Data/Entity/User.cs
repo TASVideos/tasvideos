a5c25ae84fb61900325975f8f672fcf56ce25c92
adelikat <adelikat@tasvideos.org>
2018-02-05 21:47:49 -0600

    stub in legacy user table and importer

---
+++ TASVideos.Legacy/Data/Entity/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TASVideos.Legacy.Data.Entity
+{
+    public class User
+    {
+		[Key]
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(255)]
+		[Column("name")]
+		public string Name { get; set; }
+
+		[StringLength(255)]
+		[Column("homepage")]
+		public string HomePage { get; set; }
+
+		[Column("lastchange")]
+		public int LastChange { get; set; }
+
+		[Column("createtime")]
+		public int CreateTimeStamp { get; set; }
+
+		[Column("points")]
+		public double Points { get; set; }
+    }
+}
