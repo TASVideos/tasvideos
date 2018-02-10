bd633ba24e63288b61cadc4e15eec1f84d2d9229
adelikat <adelikat@tasvideos.org>
2018-02-10 16:51:16 -0600

    plumbing for submission importer

---
+++ TASVideos.Legacy/Imports/SubmissionImporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+using TASVideos.Data;
+using TASVideos.Legacy.Data.Site;
+
+namespace TASVideos.Legacy.Imports
+{
+	public static class SubmissionImporter
+	{
+		public static void Import(
+			ApplicationDbContext context,
+			NesVideosSiteContext legacySiteContext)
+		{
+			var legacySubmissions = legacySiteContext.Submissions
+				.Where(s => s.Id > 0)
+				.ToList();
+
+			int zzz = 0;
+		}
+	}
+}
