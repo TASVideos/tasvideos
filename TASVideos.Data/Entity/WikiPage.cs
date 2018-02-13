25e0bc50776d44179f1044f18ba9d2c78b9322f6
adelikat <adelikat@tasvideos.org>
2018-02-12 21:50:39 -0600

    use sqlbulkimport to greatly speed up wiki imports, other importers can benefit here too

--- TASVideos.Data/Entity/WikiPage.cs
+++ TASVideos.Data/Entity/WikiPage.cs
@@ -16,6 +16,7 @@ namespace TASVideos.Data.Entity
 		public bool MinorEdit { get; set; }
 		public string RevisionMessage { get; set; }
 
+		public int? ChildId { get; set; }
 		public virtual WikiPage Child { get; set; } // The latest revision of a page is one with Child = null
 
 		public bool IsDeleted { get; set; }
