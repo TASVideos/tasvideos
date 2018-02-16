29db9fa5eb91883c1db671182378e72de2eac407
adelikat <adelikat@tasvideos.org>
2018-02-15 21:23:34 -0600

    start publication author impor (still some bugs)

---
+++ TASVideos.Legacy/Data/Site/Entity/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TASVideos.Legacy.Data.Site.Entity
+{
+	public class Player
+	{
+		[Key]
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("name")]
+		public string Name { get; set; }
+	}
+}
--- TASVideos.Legacy/Imports/PublicationImporter.cs
+++ TASVideos.Legacy/Imports/PublicationImporter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 using FastMember;
 
@@ -11,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using TASVideos.Data;
 using TASVideos.Data.Constants;
 using TASVideos.Data.Entity;
+using TASVideos.Data.Entity.Game;
 using TASVideos.Legacy.Data.Site;
 using TASVideos.Legacy.Data.Site.Entity;
 
@@ -51,26 +53,38 @@ namespace TASVideos.Legacy.Imports
 				.ToList();
 
 			var publications = new List<Publication>();
+			var publicationAuthors = new List<PublicationAuthor>();
+			var users = context.Users.ToList();
+			var players = legacySiteContext.Players.ToList();
+			var systems = context.GameSystems.ToList();
+			var systemFrameRates = context.GameSystemFrameRates.ToList();
+			var games = context.Games.ToList();
 
 			foreach (var legacyMovie in legacyMovies)
 			{
 				string pageName = LinkConstants.PublicationWikiPage + legacyMovie.Id;
 				var wiki = publicationWikis.Single(p => p.PageName == pageName);
 				var submission = submissions.Single(s => s.Id == legacyMovie.SubmissionId);
-
 				var files = legacyMovieFiles
 					.Where(lmf => lmf.MovieId == legacyMovie.Id)
 					.ToList();
-
+				var system = systems.Single(s => s.Id == legacyMovie.SystemId);
 				var publisher = legacyWikiUsers.Single(u => u.Id == legacyMovie.PublisherId);
+				var systemFrameRate = systemFrameRates.Single(s => s.Id == submission.SystemFrameRateId);
 
-				MovieFile movieFile = null;
+				var game = games.Single(g => g.Id == (submission.GameId ?? -1));
 
 				// Find the first of an acceptable movie type
 				var movieTypes = new[] { "B2", "BK", "C", "6", "2", "S", "B", "L", "W", "3", "Y", "G", "#", "F", "Q", "E", "Z", "X", "U", "I", "R", "8", "4", "9", "7", "F3", "MA" };
-				movieFile = files.First(f => movieTypes.Contains(f.Type));
+				var movieFile = files.First(f => movieTypes.Contains(f.Type));
+
+				var player = players.Single(p => p.Id == legacyMovie.PlayerId);
 
 				var movieFileStorage = legacyMovieFileStorage.Single(lmfs => lmfs.FileName == movieFile.FileName);
+
+				var potentialAuthors = player.Name
+				.Split("&")
+				.Select(s => Regex.Replace(s, @"\s+", ""));
 
 				var publication = new Publication
 				{
@@ -84,20 +98,42 @@ namespace TASVideos.Legacy.Imports
 					Frames = submission.Frames,
 					RerecordCount = submission.RerecordCount,
 					RomId = -1, // Place holder
-					GameId = submission.Id,
+					GameId = submission.GameId ?? -1,
+					Game = game,
 					MovieFile = movieFileStorage.FileData,
 					MovieFileName = movieFile.FileName,
 					SystemFrameRateId = submission.SystemFrameRateId.Value,
+					SystemFrameRate = systemFrameRate,
 					SystemId = legacyMovie.SystemId,
+					System = system,
 					Branch = legacyMovie.Branch
 				};
 
-				//publication.GenerateTitle(); // TODO
+				var pauthors = users
+					.Where(u => potentialAuthors.Contains(u.UserName))
+					.Select(u => new PublicationAuthor
+					{
+						UserId = u.Id,
+						Author = u,
+						PublicationId = legacyMovie.Id,
+						Pubmisison = publication,
+					})
+					.ToList();
+
+				publicationAuthors.AddRange(pauthors);
+
+				foreach (var author in pauthors)
+				{
+					publication.Authors.Add(author);
+				}
+				
+				publication.GenerateTitle();
 				publications.Add(publication);
 			}
 
 			var copyParams = new[]
 			{
+				nameof(Publication.Branch),
 				nameof(Publication.Id),
 				nameof(Publication.SubmissionId),
 				nameof(Publication.TierId),
@@ -111,7 +147,14 @@ namespace TASVideos.Legacy.Imports
 				nameof(Publication.MovieFile),
 				nameof(Publication.MovieFileName),
 				nameof(Publication.SystemFrameRateId),
-				nameof(Publication.SystemId)
+				nameof(Publication.SystemId),
+				nameof(Publication.Title)
+			};
+
+			var authorParams = new[]
+			{
+				nameof(PublicationAuthor.UserId),
+				nameof(PublicationAuthor.PublicationId)
 			};
 
 			using (var sqlCopy = new SqlBulkCopy(context.Database.GetDbConnection().ConnectionString, SqlBulkCopyOptions.KeepIdentity))
@@ -127,6 +170,22 @@ namespace TASVideos.Legacy.Imports
 				using (var reader = ObjectReader.Create(publications, copyParams))
 				{
 					sqlCopy.WriteToServer(reader);
+				}
+			}
+
+			using (var authorSqlCopy = new SqlBulkCopy(context.Database.GetDbConnection().ConnectionString))
+			{
+				authorSqlCopy.DestinationTableName = $"[{nameof(ApplicationDbContext.PublicationAuthors)}]";
+				authorSqlCopy.BatchSize = 10000;
+
+				foreach (var param in authorParams)
+				{
+					authorSqlCopy.ColumnMappings.Add(param, param);
+				}
+
+				using (var reader = ObjectReader.Create(publicationAuthors, authorParams))
+				{
+					authorSqlCopy.WriteToServer(reader);
 				}
 			}
 		}
