db6398dd6059f89832ae2232da298db75c093d6c
adelikat <adelikat@tasvideos.org>
2018-02-16 17:30:00 -0600

    publication import - user the user_player mappings to better find authors, user import - import some legacy authors who were never registered

---
+++ TASVideos.Legacy/Data/Site/Entity/UserPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TASVideos.Legacy.Data.Site.Entity
+{
+    public class UserPlayer
+    {
+		[Column("userid")]
+		public int UserId { get; set; }
+
+		[Column("playerid")]
+		public int PlayerId { get; set; }
+    }
+}
