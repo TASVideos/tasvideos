be70d4cbc3dd4412b7d56fb6da1ea0a40162daf0
adelikat <adelikat@tasvideos.org>
2018-02-17 09:03:08 -0600

    publication importer - import screenshots

--- TASVideos.Data/Entity/PublicationFile.cs
+++ TASVideos.Data/Entity/PublicationFile.cs
@@ -8,6 +8,8 @@
 	public class PublicationFile : BaseEntity
 	{
 		public int Id { get; set; }
+
+		public int PublicationId { get; set; }
 		public virtual Publication Publication { get; set; }
 
 		public string Path { get; set; }
