7f4b042ab2fe46338842b33ec72ed644a8e8c827
adelikat <adelikat@tasvideos.org>
2018-03-17 19:40:41 -0500

    Start Forum/Index page

---
+++ TASVideos/Controllers/ForumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Tasks;
+
+namespace TASVideos.Controllers
+{
+	public class ForumController : BaseController
+	{
+		public ForumController(UserTasks userTasks)
+			: base(userTasks)
+		{
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
