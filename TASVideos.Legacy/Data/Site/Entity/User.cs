1f8190d99fb66df2a37ee99216a9d57fb8d4e2f8
adelikat <adelikat@tasvideos.org>
2018-03-21 11:27:04 -0500

    Speed up and simplify user importer by using navigation properties on legacy site user/role tables

--- TASVideos.Legacy/Data/Site/Entity/User.cs
+++ TASVideos.Legacy/Data/Site/Entity/User.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TASVideos.Legacy.Data.Site.Entity
@@ -26,5 +27,7 @@ namespace TASVideos.Legacy.Data.Site.Entity
 
 		[Column("points")]
 		public double Points { get; set; }
+
+		public virtual ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
     }
 }
