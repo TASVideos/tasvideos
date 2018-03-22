0328d188becee090c760faa00a4c9aae46bc300f
adelikat <adelikat@tasvideos.org>
2018-03-22 13:16:32 -0500

    Speedup and cleanup GameImporter logic

--- TASVideos.Legacy/Imports/GameImporter.cs
+++ TASVideos.Legacy/Imports/GameImporter.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using TASVideos.Data;
 using TASVideos.Data.Entity.Game;
 using TASVideos.Legacy.Data.Site;
+using TASVideos.Legacy.Data.Site.Entity;
 
 namespace TASVideos.Legacy.Imports
 {
@@ -14,16 +15,35 @@ namespace TASVideos.Legacy.Imports
 			ApplicationDbContext context,
 			NesVideosSiteContext legacySiteContext)
 		{
-			var legacyGameNames = legacySiteContext.GameNames.ToList();
-			var legacyClassTypes = legacySiteContext.ClassTypes.ToList();
-			var legacyMovieClass = legacySiteContext.MovieClass.ToList();
-			var legacySubmissions = legacySiteContext.Submissions
-				.Select(s => new { s.Id, s.GameNameId })
-				.ToList();
+			List<GameName> legacyGameNames;
+			List<ClassTypeDto> legacyClassTypes;
+			List<MovieClass> legacyMovieClass;
+			List<PublicationDto> legacyPublications;
 
-			var legacyPublications = legacySiteContext.Movies
-				.Select(m => new { m.Id, m.SubmissionId })
-				.ToList();
+			using (legacySiteContext.Database.BeginTransaction())
+			{
+				legacyGameNames = legacySiteContext.GameNames.ToList();
+
+				legacyClassTypes = legacySiteContext.ClassTypes
+					.Where(c => c.PositiveText.StartsWith("Genre"))
+					.Select(c => new ClassTypeDto
+					{
+						Id = c.Id,
+						PositiveText = c.PositiveText.Replace("Genre: ", "")
+					})
+					.ToList();
+
+				legacyMovieClass = legacySiteContext.MovieClass.ToList();
+
+				legacyPublications = legacySiteContext.Movies
+					.Where(m => m.Submission.GameNameId.HasValue)
+					.Select(m => new PublicationDto
+					{
+						Id = m.Id,
+						GameNameId = m.Submission.GameNameId.Value
+					})
+					.ToList();
+			}
 
 			var genres = context.Genres.ToList();
 
@@ -41,16 +61,11 @@ namespace TASVideos.Legacy.Imports
 					g.SearchKey,
 					g.YoutubeTags,
 					GameGenres =
-						(from s in legacySubmissions
-						 join p in legacyPublications on s.Id equals p.SubmissionId
+						(from p in legacyPublications
 						 join mc in legacyMovieClass on p.Id equals mc.MovieId
 						 join c in legacyClassTypes on mc.ClassId equals c.Id
-						 where s.GameNameId == g.Id && c.PositiveText.Contains("Genre")
-						 select new
-						 {
-							 c.Id,
-							 c.PositiveText
-						 })
+						 where p.GameNameId == g.Id
+						 select c)
 						.Distinct()
 						.ToList()
 				})
@@ -58,18 +73,16 @@ namespace TASVideos.Legacy.Imports
 
 			foreach (var legacyGameName in lgn)
 			{
-				var legacyGameGenres = legacyGameName.GameGenres;
-
-				foreach (var lgg in legacyGameGenres)
+				foreach (var lgg in legacyGameName.GameGenres)
 				{
 					gameGenres.Add(new GameGenre
 					{
-						GenreId = genres.Single(g => g.DisplayName == lgg.PositiveText.Replace("Genre: ", "")).Id,
+						GenreId = genres.Single(g => g.DisplayName == lgg.PositiveText).Id,
 						GameId = legacyGameName.Id
 					});
 				}
 
-				var game = new Game
+				games.Add(new Game
 				{
 					Id = legacyGameName.Id,
 					SystemId = legacyGameName.SystemId,
@@ -80,30 +93,10 @@ namespace TASVideos.Legacy.Imports
 					YoutubeTags = legacyGameName.YoutubeTags,
 					CreateTimeStamp = DateTime.UtcNow,
 					LastUpdateTimeStamp = DateTime.UtcNow
-				};
-
-				games.Add(game);
+				});
 			}
 
-			// The legacy system did not strictly enforce a game for publications
-			// but the new system demands fully cataloged publications
-			// we want a placeholder game entry for publications that lack a game entry
-			// And also for the placeholder rom because roms are also strictly enforced
-			// to have a game
-			games.Add(new Game
-			{
-				Id = -1,
-				SystemId = 1, // Arbitruary, I'd rather not have a placeholder system because it could clutter a lot of parts of the code
-				GoodName = "Unknown Game",
-				DisplayName = "Unknown Game",
-				Abbreviation = "Unknown",
-				SearchKey = "",
-				YoutubeTags = "",
-				CreateUserName = "adelikat",
-				LastUpdateUserName = "adelikat",
-				CreateTimeStamp = DateTime.UtcNow,
-				LastUpdateTimeStamp = DateTime.UtcNow,
-			});
+			games.Add(UnknownGame);
 
 			var columns = new[]
 			{
@@ -127,6 +120,38 @@ namespace TASVideos.Legacy.Imports
 			};
 
 			gameGenres.BulkInsert(context, gameGenreColumns, nameof(ApplicationDbContext.GameGenres));
+		}
+
+		// The legacy system did not strictly enforce a game for publications
+		// but the new system demands fully cataloged publications
+		// we want a placeholder game entry for publications that lack a game entry
+		// And also for the placeholder rom because roms are also strictly enforced
+		// to have a game
+		private static readonly Game UnknownGame = new Game
+		{
+			Id = -1,
+			SystemId = 1, // Arbitruary, I'd rather not have a placeholder system because it could clutter a lot of parts of the code
+			GoodName = "Unknown Game",
+			DisplayName = "Unknown Game",
+			Abbreviation = "Unknown",
+			SearchKey = "",
+			YoutubeTags = "",
+			CreateUserName = "adelikat",
+			LastUpdateUserName = "adelikat",
+			CreateTimeStamp = DateTime.UtcNow,
+			LastUpdateTimeStamp = DateTime.UtcNow,
+		};
+
+		private class PublicationDto
+		{
+			public int Id { get; set; }
+			public int GameNameId { get; set; }
+		}
+
+		private class ClassTypeDto
+		{
+			public int Id { get; set; }
+			public string PositiveText { get; set; }
 		}
 	}
 }
