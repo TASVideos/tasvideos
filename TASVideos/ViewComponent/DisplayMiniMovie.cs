e187f7f377511549c85b812331bd7b9e089f489d
adelikat <adelikat@tasvideos.org>
2018-03-24 11:04:31 -0500

    better temp logic for DisplayMiniMovie module (at least works with dev sample data)

--- TASVideos/ViewComponent/DisplayMiniMovie.cs
+++ TASVideos/ViewComponent/DisplayMiniMovie.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using TASVideos.Data.Entity;
+using TASVideos.Data.SampleData;
 using TASVideos.Tasks;
 
 namespace TASVideos.ViewComponents
 {
+	// TODO: a better name for this is FrontPageMovie or something like that
 	public class DisplayMiniMovie : ViewComponent
 	{
 		private readonly PublicationTasks _publicationTasks;
@@ -17,8 +20,9 @@ namespace TASVideos.ViewComponents
 
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
 		{
-			int id = new Random().Next(0, 2000); // TODO
-			var movie = await _publicationTasks.GetPublicationMiniMovie(id); // TODO
+			var candidateIds = await _publicationTasks.FrontPageMovieCandidates();
+			var id = candidateIds.ToList().AtRandom();
+			var movie = await _publicationTasks.GetPublicationMiniMovie(id);
 			return View(movie);
 		}
 	}
