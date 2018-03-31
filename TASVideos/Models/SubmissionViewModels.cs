620925e141e3b296dbb92566edb1d41e98ae5343
adelikat <adelikat@tasvideos.org>
2018-03-31 14:14:40 -0500

    Submission List - refactor viewmodel in prep for filtering

--- TASVideos/Models/SubmissionViewModels.cs
+++ TASVideos/Models/SubmissionViewModels.cs
@@ -229,25 +229,30 @@ namespace TASVideos.Models
 	/// </summary>
 	public class SubmissionListViewModel
 	{
-		[Display(Name = "Movie name")]
-		public string Title => $"{System} {GameName}"
+		public IEnumerable<Entry> Entries { get; set; } = new List<Entry>();
+
+		public class Entry
+		{
+			[Display(Name = "Movie name")]
+			public string Title => $"{System} {GameName}"
 			+ (!string.IsNullOrWhiteSpace(Branch) ? $" \"{Branch}\" " : "")
 			+ $" in {Time:g}";
 
-		[Display(Name = "Author")]
-		public string Author { get; set; }
+			[Display(Name = "Author")]
+			public string Author { get; set; }
 
-		[Display(Name = "Submitted")]
-		public DateTime Submitted { get; set; }
+			[Display(Name = "Submitted")]
+			public DateTime Submitted { get; set; }
 
-		[Display(Name = "Status")]
-		public SubmissionStatus Status { get; set; }
+			[Display(Name = "Status")]
+			public SubmissionStatus Status { get; set; }
 
-		public int Id { get; set; }
-		public string System { get; set; }
-		public string GameName { get; set; }
-		public TimeSpan Time { get; set; }
-		public string Branch { get; set; }
+			public int Id { get; set; }
+			public string System { get; set; }
+			public string GameName { get; set; }
+			public TimeSpan Time { get; set; }
+			public string Branch { get; set; }
+		}
 	}
 
 	/// <summary>
