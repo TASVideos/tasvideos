6d317d8d01be775074058ecf84e477760eadd557
adelikat <adelikat@tasvideos.org>
2018-04-09 10:25:07 -0500

    AwardTasks - cache logic

--- TASVideos/Tasks/AwardTasks.cs
+++ TASVideos/Tasks/AwardTasks.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 using TASVideos.Data;
+using TASVideos.Data.Constants;
 using TASVideos.Data.Entity.Awards;
 using TASVideos.Models;
 using TASVideos.Services;
@@ -13,6 +14,8 @@ namespace TASVideos.Tasks
 {
 	public class AwardTasks
 	{
+		private const string AwardCacheKey = "AwardsCache";
+
 		private readonly ApplicationDbContext _db;
 		private readonly ICacheService _cache;
 
@@ -29,19 +32,68 @@ namespace TASVideos.Tasks
 		/// </summary>
 		public async Task<IEnumerable<AwardDisplayModel>> GetAllAwardsForUser(int userId)
 		{
-			// TODO: caching - cache all awards (for a really long time) then select the ones relevant to the user
+			var allAwards = await AllAwardsCache();
 
-			var userAwards = await _db.UserAwards
-				.Where(ua => ua.UserId == userId)
+			return allAwards
+				.Where(a => a.UserIds.Contains(userId))
 				.Select(ua => new AwardDisplayModel
 				{
-					ShortName = ua.Award.ShortName,
-					Description = ua.Award.Description,
+					ShortName = ua.ShortName,
+					Description = ua.Description,
 					Year = ua.Year
 				})
-				.ToListAsync();
+				.ToList();
+		}
 
-			return Enumerable.Empty<AwardDisplayModel>();
+		private class AwardDto
+		{
+			public string ShortName { get; set; }
+			public string Description { get; set; }
+			public int Year { get; set; }
+			public AwardType Type { get; set; }
+			public int? PublicationId { get; set; }
+			public IEnumerable<int> UserIds { get; set; } = new HashSet<int>();
+		}
+
+		private async Task<IEnumerable<AwardDto>> AllAwardsCache()
+		{
+			if (_cache.TryGetValue(AwardCacheKey, out IEnumerable<AwardDto> awards))
+			{
+				return awards;
+			}
+
+			using (_db.Database.BeginTransactionAsync())
+			{
+				var userAwards = await _db.UserAwards
+					.Select(ua => new AwardDto
+					{
+						ShortName = ua.Award.ShortName,
+						Year = ua.Year,
+						Type = AwardType.User,
+						PublicationId = null,
+						UserIds = new[] { ua.UserId }
+					})
+					.ToListAsync();
+
+				var movieAwards = await _db.PublicationAwards
+					.Select(pa => new AwardDto
+					{
+						ShortName = pa.Award.ShortName,
+						Year = pa.Year,
+						Type = AwardType.Movie,
+						PublicationId = pa.PublicationId,
+						UserIds = pa.Publication.Authors
+							.Select(a => a.UserId)
+							.ToList()
+					})
+					.ToListAsync();
+
+				var allAwards = userAwards.Concat(movieAwards);
+
+				_cache.Set(AwardCacheKey, allAwards, DurationConstants.OneWeekInSeconds);
+
+				return allAwards;
+			}
 		}
 	}
 }
