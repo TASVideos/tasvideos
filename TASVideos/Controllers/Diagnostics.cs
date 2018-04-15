5c0066ac6e14803c9ff3be5671ac18f59ed18ad3
adelikat <adelikat@tasvideos.org>
2018-04-15 10:00:17 -0500

    Start diagnostics controller with total memory usage, and maybe a few other useful things

---
+++ TASVideos/Controllers/Diagnostics.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Data.Entity;
+using TASVideos.Filter;
+using TASVideos.Tasks;
+
+namespace TASVideos.Controllers
+{
+	[RequirePermission(PermissionTo.SeeDiagnostics)]
+	public class DiagnosticsController : BaseController
+	{
+		public DiagnosticsController(UserTasks userTasks)
+			: base(userTasks)
+		{
+		}
+
+		public IActionResult Index()
+		{
+			var data = new
+			{
+				Version,
+				Environment.MachineName,
+				Environment.Is64BitOperatingSystem,
+				OSVersion = Environment.OSVersion.ToString(),
+				Environment.ProcessorCount,
+				Environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"),
+				ProcessInfo = GetProcessInfo()
+			};
+
+			return Ok(data);
+		}
+
+		private object GetProcessInfo()
+		{
+			try
+			{
+				var process = Process.GetCurrentProcess();
+
+				return new
+				{
+
+					TotalMemoryUsage = $"{(process.PrivateMemorySize64 / 1024 / 1024):n0} MB"
+				};
+			}
+			catch (PlatformNotSupportedException)
+			{
+				return "Platform Not Supported";
+			}
+		}
+	}
+}
