ae4e1168048e558d4295d72220d4e3a9e2afc9f0
adelikat <adelikat@tasvideos.org>
2018-04-18 08:58:54 -0500

    legacy import - add entity object for movie_flag

---
+++ TASVideos.Legacy/Data/Site/Entity/MovieFlag.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TASVideos.Legacy.Data.Site.Entity
+{
+	public class MovieFlag
+	{
+		[Column("movie")]
+		public int MovieId { get; set; }
+
+		[Column("flag")]
+		public int FlagId { get; set; }
+	}
+}
