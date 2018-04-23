f65d9a55bd4a1ab7dbaaef2dc76d14023ab989cd
adelikat <adelikat@tasvideos.org>
2018-04-23 12:09:31 -0500

    break off private message task methods into their own class

--- TASVideos/Tasks/ForumTasks.cs
+++ TASVideos/Tasks/ForumTasks.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
 
 using TASVideos.Data;
-using TASVideos.Data.Constants;
 using TASVideos.Data.Entity;
 using TASVideos.Data.Entity.Forum;
 using TASVideos.Models;
-using TASVideos.Services;
 using TASVideos.ViewComponents;
 
 namespace TASVideos.Tasks
@@ -19,16 +16,13 @@ namespace TASVideos.Tasks
 	{
 		private readonly ApplicationDbContext _db;
 		private readonly AwardTasks _awardTasks;
-		private readonly ICacheService _cache;
 
 		public ForumTasks(
 			ApplicationDbContext db,
-			AwardTasks awardTasks,
-			ICacheService cache)
+			AwardTasks awardTasks)
 		{
 			_db = db;
 			_awardTasks = awardTasks;
-			_cache = cache;
 		}
 
 		/// <summary>
@@ -167,74 +161,6 @@ namespace TASVideos.Tasks
 				.OrderByDescending(p => p.PostTime)
 				.Take(limit)
 				.ToListAsync();
-		}
-
-		// TODO: document
-		public async Task<ForumInboxModel> GetUserInBox(User user)
-		{
-			return new ForumInboxModel
-			{
-				UserId = user.Id,
-				UserName = user.UserName,
-				Inbox = await _db.ForumPrivateMessages
-					.Where(pm => pm.ToUserId == user.Id)
-					.Select(pm => new ForumInboxModel.InboxEntry
-					{
-						Id = pm.Id,
-						Subject = pm.Subject,
-						SendDate = pm.CreateTimeStamp,
-						FromUser = pm.FromUser.UserName,
-						IsRead = pm.ReadOn.HasValue
-					})
-					.ToListAsync()
-			};
-		}
-
-		// TODO: document
-		public async Task<ForumPrivateMessageModel> GetPrivateMessage(User user, int id)
-		{
-			var pm = await _db.ForumPrivateMessages
-				.Include(p => p.FromUser)
-				.Where(p => p.Id == id)
-				.Where(p => p.ToUserId == user.Id)
-				.SingleOrDefaultAsync();
-
-			if (pm == null)
-			{
-				return null;
-			}
-
-			pm.ReadOn = DateTime.UtcNow;
-			await _db.SaveChangesAsync();
-
-			var model = new ForumPrivateMessageModel
-			{
-				Id = pm.Id,
-				Subject = pm.Subject,
-				SentOn = pm.CreateTimeStamp,
-				Text = pm.Text,
-				FromUserId = pm.FromUserId,
-				FromUserName = pm.FromUser.UserName
-			};
-
-			return model;
-		}
-
-		// TODO: document
-		public async Task<int> GetUnreadMessageCount(User user)
-		{
-			var cacheKey = $"{nameof(ForumTasks)}-{nameof(GetUnreadMessageCount)}-{user.Id}";
-			if (_cache.TryGetValue(cacheKey, out int unreadMessageCount))
-			{
-				return unreadMessageCount;
-			}
-
-			unreadMessageCount = await _db.ForumPrivateMessages
-				.Where(pm => pm.ToUserId == user.Id)
-				.CountAsync(pm => pm.ReadOn == null);
-
-			_cache.Set(cacheKey, unreadMessageCount, DurationConstants.OneMinuteInSeconds);
-			return unreadMessageCount;
 		}
 	}
 }
---
+++ TASVideos/Tasks/PrivateMessageTasks.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
+using TASVideos.Data.Constants;
+using TASVideos.Data.Entity;
+using TASVideos.Data.Entity.Forum;
+using TASVideos.Models;
+using TASVideos.Services;
+using TASVideos.ViewComponents;
+
+namespace TASVideos.Tasks
+{
+    public class PrivateMessageTasks
+    {
+		private readonly ApplicationDbContext _db;
+		private readonly ICacheService _cache;
+
+		public PrivateMessageTasks(
+			ApplicationDbContext db,
+			ICacheService cache)
+		{
+			_db = db;
+			_cache = cache;
+		}
+
+		// TODO: document
+		public async Task<ForumInboxModel> GetUserInBox(User user)
+		{
+			return new ForumInboxModel
+			{
+				UserId = user.Id,
+				UserName = user.UserName,
+				Inbox = await _db.ForumPrivateMessages
+					.Where(pm => pm.ToUserId == user.Id)
+					.Select(pm => new ForumInboxModel.InboxEntry
+					{
+						Id = pm.Id,
+						Subject = pm.Subject,
+						SendDate = pm.CreateTimeStamp,
+						FromUser = pm.FromUser.UserName,
+						IsRead = pm.ReadOn.HasValue
+					})
+					.ToListAsync()
+			};
+		}
+
+		// TODO: document
+		public async Task<ForumPrivateMessageModel> GetPrivateMessage(User user, int id)
+		{
+			var pm = await _db.ForumPrivateMessages
+				.Include(p => p.FromUser)
+				.Where(p => p.Id == id)
+				.Where(p => p.ToUserId == user.Id)
+				.SingleOrDefaultAsync();
+
+			if (pm == null)
+			{
+				return null;
+			}
+
+			pm.ReadOn = DateTime.UtcNow;
+			await _db.SaveChangesAsync();
+
+			var model = new ForumPrivateMessageModel
+			{
+				Id = pm.Id,
+				Subject = pm.Subject,
+				SentOn = pm.CreateTimeStamp,
+				Text = pm.Text,
+				FromUserId = pm.FromUserId,
+				FromUserName = pm.FromUser.UserName
+			};
+
+			return model;
+		}
+
+		// TODO: document
+		public async Task<int> GetUnreadMessageCount(User user)
+		{
+			var cacheKey = $"{nameof(ForumTasks)}-{nameof(GetUnreadMessageCount)}-{user.Id}";
+			if (_cache.TryGetValue(cacheKey, out int unreadMessageCount))
+			{
+				return unreadMessageCount;
+			}
+
+			unreadMessageCount = await _db.ForumPrivateMessages
+				.Where(pm => pm.ToUserId == user.Id)
+				.CountAsync(pm => pm.ReadOn == null);
+
+			_cache.Set(cacheKey, unreadMessageCount, DurationConstants.OneMinuteInSeconds);
+			return unreadMessageCount;
+		}
+	}
+}
