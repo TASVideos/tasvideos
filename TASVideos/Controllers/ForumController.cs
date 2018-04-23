788296db4b8858765518c8df919e6a9b2c737b30
adelikat <adelikat@tasvideos.org>
2018-04-23 14:33:00 -0500

    refactor private message controller actions to a MessagesController

--- TASVideos/Controllers/ForumController.cs
+++ TASVideos/Controllers/ForumController.cs
@@ -1,9 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
-using TASVideos.Data.Entity;
 using TASVideos.Models;
 using TASVideos.Tasks;
 
@@ -11,20 +9,14 @@ namespace TASVideos.Controllers
 {
 	public class ForumController : BaseController
 	{
-		private readonly UserManager<User> _userManager;
 		private readonly ForumTasks _forumTasks;
-		private readonly PrivateMessageTasks _pmTasks;
 
 		public ForumController(
 			UserTasks userTasks,
-			UserManager<User> userManager,
-			ForumTasks forumTasks,
-			PrivateMessageTasks pmTasks)
+			ForumTasks forumTasks)
 			: base(userTasks)
 		{
-			_userManager = userManager;
 			_forumTasks = forumTasks;
-			_pmTasks = pmTasks;
 		}
 
 		[AllowAnonymous]
@@ -45,28 +37,6 @@ namespace TASVideos.Controllers
 			}
 
 			return NotFound();
-		}
-
-		[Authorize]
-		public async Task<IActionResult> Inbox()
-		{
-			var user = await _userManager.GetUserAsync(User);
-			var model = await _pmTasks.GetUserInBox(user);
-			return View(model);
-		}
-
-		[Authorize]
-		public async Task<IActionResult> PrivateMessage(int id)
-		{
-			var user = await _userManager.GetUserAsync(User);
-			var model = await _pmTasks.GetPrivateMessage(user, id);
-
-			if (model == null)
-			{
-				return NotFound();
-			}
-
-			return View(model);
 		}
 	}
 }
