bc81f2ec0240cae6e509f0a4bdcad9b4f8b326c9
adelikat <adelikat@tasvideos.org>
2018-04-25 09:55:20 -0500

    Import forum poll table (still todo: sub tables)

--- TASVideos.Legacy/Imports/ForumPollImporter.cs
+++ TASVideos.Legacy/Imports/ForumPollImporter.cs
@@ -1,4 +1,8 @@
-﻿using TASVideos.Data;
+﻿using System;
+using System.Linq;
+
+using TASVideos.Data;
+using TASVideos.Data.Entity.Forum;
 using TASVideos.Legacy.Data.Forum;
 
 namespace TASVideos.Legacy.Imports
@@ -9,7 +13,38 @@ namespace TASVideos.Legacy.Imports
 			ApplicationDbContext context,
 			NesVideosForumContext legacyForumContext)
 		{
+			var legVoteDescriptions = (from p in legacyForumContext.VoteDescription
+				join t in legacyForumContext.Topics on p.TopicId equals t.Id // The join is to filter out orphan polls
+				select p)
+				.ToList();
 
+			var forumPolls = legVoteDescriptions
+				.Select(v => new ForumPoll
+				{
+					Id = v.Id,
+					TopicId = v.TopicId,
+					Question = ImportHelper.FixString(v.Text),
+					CreateTimeStamp = ImportHelper.UnixTimeStampToDateTime(v.VoteStart),
+					CreateUserName = "Unknown", // TODO: we could try to get the topic creator when not -1 if it is worth it
+					LastUpdateUserName = "Unknown", // Ditto
+					CloseDate = v.VoteLength == 0
+						? (DateTime?)null
+						: ImportHelper.UnixTimeStampToDateTime(v.VoteStart + v.VoteLength)
+				});
+
+			var columns = new[]
+			{
+				nameof(ForumPoll.Id),
+				nameof(ForumPoll.TopicId),
+				nameof(ForumPoll.Question),
+				nameof(ForumPoll.CloseDate),
+				nameof(ForumPoll.CreateTimeStamp),
+				nameof(ForumPoll.CreateUserName),
+				nameof(ForumPoll.LastUpdateTimeStamp),
+				nameof(ForumPoll.LastUpdateUserName)
+			};
+
+			forumPolls.BulkInsert(context, columns, nameof(ApplicationDbContext.ForumPolls));
 		}
 	}
 }
