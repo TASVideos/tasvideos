ed7a34d77190f1a64483925b7cf54d7e9a1bc4f8
adelikat <adelikat@tasvideos.org>
2018-04-25 12:47:52 -0500

    Import - Add console logging of import time

--- TASVideos.Legacy/LegacyImporter.cs
+++ TASVideos.Legacy/LegacyImporter.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Microsoft.EntityFrameworkCore;
 
 using TASVideos.Data;
 using TASVideos.Legacy.Data.Forum;
@@ -10,6 +14,8 @@ namespace TASVideos.Legacy
 {
 	public static class LegacyImporter
 	{
+		private static readonly Dictionary<string, long> ImportDurations = new Dictionary<string, long>();
+
 		public static void RunLegacyImport(
 			ApplicationDbContext context,
 			NesVideosSiteContext legacySiteContext,
@@ -20,24 +26,43 @@ namespace TASVideos.Legacy
 			legacySiteContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
 			legacyForumContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
 
-			TagImporter.Import(context, legacySiteContext);
-			RomImporter.Import(context, legacySiteContext);
-			GameImporter.Import(context, legacySiteContext);
-			UserImporter.Import(context, legacySiteContext, legacyForumContext);
-			AwardImporter.Import(context, legacySiteContext);
+			var stopwatch = Stopwatch.StartNew();
 
-			ForumCategoriesImporter.Import(context, legacyForumContext);
-			ForumImporter.Import(context, legacyForumContext);
-			ForumTopicImporter.Import(context, legacyForumContext);
-			ForumPostsImporter.Import(context, legacyForumContext);
-			ForumPrivateMessagesImporter.Import(context, legacyForumContext);
-			ForumPollImporter.Import(context, legacyForumContext);
+			Run("Tags", () => TagImporter.Import(context, legacySiteContext));
+			Run("Roms", () => RomImporter.Import(context, legacySiteContext));
+			Run("Games", () => GameImporter.Import(context, legacySiteContext));
+			Run("Users", () => UserImporter.Import(context, legacySiteContext, legacyForumContext));
+			Run("Award", () => AwardImporter.Import(context, legacySiteContext));
 
-			//WikiImporter.Import(context, legacySiteContext);
-			//SubmissionImporter.Import(context, legacySiteContext);
-			//PublicationImporter.Import(context, legacySiteContext);
-			//PublicationRatingImporter.Import(context, legacySiteContext);
-			//PublicationFlagImporter.Import(context, legacySiteContext);
+			Run("Forum Categories", () => ForumCategoriesImporter.Import(context, legacyForumContext));
+			Run("Forums", () => ForumImporter.Import(context, legacyForumContext));
+			Run("Forum Topics", () => ForumTopicImporter.Import(context, legacyForumContext));
+			Run("Forum Posts", () => ForumPostsImporter.Import(context, legacyForumContext));
+			Run("Forum Private Messages", () => ForumPrivateMessagesImporter.Import(context, legacyForumContext));
+			Run("Forum Polls Importer", () => ForumPollImporter.Import(context, legacyForumContext));
+
+			Run("Wiki", () => WikiImporter.Import(context, legacySiteContext));
+			Run("Submissions", () => SubmissionImporter.Import(context, legacySiteContext));
+			Run("Publications", () => PublicationImporter.Import(context, legacySiteContext));
+			Run("Publication Ratings", () => PublicationRatingImporter.Import(context, legacySiteContext));
+			Run("Publication Flags", () => PublicationFlagImporter.Import(context, legacySiteContext));
+
+			var elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+			stopwatch.Stop();
+			Console.WriteLine($"Import finished. Total time: {elapsedMilliseconds / 1000.0} seconds");
+			Console.WriteLine("Import breakdown:");
+			foreach (var entry in ImportDurations)
+			{
+				Console.WriteLine($"{entry.Key}: {entry.Value}");
+			}
+		}
+
+		private static void Run(string name, Action import)
+		{
+			var stopwatch = Stopwatch.StartNew();
+			import();
+			ImportDurations.Add($"{name} import", stopwatch.ElapsedMilliseconds);
+			stopwatch.Stop();
 		}
 	}
 }
