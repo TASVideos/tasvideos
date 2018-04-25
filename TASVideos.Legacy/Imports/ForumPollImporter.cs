3e4e431cd27d34b6458a70424f347fc5234c46da
adelikat <adelikat@tasvideos.org>
2018-04-25 11:36:56 -0500

    Forum Poll Option import

--- TASVideos.Legacy/Imports/ForumPollImporter.cs
+++ TASVideos.Legacy/Imports/ForumPollImporter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Linq;
 
 using TASVideos.Data;
@@ -13,6 +14,7 @@ namespace TASVideos.Legacy.Imports
 			ApplicationDbContext context,
 			NesVideosForumContext legacyForumContext)
 		{
+			/******** ForumPoll ********/
 			var legVoteDescriptions = (from p in legacyForumContext.VoteDescription
 				join t in legacyForumContext.Topics on p.TopicId equals t.Id // The join is to filter out orphan polls
 				select p)
@@ -32,7 +34,7 @@ namespace TASVideos.Legacy.Imports
 						: ImportHelper.UnixTimeStampToDateTime(v.VoteStart + v.VoteLength)
 				});
 
-			var columns = new[]
+			var pollColumns = new[]
 			{
 				nameof(ForumPoll.Id),
 				nameof(ForumPoll.TopicId),
@@ -44,7 +46,35 @@ namespace TASVideos.Legacy.Imports
 				nameof(ForumPoll.LastUpdateUserName)
 			};
 
-			forumPolls.BulkInsert(context, columns, nameof(ApplicationDbContext.ForumPolls));
+			forumPolls.BulkInsert(context, pollColumns, nameof(ApplicationDbContext.ForumPolls));
+
+			/******** ForumPollOption ********/
+			var forumPollOptions = (from vr in legacyForumContext.VoteResult
+				join v in legacyForumContext.VoteDescription on vr.Id equals v.Id // The joins are to filter out orphan options
+				join t in legacyForumContext.Topics on v.TopicId equals t.Id 
+				select vr)
+				.Select(r => new ForumPollOption
+				{
+					Text = r.VoteOptionText,
+					PollId = r.Id,
+					CreateTimeStamp = DateTime.UtcNow,
+					LastUpdateTimeStamp = DateTime.UtcNow,
+					CreateUserName = "Unknown", // TODO: could use the topic creator
+					LastUpdateUserName = "Unknown"
+				})
+				.ToList();
+
+			var pollOptionColumns = new[]
+			{
+				nameof(ForumPollOption.Text),
+				nameof(ForumPollOption.PollId),
+				nameof(ForumPollOption.CreateTimeStamp),
+				nameof(ForumPollOption.CreateUserName),
+				nameof(ForumPollOption.LastUpdateTimeStamp),
+				nameof(ForumPollOption.LastUpdateUserName)
+			};
+
+			forumPollOptions.BulkInsert(context, pollOptionColumns, nameof(ApplicationDbContext.ForumPollOptions), SqlBulkCopyOptions.Default);
 		}
 	}
 }
