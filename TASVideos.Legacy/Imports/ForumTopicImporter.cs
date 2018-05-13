56413d42d8e1665f8ff338bc285ffcac4b3acca6
adelikat <adelikat@tasvideos.org>
2018-05-13 11:36:56 -0500

    Import topic poll ids

--- TASVideos.Legacy/Imports/ForumTopicImporter.cs
+++ TASVideos.Legacy/Imports/ForumTopicImporter.cs
@@ -12,15 +12,25 @@ namespace TASVideos.Legacy.Data.Forum.Entity
 			ApplicationDbContext context,
 			NesVideosForumContext legacyForumContext)
 		{
-			var topics = legacyForumContext
-				.Topics
-				.Where(t => t.TopicMovedId == 0) // Topic moved id indicates it is not a valid topic anymore, so simply filter them out
-				.Select(t => new
+			var legTopics = (from t in legacyForumContext.Topics
+				join p in legacyForumContext.VoteDescription on t.Id equals p.TopicId into poll
+				from p in poll.DefaultIfEmpty()
+				where t.TopicMovedId == 0  // Topic moved id indicates it is not a valid topic anymore, so simply filter them out
+				select new
 				{
-					t.Id, t.ForumId, t.Title, t.PosterId, t.Timestamp, t.Views, t.Type,
-					Author = t.PosterId > 0 ? t.Poster.UserName : "Unknown"
+					t.Id,
+					t.ForumId,
+					t.Title,
+					t.PosterId,
+					t.Timestamp,
+					t.Views,
+					t.Type,
+					Author = t.PosterId > 0 ? t.Poster.UserName : "Unknown",
+					PollId = p != null ? p.Id : (int?)null
 				})
-				.ToList()
+				.ToList();
+
+			var topics = legTopics
 				.Select(t => new ForumTopic
 				{
 					Id = t.Id,
