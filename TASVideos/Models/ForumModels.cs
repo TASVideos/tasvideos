72f65668d623134a8e08f789f0fb70705901cdec
adelikat <adelikat@tasvideos.org>
2018-05-14 13:47:54 -0500

    Add ability to view a forum poll result, if given the proper permission

--- TASVideos/Models/ForumModels.cs
+++ TASVideos/Models/ForumModels.cs
@@ -154,4 +154,34 @@ namespace TASVideos.Models
 		public string UserAvatar { get; set; }
 		public string UserSignature { get; set; }
 	}
+
+	public class PollResultModel
+	{
+		public string TopicTitle { get; set; }
+		public int TopicId { get; set; }
+
+		public int PollId { get; set; }
+		public string Question { get; set; }
+
+		public IEnumerable<VoteResult> Votes { get; set; } = new List<VoteResult>();
+
+		public class VoteResult
+		{
+			public int UserId { get; set; }
+
+			[Display(Name = "User")]
+			public string UserName { get; set; }
+
+			public int Ordinal { get; set; }
+
+			[Display(Name = "Option")]
+			public string OptionText { get; set; }
+
+			[Display(Name = "Vote On")]
+			public DateTime CreateTimestamp { get; set; }
+
+			[Display(Name = "IP Address")]
+			public string IpAddress { get; set; }
+		}
+	}
 }
