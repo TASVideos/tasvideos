f8ae6303a33c9d3163b30bbad53d4edf3f073e38
adelikat <adelikat@tasvideos.org>
2018-05-20 19:23:54 -0500

    rough in for the GameSubpages module

---
+++ TASVideos/ViewComponent/GameSubPages.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Data.Entity;
+using TASVideos.Tasks;
+
+namespace TASVideos.ViewComponents
+{
+	public class GameSubPages : ViewComponent
+	{
+		private readonly WikiTasks _wikiTasks;
+
+		public GameSubPages(WikiTasks wikiTasks)
+		{
+			_wikiTasks = wikiTasks;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
+		{
+			var model = await _wikiTasks.GetGameResourcesSubPages();
+			return View(model);
+		}
+	}
+}
---
+++ TASVideos/ViewComponent/Models/GameSubpageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TASVideos.ViewComponents
+{
+    public class GameSubpageModel
+    {
+		public string SystemCode { get; set; }
+		public string SystemDescription { get; set; }
+		public string PageLink { get; set; }
+    }
+}
