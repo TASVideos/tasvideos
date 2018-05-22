a9b7612ea6318bef0bffd0baa49cfb5c6e3845c6
Scepheo <scepheo@gmail.com>
2018-05-22 22:37:19 +0200

    Create file storage implementation

---
+++ TASVideos.Data/Entity/DatabaseFile.cs
@@ -0,0 +1,23 @@
+﻿namespace TASVideos.Data.Entity
+{
+	public enum Compression
+	{
+		None,
+		Gzip
+	}
+
+	public class DatabaseFile
+	{
+		public int Id { get; set; }
+
+		public string Filename { get; set; }
+
+		public Compression Compression { get; set; }
+
+		public int CompressedSize { get; set; }
+
+		public int OriginalSize { get; set; }
+
+		public byte[] Data { get; set; }
+	}
+}
---
+++ TASVideos/Models/FileModels.cs
@@ -0,0 +1,11 @@
+﻿namespace TASVideos.Models
+{
+	public class FileViewModel
+	{
+		public int Id { get; set; }
+
+		public string Filename { get; set; }
+
+		public int Size { get; set; }
+	}
+}
