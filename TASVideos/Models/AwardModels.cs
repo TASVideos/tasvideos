ecba90487889673363c0948e3448e1104a59588d
adelikat <adelikat@tasvideos.org>
2018-05-27 15:29:55 -0500

    award module ui

--- TASVideos/Models/AwardModels.cs
+++ TASVideos/Models/AwardModels.cs
@@ -13,8 +13,20 @@ namespace TASVideos.Models
 		public string Description { get; set; }
 		public int Year { get; set; }
 		public AwardType Type { get; set; }
-		public IEnumerable<int> PublicationIds { get; set; } = new HashSet<int>();
-		public IEnumerable<int> UserIds { get; set; } = new HashSet<int>();
+		public IEnumerable<PublicationModel> Publications { get; set; } = new HashSet<PublicationModel>();
+		public IEnumerable<UserModel> Users { get; set; } = new HashSet<UserModel>();
+
+		public class UserModel
+		{
+			public int Id { get; set; }
+			public string UserName { get; set; }
+		}
+
+		public class PublicationModel
+		{
+			public int Id { get; set; }
+			public string Title { get; set; }
+		}
 	}
 
 	/// <summary>
