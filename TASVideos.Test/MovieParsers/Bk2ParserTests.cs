3d1806f6818301417de99a8cd2a493cd373a597e
adelikat <adelikat@tasvideos.org>
2018-06-25 15:35:39 -0500

    more bk2 unit tests

--- TASVideos.Test/MovieParsers/Bk2ParserTests.cs
+++ TASVideos.Test/MovieParsers/Bk2ParserTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Reflection;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,6 +11,7 @@ namespace TASVideos.Test.MovieParsers
 	[TestCategory("BK2Parsers")]
 	public class Bk2ParserTests
     {
+		private const string Bk2ResourcesPath = "TASVideos.Test.MovieParsers.SampleMovieFiles.";
 		private Bk2 _bk2Parser;
 
 		[TestInitialize]
@@ -21,8 +23,7 @@ namespace TASVideos.Test.MovieParsers
 		[TestMethod]
 		public void MissingHeader_ErrorResult()
 		{
-			var embbedded = Assembly.GetAssembly(typeof(Bk2ParserTests)).GetManifestResourceStream("TASVideos.Test.MovieParsers.SampleMovieFiles.MissingHeader.bk2");
-			var result = _bk2Parser.Parse(embbedded);
+			var result = _bk2Parser.Parse(Embedded("MissingHeader.bk2"));
 			Assert.AreEqual(false, result.Success, "Result should not be successfull");
 			Assert.IsNotNull(result.Errors, "Errors should not be null");
 			Assert.IsTrue(result.Errors.Any(), "Must be at least one error");
@@ -31,8 +32,7 @@ namespace TASVideos.Test.MovieParsers
 		[TestMethod]
 		public void MissingInputLog_ErrorResult()
 		{
-			var embbedded = Assembly.GetAssembly(typeof(Bk2ParserTests)).GetManifestResourceStream("TASVideos.Test.MovieParsers.SampleMovieFiles.MissingInputLog.bk2");
-			var result = _bk2Parser.Parse(embbedded);
+			var result = _bk2Parser.Parse(Embedded("MissingInputLog.bk2"));
 			Assert.AreEqual(false, result.Success, "Result should not be successfull");
 			Assert.IsNotNull(result.Errors, "Errors should not be null");
 			Assert.IsTrue(result.Errors.Any(), "Must be at least one error");
@@ -41,9 +41,7 @@ namespace TASVideos.Test.MovieParsers
 		[TestMethod]
 		public void Frames_CorrectResult()
 		{
-			var embbedded = Assembly.GetAssembly(typeof(Bk2ParserTests)).GetManifestResourceStream("TASVideos.Test.MovieParsers.SampleMovieFiles.2Frames.bk2");
-			var result = _bk2Parser.Parse(embbedded);
-
+			var result = _bk2Parser.Parse(Embedded("2Frames.bk2"));
 			Assert.AreEqual(true, result.Success, "Parsing must be successful");
 			Assert.AreEqual(2, result.Frames, "Result should have 2 frames");
 		}
@@ -51,11 +49,40 @@ namespace TASVideos.Test.MovieParsers
 		[TestMethod]
 		public void Frames_NoInputFrames_Returns0()
 		{
-			var embbedded = Assembly.GetAssembly(typeof(Bk2ParserTests)).GetManifestResourceStream("TASVideos.Test.MovieParsers.SampleMovieFiles.0Frames.bk2");
-			var result = _bk2Parser.Parse(embbedded);
-
+			var result = _bk2Parser.Parse(Embedded("0Frames.bk2"));
 			Assert.AreEqual(true, result.Success, "Parsing must be successful");
 			Assert.AreEqual(0, result.Frames, "Result should have 0 frames");
+		}
+
+		[TestMethod]
+		public void System_Nes()
+		{
+			var result = _bk2Parser.Parse(Embedded("Nes.bk2"));
+			Assert.IsTrue(result.Success, "Result is successful");
+			Assert.AreEqual("nes", result.SystemCode, "System should be NES");
+		}
+
+		[TestMethod]
+		public void RerecordCount_CorrectResult()
+		{
+			var result = _bk2Parser.Parse(Embedded("RerecordCount1.bk2"));
+			Assert.IsTrue(result.Success, "Result is successful");
+			Assert.AreEqual(1, result.RerecordCount, "Rerecord count must be 1");
+		}
+
+		[TestMethod]
+		public void RerecordCount_Missing_Returns0()
+		{
+			var result = _bk2Parser.Parse(Embedded("RerecordCountMissing.bk2"));
+			Assert.IsTrue(result.Success, "Result is successful");
+			Assert.IsNotNull(result.Warnings, "Warnings should not be null");
+			Assert.IsTrue(result.Warnings.Any(), "Must be at least one warning");
+			Assert.AreEqual(0, result.RerecordCount, "Rerecord count is assumed to be 0");
+		}
+
+		private Stream Embedded(string name)
+		{
+			return Assembly.GetAssembly(typeof(Bk2ParserTests)).GetManifestResourceStream(Bk2ResourcesPath + name);
 		}
 	}
 }
