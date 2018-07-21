decf3f4d6a407a6d79143b03766f0c4dc7c4cca4
adelikat <adelikat@tasvideos.org>
2018-07-21 12:04:01 -0500

    Remove auto-increment id on the Tiers table, explicitely set the id on tier seed data, also put a unique constraint on tier name

--- TASVideos.Data/SeedData/TierSeedData.cs
+++ TASVideos.Data/SeedData/TierSeedData.cs
@@ -9,6 +9,15 @@ namespace TASVideos.Data.SeedData
 		{
 			new Tier
 			{
+				Id = 1,
+				Name = "Vault",
+				Weight = 0.75,
+				IconPath = "images/vaulttier.png",
+				Link = "Vault"
+			},
+			new Tier
+			{
+				Id = 2,
 				Name = "Moons",
 				Weight = 1,
 				IconPath = "images/moontier.png",
@@ -16,17 +25,11 @@ namespace TASVideos.Data.SeedData
 			},
 			new Tier
 			{
+				Id = 3,
 				Name = "Stars",
 				Weight = 1.5,
 				IconPath = "images/startier.png",
 				Link = "Stars"
-			},
-			new Tier
-			{
-				Name = "Vault",
-				Weight = 0.75,
-				IconPath = "images/vaulttier.png",
-				Link = "Vault"
 			}
 		};
 	}
