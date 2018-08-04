4505a6a1f6d3859791a3e1e329dbb7fc74f794f5
adelikat <adelikat@tasvideos.org>
2018-08-04 10:22:09 -0500

    add an IDistributor implementation that stores posts in the db, cleanup

---
+++ TASVideos/Services/ExternalMediaPublisher/Distributors/DistributorStorage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Services.ExternalMediaPublisher.Distributors
+{
+    public class DistributorStorage : IPostDistributor
+    {
+		private static readonly IEnumerable<PostType> PostTypes = Enum
+			.GetValues(typeof(PostType))
+			.OfType<PostType>()
+			.ToList();
+
+		private readonly ApplicationDbContext _db;
+
+		public DistributorStorage(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IEnumerable<PostType> Types => PostTypes;
+
+		public void Post(IPostable post)
+		{
+			_db.MediaPosts.Add(new MediaPosts
+			{
+				Title = post.Title,
+				Link = post.Link,
+				Body = post.Body,
+				Group = post.Group,
+				Type = post.Type.ToString()
+			});
+			_db.SaveChanges();
+		}
+    }
+}
