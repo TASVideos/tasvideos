81916508043f0bc80439bca48ed194205c479d1c
adelikat <adelikat@tasvideos.org>
2018-08-09 16:57:13 -0500

    Radical idea - user module links to the user's profile page instead of wiki homepage

--- TASVideos/ViewComponent/WikiLink.cs
+++ TASVideos/ViewComponent/WikiLink.cs
@@ -1,69 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TASVideos.Data.Entity;
 using TASVideos.Data.Helpers;
 using TASVideos.Extensions;
 using TASVideos.Tasks;
-using TASVideos.WikiEngine;
 
 namespace TASVideos.ViewComponents
 {
-    public class WikiLink : ViewComponent
-    {
-        private readonly SubmissionTasks _submissionTasks;
-        private readonly PublicationTasks _publicationTasks;
+	public class WikiLink : ViewComponent
+	{
+		private readonly SubmissionTasks _submissionTasks;
+		private readonly PublicationTasks _publicationTasks;
 
-        public WikiLink(SubmissionTasks submissionTasks, PublicationTasks publicationTasks)
-        {
-            _submissionTasks = submissionTasks;
-            _publicationTasks = publicationTasks;
-        }
+		public WikiLink(SubmissionTasks submissionTasks, PublicationTasks publicationTasks)
+		{
+			_submissionTasks = submissionTasks;
+			_publicationTasks = publicationTasks;
+		}
 
-        public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
-        {
-            var split = pp.Split('|');
+		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
+		{
+			var split = pp.Split('|');
 
-            var model = new WikiLinkModel
-            {
-                Href = WikiHelper.NormalizeWikiPageName(split[0]),
-                DisplayText = split.Length > 1 ? split[1] : split[0]
-            };
+			var model = new WikiLinkModel
+			{
+				Href = WikiHelper.NormalizeWikiPageName(split[0]),
+				DisplayText = split.Length > 1 ? split[1] : split[0]
+			};
 
-            if (split.Length == 1)
-            {
-                if (pp.StartsWith("user:"))
-                {
-                    model.DisplayText = model.DisplayText.Substring(5);
-                }
-                else
-                {
-                    var id = SubmissionHelper.IsSubmissionLink(pp);
-                    if (id.HasValue)
-                    {
-                        var title = await _submissionTasks.GetTitle(id.Value);
-                        if (!string.IsNullOrWhiteSpace(title))
-                        {
-                            model.DisplayText = title;
-                        }
-                    }
-                    else
-                    {
-                        var mid = SubmissionHelper.IsPublicationLink(pp);
-                        if (mid.HasValue)
-                        {
-                            var title = $"[{mid.Value}]" + (await _publicationTasks.GetTitle(mid.Value));
-                            if (!string.IsNullOrWhiteSpace(title))
-                            {
-                                model.DisplayText = title;
-                            }
-                        }
-                    }
-                }
-            }
+			if (split.Length == 1)
+			{
+				if (pp.StartsWith("user:"))
+				{
+					model.DisplayText = model.DisplayText.Substring(5);
+					model.Href = "User/Profile/" + model.DisplayText;
+				}
+				else
+				{
+					var id = SubmissionHelper.IsSubmissionLink(pp);
+					if (id.HasValue)
+					{
+						var title = await _submissionTasks.GetTitle(id.Value);
+						if (!string.IsNullOrWhiteSpace(title))
+						{
+							model.DisplayText = title;
+						}
+					}
+					else
+					{
+						var mid = SubmissionHelper.IsPublicationLink(pp);
+						if (mid.HasValue)
+						{
+							var title = $"[{mid.Value}]" + (await _publicationTasks.GetTitle(mid.Value));
+							if (!string.IsNullOrWhiteSpace(title))
+							{
+								model.DisplayText = title;
+							}
+						}
+					}
+				}
+			}
 
-            return View(model);
-        }
-    }
+			return View(model);
+		}
+	}
 }
