79bb5da60111ff22acfe5fd3c48cb9828e9e1d31
adelikat <adelikat@tasvideos.org>
2018-10-30 21:16:36 -0500

    add a permission tag helper for specifically checking the user permissions, simplifies previous usages of condition

---
+++ TASVideos/TagHelpers/PermissionTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using TASVideos.Data.Entity;
+using TASVideos.Extensions;
+
+namespace TASVideos.TagHelpers
+{
+	[HtmlTargetElement(Attributes = nameof(Permission))]
+	public class PermissionTagHelper : TagHelper
+	{
+		public PermissionTo Permission { get; set; }
+
+		[HtmlAttributeNotBound]
+		[ViewContext]
+		public ViewContext ViewContext { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			if (!ViewContext.ViewData.UserHas(Permission))
+			{
+				output.SuppressOutput();
+			}
+		}
+	}
+}
