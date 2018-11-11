126c70d0319d8baeaefd664f9c43767f1cc996bf
adelikat <adelikat@tasvideos.org>
2018-11-11 17:43:25 -0600

    topic watch table and import step

---
+++ TASVideos.Data/Entity/Forum/ForumTopicWatch.cs
@@ -0,0 +1,13 @@
+﻿namespace TASVideos.Data.Entity.Forum
+{
+	public class ForumTopicWatch
+	{
+		public int UserId { get; set; }
+		public virtual User User { get; set; }
+
+		public int ForumTopicId { get; set; }
+		public virtual ForumTopic ForumTopic { get; set; }
+
+		public bool IsNotified { get; set; }
+	}
+}
