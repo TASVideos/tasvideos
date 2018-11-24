6e3e422aeb99e4836fe675c021b594fc6bf8b082
adelikat <adelikat@tasvideos.org>
2018-11-24 16:06:12 -0600

    legacy import - remove unused navigation property

--- TASVideos.Legacy/Data/Site/Entity/User.cs
+++ TASVideos.Legacy/Data/Site/Entity/User.cs
@@ -29,6 +29,5 @@ namespace TASVideos.Legacy.Data.Site.Entity
 		public decimal Points { get; set; }
 
 		public virtual ICollection<UserRole> UserRoles { get; set; } = new HashSet<UserRole>();
-		public virtual ICollection<SiteText> SiteTexts { get; set; } = new HashSet<SiteText>();
     }
 }
