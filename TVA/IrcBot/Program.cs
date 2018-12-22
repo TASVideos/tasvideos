8ff1a191a63d36227783d0635dcf16000573533c
adelikat <adelikat@tasvideos.org>
2018-12-22 08:24:19 -0600

    proof of concept code, nothing to see yet

---
+++ TVA/IrcBot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace TASVideoAgent
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var ircBot = new Tva();
+            await ircBot.Start();
+        }
+    }
+}
---
+++ TVA/IrcBot/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace TASVideoAgent
+{
+    public class Settings
+    {
+        public string Server { get; } = "irc.freenode.net";
+        public int Port { get; } = 6667;
+        public string Nick { get; } = "TASVideosAgentD";
+        public string Channel { get; } = "#tasvideosdevirc";
+        public string Password { get; } = "6yR4Bh2NErzFYpby";
+        public int MaxRetries { get; } = 3;
+    }
+}
