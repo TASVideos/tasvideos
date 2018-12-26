8082691c1d0ec42ce96ca0da4a344f0dd6329970
adelikat <adelikat@tasvideos.org>
2018-12-26 11:55:58 -0600

    simplify query that was convoluted due to EF 2.0 being bad

--- TASVideos/Tasks/GameTasks.cs
+++ TASVideos/Tasks/GameTasks.cs
@@ -19,28 +19,20 @@ namespace TASVideos.Tasks
 
 		public async Task<GameDisplayModel> GetGameForDisplay(int id)
 		{
-			var game = await _db.Games
-				.Include(g => g.System)
-				.Include(g => g.GameGenres)
-				.ThenInclude(gg => gg.Genre)
-				.SingleOrDefaultAsync(g => g.Id == id);
-
-			if (game != null)
-			{
-				var model = new GameDisplayModel
+			return await _db.Games
+				.Where(g => g.Id == id)
+				.Select(g => new GameDisplayModel
 				{
-					Id = game.Id,
-					DisplayName = game.DisplayName,
-					Abbreviation = game.Abbreviation,
-					ScreenshotUrl = game.ScreenshotUrl,
-					SystemCode = game.System.Code,
-					Genres = game.GameGenres.Select(gg => gg.Genre.DisplayName)
-				};
-
-				return model;
-			}
-
-			return null;
+					Id = g.Id,
+					DisplayName = g.DisplayName,
+					Abbreviation = g.Abbreviation,
+					ScreenshotUrl = g.ScreenshotUrl,
+					SystemCode = g.System.Code,
+					Genres = g.GameGenres
+						.Select(gg => gg.Genre.DisplayName)
+						.ToList()
+				})
+				.SingleOrDefaultAsync();
 		}
 	}
 }
