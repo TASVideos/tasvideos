2dd8a22a7913f4fef68a9a58a16fb12ce4ff4f49
adelikat <adelikat@tasvideos.org>
2018-12-27 17:15:25 -0600

    port Permissions/Index page to razor pages, set up the basic base controller type things and global SetViewBag attribute logic

---
+++ TASVideos/Pages/Permissions/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using TASVideos.Models;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Permissions
+{
+	[Authorize]
+	public class IndexModel : BasePageModel
+	{
+		private readonly PermissionTasks _permissionTasks;
+		public IndexModel(PermissionTasks permissionsTasks, UserTasks userTasks)
+			: base(userTasks)
+		{
+			_permissionTasks = permissionsTasks;
+		}
+
+		public IEnumerable<PermissionDisplayModel> Model { get; set; }
+
+		public async Task OnGetAsync()
+		{
+			Model = await _permissionTasks.GetAllPermissionsForDisplay();
+		}
+	}
+}
