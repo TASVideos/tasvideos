aab1ab501fc05d2f16c9eb089632a1f5f5de3733
adelikat <adelikat@tasvideos.org>
2018-12-28 17:19:19 -0600

    convert Account/Logout to razor pages

---
+++ TASVideos/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Account
+{
+	public class LogoutModel : BasePageModel
+	{
+		private readonly SignInManager<User> _signInManager;
+
+		public LogoutModel(
+			SignInManager<User> signInManager,
+			UserTasks userTasks)
+			: base(userTasks)
+		{
+			_signInManager = signInManager;
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			await _signInManager.SignOutAsync();
+			return Login();
+		}
+	}
+}
