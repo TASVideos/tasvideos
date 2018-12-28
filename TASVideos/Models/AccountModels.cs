b3d3efd716b1dd195e56a872487335798bd4d58e
adelikat <adelikat@tasvideos.org>
2018-12-28 14:59:50 -0600

    port Account/ResetPassword to razor pages

--- TASVideos/Models/AccountModels.cs
+++ TASVideos/Models/AccountModels.cs
@@ -15,23 +15,4 @@ namespace TASVideos.Models
 		[Display(Name = "Remember me?")]
 		public bool RememberMe { get; set; }
 	}
-
-	public class ResetPasswordModel
-	{
-		[Required]
-		[EmailAddress]
-		public string Email { get; set; }
-
-		[Required]
-		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 12)]
-		[DataType(DataType.Password)]
-		public string Password { get; set; }
-
-		[DataType(DataType.Password)]
-		[Display(Name = "Confirm password")]
-		[Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
-		public string ConfirmPassword { get; set; }
-
-		public string Code { get; set; }
-	}
 }
---
+++ TASVideos/Pages/Account/ResetPassword.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Data.Entity;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Account
+{
+	public class ResetPasswordModel : BasePageModel
+	{
+		private readonly UserManager<User> _userManager;
+
+		public ResetPasswordModel(
+			UserManager<User> userManager,
+			UserTasks userTasks)
+			: base(userTasks)
+		{
+			_userManager = userManager;
+		}
+
+		[BindProperty]
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[BindProperty]
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 12)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[BindProperty]
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirm password")]
+		[Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+		public string ConfirmPassword { get; set; }
+
+		[FromQuery]
+		public string Code { get; set; }
+
+		public IActionResult OnGet()
+		{
+			if (Code == null)
+			{
+				return Home();
+			}
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if (ModelState.IsValid)
+			{
+				
+				var user = await _userManager.FindByEmailAsync(Email);
+				if (user == null)
+				{
+					// Don't reveal that the user does not exist
+					return RedirectToPage("ResetPasswordConfirmation");
+				}
+
+				var result = await _userManager.ResetPasswordAsync(user, Code, Password);
+				if (result.Succeeded)
+				{
+					return RedirectToPage("ResetPasswordConfirmation");
+				}
+
+				AddErrors(result);
+			}
+
+			// If we got this far, something failed, redisplay form
+			return Page();
+		}
+	}
+}
