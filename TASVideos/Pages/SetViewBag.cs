b0840b60fa6b33d24eb36bad2ba1229d064c252c
adelikat <adelikat@tasvideos.org>
2018-12-28 12:51:55 -0600

    create a SetViewBag filter for razor pages that gets data directly from a service instead of relying on the controller/page, removes the need to have a backing Page model for every razor page, remove backing model for main page

---
+++ TASVideos/Pages/SetViewBag.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using TASVideos.Data.Entity;
+using TASVideos.Extensions;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages
+{
+	[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+	public class SetViewBag : ResultFilterAttribute
+	{
+		private static readonly Version VersionInfo = Assembly.GetExecutingAssembly().GetName().Version;
+		private string Version => $"{VersionInfo.Major}.{VersionInfo.Minor}.{VersionInfo.Revision}";
+		
+		public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+		{
+			var viewData = ((PageResult)context.Result).ViewData;
+			viewData["Version"] = Version;
+
+			var userClaimsPrincipal = context.HttpContext.User;
+			if (userClaimsPrincipal.Identity.IsAuthenticated)
+			{
+				var userTasks = (UserTasks)context.HttpContext.RequestServices.GetService(typeof(UserTasks));
+				viewData["UserPermissions"] = await userTasks.GetUserPermissionsById(userClaimsPrincipal.GetUserId());
+			}
+			else
+			{
+				viewData["UserPermissions"] = Enumerable.Empty<PermissionTo>();
+			}
+
+			await next.Invoke();
+		}
+	}
+}
