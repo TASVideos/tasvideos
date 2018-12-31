9955f33b9fbc63ff9289fc179959dcf08247b621
adelikat <adelikat@tasvideos.org>
2018-12-31 14:42:08 -0600

    port  Ratings controller to razor pages, also fix bug where you could not rate a movie with less than 3 ratings

---
+++ TASVideos/Pages/Ratings/ViewPublication.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Data.Entity;
+using TASVideos.Models;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Ratings
+{
+	[AllowAnonymous]
+	public class ViewPublicationModel : BasePageModel
+	{
+		private readonly RatingsTasks _ratingsTasks;
+
+		public ViewPublicationModel(
+			RatingsTasks ratings,
+			UserTasks userTasks) 
+			: base(userTasks)
+		{
+			_ratingsTasks = ratings;
+		}
+
+		[FromRoute]
+		public int Id { get; set; }
+
+		public PublicationRatingsModel Publication { get; set; } = new PublicationRatingsModel();
+
+		public IEnumerable<PublicationRatingsModel.RatingEntry> VisibleRatings => UserHas(PermissionTo.SeePrivateRatings)
+			? Publication.Ratings
+			: Publication.Ratings.Where(r => r.IsPublic);
+
+		public async Task<IActionResult> OnGet()
+		{
+			Publication = await _ratingsTasks.GetRatingsForPublication(Id);
+			if (Publication == null)
+			{
+				return NotFound();
+			}
+
+			return Page();
+		}
+	}
+}
