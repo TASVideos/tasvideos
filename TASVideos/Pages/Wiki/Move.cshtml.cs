3f10e5d8bb894a2fe2c82ca3da5e067c70c06438
adelikat <adelikat@tasvideos.org>
2018-12-30 21:37:11 -0600

    port Wiki/Move to razor pages

---
+++ TASVideos/Pages/Wiki/Move.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Data.Entity;
+using TASVideos.Models;
+using TASVideos.Services;
+using TASVideos.Services.ExternalMediaPublisher;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Wiki
+{
+	[RequirePermission(PermissionTo.MoveWikiPages)]
+	public class MoveModel : BasePageModel
+	{
+		private readonly IWikiPages _wikiPages;
+		private readonly ExternalMediaPublisher _publisher;
+
+		public MoveModel(
+			IWikiPages wikiPages,
+			ExternalMediaPublisher publisher,
+			UserTasks userTasks)
+			: base(userTasks)
+		{
+			_wikiPages = wikiPages;
+			_publisher = publisher;
+		}
+
+		[FromQuery]
+		public string Path { get; set; }
+
+		[BindProperty]
+		public WikiMoveModel Move { get; set; } = new WikiMoveModel();
+
+		public IActionResult OnGet()
+		{
+			if (!string.IsNullOrWhiteSpace(Path))
+			{
+				Path = Path.Trim('/');
+				if (_wikiPages.Exists(Path))
+				{
+					Move = new WikiMoveModel
+					{
+						OriginalPageName = Path,
+						DestinationPageName = Path
+					};
+					return Page();
+				}
+			}
+
+			return NotFound();
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			Move.OriginalPageName = Move?.OriginalPageName.Trim('/');
+			Move.DestinationPageName = Move?.DestinationPageName.Trim('/');
+
+			if (_wikiPages.Exists(Move.DestinationPageName, includeDeleted: true))
+			{
+				ModelState.AddModelError("Move.DestinationPageName", "The destination page already exists.");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			await _wikiPages.Move(Move.OriginalPageName, Move.DestinationPageName);
+
+			_publisher.SendGeneralWiki(
+						$"Page {Move.OriginalPageName} moved to {Move.DestinationPageName} by {User.Identity.Name}",
+						"",
+						$"{BaseUrl}/{Move.DestinationPageName}");
+
+			return Redirect("/" + Move.DestinationPageName);
+		}
+	}
+}
