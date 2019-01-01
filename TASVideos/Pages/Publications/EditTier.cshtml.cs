87a7d8355ba4115960857b7776fc26ec1d1cb462
adelikat <adelikat@tasvideos.org>
2019-01-01 12:33:49 -0600

    move Publication/EditTier to razor pages

---
+++ TASVideos/Pages/Publications/EditTier.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Data.Entity;
+using TASVideos.Models;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Publications
+{
+	[RequirePermission(PermissionTo.SetTier)]
+	public class EditTierModel : BasePageModel
+	{
+		private readonly PublicationTasks _publicationTasks;
+
+		public EditTierModel(
+			PublicationTasks publicationTasks,
+			UserTasks userTasks)
+			: base(userTasks)
+		{
+			_publicationTasks = publicationTasks;
+		}
+
+		[FromRoute]
+		public int Id { get; set; }
+
+		[BindProperty]
+		public PublicationTierEditModel Publication { get; set; }
+
+		public async Task<IActionResult> OnGet()
+		{
+			Publication = await _publicationTasks.GetTiersForEdit(Id);
+			if (Publication == null)
+			{
+				return NotFound();
+			}
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			var result = await _publicationTasks.UpdateTier(Id, Publication.TierId);
+			if (result)
+			{
+				return RedirectToPage("View", new { Id });
+			}
+
+			return NotFound();
+		}
+	}
+}
