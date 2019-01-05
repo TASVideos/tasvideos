d5b09086b17c0880ea37213df4c918e35ba22450
adelikat <adelikat@tasvideos.org>
2019-01-05 15:09:47 -0600

    port cache control page to razor pages

---
+++ TASVideos/Pages/Diagnostics/CacheControl.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Data.Entity;
+using TASVideos.Services;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Diagnostics
+{
+	[RequirePermission(PermissionTo.SeeDiagnostics)]
+	public class CacheControlModel : BasePageModel
+	{
+		private readonly IWikiPages _wikiPages;
+		private readonly AwardTasks _awardTasks;
+
+		public CacheControlModel(
+			IWikiPages wikiPages,
+			AwardTasks awardTasks,
+			UserTasks userTasks)
+			: base(userTasks)
+		{
+			_wikiPages = wikiPages;
+			_awardTasks = awardTasks;
+		}
+
+		public async Task<IActionResult> OnPostFlushWikiCache()
+		{
+			await _wikiPages.FlushCache();
+			return RedirectToPage("CacheControl");
+		}
+
+		public IActionResult OnPostClearAwardsCache()
+		{
+			// TODO: implement flush instead of clear
+			_awardTasks.ClearAwardsCache();
+			return RedirectToPage("CacheControl");
+		}
+	}
+}
