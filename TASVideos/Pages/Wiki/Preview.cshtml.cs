4bfeb24d64be385c225e6cce03640133d1789e49
adelikat <adelikat@tasvideos.org>
2019-01-06 12:47:05 -0600

    move Wiki preview to razor pages

---
+++ TASVideos/Pages/Wiki/Preview.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Models;
+using TASVideos.Razor;
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Wiki
+{
+	[AllowAnonymous]
+	[IgnoreAntiforgeryToken]
+	public class PreviewModel : BasePageModel
+	{
+		private readonly WikiMarkupFileProvider _wikiMarkupFileProvider;
+
+		public PreviewModel(
+			WikiMarkupFileProvider wikiMarkupFileProvider,
+			UserTasks userTasks)
+			: base(userTasks)
+		{
+			_wikiMarkupFileProvider = wikiMarkupFileProvider;
+		}
+
+		public IActionResult OnPost()
+		{
+			var input = new StreamReader(Request.Body, Encoding.UTF8).ReadToEnd();
+
+			ViewData["WikiPage"] = null;
+			ViewData["Title"] = "Generated Preview";
+			ViewData["Layout"] = null;
+			var name = _wikiMarkupFileProvider.SetPreviewMarkup(input);
+
+			return Partial(name);
+		}
+	}
+}
