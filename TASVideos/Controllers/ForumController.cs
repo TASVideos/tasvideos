f656a63c5b02ef8f17c90c49b4e7fa9dfb8feecf
adelikat <adelikat@tasvideos.org>
2019-01-06 16:59:39 -0600

    port direct post linking to razor pages, with some todos

--- TASVideos/Controllers/ForumController.cs
+++ TASVideos/Controllers/ForumController.cs
@@ -20,28 +20,28 @@ namespace TASVideos.Controllers
 		}
 
 		// TODO: this can be done with page routing
-		[AllowAnonymous]
-		public async Task<IActionResult> LegacyPost(int p)
-		{
-			return await Post(p);
-		}
+		//[AllowAnonymous]
+		//public async Task<IActionResult> LegacyPost(int p)
+		//{
+		//	return await Post(p);
+		//}
 
-		// TODO: how to do this without a redirect
-		[AllowAnonymous]
-		public async Task<IActionResult> Post(int id)
-		{
-			var model = await _forumTasks.GetPostPosition(id, UserHas(PermissionTo.SeeRestrictedForums));
-			if (model == null)
-			{
-				return NotFound();
-			}
+		//// TODO: how to do this without a redirect
+		//[AllowAnonymous]
+		//public async Task<IActionResult> Post(int id)
+		//{
+		//	var model = await _forumTasks.GetPostPosition(id, UserHas(PermissionTo.SeeRestrictedForums));
+		//	if (model == null)
+		//	{
+		//		return NotFound();
+		//	}
 
-			return RedirectToPage("/Forum/Topics/Index", new { Id = model.TopicId, Highlight = id, CurrentPage = model.Page });
-			//return await Topic(new TopicRequest
-			//{
-			//	Id = model.TopicId,
-			//	Highlight = id
-			//});
-		}
+		//	return RedirectToPage("/Forum/Topics/Index", new { Id = model.TopicId, Highlight = id, CurrentPage = model.Page });
+		//	//return await Topic(new TopicRequest
+		//	//{
+		//	//	Id = model.TopicId,
+		//	//	Highlight = id
+		//	//});
+		//}
 	}
 }
--- TASVideos/Extensions/ApplicationBuilderExtensions.cs
+++ TASVideos/Extensions/ApplicationBuilderExtensions.cs
@@ -59,8 +59,8 @@ namespace TASVideos.Extensions
 			app.UseMvc(routes =>
 			{
 				routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-				routes.MapRoute("forum-post", "forum/p/{id}", defaults: new { controller = "Forum", action = nameof(ForumController.Post) });
-				routes.MapRoute("legacy-post", "forum/viewtopic.php", defaults: new { controller = "Forum", action = nameof(ForumController.LegacyPost) });
+				// TODO: this is wrong anyway
+				//routes.MapRoute("legacy-post", "forum/viewtopic.php", defaults: new { controller = "Forum", action = nameof(ForumController.LegacyPost) });
 				routes.MapRoute("wiki", "{*url}", defaults: new { controller = "Wiki", action = nameof(WikiController.RenderWikiPage) });
 			});
 
