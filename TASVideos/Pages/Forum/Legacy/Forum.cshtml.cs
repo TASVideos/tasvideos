69e19224270d7a0d1140ad5b86fdd8c61e46b465
adelikat <adelikat@tasvideos.org>
2019-01-10 19:37:20 -0600

    add legacy support for /forum/viewforum.php?f=

---
+++ TASVideos/Pages/Forum/Legacy/Forum.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Tasks;
+
+namespace TASVideos.Pages.Forum.Legacy
+{
+	// Handles legacy forum links to viewforum.php
+	[AllowAnonymous]
+	public class ForumModel : BasePageModel
+	{
+		public ForumModel(
+			UserTasks userTasks) 
+			: base(userTasks)
+		{
+		}
+
+		[FromQuery]
+		public int? F { get; set; }
+
+		public async Task<IActionResult> OnGet()
+		{
+			if (!F.HasValue)
+			{
+				return NotFound();
+			}
+
+			return RedirectToPage("/Forum/Subforum/Index", new { Id = F.Value });
+		}
+	}
+}
