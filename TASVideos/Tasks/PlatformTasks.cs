a523bedbe30b211a1bc85286241e0ad5404278aa
adelikat <adelikat@tasvideos.org>
2019-01-10 18:58:34 -0600

    move a method from a tasks class to directly in the view component

--- TASVideos/Tasks/PlatformTasks.cs
+++ TASVideos/Tasks/PlatformTasks.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TASVideos.Data;
-using TASVideos.ViewComponents;
 
 namespace TASVideos.Tasks
 {
@@ -15,21 +14,6 @@ namespace TASVideos.Tasks
 		public PlatformTasks(ApplicationDbContext db)
 		{
 			_db = db;
-		}
-
-		public async Task<IEnumerable<PlatformFramerateModel>> GetAllPlatformFrameRates()
-		{
-			return await _db.GameSystemFrameRates
-				.Select(sf => new PlatformFramerateModel
-				{
-					SystemCode = sf.System.Code,
-					FrameRate = sf.FrameRate,
-					RegionCode = sf.RegionCode,
-					Preliminary = sf.Preliminary
-				})
-				.OrderBy(sf => sf.SystemCode)
-				.ThenBy(sf => sf.RegionCode)
-				.ToListAsync();
 		}
 
 		public async Task<IEnumerable<SelectListItem>> GetGameSystemDropdownList()
