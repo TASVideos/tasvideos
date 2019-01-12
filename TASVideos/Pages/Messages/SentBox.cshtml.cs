15fbcc233c490b5d5c261c0b7987571c3b7f749c
adelikat <adelikat@tasvideos.org>
2019-01-11 19:33:50 -0600

    messages pages - move some logic from tasks to directly in the page

--- TASVideos/Pages/Messages/SentBox.cshtml.cs
+++ TASVideos/Pages/Messages/SentBox.cshtml.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+using TASVideos.Data;
 using TASVideos.Data.Entity;
+using TASVideos.Data.Entity.Forum;
+using TASVideos.Models;
 using TASVideos.Tasks;
 namespace TASVideos.Pages.Messages
 {
@@ -13,26 +17,35 @@ namespace TASVideos.Pages.Messages
 	public class SentboxModel : BasePageModel
 	{
 		private readonly UserManager<User> _userManager;
-		private readonly PrivateMessageTasks _pmTasks;
+		private readonly ApplicationDbContext _db;
 
 		public SentboxModel(
 			UserManager<User> userManager,
-			PrivateMessageTasks privateMessageTasks,
+			ApplicationDbContext db,
 			UserTasks userTasks)
 			: base(userTasks)
 		{
 			_userManager = userManager;
-			_pmTasks = privateMessageTasks;
+			_db = db;
 		}
 
-		// TODO: rename this model
-		[BindProperty]
-		public IEnumerable<Models.SentboxModel> SentBox { get; set; } = new List<Models.SentboxModel>();
+		public IEnumerable<SentboxEntry> SentBox { get; set; } = new List<SentboxEntry>();
 
 		public async Task OnGet()
 		{
 			var user = await _userManager.GetUserAsync(User);
-			SentBox = await _pmTasks.GetUserSentBox(user);
+			SentBox = await _db.PrivateMessages
+				.ThatAreNotToUserDeleted()
+				.Where(pm => pm.FromUserId == user.Id)
+				.Select(pm => new SentboxEntry
+				{
+					Id = pm.Id,
+					Subject = pm.Subject,
+					ToUser = pm.ToUser.UserName,
+					SendDate = pm.CreateTimeStamp,
+					HasBeenRead = pm.ReadOn.HasValue
+				})
+				.ToListAsync();
 		}
 	}
 }
