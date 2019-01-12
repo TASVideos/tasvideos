0adf2ce83fec2cfe12d984b6ab27dd3f3d9f1aa6
adelikat <adelikat@tasvideos.org>
2019-01-11 20:17:40 -0600

    de-taskify more

--- TASVideos/ViewComponent/BrokenLinks.cs
+++ TASVideos/ViewComponent/BrokenLinks.cs
@@ -1,22 +1,40 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 using TASVideos.Data.Entity;
-using TASVideos.Tasks;
+using TASVideos.Data.Helpers;
 
 namespace TASVideos.ViewComponents
 {
 	public class BrokenLinks : ViewComponent
 	{
-		private readonly WikiTasks _wikiTasks;
+		private readonly ApplicationDbContext _db;
 
-		public BrokenLinks(WikiTasks wikiTasks)
+		public BrokenLinks(ApplicationDbContext db)
 		{
-			_wikiTasks = wikiTasks;
+			_db = db;
 		}
 
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
 		{
-			var orphans = await _wikiTasks.GetAllBrokenLinks();
+			var orphans = (await _db.WikiReferrals
+					.Where(wr => wr.Referrer != "SandBox")
+					.Where(wr => wr.Referral != "Players-List")
+					.Where(wr => !_db.WikiPages.Any(wp => wp.PageName == wr.Referral))
+					.Where(wr => !wr.Referral.StartsWith("Subs-"))
+					.Where(wr => !wr.Referral.StartsWith("Movies-"))
+					.Where(wr => !wr.Referral.StartsWith("/forum"))
+					.Where(wr => !wr.Referral.StartsWith("/userfiles"))
+					.Where(wr => !string.IsNullOrWhiteSpace(wr.Referral))
+					.Where(wr => wr.Referral != "FrontPage")
+					.ToListAsync())
+				.Where(wr => !SubmissionHelper.IsSubmissionLink(wr.Referral).HasValue)
+				.Where(wr => !SubmissionHelper.IsPublicationLink(wr.Referral).HasValue)
+				.Where(wr => !SubmissionHelper.IsGamePageLink(wr.Referral).HasValue);
+
 			return View(orphans);
 		}
 	}
