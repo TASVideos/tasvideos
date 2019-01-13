f1c21ffcd17bffb35fbbde52b590884c20368f19
adelikat <adelikat@tasvideos.org>
2019-01-12 21:48:35 -0600

    de-taskify

--- TASVideos/Tasks/WikiTasks.cs
+++ TASVideos/Tasks/WikiTasks.cs
@@ -7,7 +7,6 @@ using TASVideos.Data;
 using TASVideos.Data.Entity;
 using TASVideos.Models;
 using TASVideos.Services;
-using TASVideos.ViewComponents;
 
 namespace TASVideos.Tasks
 {
@@ -95,28 +94,6 @@ namespace TASVideos.Tasks
 				LeftMarkup = fromRevision > 0 ? revisions.Single(wp => wp.Revision == fromRevision).Markup : "",
 				RightMarkup = revisions.Single(wp => wp.Revision == toRevision).Markup
 			};
-		}
-
-		/// <summary>
-		/// Returns a list of all <see cref="WikiPage"/> records that at not linked by another page
-		/// </summary>
-		public async Task<IEnumerable<WikiOrphanModel>> GetAllOrphans()
-		{
-			return await _db.WikiPages
-				.ThatAreNotDeleted()
-				.ThatAreCurrentRevisions()
-				.Where(wp => wp.PageName != "MediaPosts") // Linked by the navbar
-				.Where(wp => !_db.WikiReferrals.Any(wr => wr.Referral == wp.PageName))
-				.Where(wp => !wp.PageName.StartsWith("System/")
-					&& !wp.PageName.StartsWith("InternalSystem")) // These by design aren't orphans they are directly used in the system
-				.Where(wp => !wp.PageName.Contains("/")) // Subpages are linked by default by the parents, so we know they are not orphans
-				.Select(wp => new WikiOrphanModel
-				{
-					PageName = wp.PageName,
-					LastUpdateTimeStamp = wp.LastUpdateTimeStamp,
-					LastUpdateUserName = wp.LastUpdateUserName ?? wp.CreateUserName
-				})
-				.ToListAsync();
 		}
 
 		/// <summary>
