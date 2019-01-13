bc516224112e5a57383aeb12d796fe97271df9f9
adelikat <adelikat@tasvideos.org>
2019-01-12 21:30:24 -0600

    de-taskify

--- TASVideos/ViewComponent/GameSubPages.cs
+++ TASVideos/ViewComponent/GameSubPages.cs
@@ -1,24 +1,60 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 using TASVideos.Data.Entity;
-using TASVideos.Tasks;
+using TASVideos.Services;
 
 namespace TASVideos.ViewComponents
 {
 	public class GameSubPages : ViewComponent
 	{
-		private readonly WikiTasks _wikiTasks;
+		private readonly IWikiPages _wikiPages;
+		private readonly ApplicationDbContext _db;
 
-		public GameSubPages(WikiTasks wikiTasks)
+		public GameSubPages(
+			IWikiPages wikiPages,
+			ApplicationDbContext db)
 		{
-			_wikiTasks = wikiTasks;
+			_wikiPages = wikiPages;
+			_db = db;
 		}
 
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
 		{
-			var model = await _wikiTasks.GetGameResourcesSubPages();
+			var model = await GetGameResourcesSubPages();
 			return View(model);
+		}
+
+		private async Task<IEnumerable<GameSubpageModel>> GetGameResourcesSubPages()
+		{
+			using (await _db.Database.BeginTransactionAsync())
+			{
+				var systems = await _db.GameSystems.ToListAsync();
+				var gameResourceSystems = systems.Select(s => "GameResources/" + s.Code);
+
+				var pages = _wikiPages
+					.ThatAreNotDeleted()
+					.ThatAreCurrentRevisions()
+					.Where(wp => gameResourceSystems.Contains(wp.PageName))
+					.Select(wp => wp.PageName)
+					.ToList();
+
+				return
+					(from s in systems
+					join wp in pages on s.Code equals wp.Split('/').Last()
+					select new GameSubpageModel
+					{
+						SystemCode = s.Code,
+						SystemDescription = s.DisplayName,
+						PageLink = "GameResources/" + s.Code
+					})
+					.ToList();
+			}
 		}
 	}
 }
