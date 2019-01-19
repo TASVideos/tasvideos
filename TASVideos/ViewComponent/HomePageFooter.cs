07c7591aefe47012202b580566f3acf2421f442d
adelikat <adelikat@tasvideos.org>
2019-01-19 16:21:10 -0600

    move logic from task layer to view component

--- TASVideos/ViewComponent/HomePageFooter.cs
+++ TASVideos/ViewComponent/HomePageFooter.cs
@@ -1,28 +1,51 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 using TASVideos.Data.Entity;
+using TASVideos.Models;
 using TASVideos.Tasks;
 
 namespace TASVideos.ViewComponents
 {
     public class HomePageFooter : ViewComponent
     {
-		private readonly UserTasks _userTasks;
+		private readonly ApplicationDbContext _db;
 		private readonly AwardTasks _awardTasks;
 
 		public HomePageFooter(
-			UserTasks userTasks,
+			ApplicationDbContext db,
 			AwardTasks awardTasks)
 		{
-			_userTasks = userTasks;
+			_db = db;
 			_awardTasks = awardTasks;
 		}
 
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData)
 		{
-			var model = await _userTasks.GetUserSummary(pageData.PageName.Replace("HomePages/", "").Split('/').First());
+			var userName = pageData.PageName.Replace("HomePages/", "").Split('/').First();
+			var model = await _db.Users
+				.Where(u => u.UserName == userName)
+				.Select(user => new UserSummaryModel
+				{
+					Id = user.Id,
+					UserName = user.UserName,
+					EditCount = _db.WikiPages.Count(wp => wp.CreateUserName == userName),
+					MovieCount = _db.Publications
+						.Count(p => p.Authors
+							.Select(sa => sa.Author.UserName)
+							.Contains(userName)),
+					SubmissionCount = _db.Submissions
+						.Count(s => s.SubmissionAuthors
+							.Select(sa => sa.Author.UserName)
+							.Contains(userName))
+				})
+				.SingleOrDefaultAsync();
+
 			model.AwardsWon = (await _awardTasks.GetAllAwardsForUser(model.Id)).Count();
+
 			ViewData["pageData"] = pageData;
 			return View(model);
 		}
