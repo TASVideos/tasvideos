3b2778c96c5d90001920661a1083bf6cf0219e83
adelikat <adelikat@tasvideos.org>
2019-01-19 15:41:02 -0600

    move some logic from wikitasks to diff.cshtml.cs

--- TASVideos/Pages/Wiki/Diff.cshtml.cs
+++ TASVideos/Pages/Wiki/Diff.cshtml.cs
@@ -1,8 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+using TASVideos.Data;
+using TASVideos.Data.Entity;
 using TASVideos.Models;
 using TASVideos.Tasks;
 
@@ -11,14 +16,14 @@ namespace TASVideos.Pages.Wiki
 	[AllowAnonymous]
 	public class DiffModel : BasePageModel
 	{
-		private readonly WikiTasks _wikiTasks;
+		private readonly ApplicationDbContext _db;
 
 		public DiffModel(
-			WikiTasks wikiTasks,
+			ApplicationDbContext db,
 			UserTasks userTasks)
 			: base(userTasks)
 		{
-			_wikiTasks = wikiTasks;
+			_db = db;
 		}
 
 		[FromQuery]
@@ -37,14 +42,74 @@ namespace TASVideos.Pages.Wiki
 			Path = Path?.Trim('/');
 
 			Diff = FromRevision.HasValue && ToRevision.HasValue
-				? await _wikiTasks.GetPageDiff(Path, FromRevision.Value, ToRevision.Value)
-				: await _wikiTasks.GetLatestPageDiff(Path);
+				? await GetPageDiff(Path, FromRevision.Value, ToRevision.Value)
+				: await GetLatestPageDiff(Path);
 		}
 
 		public async Task<IActionResult> OnGetDiffData(string path, int fromRevision, int toRevision)
 		{
-			var data = await _wikiTasks.GetPageDiff(path.Trim('/'), fromRevision, toRevision);
+			var data = await GetPageDiff(path.Trim('/'), fromRevision, toRevision);
 			return new JsonResult(data);
+		}
+
+		private async Task<WikiDiffModel> GetPageDiff(string pageName, int fromRevision, int toRevision)
+		{
+			var revisions = await _db.WikiPages
+				.ForPage(pageName)
+				.Where(wp => wp.Revision == fromRevision
+					|| wp.Revision == toRevision)
+				.ToListAsync();
+
+			if (fromRevision > 0 && revisions.Count != 2)
+			{
+				throw new InvalidOperationException($"Page \"{pageName}\" or revisions {fromRevision}-{toRevision} could not be found");
+			}
+
+			return new WikiDiffModel
+			{
+				PageName = pageName,
+				LeftRevision = fromRevision,
+				RightRevision = toRevision,
+				LeftMarkup = fromRevision > 0 ? revisions.Single(wp => wp.Revision == fromRevision).Markup : "",
+				RightMarkup = revisions.Single(wp => wp.Revision == toRevision).Markup
+			};
+		}
+
+		private async Task<WikiDiffModel> GetLatestPageDiff(string pageName)
+		{
+			var revisions = await _db.WikiPages
+				.ForPage(pageName)
+				.ThatAreNotDeleted()
+				.OrderByDescending(wp => wp.Revision)
+				.Take(2)
+				.ToListAsync();
+
+			if (!revisions.Any())
+			{
+				throw new InvalidOperationException($"Page \"{pageName}\" could not be found");
+			}
+
+			// If count is 1, it must be a new page with no history, so compare against nothing
+			if (revisions.Count == 1)
+			{
+				return new WikiDiffModel
+				{
+					PageName = pageName,
+					LeftRevision = revisions.First().Revision - 1,
+					LeftMarkup = "",
+					RightRevision = revisions.First().Revision,
+					RightMarkup = revisions.First().Markup
+				};
+			}
+			
+			return new WikiDiffModel
+			{
+				PageName = pageName,
+				LeftRevision = revisions[1].Revision,
+				LeftMarkup = revisions[1].Markup,
+				RightRevision = revisions[0].Revision,
+				RightMarkup = revisions[0].Markup
+			};
 		}
 	}
 }
--- TASVideos/Tasks/WikiTasks.cs
+++ TASVideos/Tasks/WikiTasks.cs
@@ -1,99 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TASVideos.Data;
 using TASVideos.Data.Entity;
 using TASVideos.Models;
-using TASVideos.Services;
 
 namespace TASVideos.Tasks
 {
 	public class WikiTasks
 	{
 		private readonly ApplicationDbContext _db;
-		private readonly IWikiPages _wikiPages;
 
-		public WikiTasks(
-			ApplicationDbContext db,
-			IWikiPages wikiPages)
+		public WikiTasks(ApplicationDbContext db)
 		{
 			_db = db;
-			_wikiPages = wikiPages;
-		}
-
-		/// <summary>
-		/// Returns the data necessary to generate a diff of the
-		/// latest revision of the wiki page with the given <see cref="pageName"/>
-		/// compared against the previous revision
-		/// </summary>
-		/// <exception cref="InvalidOperationException">thrown if the given <see cref="pageName" /> does not exist</exception>
-		public async Task<WikiDiffModel> GetLatestPageDiff(string pageName)
-		{
-			var revisions = await _db.WikiPages
-				.ForPage(pageName)
-				.ThatAreNotDeleted()
-				.OrderByDescending(wp => wp.Revision)
-				.Take(2)
-				.ToListAsync();
-
-			if (!revisions.Any())
-			{
-				throw new InvalidOperationException($"Page \"{pageName}\" could not be found");
-			}
-
-			// If count is 1, it must be a new page with no history, so compare against nothing
-			if (revisions.Count == 1)
-			{
-				return new WikiDiffModel
-				{
-					PageName = pageName,
-					LeftRevision = revisions.First().Revision - 1,
-					LeftMarkup = "",
-					RightRevision = revisions.First().Revision,
-					RightMarkup = revisions.First().Markup
-				};
-			}
-			
-			return new WikiDiffModel
-			{
-				PageName = pageName,
-				LeftRevision = revisions[1].Revision,
-				LeftMarkup = revisions[1].Markup,
-				RightRevision = revisions[0].Revision,
-				RightMarkup = revisions[0].Markup
-			};
-		}
-
-		/// <summary>
-		/// Returns the data necessary to generate a diff between
-		/// two revisions of the given page
-		/// </summary>
-		/// <exception cref="InvalidOperationException">If the given <see cref="pageName"/> does not exists
-		/// or if the given <see cref="fromRevision"/> or <see cref="toRevision"/> do not exist
-		/// </exception>
-		public async Task<WikiDiffModel> GetPageDiff(string pageName, int fromRevision, int toRevision)
-		{
-			var revisions = await _db.WikiPages
-				.ForPage(pageName)
-				.Where(wp => wp.Revision == fromRevision
-					|| wp.Revision == toRevision)
-				.ToListAsync();
-
-			if (fromRevision > 0 && revisions.Count != 2)
-			{
-				throw new InvalidOperationException($"Page \"{pageName}\" or revisions {fromRevision}-{toRevision} could not be found");
-			}
-
-			return new WikiDiffModel
-			{
-				PageName = pageName,
-				LeftRevision = fromRevision,
-				RightRevision = toRevision,
-				LeftMarkup = fromRevision > 0 ? revisions.Single(wp => wp.Revision == fromRevision).Markup : "",
-				RightMarkup = revisions.Single(wp => wp.Revision == toRevision).Markup
-			};
 		}
 
 		/// <summary>
