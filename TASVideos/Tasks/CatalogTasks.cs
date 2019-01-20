726aab16bb0a8962dcec7ce710ad2cd83a4a94cd
adelikat <adelikat@tasvideos.org>
2019-01-20 15:23:52 -0600

    more more logic into Game/List

--- TASVideos/Tasks/CatalogTasks.cs
+++ TASVideos/Tasks/CatalogTasks.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using TASVideos.Constants;
 using TASVideos.Data;
 using TASVideos.Data.Entity.Game;
 using TASVideos.Models;
@@ -21,33 +18,6 @@ namespace TASVideos.Tasks
 		{
 			_db = db;
 			_mapper = mapper;
-		}
-
-		public SystemPageOf<GameListModel> GetPageOfGames(GameListRequest paging)
-		{
-			var query = !string.IsNullOrWhiteSpace(paging.SystemCode)
-				? _db.Games.Where(g => g.System.Code == paging.SystemCode)
-				: _db.Games;
-
-			var data = query
-				.Select(g => new GameListModel
-				{
-					Id = g.Id,
-					DisplayName = g.DisplayName,
-					SystemCode = g.System.Code
-				})
-				.SortBy(paging)
-				.SortedPageOf(_db, paging);
-
-			return new SystemPageOf<GameListModel>(data)
-			{
-				SystemCode = paging.SystemCode,
-				PageSize = data.PageSize,
-				CurrentPage = data.CurrentPage,
-				RowCount = data.RowCount,
-				SortDescending = data.SortDescending,
-				SortBy = data.SortBy
-			};
 		}
 
 		public async Task<RomListModel> GetRomsForGame(int gameId)
@@ -138,57 +108,6 @@ namespace TASVideos.Tasks
 			_db.Roms.Remove(rom);
 			await _db.SaveChangesAsync();
 			return true;
-		}
-
-		public async Task<IEnumerable<SelectListItem>> GetFrameRateDropDownForSystem(int systemId, bool includeEmpty)
-		{
-			var items = (await _db.GameSystemFrameRates
-				.ForSystem(systemId)
-				.Select(g => new SelectListItem
-				{
-					Value = g.Id.ToString(),
-					Text = g.RegionCode + " " + g.FrameRate
-				})
-				.ToListAsync())
-				.OrderBy(r => r.Value);
-
-			return includeEmpty
-				? UiDefaults.DefaultEntry.Concat(items)
-				: items;
-		}
-
-		public async Task<IEnumerable<SelectListItem>> GetGameDropDownForSystem(int systemId, bool includeEmpty)
-		{
-			var items = (await _db.Games
-				.ForSystem(systemId)
-				.Select(g => new SelectListItem
-				{
-					Value = g.Id.ToString(),
-					Text = g.GoodName
-				})
-				.ToListAsync())
-				.OrderBy(r => r.Value);
-
-			return includeEmpty
-				? UiDefaults.DefaultEntry.Concat(items)
-				: items;
-		}
-
-		public async Task<IEnumerable<SelectListItem>> GetRomDropDownForGame(int gameId, bool includeEmpty)
-		{
-			var items = (await _db.Roms
-				.Where(r => r.GameId == gameId)
-				.Select(r => new SelectListItem
-				{
-					Value = r.Id.ToString(),
-					Text = r.Name
-				})
-				.ToListAsync())
-				.OrderBy(r => r.Value);
-
-			return includeEmpty
-				? UiDefaults.DefaultEntry.Concat(items)
-				: items;
 		}
 	}
 }
