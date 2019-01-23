f070b007ea703299eafff782381399616b40c294
adelikat <adelikat@tasvideos.org>
2019-01-22 20:39:18 -0600

    move some logic from task layer to page model

--- TASVideos/Pages/Forum/Subforum/Index.cshtml.cs
+++ TASVideos/Pages/Forum/Subforum/Index.cshtml.cs
@@ -1,8 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 using TASVideos.Data.Entity;
+using TASVideos.Data.Entity.Forum;
 using TASVideos.Models;
 using TASVideos.Tasks;
 
@@ -11,14 +17,14 @@ namespace TASVideos.Pages.Forum.Subforum
 	[AllowAnonymous]
 	public class IndexModel : BasePageModel
 	{
-		private readonly ForumTasks _forumTasks;
+		private readonly ApplicationDbContext _db;
 
 		public IndexModel(
-			ForumTasks forumTasks,
+			ApplicationDbContext db,
 			UserTasks userTasks) 
 			: base(userTasks)
 		{
-			_forumTasks = forumTasks;
+			_db = db;
 		}
 
 		[FromQuery]
@@ -31,17 +37,60 @@ namespace TASVideos.Pages.Forum.Subforum
 
 		public async Task<IActionResult> OnGet()
 		{
-			Forum = await _forumTasks
-				.GetForumForDisplay(Id, Search, UserHas(PermissionTo.SeeRestrictedForums));
-
-			if (Forum == null)
+			using (await _db.Database.BeginTransactionAsync())
 			{
-				return NotFound();
+				Forum = await _db.Forums
+					.ExcludeRestricted(UserHas(PermissionTo.SeeRestrictedForums))
+					.Select(f => new ForumModel
+					{
+						Id = f.Id,
+						Name = f.Name,
+						Description = f.Description
+					})
+					.SingleOrDefaultAsync(f => f.Id == Id);
+
+				if (Forum == null)
+				{
+					return NotFound();
+				}
+
+				var rowsToSkip = Search.GetRowsToSkip();
+				var rowCount = await _db.ForumTopics
+					.ForForum(Id)
+					.CountAsync();
+
+				var results = await _db.ForumTopics
+					.ForForum(Id)
+					.Select(ft => new ForumModel.ForumTopicEntry
+					{
+						Id = ft.Id,
+						Title = ft.Title,
+						CreateUserName = ft.CreateUserName,
+						CreateTimestamp = ft.CreateTimeStamp,
+						Type = ft.Type,
+						Views = ft.Views,
+						PostCount = ft.ForumPosts.Count,
+						LastPost = ft.ForumPosts.Max(fp => (DateTime?)fp.CreateTimeStamp)
+					})
+					.OrderByDescending(ft => ft.Type == ForumTopicType.Sticky)
+					.ThenByDescending(ft => ft.Type == ForumTopicType.Announcement)
+					.ThenByDescending(ft => ft.LastPost)
+					.Skip(rowsToSkip)
+					.Take(Search.PageSize)
+					.ToListAsync();
+
+				Forum.Topics = new PageOf<ForumModel.ForumTopicEntry>(results)
+				{
+					PageSize = Search.PageSize,
+					CurrentPage = Search.CurrentPage,
+					RowCount = rowCount,
+					SortDescending = Search.SortDescending,
+					SortBy = Search.SortBy
+				};
+
+				Forum.Description = RenderHtml(Forum.Description);
+				return Page();
 			}
-
-			Forum.Description = RenderHtml(Forum.Description);
-
-			return Page();
 		}
 	}
 }
