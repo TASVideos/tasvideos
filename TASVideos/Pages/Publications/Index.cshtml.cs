e2fa90bbf52188560267e76a89453a273d651d0f
adelikat <adelikat@tasvideos.org>
2019-01-26 17:58:35 -0600

    move logic to page model

--- TASVideos/Pages/Publications/Index.cshtml.cs
+++ TASVideos/Pages/Publications/Index.cshtml.cs
@@ -5,25 +5,33 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
+using TASVideos.Data;
+using TASVideos.Data.Entity;
 using TASVideos.Models;
+using TASVideos.Services;
 using TASVideos.Tasks;
 
 namespace TASVideos.Pages.Publications
 {
+	// TODO: add paging
 	[AllowAnonymous]
 	public class IndexModel : BasePageModel
 	{
-		private readonly PublicationTasks _publicationTasks;
+		private readonly ApplicationDbContext _db;
+		private readonly ICacheService _cache;
 		private readonly RatingsTasks _ratingsTasks;
 
 		public IndexModel(
-			PublicationTasks publicationTasks,
+			ApplicationDbContext db,
+			ICacheService cache,
 			RatingsTasks ratingsTasks,
 			UserTasks userTasks) 
 			: base(userTasks)
 		{
-			_publicationTasks = publicationTasks;
+			_db = db;
+			_cache = cache;
 			_ratingsTasks = ratingsTasks;
 		}
 
@@ -34,7 +42,7 @@ namespace TASVideos.Pages.Publications
 
 		public async Task<IActionResult> OnGet()
 		{
-			var tokenLookup = await _publicationTasks.GetMovieTokenData();
+			var tokenLookup = await GetMovieTokenData();
 
 			var tokens = Query
 				.Split(new[] { "-" }, StringSplitOptions.RemoveEmptyEntries)
@@ -71,9 +79,7 @@ namespace TASVideos.Pages.Publications
 				return Redirect("Movies");
 			}
 
-			Movies = (await _publicationTasks
-				.GetMovieList(searchModel))
-				.ToList();
+			Movies = await GetMovieList(searchModel);
 
 			var ratings = await _ratingsTasks.GetOverallRatingsForPublications(Movies.Select(m => m.Id));
 
@@ -83,6 +89,131 @@ namespace TASVideos.Pages.Publications
 			}
 
 			return Page();
+		}
+
+		private async Task<PublicationSearchModel> GetMovieTokenData()
+		{
+			var cacheKey = "MovieTokenData"; // TODO: make a constants class for cache keys
+			if (_cache.TryGetValue(cacheKey, out PublicationSearchModel cachedResult))
+			{
+				return cachedResult;
+			}
+
+			using (await _db.Database.BeginTransactionAsync())
+			{
+				var result = new PublicationSearchModel
+				{
+					Tiers = await _db.Tiers.Select(t => t.Name.ToLower()).ToListAsync(),
+					SystemCodes = await _db.GameSystems.Select(s => s.Code.ToLower()).ToListAsync(),
+					Tags = await _db.Tags.Select(t => t.Code.ToLower()).ToListAsync(),
+					Genres = await _db.Genres.Select(g => g.DisplayName.ToLower()).ToListAsync(),
+					Flags = await _db.Flags.Select(f => f.Token.ToLower()).ToListAsync()
+				};
+
+				_cache.Set(cacheKey, result);
+
+				return result;
+			}
+		}
+
+		private async Task<IList<PublicationModel>> GetMovieList(PublicationSearchModel searchCriteria)
+		{
+			var query = _db.Publications
+				.AsQueryable();
+
+			if (searchCriteria.MovieIds.Any())
+			{
+				query = query.Where(p => searchCriteria.MovieIds.Contains(p.Id));
+			}
+			else
+			{
+				if (searchCriteria.SystemCodes.Any())
+				{
+					query = query.Where(p => searchCriteria.SystemCodes.Contains(p.System.Code));
+				}
+
+				if (searchCriteria.Tiers.Any())
+				{
+					query = query.Where(p => searchCriteria.Tiers.Contains(p.Tier.Name));
+				}
+
+				if (!searchCriteria.ShowObsoleted)
+				{
+					query = query.ThatAreCurrent();
+				}
+
+				if (searchCriteria.Years.Any())
+				{
+					query = query.Where(p => searchCriteria.Years.Contains(p.CreateTimeStamp.Year));
+				}
+
+				if (searchCriteria.Tags.Any())
+				{
+					query = query.Where(p => p.PublicationTags.Any(t => searchCriteria.Tags.Contains(t.Tag.Code)));
+				}
+
+				if (searchCriteria.Genres.Any())
+				{
+					query = query.Where(p => p.Game.GameGenres.Any(gg => searchCriteria.Genres.Contains(gg.Genre.DisplayName)));
+				}
+
+				if (searchCriteria.Flags.Any())
+				{
+					query = query.Where(p => p.PublicationFlags.Any(f => searchCriteria.Flags.Contains(f.Flag.Token)));
+				}
+
+				if (searchCriteria.Authors.Any())
+				{
+					query = query.Where(p => p.Authors.Select(a => a.UserId).Any(a => searchCriteria.Authors.Contains(a)));
+				}
+			}
+
+			// TODO: AutoMapper, single movie is the same logic
+			return await query
+				.OrderBy(p => p.System.Code)
+				.ThenBy(p => p.Game.DisplayName)
+				.Select(p => new PublicationModel
+				{
+					Id = p.Id,
+					CreateTimeStamp = p.CreateTimeStamp,
+					Title = p.Title,
+					OnlineWatchingUrl = p.OnlineWatchingUrl,
+					MirrorSiteUrl = p.MirrorSiteUrl,
+					ObsoletedBy = p.ObsoletedById,
+					MovieFileName = p.MovieFileName,
+					SubmissionId = p.SubmissionId,
+					RatingCount = p.PublicationRatings.Count / 2,
+					TierIconPath = p.Tier.IconPath,
+					Files = p.Files.Select(f => new PublicationModel.FileModel
+					{
+						Path = f.Path,
+						Type = f.Type
+					}).ToList(),
+					Tags = p.PublicationTags
+						.Select(pt => new PublicationModel.TagModel
+						{
+							DisplayName = pt.Tag.DisplayName,
+							Code = pt.Tag.Code
+						})
+						.ToList(),
+					GenreTags = p.Game.GameGenres
+						.Select(gg => new PublicationModel.TagModel
+						{
+							DisplayName = gg.Genre.DisplayName,
+							Code = gg.Genre.DisplayName // TODO
+						})
+						.ToList(),
+					Flags = p.PublicationFlags
+						.Where(pf => pf.Flag.IconPath != null)
+						.Select(pf => new PublicationModel.FlagModel
+						{
+							IconPath = pf.Flag.IconPath,
+							LinkPath = pf.Flag.LinkPath,
+							Name = pf.Flag.Name
+						})
+						.ToList()
+				})
+				.ToListAsync();
 		}
 	}
 }
