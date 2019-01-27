8d351b9a59d1e7811d545e98f58403cb24b5c435
adelikat <adelikat@tasvideos.org>
2019-01-27 15:00:09 -0600

    move some logic from UserTasks to UserManager

--- TASVideos/Pages/Profile/Ratings.cshtml.cs
+++ TASVideos/Pages/Profile/Ratings.cshtml.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Authorization;
 
 using TASVideos.Models;
+using TASVideos.Services;
 using TASVideos.Tasks;
 
 namespace TASVideos.Pages.Profile
@@ -10,15 +11,20 @@ namespace TASVideos.Pages.Profile
 	[Authorize]
 	public class RatingsModel : BasePageModel
 	{
-		public RatingsModel(UserTasks userTasks) : base(userTasks)
+		private readonly UserManager _userManager;
+		public RatingsModel(
+			UserManager userManager,
+			UserTasks userTasks) 
+			: base(userTasks)
 		{
+			_userManager = userManager;
 		}
 
 		public UserRatingsModel Ratings { get; set; } = new UserRatingsModel();
 
 		public async Task OnGet()
 		{
-			Ratings = await UserTasks.GetUserRatings(User.Identity.Name, includeHidden: true);
+			Ratings = await _userManager.GetUserRatings(User.Identity.Name, includeHidden: true);
 		}
 	}
 }
