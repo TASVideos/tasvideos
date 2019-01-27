68225375c428aff8f484900501dd1e73c203854a
adelikat <adelikat@tasvideos.org>
2019-01-27 17:17:06 -0600

    move award dtos to their own file

--- TASVideos/Services/AwardsCache.cs
+++ TASVideos/Services/AwardsCache.cs
@@ -130,33 +130,4 @@ namespace TASVideos.Services
 				.ToList();
 		}
 	}
-
-	public class AwardDto
-	{
-		public string ShortName { get; set; }
-		public string Description { get; set; }
-		public int Year { get; set; }
-		public AwardType Type { get; set; }
-		public IEnumerable<PublicationDto> Publications { get; set; } = new HashSet<PublicationDto>();
-		public IEnumerable<UserDto> Users { get; set; } = new HashSet<UserDto>();
-
-		public class UserDto
-		{
-			public int Id { get; set; }
-			public string UserName { get; set; }
-		}
-
-		public class PublicationDto
-		{
-			public int Id { get; set; }
-			public string Title { get; set; }
-		}
-	}
-
-	public class AwardEntryDto
-	{
-		public string ShortName { get; set; }
-		public string Description { get; set; }
-		public int Year { get; set; }
-	}
 }
---
+++ TASVideos/Services/Dtos/AwardDtos.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using TASVideos.Data.Entity.Awards;
+
+namespace TASVideos.Services
+{
+	public class AwardDto
+	{
+		public string ShortName { get; set; }
+		public string Description { get; set; }
+		public int Year { get; set; }
+		public AwardType Type { get; set; }
+		public IEnumerable<PublicationDto> Publications { get; set; } = new HashSet<PublicationDto>();
+		public IEnumerable<UserDto> Users { get; set; } = new HashSet<UserDto>();
+
+		public class UserDto
+		{
+			public int Id { get; set; }
+			public string UserName { get; set; }
+		}
+
+		public class PublicationDto
+		{
+			public int Id { get; set; }
+			public string Title { get; set; }
+		}
+	}
+
+	public class AwardEntryDto
+	{
+		public string ShortName { get; set; }
+		public string Description { get; set; }
+		public int Year { get; set; }
+	}
+}
