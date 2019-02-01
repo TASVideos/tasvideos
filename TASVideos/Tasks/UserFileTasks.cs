f51faa98bcc491079ef7a1000c5dfd23eda52017
adelikat <adelikat@tasvideos.org>
2019-02-01 17:52:54 -0600

    move some logic from task layer to page model

--- TASVideos/Tasks/UserFileTasks.cs
+++ TASVideos/Tasks/UserFileTasks.cs
@@ -84,33 +84,6 @@ namespace TASVideos.Tasks
 			await _db.SaveChangesAsync();
 		}
 
-		public async Task<UserFileIndexModel> GetIndex()
-		{
-			var model = new UserFileIndexModel
-			{
-				UsersWithMovies = await _db.UserFiles
-					.Where(uf => !uf.Hidden)
-					.GroupBy(gkey => gkey.Author.UserName, gvalue => gvalue.UploadTimestamp).Select(
-						uf => new UserFileIndexModel.UserWithMovie { UserName = uf.Key, Latest = uf.Max() })
-					.ToListAsync(),
-				LatestMovies = await GetLatest(10),
-				GamesWithMovies = await _db.Games
-					.Where(g => g.UserFiles.Any())
-					.OrderBy(g => g.System.Code)
-					.ThenBy(g => g.DisplayName)
-					.Select(g => new UserFileIndexModel.GameWithMovie
-					{
-						GameId = g.Id,
-						GameName = g.DisplayName,
-						SystemCode = g.System.Code,
-						Latest = g.UserFiles.Select(uf => uf.UploadTimestamp).Max()
-					})
-					.ToListAsync()
-			};
-
-			return model;
-		}
-
 		public async Task<GameFileModel> GetFilesForGame(int gameId)
 		{
 			var game = await _db.Games
