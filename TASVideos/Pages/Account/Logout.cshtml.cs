7b464eb2cba40817a5b8f19391bf0d3d553d5368
adelikat <adelikat@tasvideos.org>
2019-02-10 17:51:11 -0600

    when signing a user out, clear out their claims, so they are repopulated on next sign in

--- TASVideos/Pages/Account/Logout.cshtml.cs
+++ TASVideos/Pages/Account/Logout.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 using TASVideos.Data.Entity;
+using TASVideos.Services;
 
 namespace TASVideos.Pages.Account
 {
@@ -12,14 +13,20 @@ namespace TASVideos.Pages.Account
 	public class LogoutModel : BasePageModel
 	{
 		private readonly SignInManager<User> _signInManager;
+		private readonly UserManager _userManager;
 
-		public LogoutModel(SignInManager<User> signInManager)
+		public LogoutModel(
+			SignInManager<User> signInManager,
+			UserManager userManager)
 		{
 			_signInManager = signInManager;
+			_userManager = userManager;
 		}
 
 		public async Task<IActionResult> OnPost()
 		{
+			var user = await _userManager.GetUserAsync(User);
+			await _userManager.RemoveClaimsAsync(user, User.Claims);
 			await _signInManager.SignOutAsync();
 			return Login();
 		}
