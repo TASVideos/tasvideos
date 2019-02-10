1002614fdec4fadb2c283b720aa497b7c0b7752d
adelikat <adelikat@tasvideos.org>
2019-02-10 10:49:45 -0600

    tags list page

---
+++ TASVideos/Pages/Tags/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Pages.Tags
+{
+	public class IndexModel : BasePageModel
+	{
+		private readonly ApplicationDbContext _db;
+
+		public IndexModel(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public IEnumerable<Tag> Tags { get; set; } = new List<Tag>();
+
+		public async Task OnGet()
+		{
+			Tags = await _db.Tags.ToListAsync();
+		}
+	}
+}
