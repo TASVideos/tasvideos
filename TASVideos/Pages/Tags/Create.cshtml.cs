57dc8b2828fcabc9bc1f73d149cc54f4f14d0f27
adelikat <adelikat@tasvideos.org>
2019-02-10 11:28:22 -0600

    Tag creation

---
+++ TASVideos/Pages/Tags/Create.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
+using TASVideos.Data.Constants;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Pages.Tags
+{
+	public class CreateModel : BasePageModel
+	{
+		private readonly ApplicationDbContext _db;
+
+		public CreateModel(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		[TempData]
+		public string Message { get; set; }
+
+		[TempData]
+		public string MessageType { get; set; }
+
+		[FromRoute]
+		public int Id { get; set; }
+
+		[BindProperty]
+		public Tag Tag { get; set; } = new Tag();
+
+		public async Task<IActionResult> OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			_db.Tags.Add(new Tag
+			{
+				Code = Tag.Code,
+				DisplayName = Tag.DisplayName
+			});
+
+			try
+			{
+				MessageType = Styles.Success;
+				Message = "Tag successfully created.";
+				await _db.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				MessageType = Styles.Danger;
+				Message = "Unable to create Tag.";
+			}
+
+			return RedirectToPage("Index");
+		}
+	}
+}
