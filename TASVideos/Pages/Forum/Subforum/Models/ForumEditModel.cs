cd72fe11b6ec634f54c9e79692a5953f13f64aca
adelikat <adelikat@tasvideos.org>
2019-02-10 19:30:15 -0600

    Forum edit - ability to edit ShortName

--- TASVideos/Pages/Forum/Subforum/Models/ForumEditModel.cs
+++ TASVideos/Pages/Forum/Subforum/Models/ForumEditModel.cs
@@ -9,6 +9,11 @@ namespace TASVideos.Pages.Forum.Subforum.Models
 		public string Name { get; set; }
 
 		[Required]
+		[StringLength(10)]
+		[Display(Name = "Short Name", Description = "Used for IRC notifications and other external posts")]
+		public string ShortName { get; set; }
+
+		[Required]
 		[StringLength(1000)]
 		public string Description { get; set; }
 	}
