c5b445394ca2a29d57b2ed76f660197fcb373240
adelikat <adelikat@tasvideos.org>
2019-02-28 18:05:55 -0600

    Plumbing for poll create partial view

---
+++ TASVideos/Pages/Forum/Topics/Models/PollCreateModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TASVideos.Pages.Forum.Topics.Models
+{
+	public class PollCreateModel
+	{
+		[Display(Name = "Poll Question")]
+		[StringLength(200, MinimumLength = 8)]
+		public string Question { get; set; }
+
+		[Display(Name = "Run poll for")]
+		[MaxLength(365)]
+		public int? DaysOpen { get; set; }
+
+		public IEnumerable<string> PollOptions { get; set; } = new List<string>();
+	}
+}
