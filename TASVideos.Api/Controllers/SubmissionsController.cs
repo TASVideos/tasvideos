1ce0581f7578da41b775d63ff2ade8716e32e6d8
adelikat <adelikat@tasvideos.org>
2019-03-11 18:24:02 -0500

    api - submissions endpoints, no filtering

---
+++ TASVideos.Api/Controllers/SubmissionsController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper.QueryableExtensions;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Api.Requests;
+using TASVideos.Api.Responses;
+using TASVideos.Data;
+
+namespace TASVideos.Api.Controllers
+{
+	/// <summary>
+	/// The submissions of TASVideos
+	/// </summary>
+	[AllowAnonymous]
+	[Route("api/v1/[controller]")]
+	public class SubmissionsController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="SubmissionsController"/> class. 
+		/// </summary>
+		public SubmissionsController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		/// <summary>
+		/// Returns a submission with the given id
+		/// </summary>
+		/// <response code="200">Returns the list of publications</response>
+		/// <response code="400">The request parameters are invalid</response>
+		/// <response code="404">A submission with the given id was not found</response>
+		[HttpGet("{id}")]
+		[ProducesResponseType(typeof(SubmissionsResponse), 200)]
+		public async Task<IActionResult> Get(int id)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+
+			var sub = await _db.Submissions
+				.ProjectTo<SubmissionsResponse>()
+				.SingleOrDefaultAsync(p => p.Id == id);
+
+			if (sub == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(sub);
+		}
+
+		/// <summary>
+		/// Returns a list of publications, filtered by the given criteria
+		/// </summary>
+		/// <response code="200">Returns the list of publications</response>
+		/// <response code="400">The request parameters are invalid</response>
+		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<SubmissionsResponse>), 200)]
+		public async Task<IActionResult> GetAll(SubmissionsRequest request)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+
+			var subs = (await _db.Submissions
+				.ProjectTo<SubmissionsResponse>()
+				.SortBy(request)
+				.Paginate(request)
+				.ToListAsync())
+				.FieldSelect(request);
+
+			return Ok(subs);
+		}
+	}
+}
---
+++ TASVideos.Api/Responses/SubmissionsResponse.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+
+#pragma warning disable 1591
+
+namespace TASVideos.Api.Responses
+{
+	public class SubmissionsResponse
+	{
+		[Sortable]
+		public int Id { get; set; }
+
+		[Sortable]
+		public string Title { get; set; }
+
+		[Sortable]
+		public string Tier { get; set; }
+
+		[Sortable]
+		public string Judge { get; set; }
+
+		[Sortable]
+		public string Publisher { get; set; }
+
+		[Sortable]
+		public string Status { get; set; }
+
+		[Sortable]
+		public string MovieExtension { get; set; }
+
+		[Sortable]
+		public int? GameId { get; set; }
+
+		[Sortable]
+		public int? RomId { get; set; }
+
+		[Sortable]
+		public string SystemCode { get; set; }
+
+		[Sortable]
+		public double? SystemFrameRate { get; set; }
+
+		[Sortable]
+		public int Frames { get; set; }
+		
+		[Sortable]
+		public int RerecordCount { get; set; }
+
+		[Sortable]
+		public string EncodeEmbedLink { get; set; }
+
+		[Sortable]
+		public string GameVersion { get; set; }
+
+		[Sortable]
+		public string GameName { get; set; }
+
+		[Sortable]
+		public string Branch { get; set; }
+
+		[Sortable]
+		public string RomName { get; set; }
+		
+		[Sortable]
+		public string EmulatorVersion { get; set; }
+
+		[Sortable]
+		public int? MovieStartType { get; set; }
+
+		public IEnumerable<string> Authors { get; set; } = new string[0];
+	}
+}
