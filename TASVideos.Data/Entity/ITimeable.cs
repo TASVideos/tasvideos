b75440e1f4e0f4321dd23b89afd01d75589834a9
adelikat <adelikat@tasvideos.org>
2019-03-13 17:24:36 -0500

    consolidate logic that calculates submission/publication time

---
+++ TASVideos.Data/Entity/ITimeable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TASVideos.Data.Entity
+{
+	public interface ITimeable
+	{
+		double FrameRate { get; }
+		int Frames { get; } 
+	}
+
+	public static class TimeableExtensions
+	{
+		public static TimeSpan Time(this ITimeable timeable)
+		{
+			int seconds = (int)(timeable.Frames / timeable.FrameRate);
+			double fractionalSeconds = (timeable.Frames / timeable.FrameRate) - seconds;
+			int milliseconds = (int)(Math.Round(fractionalSeconds, 2) * 1000);
+			var timespan = new TimeSpan(0, 0, 0, seconds, milliseconds);
+			return timespan;
+		}
+	}
+}
