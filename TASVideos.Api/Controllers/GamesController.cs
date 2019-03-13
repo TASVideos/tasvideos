4910aab659f4b665cfac0eb9a8686049cb52c75b
adelikat <adelikat@tasvideos.org>
2019-03-13 15:51:06 -0500

    api - games endpoint

---
+++ TASVideos.Api/Controllers/GamesController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using AutoMapper.QueryableExtensions;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Api.Requests;
+using TASVideos.Api.Responses;
+using TASVideos.Data;
+
+namespace TASVideos.Api.Controllers
+{
+	/// <summary>
+	/// The games of TASVideos that can be associated with content
+	/// </summary>
+	[AllowAnonymous]
+	[Route("api/v1/[controller]")]
+	public class GamesController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="GamesController"/> class. 
+		/// </summary>
+		public GamesController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		/// <summary>
+		/// Returns a list of available games
+		/// </summary>
+		/// <response code="200">Returns the list of games</response>
+		/// /// <response code="400">The request parameters are invalid</response>
+		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<SystemsResponse>), 200)]
+		public async Task<IActionResult> GetAll(GamesRequest request)
+		{
+			if (!request.IsValidSort(typeof(GamesResponse)))
+			{
+				ModelState.AddModelError(nameof(request.Sort), "Invalid Sort parameter");
+			}
+
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var query = _db.Games.AsQueryable();
+
+			if (request.SystemCodes.Any())
+			{
+				query = query.Where(g => request.SystemCodes.Contains(g.System.Code));
+			}
+
+			var games = (await query
+				.ProjectTo<GamesResponse>()
+				.SortBy(request)
+				.Paginate(request)
+				.ToListAsync())
+				.FieldSelect(request);
+
+			return Ok(games);
+		}
+	}
+}
---
+++ TASVideos.Api/Responses/GamesResponse.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+using TASVideos.Data;
+using TASVideos.Data.Entity.Game;
+
+#pragma warning disable 1591
+namespace TASVideos.Api.Responses
+{
+	/// <summary>
+	/// Represents a game returned by the games endpoint
+	/// </summary>
+	public class GamesResponse
+	{
+		[Sortable]
+		public int Id { get; set; }
+
+		public IEnumerable<GameRom> Roms { get; set; } = new List<GameRom>();
+
+		[Sortable]
+		public string SystemCode { get; set; }
+
+		[Sortable]
+		public string GoodName { get; set; }
+
+		[Sortable]
+		public string DisplayName { get; set; }
+
+		[Sortable]
+		public string Abbreviation { get; set; }
+
+		[Sortable]
+		public string SearchKey { get; set; }
+
+		[Sortable]
+		public string YoutubeTags { get; set; }
+
+		[Sortable]
+		public string ScreenshotUrl { get; set; }
+
+		public class GameRom
+		{
+			public int Id { get; set; }
+			public string Md5 { get; set; }
+			public string Sha1 { get; set; }
+			public string Name { get; set; }
+			public RomTypes Type { get; set; }
+			public string Region { get; set; }
+			public string Version { get; set; }
+		}
+	}
+}
