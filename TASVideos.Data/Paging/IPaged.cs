d6d0cecd950cb7cec2889420c55b67dbec4b8f97
adelikat <adelikat@tasvideos.org>
2019-03-15 15:33:23 -0500

    trying stuff

--- TASVideos.Data/Paging/IPaged.cs
+++ TASVideos.Data/Paging/IPaged.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace TASVideos.Data
 {
-	public interface IPaged : IPageable
+	public interface IPaged : IPageable, ISortable
 	{
 		int RowCount { get; }
 	}
@@ -18,5 +21,30 @@ namespace TASVideos.Data
 		{
 			return Math.Min(paged.RowCount, paged.StartRow() + paged.PageSize - 1);
 		} 
+
+		public static IDictionary<string, string> AdditionalProperties(this IPaged paged)
+		{
+			var existing = typeof(IPaged)
+				.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+				.Concat(typeof(IPaged)
+					.GetInterfaces()
+					.SelectMany(i => i.GetProperties()))
+				.ToList();
+
+			var existingNames = existing.Select(p => p.Name);
+
+			var all = paged
+				.GetType()
+				.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+				.ToList();
+
+			var additional = all
+				.Where(p => !existingNames.Contains(p.Name))
+				.ToList();
+
+			var myDic = additional.ToDictionary(tkey => tkey.Name, tvalue => tvalue.GetValue(paged)?.ToString());
+
+			return myDic;
+		}
 	}
 }
