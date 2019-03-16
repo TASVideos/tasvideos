454401859761c0d62602dc5f9b66465435a2c64d
adelikat <adelikat@tasvideos.org>
2019-03-16 11:17:34 -0500

    conslidate more paging logic

--- TASVideos/Pages/Game/Model/GameListRequest.cs
+++ TASVideos/Pages/Game/Model/GameListRequest.cs
@@ -1,4 +1,8 @@
-﻿using TASVideos.Data;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using TASVideos.Data;
 
 namespace TASVideos.Pages.Game.Model
 {
@@ -10,5 +14,21 @@ namespace TASVideos.Pages.Game.Model
 		}
 
 		public string SystemCode { get; set; }
+	}
+
+	public class SystemPageOf<T> : PagedModel, IEnumerable<T>
+	{
+		private readonly IEnumerable<T> _items;
+
+		public SystemPageOf(IEnumerable<T> items)
+		{
+			_items = items;
+		}
+
+		[Display(Name = "System")]
+		public string SystemCode { get; set; }
+
+		public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+		IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
 	}
 }
