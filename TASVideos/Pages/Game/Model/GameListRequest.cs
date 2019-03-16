c9ce796f18e51003f20b7918a1dae51c50fc881b
adelikat <adelikat@tasvideos.org>
2019-03-16 11:33:36 -0500

    simplify SystemPageOf

--- TASVideos/Pages/Game/Model/GameListRequest.cs
+++ TASVideos/Pages/Game/Model/GameListRequest.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-using TASVideos.Data;
+﻿using TASVideos.Data;
 
 namespace TASVideos.Pages.Game.Model
 {
@@ -14,21 +10,5 @@ namespace TASVideos.Pages.Game.Model
 		}
 
 		public string SystemCode { get; set; }
-	}
-
-	public class SystemPageOf<T> : PagedModel, IEnumerable<T>
-	{
-		private readonly IEnumerable<T> _items;
-
-		public SystemPageOf(IEnumerable<T> items)
-		{
-			_items = items;
-		}
-
-		[Display(Name = "System")]
-		public string SystemCode { get; set; }
-
-		public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
-		IEnumerator IEnumerable.GetEnumerator() => _items.GetEnumerator();
 	}
 }
---
+++ TASVideos/Pages/Game/Model/SystemPageOf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using TASVideos.Data;
+
+namespace TASVideos.Pages.Game.Model
+{
+	public class SystemPageOf<T> : PageOf<T>
+	{
+		public SystemPageOf(IEnumerable<T> items)
+			: base(items)
+		{
+		}
+
+		[Display(Name = "System")]
+		public string SystemCode { get; set; }
+	}
+}
