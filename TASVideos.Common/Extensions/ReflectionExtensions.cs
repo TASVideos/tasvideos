7dc8e6c4a1dc35278264a9a96db2a9587ad59c96
adelikat <adelikat@tasvideos.org>
2019-03-17 18:36:38 -0500

    Move some common logic into a TASVideos.Common project

---
+++ TASVideos.Common/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Reflection;
+
+namespace TASVideos.Common
+{
+	public static class ReflectionExtensions
+	{
+		/// <summary>
+		/// Returns the value of the given property, in a UI consumable way
+		/// For example, collections will be turned into a pipe separated list
+		/// </summary>
+		public static string ToValue(this PropertyInfo property, object obj)
+		{
+			if (obj == null || property == null)
+			{
+				return null;
+			}
+
+			if (typeof(IEnumerable).IsAssignableFrom(property.PropertyType)
+				&& property.PropertyType.IsGenericType)
+			{
+				var values = ((IEnumerable)property.GetValue(obj)).Cast<object>();
+				var val = string.Join("|", values);
+				return val;
+			}
+
+			return property.GetValue(obj)?.ToString();
+		}
+	}
+}
