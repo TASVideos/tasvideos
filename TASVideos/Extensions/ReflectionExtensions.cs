a81e0886048107702f0fa4fcfb3f4023fcc6bc39
adelikat <adelikat@tasvideos.org>
2019-03-17 19:08:50 -0500

    move some more extension methods to common

--- TASVideos/Extensions/ReflectionExtensions.cs
+++ TASVideos/Extensions/ReflectionExtensions.cs
@@ -3,7 +3,6 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
-using TASVideos.Attributes;
 
 namespace TASVideos.Extensions
 {
@@ -40,26 +39,6 @@ namespace TASVideos.Extensions
 		}
 
 		/// <summary>
-		/// Returns the <seealso cref="GroupAttribute"/> of an Enum if it exists
-		/// Else it will return and empty string
-		/// If the value is null, an empty string will be returned
-		/// </summary>
-		public static string Group(this Enum enumValue)
-		{
-			var descriptionAttribute = enumValue?.GetType()
-				.GetMember(enumValue.ToString())
-				.Single()
-				.GetCustomAttribute<GroupAttribute>();
-
-			if (descriptionAttribute != null)
-			{
-				return descriptionAttribute.Name;
-			}
-
-			return string.Empty;
-		}
-
-		/// <summary>
 		/// Returns the DisplayAttribute value of a Enum if exists
 		/// Else it will return the name of the enum
 		/// If the value is null, an empty string will be returned
@@ -83,17 +62,6 @@ namespace TASVideos.Extensions
 			}
 
 			return enumValue.ToString();
-		}
-
-		public static string DisplayName(this PropertyInfo propertyInfo)
-		{
-			var displayAttr = propertyInfo.GetCustomAttribute<DisplayNameAttribute>();
-			if (displayAttr != null)
-			{
-				return displayAttr.DisplayName;
-			}
-
-			return propertyInfo.Name;
 		}
 	}
 }
