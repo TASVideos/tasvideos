a8c590114f1ff6744c7dc4d1a0ef91a699ccbee3
adelikat <adelikat@tasvideos.org>
2019-03-18 10:33:45 -0500

    more extension method unit tests

---
+++ TASVideos.Test/Common/Extensions/NumberExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TASVideos.Extensions;
+
+namespace TASVideos.Test.Common.Extensions
+{
+	[TestClass]
+	public class NumberExtensions
+	{
+		[TestMethod]
+		[DataRow(0, 0, 0, 0)]
+		[DataRow(5, 1, 4, 4)]
+		[DataRow(5, 6, 10, 6)]
+		[DataRow(5, 0, 10, 5)]
+		[DataRow(5, 10, 0, 5)]
+		[DataRow(5, 4, 1, 4)]
+		[DataRow(5, 1, 4, 4)]
+		public void Clamp_Tests(int source, int start, int end, int expected)
+		{
+			var actual = source.Clamp(start, end);
+			Assert.IsNotNull(actual);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
