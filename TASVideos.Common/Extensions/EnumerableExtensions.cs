0a013b85e2f66c86ebde612753e968cff78535ee
adelikat <adelikat@tasvideos.org>
2019-03-18 10:29:33 -0500

    unit tests for some extension methods

--- TASVideos.Common/Extensions/EnumerableExtensions.cs
+++ TASVideos.Common/Extensions/EnumerableExtensions.cs
@@ -4,29 +4,8 @@ using System.Linq;
 
 namespace TASVideos.Extensions
 {
-    public static class EnumerableExtensions
-    {
-		/// <summary>
-		///   Limit a value to a certain range. When the value is smaller/bigger than the range, snap it to the range border.
-		/// </summary>
-		/// <typeparam name = "T">The type of the value to limit.</typeparam>
-		/// <param name = "source">The source for this extension method.</param>
-		/// <param name = "start">The start of the interval, included in the interval.</param>
-		/// <param name = "end">The end of the interval, included in the interval.</param>
-		public static T Clamp<T>(this T source, T start, T end)
-			where T : IComparable
-		{
-			bool isReversed = start.CompareTo(end) > 0;
-			T smallest = isReversed ? end : start;
-			T biggest = isReversed ? start : end;
-
-			return source.CompareTo(smallest) < 0
-				? smallest
-				: source.CompareTo(biggest) > 0
-					? biggest
-					: source;
-		}
-
+	public static class EnumerableExtensions
+	{
 		/// <summary>
 		/// Returns the first half of the given collection,
 		/// in the case of odd numbers, the odd item is considered in the first half
@@ -54,6 +33,11 @@ namespace TASVideos.Extensions
 		/// </summary>
 		public static T AtRandom<T>(this ICollection<T> collection)
 		{
+			if (collection == null)
+			{
+				throw new ArgumentNullException($"{nameof(collection)} can not be null");
+			}
+
 			var randomIndex = new Random(DateTime.Now.Millisecond).Next(0, collection.Count);
 			return collection.ElementAtOrDefault(randomIndex);
 		}
---
+++ TASVideos.Test/Common/Extensions/EnumerableExtensionTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TASVideos.Extensions;
+
+namespace TASVideos.Test.Common.Extensions
+{
+	[TestClass]
+	public class EnumerableExtensionTests
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void FirstHalf_Null_Throws()
+		{
+			((IEnumerable<int>)null).FirstHalf();
+		}
+
+		[TestMethod]
+		[DataRow(new int[0], new int[0])]
+		[DataRow(new[] { 1, 2 }, new[] { 1 })]
+		[DataRow(new[] { 1, 2, 3 }, new[] { 1, 2 })]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 2 })]
+		public void FirstHalf_Tests(int[] list, int[] expected)
+		{
+			var actual = list.FirstHalf();
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(expected.SequenceEqual(actual));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void SecondHalf_Null_Throws()
+		{
+			((IEnumerable<int>)null).SecondHalf();
+		}
+
+		[TestMethod]
+		[DataRow(new int[0], new int[0])]
+		[DataRow(new[] { 1, 2 }, new[] { 2 })]
+		[DataRow(new[] { 1, 2, 3 }, new[] { 3 })]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 3, 4 })]
+		public void SecondHalf_Tests(int[] list, int[] expected)
+		{
+			var actual = list.SecondHalf();
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(expected.SequenceEqual(actual));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void AtRandom_Null_Throws()
+		{
+			((ICollection<int>)null).AtRandom();
+		}
+
+		[TestMethod]
+		public void AtRandom_Basic_Test()
+		{
+			var collection = new[] { 1, 2, 3, 4, 5 };
+			var actual = collection.AtRandom();
+			Assert.IsNotNull(actual);
+			Assert.IsTrue(collection.Contains(actual));
+		}
+	}
+}
