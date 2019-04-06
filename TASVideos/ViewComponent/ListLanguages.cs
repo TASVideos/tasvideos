9564a0c17b82a26fda09e553179f55d45a3e44b0
adelikat <adelikat@tasvideos.org>
2019-04-06 15:55:49 -0500

    listlanguages module - plumbing

---
+++ TASVideos/ViewComponent/ListLanguages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using TASVideos.Data.Entity;
+using TASVideos.Services;
+
+namespace TASVideos.ViewComponents
+{
+	public class ListLanguages : ViewComponent
+	{
+		private readonly IWikiPages _wikiPages;
+
+		public ListLanguages(IWikiPages wikiPages)
+		{
+			_wikiPages = wikiPages;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
+		{
+			return View();
+		}
+	}
+}
