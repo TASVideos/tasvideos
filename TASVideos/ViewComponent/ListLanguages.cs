1e0578b7191d55c03c0a38a6da9581dcc390e1fb
adelikat <adelikat@tasvideos.org>
2019-04-06 16:04:25 -0500

    listlanguages rough in for query logic

--- TASVideos/ViewComponent/ListLanguages.cs
+++ TASVideos/ViewComponent/ListLanguages.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 using TASVideos.Data.Entity;
 using TASVideos.Services;
@@ -21,6 +22,36 @@ namespace TASVideos.ViewComponents
 
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
 		{
+			if (string.IsNullOrWhiteSpace(pageData?.PageName))
+			{
+				return new ContentViewComponentResult("");
+			}
+
+			var languagesMarkup = _wikiPages
+				.Page("System/Languages")
+				?.Markup;
+
+			if (string.IsNullOrWhiteSpace(languagesMarkup))
+			{
+				return new ContentViewComponentResult("");
+			}
+
+			var languages = languagesMarkup
+				.Split(',')
+				.ToDictionary(
+					tkey => tkey.Split(":").FirstOrDefault(),
+					tvalue => tvalue.Split(":".LastOrDefault()));
+
+			var translationPages = languages.Keys
+				.Select(l => l + "/" + pageData.PageName)
+				.ToList();
+
+			var translations = _wikiPages
+				.ThatAreCurrentRevisions()
+				.Where(wp => translationPages.Contains(wp.PageName))
+				.ToList();
+
+
 			return View();
 		}
 	}
