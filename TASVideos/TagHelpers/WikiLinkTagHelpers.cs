2ffdb90cb4263eec52e71d33dc3df2e199147d78
adelikat <adelikat@tasvideos.org>
2019-04-11 20:33:39 -0500

    tag helpers to make submission/publication/game links a bit easier

---
+++ TASVideos/TagHelpers/WikiLinkTagHelpers.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers
+{
+	public class PubLinkTagHelper : TagHelper
+	{
+		public int Id { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.Attributes.Add("href", $"/{Id}M");
+		}
+	}
+
+	public class SubLinkTagHelper : TagHelper
+	{
+		public int Id { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.Attributes.Add("href", $"/{Id}S");
+		}
+	}
+
+	public class GameLinkTagHelper : TagHelper
+	{
+		public int Id { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			output.Attributes.Add("href", $"/{Id}G");
+		}
+	}
+}
