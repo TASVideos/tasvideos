60fe08b4b53ac7c4189ac77680c0c42ed82e31cf
adelikat <adelikat@tasvideos.org>
2019-04-11 20:24:57 -0500

    Implement the GameName module

--- TASVideos/ViewComponent/GameName.cs
+++ TASVideos/ViewComponent/GameName.cs
@@ -1,15 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 using TASVideos.Data.Entity;
+using TASVideos.Extensions;
+using TASVideos.ViewComponents.Models;
 
 namespace TASVideos.ViewComponents
 {
 	public class GameName : ViewComponent
 	{
-		public IViewComponentResult Invoke(WikiPage pageData, string pp)
+		private readonly ApplicationDbContext _db;
+
+		public GameName(ApplicationDbContext db)
 		{
-			// TODO: this needs to look up from the Game table
-			// Also, for pages like GameResources/NES it could look up the system name
-			return View();
+			_db = db;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
+		{
+			var path = HttpContext.Request.Path.ToString().Trim('/');
+
+			var model = new GameNameModel();
+			if (path.IsSystemGameResourcePath())
+			{
+				var system = await _db.GameSystems
+					.SingleOrDefaultAsync(s => s.Code == path.SystemGameResourcePath());
+				if (system != null)
+				{
+					model.System = system.DisplayName;
+				}
+			}
+			else
+			{
+				var game = await _db.Games
+					.SingleOrDefaultAsync(g => g.GameResourcesPage == path);
+				if (game != null)
+				{
+					model.GameId = game.Id;
+					model.DisplayName = game.DisplayName;
+				}
+			}
+
+			return View(model);
 		}
 	}
 }
---
+++ TASVideos/ViewComponent/Models/GameNameModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TASVideos.ViewComponents.Models
+{
+	public class GameNameModel
+	{
+		public int GameId { get; set; }
+		public string DisplayName { get; set; }
+
+		public string System { get; set; }
+
+		public bool IsSystem => !string.IsNullOrWhiteSpace(System);
+	}
+}
