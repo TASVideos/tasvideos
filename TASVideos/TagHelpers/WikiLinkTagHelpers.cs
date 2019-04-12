0677d2c0bcb100a4acd1f19f1451da76a987ae07
adelikat <adelikat@tasvideos.org>
2019-04-12 16:21:08 -0500

    create a profile-link tag helper and use it in a bunch of places

--- TASVideos/TagHelpers/WikiLinkTagHelpers.cs
+++ TASVideos/TagHelpers/WikiLinkTagHelpers.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace TASVideos.TagHelpers
 {
@@ -32,6 +38,24 @@ namespace TASVideos.TagHelpers
 		{
 			output.TagName = "a";
 			output.Attributes.Add("href", $"/{Id}G");
+		}
+	}
+
+	[HtmlTargetElement("profile-link")]
+	public class ProfileLinkTagHelper : AnchorTagHelper
+	{
+		public ProfileLinkTagHelper(IHtmlGenerator htmlGenerator) : base(htmlGenerator)
+		{
+		}
+
+		public string Username { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+			Page = "/Users/Profile";
+			RouteValues.Add("UserName", Username);
+			base.Process(context, output);
 		}
 	}
 }
