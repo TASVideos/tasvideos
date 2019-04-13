9228a2c8068df76e1c1cf1ba0383185420174290
adelikat <adelikat@tasvideos.org>
2019-04-13 10:00:11 -0500

    show screenshot description on front page modules

--- TASVideos/ViewComponent/Models/MiniMovieModel.cs
+++ TASVideos/ViewComponent/Models/MiniMovieModel.cs
@@ -4,7 +4,13 @@
 	{
 		public int Id { get; set; }
 		public string Title { get; set; }
-		public string Screenshot { get; set; }
+		public ScreenshotFile Screenshot { get; set; } = new ScreenshotFile();
 		public string OnlineWatchingUrl { get; set; }
+
+		public class ScreenshotFile
+		{
+			public string Path { get; set; }
+			public string Description { get; set; }
+		}
 	}
 }
