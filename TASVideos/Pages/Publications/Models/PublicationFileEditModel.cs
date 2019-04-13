d3a9bbbe311e31d1d67b294a60828cbe8128ff98
adelikat <adelikat@tasvideos.org>
2019-04-13 12:25:09 -0500

    plumbing for the Publications/EditFile page

---
+++ TASVideos/Pages/Publications/Models/PublicationFileEditModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TASVideos.Pages.Publications.Models
+{
+	public class PublicationFileEditModel
+	{
+		public string Title { get; set; }
+
+		public ScreenshotFile Screenshot { get; set; } = new ScreenshotFile();
+
+		public class ScreenshotFile
+		{
+			public string Path { get; set; }
+			public string Description { get; set; }
+		}
+	}
+}
