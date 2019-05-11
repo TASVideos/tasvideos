3856aed17c7512b042a249d67f3ce6d047a6a13d
adelikat <adelikat@tasvideos.org>
2019-05-11 12:55:22 -0500

    wiki users module - logics

--- TASVideos/ViewComponent/Models/WikiUserEntry.cs
+++ TASVideos/ViewComponent/Models/WikiUserEntry.cs
@@ -4,6 +4,6 @@
 	{
 		public string UserName { get; set; }
 		public int SubmissionCount { get; set; }
-		public int EditCount { get; set; }
+		public int PublicationCount { get; set; }
 	}
 }
--- TASVideos/ViewComponent/WikiUsers.cs
+++ TASVideos/ViewComponent/WikiUsers.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 using TASVideos.Data;
 using TASVideos.Data.Entity;
@@ -21,10 +22,38 @@ namespace TASVideos.ViewComponents
 
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
 		{
-			ParamHelper.GetValueFor(pp, "role");
+			var role = TranslateLegacyName(
+				ParamHelper.GetValueFor(pp, "roles"));
 
-			var model = new List<WikiUserEntry>();
+			var model = await _db.Users
+				.Where(u => u.UserRoles.Any(ur => ur.Role.Name == role))
+				.Select(u => new WikiUserEntry
+				{
+					UserName = u.UserName,
+					PublicationCount = u.Publications.Count,
+					SubmissionCount = u.Submissions.Count,
+				})
+				.ToListAsync();
+
 			return View(model);
+		}
+
+		private string TranslateLegacyName(string role)
+		{
+			// Translate legacy names for roles into modern ones
+			switch (role)
+			{
+				default:
+					return role;
+				case "admin":
+					return "Site Admin";
+				case "adminassistant":
+					return "Admin Assistant";
+				case "seniorjudge":
+					return "Senior Judge";
+				case "senior publisher":
+					return "Senior Publisher";
+			}
 		}
 	}
 }
