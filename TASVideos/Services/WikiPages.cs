dcc7485f6d029dc7ec9a6107449064507512e9e9
adelikat <adelikat@tasvideos.org>
2019-05-26 11:34:13 -0500

    IWikiPages - don't inherit IEnumerable, and refactor accordingly, this functionality depended on all current revisions eing in cache at all times

--- TASVideos/Services/WikiPages.cs
+++ TASVideos/Services/WikiPages.cs
@@ -10,7 +10,7 @@ using TASVideos.WikiEngine;
 
 namespace TASVideos.Services
 {
-	public interface IWikiPages : IEnumerable<WikiPage>
+	public interface IWikiPages
 	{
 		/// <summary>
 		/// Returns whether or not any revision of the given page exists
@@ -67,6 +67,24 @@ namespace TASVideos.Services
 		/// Clears the wiki cache
 		/// </summary>
 		Task FlushCache();
+
+		/// <summary>
+		/// Filters the list of wiki pages to only pages that are nest beneath the given page.
+		/// If no pageName is provided, then a master list of subpages is provided
+		/// ex: /Foo/Bar, /Foo/Bar2 and /Foo/Bar/Baz are all subpages of /Foo
+		/// </summary>
+		/// <seealso cref="WikiPage"/>
+		/// <param name="pageName">the name of the page to get sub pages from</param>
+		IEnumerable<WikiPage> ThatAreSubpagesOf(string pageName);
+
+		/// <summary>
+		/// Filters the list of wiki pages to only pages that are parents of the given page
+		/// ex: /Foo is a parent of /Foo/Bar
+		/// ex: /Foo and /Foo/Bar are parents of /Foo/Bar/Baz
+		/// </summary>
+		/// <seealso cref="WikiPage"/>
+		/// <param name="pageName">the name of the page to get parent pages from</param>
+		IEnumerable<WikiPage> ThatAreParentsOf(string pageName);
 	}
 
 	public class WikiPages : IWikiPages
@@ -98,14 +116,38 @@ namespace TASVideos.Services
 			}
 		}
 
-		public IEnumerator<WikiPage> GetEnumerator()
+		// TODO: consider caching these
+		public IEnumerable<WikiPage> ThatAreSubpagesOf(string pageName)
 		{
-			return CurrentRevisionCache.GetEnumerator();
+			pageName = pageName.Trim('/');
+			var query = _db.WikiPages
+				.ThatAreNotDeleted()
+				.ThatAreCurrentRevisions()
+				.Where(wp => wp.PageName != pageName);
+
+			if (!string.IsNullOrWhiteSpace(pageName))
+			{
+				query = query.Where(wp => wp.PageName.StartsWith(pageName + "/"));
+			}
+
+			return query;
 		}
 
-		IEnumerator IEnumerable.GetEnumerator()
+		// TODO: consider caching these
+		public IEnumerable<WikiPage> ThatAreParentsOf(string pageName)
 		{
-			return GetEnumerator();
+			pageName = (pageName ?? "").Trim('/');
+			if (string.IsNullOrWhiteSpace(pageName)
+				|| !pageName.Contains('/')) // Easy optimization, pages without a / have no parents
+			{
+				return Enumerable.Empty<WikiPage>();
+			}
+
+			return _db.WikiPages
+				.ThatAreNotDeleted()
+				.ThatAreCurrentRevisions()
+				.Where(wp => wp.PageName != pageName)
+				.Where(wp => pageName.StartsWith(wp.PageName));
 		}
 
 		public async Task<bool> Exists(string pageName, bool includeDeleted = false)
@@ -403,54 +445,6 @@ namespace TASVideos.Services
 
 	public static class WikiPageExtensions
 	{
-		/// <summary>
-		/// Filters the list of wiki pages to only pages that are nest beneath the given page.
-		/// If no pageName is provided, then a master list of subpages is provided
-		/// ex: /Foo/Bar, /Foo/Bar2 and /Foo/Bar/Baz are all subpages of /Foo
-		/// </summary>
-		/// <seealso cref="WikiPage"/>
-		/// <param name="list">The list of wiki pages</param>
-		/// <param name="pageName">the name of the page to get sub pages from</param>
-		public static IEnumerable<WikiPage> ThatAreSubpagesOf(this IEnumerable<WikiPage> list, string pageName)
-		{
-			pageName = pageName.Trim('/');
-			var query = list
-				.ThatAreNotDeleted()
-				.ThatAreCurrentRevisions()
-				.Where(wp => wp.PageName != pageName);
-
-			if (!string.IsNullOrWhiteSpace(pageName))
-			{
-				query = query.Where(wp => wp.PageName.StartsWith(pageName + "/"));
-			}
-
-			return query;
-		}
-
-		/// <summary>
-		/// Filters the list of wiki pages to only pages that are parents of the given page
-		/// ex: /Foo is a parent of /Foo/Bar
-		/// ex: /Foo and /Foo/Bar are parents of /Foo/Bar/Baz
-		/// </summary>
-		/// <seealso cref="WikiPage"/>
-		/// <param name="list">The list of wiki pages</param>
-		/// <param name="pageName">the name of the page to get parent pages from</param>
-		public static IEnumerable<WikiPage> ThatAreParentsOf(this IEnumerable<WikiPage> list, string pageName)
-		{
-			pageName = (pageName ?? "").Trim('/');
-			if (string.IsNullOrWhiteSpace(pageName)
-				|| !pageName.Contains('/')) // Easy optimization, pages without a / have no parents
-			{
-				return Enumerable.Empty<WikiPage>();
-			}
-
-			return list
-				.ThatAreNotDeleted()
-				.ThatAreCurrentRevisions()
-				.Where(wp => wp.PageName != pageName)
-				.Where(wp => pageName.StartsWith(wp.PageName));
-		}
-
 		/// <summary>
 		/// Returns a System page with the given page suffix
 		/// <example>SystemPage("Languages") will return the page System/Languages</example>
