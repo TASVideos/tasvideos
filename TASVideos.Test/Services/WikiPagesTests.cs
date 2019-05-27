6de3450a8fb353a00fbf7eb2d263620a5242ffdb
adelikat <adelikat@tasvideos.org>
2019-05-26 23:07:36 -0500

    WikiPage.Exists() tests - test cache behavior

--- TASVideos.Test/Services/WikiPagesTests.cs
+++ TASVideos.Test/Services/WikiPagesTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
@@ -16,7 +17,7 @@ namespace TASVideos.Test.Services
 	{
 		private IWikiPages _wikiPages;
 		private ApplicationDbContext _db;
-		private ICacheService _cache;
+		private StaticCache _cache;
 
 		[TestInitialize]
 		public void Initialize()
@@ -26,7 +27,7 @@ namespace TASVideos.Test.Services
 				.Options;
 			_db = new ApplicationDbContext(options, null);
 			_db.Database.EnsureDeleted();
-			_cache = new NoCacheService();
+			_cache = new StaticCache();
 			_wikiPages = new WikiPages(_db, _cache);
 		}
 
@@ -37,6 +38,8 @@ namespace TASVideos.Test.Services
 			AddPage(existingPage);
 
 			var actual = await _wikiPages.Exists(existingPage, false);
+			Assert.AreEqual(1, _cache.PageCache.Count, "Cache should have  1 record");
+			Assert.AreEqual(existingPage, _cache.PageCache.First().PageName, "Cache should match page checked");
 			Assert.IsTrue(actual);
 		}
 
@@ -57,6 +60,8 @@ namespace TASVideos.Test.Services
 			AddPage(existingPage, isDeleted: true);
 
 			var actual = await _wikiPages.Exists(existingPage, includeDeleted: true);
+			Assert.AreEqual(1, _cache.PageCache.Count, "Cache should have  1 record");
+			Assert.AreEqual(existingPage, _cache.PageCache.First().PageName, "Cache should match page checked");
 			Assert.IsTrue(actual);
 		}
 
@@ -67,6 +72,7 @@ namespace TASVideos.Test.Services
 			AddPage(existingPage, isDeleted: true);
 
 			var actual = await _wikiPages.Exists(existingPage, includeDeleted: false);
+			Assert.AreEqual(0, _cache.PageCache.Count, "Non-existent page was not cached.");
 			Assert.IsFalse(actual);
 		}
 
@@ -75,6 +81,42 @@ namespace TASVideos.Test.Services
 			_db.Add(new WikiPage { PageName = name, IsDeleted = isDeleted });
 			_db.SaveChanges();
 			_wikiPages.FlushCache();
+		}
+	}
+
+	internal class StaticCache : ICacheService
+	{
+		private readonly Dictionary<string, object> _cache = new Dictionary<string, object>();
+
+		public List<WikiPage> PageCache
+		{
+			get
+			{
+				var result = _cache.TryGetValue(CacheKeys.WikiCache, out object list);
+				if (result)
+				{
+					return list as List<WikiPage>;
+				}
+
+				return new List<WikiPage>();
+			}
+		}
+
+		public void Remove(string key)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Set(string key, object data, int? cacheTime = null)
+		{
+			_cache[key] = data;
+		}
+
+		public bool TryGetValue<T>(string key, out T value)
+		{
+			var result = _cache.TryGetValue(key, out object cached);
+			value = (T)cached;
+			return result;
 		}
 	}
 }
