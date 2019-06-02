dac0b1723067ffb21ca88acf795e7fd38361abf1
adelikat <adelikat@tasvideos.org>
2019-06-02 12:24:52 -0500

    Store award years as 4 digit, preventing the great y2.1k tasvideos apocolypse

--- TASVideos.Test/Services/AwardsTests.cs
+++ TASVideos.Test/Services/AwardsTests.cs
@@ -19,7 +19,7 @@ namespace TASVideos.Test.Services
 		private TestDbContext _db;
 		private ICacheService _cache;
 
-		private static readonly int CurrentYear = DateTime.UtcNow.Year - 2000;
+		private static readonly int TwoDigitCurrentYear = DateTime.UtcNow.Year - 2000;
 
 		[TestInitialize]
 		public void Initialize()
@@ -52,7 +52,7 @@ namespace TASVideos.Test.Services
 			Assert.AreEqual(1, list.Count);
 			var actualUserAward = list.Single();
 			Assert.AreEqual(award.ShortName, actualUserAward.ShortName);
-			Assert.AreEqual(CurrentYear, actualUserAward.Year);
+			Assert.AreEqual(TwoDigitCurrentYear, actualUserAward.Year);
 		}
 
 		[TestMethod]
@@ -70,7 +70,7 @@ namespace TASVideos.Test.Services
 			Assert.AreEqual(1, list.Count);
 			var actualUserAward = list.Single();
 			Assert.AreEqual(award.ShortName, actualUserAward.ShortName);
-			Assert.AreEqual(CurrentYear, actualUserAward.Year);
+			Assert.AreEqual(TwoDigitCurrentYear, actualUserAward.Year);
 		}
 
 		[TestMethod]
@@ -113,6 +113,8 @@ namespace TASVideos.Test.Services
 			Assert.AreEqual(0, actual.Count());
 		}
 
+		// For year
+
 		private User CreateUser()
 		{
 			var user = new User { UserName = "TestUser" + Guid.NewGuid() };
@@ -135,7 +137,7 @@ namespace TASVideos.Test.Services
 
 		private void GiveUserAnAward(User user, Award award)
 		{
-			_db.UserAwards.Add(new UserAward { AwardId = award.Id, UserId = user.Id, Year = CurrentYear });
+			_db.UserAwards.Add(new UserAward { AwardId = award.Id, UserId = user.Id, Year = TwoDigitCurrentYear });
 			_db.SaveChanges();
 		}
 
@@ -170,7 +172,7 @@ namespace TASVideos.Test.Services
 		{
 			_db.PublicationAwards.Add(new PublicationAward
 			{
-				AwardId = award.Id, PublicationId = publication.Id, Year = CurrentYear
+				AwardId = award.Id, PublicationId = publication.Id, Year = TwoDigitCurrentYear
 			});
 			_db.SaveChanges();
 		}
