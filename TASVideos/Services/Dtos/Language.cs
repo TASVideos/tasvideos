f4363390f936e82ccd406141a338a0f499df2fe5
adelikat <adelikat@tasvideos.org>
2019-06-18 17:45:40 -0500

    start a language service for doing translation page logic

---
+++ TASVideos/Services/Dtos/Language.cs
@@ -0,0 +1,8 @@
+﻿namespace TASVideos.Services
+{
+	public class Language
+	{
+		public string Code { get; set; }
+		public string DisplayName { get; set; }
+	}
+}
---
+++ TASVideos/Services/Languages.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TASVideos.Services
+{
+	public interface ILanguage
+	{
+		Task<IEnumerable<Language>> AvailableLanguages();
+
+		Task<bool> IsLanguagePage(string pageName);
+	}
+
+	public class Languages : ILanguage
+	{
+		private readonly IWikiPages _wikiPages;
+
+		public Languages(IWikiPages wikiPages)
+		{
+			_wikiPages = wikiPages;
+		}
+
+		public async Task<IEnumerable<Language>> AvailableLanguages()
+		{
+			var languagesMarkup = (await _wikiPages
+				.SystemPage("Languages"))
+				?.Markup;
+
+			if (string.IsNullOrWhiteSpace(languagesMarkup))
+			{
+				return Enumerable.Empty<Language>();
+			}
+
+			return languagesMarkup
+				.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(l =>
+				{
+					var split = l.Split(":");
+					return new Language
+					{
+						Code = split.FirstOrDefault(),
+						DisplayName = split.LastOrDefault()
+					};
+				})
+				.ToList();
+		}
+
+		public async Task<bool> IsLanguagePage(string pageName)
+		{
+			if (string.IsNullOrWhiteSpace(pageName))
+			{
+				return false;
+			}
+
+			string trimmed = pageName.Trim('/');
+
+			if (string.IsNullOrEmpty(trimmed))
+			{
+				return false;
+			}
+
+			var languages = await AvailableLanguages();
+
+			return languages.Any(l => trimmed.StartsWith(l + "/"));
+		}
+	}
+}
