55d1f2bb6d457c41fb586c9928d81b986091b963
adelikat <adelikat@tasvideos.org>
2019-07-16 17:54:31 -0500

    attempt to extract .ltm with sharpcomrpess, doesn't work

--- TASVideos.Parsers/Parsers/Ltm.cs
+++ TASVideos.Parsers/Parsers/Ltm.cs
@@ -1,22 +1,36 @@
+using System;
 using System.IO;
+
+using SharpCompress.Archives.Tar;
+using SharpCompress.Common;
+using SharpCompress.Compressors.Deflate;
+using SharpCompress.Readers;
+using SharpCompress.Readers.GZip;
+
 using TASVideos.MovieParsers.Result;
 
 namespace TASVideos.MovieParsers.Parsers
 {
-    [FileExtension("ltm")]
-    internal class Ltm : ParserBase, IParser
-    {
-        public override string FileExtension => "ltm";
-        
-        public IParseResult Parse(Stream file)
-        {
-            var result = new ParseResult
-            {
-                Region = RegionType.Ntsc,
-                FileExtension = FileExtension
-            };
-            
-            return result;
-        }
-    }
+	[FileExtension("ltm")]
+	internal class Ltm : ParserBase, IParser
+	{
+		public override string FileExtension => "ltm";
+
+		public IParseResult Parse(Stream file)
+		{
+			var result = new ParseResult
+			{
+				Region = RegionType.Ntsc,
+				FileExtension = FileExtension
+			};
+
+			using (var reader = ReaderFactory.Open(file))
+			{
+				var blah = reader.OpenEntryStream();
+				int zzz = 0;
+			}
+
+			return result;
+		}
+	}
 }
--- TASVideos.Test/MovieParsers/LtmTests.cs
+++ TASVideos.Test/MovieParsers/LtmTests.cs
@@ -3,24 +3,27 @@ using TASVideos.MovieParsers.Parsers;
 
 namespace TASVideos.Test.MovieParsers
 {
-    [TestClass]
-    [TestCategory("LtmParsers")]
-    public class LtmTests : BaseParserTests
-    {
-        private Ltm _ltmParser;
-        
-        public override string ResourcesPath { get; } = "TASVideos.Test.MovieParsers.LtmSampleFiles.";
-        
-        [TestInitialize]
-        public void Initialize()
-        {
-            _ltmParser = new Ltm();
-        }
-        
-        [TestMethod]
-        public void BasicTest()
-        {
-            var result = _ltmParser.Parse(Embedded("2frames.ltm"));
-        }
-    }
+	[TestClass]
+	[TestCategory("LtmParsers")]
+	public class LtmTests : BaseParserTests
+	{
+		private Ltm _ltmParser;
+
+		public override string ResourcesPath { get; } = "TASVideos.Test.MovieParsers.LtmSampleFiles.";
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_ltmParser = new Ltm();
+		}
+
+		[TestMethod]
+		public void BasicTest()
+		{
+			var result = _ltmParser.Parse(Embedded("2frames.ltm"));
+
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Success);
+		}
+	}
 }
