3f1383c701565d77ed0083b14fdfa093ac5f67b6
adelikat <adelikat@tasvideos.org>
2019-08-30 09:17:46 -0500

    gmv - parse rerecord count

--- TASVideos.Test/MovieParsers/GmvTests.cs
+++ TASVideos.Test/MovieParsers/GmvTests.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using TASVideos.MovieParsers;
 using TASVideos.MovieParsers.Parsers;
 
 namespace TASVideos.Test.MovieParsers
@@ -28,6 +29,32 @@ namespace TASVideos.Test.MovieParsers
 			AssertNoWarnings(result);
 			Assert.IsNotNull(result.Errors);
 			Assert.AreEqual(1, result.Errors.Count());
+		}
+
+		[TestMethod]
+		public void ValidHeader()
+		{
+			var result = _gmvParser.Parse(Embedded("2frames.gmv"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+		}
+
+		[TestMethod]
+		public void System()
+		{
+			var result = _gmvParser.Parse(Embedded("2frames.gmv"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+			Assert.AreEqual(SystemCodes.Genesis, result.SystemCode);
+		}
+
+		[TestMethod]
+		public void RerecordCount()
+		{
+			var result = _gmvParser.Parse(Embedded("2frames.gmv"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+			Assert.AreEqual(10319, result.RerecordCount);
 		}
 	}
 }
