e53c1bebceea67f5cef00b1d1ac94e0bd9e0ba90
adelikat <adelikat@tasvideos.org>
2019-08-30 12:19:25 -0500

    start .mar parser

---
+++ TASVideos.Parsers/Parsers/Mar.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+using TASVideos.MovieParsers.Extensions;
+using TASVideos.MovieParsers.Result;
+
+
+namespace TASVideos.MovieParsers.Parsers
+{
+	[FileExtension("mar")]
+	internal class Mar : ParserBase, IParser
+	{
+		public override string FileExtension => "mar";
+
+		public IParseResult Parse(Stream file)
+		{
+			var result = new ParseResult
+			{
+				Region = RegionType.Ntsc,
+				FileExtension = FileExtension,
+				SystemCode = SystemCodes.Arcade
+			};
+
+			using (var br = new BinaryReader(file))
+			{
+				var header = new string(br.ReadChars(8));
+				if (!header.StartsWith("MAMETAS\0"))
+				{
+					return new ErrorResult("Invalid file format, does not seem to be a .mar");
+				}
+			}
+
+			return result;
+		}
+	}
+}
---
+++ TASVideos.Test/MovieParsers/MarTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TASVideos.MovieParsers;
+using TASVideos.MovieParsers.Parsers;
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.Test.MovieParsers
+{
+	[TestClass]
+	[TestCategory("MarParsers")]
+	public class MarTests : BaseParserTests
+	{
+		private Mar _marParser;
+
+		public override string ResourcesPath { get; } = "TASVideos.Test.MovieParsers.MarSampleFiles.";
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_marParser = new Mar();
+		}
+
+		[TestMethod]
+		public void InvalidHeader()
+		{
+			var result = _marParser.Parse(Embedded("wrongheader.mar"));
+			Assert.IsFalse(result.Success);
+			AssertNoWarnings(result);
+			Assert.IsNotNull(result.Errors);
+			Assert.AreEqual(1, result.Errors.Count());
+		}
+
+		[TestMethod]
+		public void ValidHeader()
+		{
+			var result = _marParser.Parse(Embedded("2frames.mar"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+		}
+	}
+}
