f90a836c7049bbb261be3675e7fb7cf4912a9101
adelikat <adelikat@tasvideos.org>
2019-08-30 16:34:32 -0500

    wtf parser

---
+++ TASVideos.Parsers/Parsers/Wtf.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.MovieParsers.Parsers
+{
+	internal class Wtf : ParserBase, IParser
+	{
+		public override string FileExtension => "wtf";
+
+		public IParseResult Parse(Stream file)
+		{
+			var result = new ParseResult
+			{
+				Region = RegionType.Ntsc,
+				FileExtension = FileExtension,
+				SystemCode = SystemCodes.Windows,
+				Frames = (int)((file.Length - 1024) / 8)
+			};
+
+			using (var br = new BinaryReader(file))
+			{
+				var signature = br.ReadInt32(); // 0x66 0x54 0x77 0x02
+				if (signature != 41374822)
+				{
+					return new ErrorResult("Invalid file format, does not seem to be a .wtf");
+				}
+
+				br.ReadInt32(); // input frames
+				result.RerecordCount = br.ReadInt32();
+				br.ReadBytes(8); // keyboard type
+				var fps = br.ReadUInt32();
+				if (fps > 0)
+				{
+					result.FrameRateOverride = fps;
+				}
+			}
+
+			return result;
+		}
+	}
+}
