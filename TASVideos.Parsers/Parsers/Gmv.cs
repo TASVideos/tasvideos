a46728b70059f902c6fbb390762c66197c75e9b0
adelikat <adelikat@tasvideos.org>
2019-08-29 20:04:52 -0500

    gmv - plumbing

---
+++ TASVideos.Parsers/Parsers/Gmv.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.MovieParsers.Parsers
+{
+	[FileExtension("gmv")]
+	internal class Gmv : ParserBase, IParser
+	{
+		public override string FileExtension => "gmv";
+
+		public IParseResult Parse(Stream file)
+		{
+			var result = new ParseResult
+			{
+				Region = RegionType.Ntsc,
+				FileExtension = FileExtension,
+				SystemCode = SystemCodes.Genesis
+			};
+
+			using (var br = new BinaryReader(file))
+			{
+				var header = new string(br.ReadChars(16));
+				if (!header.StartsWith("Gens Movie"))
+				{
+					return new ErrorResult("Invalid file format, does not seem to be a .gmv");
+				}
+			}
+
+			return result;
+		}
+	}
+}
