3f6ecd00e0d536ef0aad0d9980f2c407612d5214
adelikat <adelikat@tasvideos.org>
2019-08-31 10:10:38 -0500

    omr parser - plumbing

---
+++ TASVideos.Parsers/Parsers/Omr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using SharpCompress.Readers;
+
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.MovieParsers.Parsers
+{
+	internal class Omr : ParserBase, IParser
+	{
+		public override string FileExtension => "omr";
+
+		public IParseResult Parse(Stream file)
+		{
+			var result = new ParseResult
+			{
+				Region = RegionType.Pal,
+				FileExtension = FileExtension,
+				SystemCode = SystemCodes.Msx
+			};
+
+			using (var reader = ReaderFactory.Open(file))
+			{
+				
+			}
+
+			return result;
+		}
+	}
+}
---
+++ TASVideos.Test/MovieParsers/OmrTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TASVideos.MovieParsers;
+using TASVideos.MovieParsers.Parsers;
+
+namespace TASVideos.Test.MovieParsers
+{
+	[TestClass]
+	[TestCategory("OmrParsers")]
+	public class OmrTests : BaseParserTests
+	{
+		private Omr _omrParser;
+
+		public override string ResourcesPath { get; } = "TASVideos.Test.MovieParsers.OmrSampleFiles.";
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_omrParser = new Omr();
+		}
+
+		[TestMethod]
+		public void System()
+		{
+			var result = _omrParser.Parse(Embedded("2frames.omr"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+			Assert.AreEqual(SystemCodes.Msx, result.SystemCode);
+		}
+	}
+}
