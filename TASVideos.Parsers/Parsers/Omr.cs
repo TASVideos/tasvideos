7f252a950c09a5bc716f8e7d11dc9fb7fb889acb
adelikat <adelikat@tasvideos.org>
2019-09-02 17:32:40 -0500

    omr - fix gz logic, worked for embedded resources but not when running against a file from the web

--- TASVideos.Parsers/Parsers/Omr.cs
+++ TASVideos.Parsers/Parsers/Omr.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Xml.XPath;
 
+using SharpCompress.Compressors;
+using SharpCompress.Compressors.Deflate;
 using SharpCompress.Readers;
 
 using TASVideos.MovieParsers.Result;
@@ -26,66 +28,62 @@ namespace TASVideos.MovieParsers.Parsers
 				SystemCode = SystemCodes.Msx
 			};
 
-			using (var reader = ReaderFactory.Open(file))
+			using (GZipStream gz = new GZipStream(file, CompressionMode.Decompress))
+			using (StreamReader unzip = new StreamReader(gz))
 			{
-				reader.MoveToNextEntry();
-				using (var entry = reader.OpenEntryStream())
-				using (var textReader = new StreamReader(entry))
+				var replay = XElement.Parse(unzip.ReadToEnd())
+					.Descendants().First(x => x.Name == "replay");
+
+				result.RerecordCount = int.Parse(replay.Descendants().First(x => x.Name == "reRecordCount").Value);
+
+				var isPowerOn = ((IEnumerable)replay.XPathEvaluate("//snapshots/item/scheduler/currentTime/time"))
+					.Cast<XElement>()
+					.Any(x => x.Value == "0");
+
+				if (!isPowerOn)
 				{
-					var replay = XElement.Parse(textReader.ReadToEnd())
-						.Descendants().First(x => x.Name == "replay");
+					result.StartType = MovieStartType.Savestate;
+				}
 
-					result.RerecordCount = int.Parse(replay.Descendants().First(x => x.Name == "reRecordCount").Value);
+				var isPal = ((IEnumerable)replay.XPathEvaluate("//snapshots/item/config/device/palTiming"))
+					.Cast<XElement>()
+					.Any(x => x.Value.ToString() == "true");
 
-					var isPowerOn = ((IEnumerable)replay.XPathEvaluate("//snapshots/item/scheduler/currentTime/time"))
-						.Cast<XElement>()
-						.Any(x => x.Value == "0");
+				if (isPal)
+				{
+					result.Region = RegionType.Pal;
+				}
 
-					if (!isPowerOn)
-					{
-						result.StartType = MovieStartType.Savestate;
-					}
+				var lengthTimestamp = long.Parse(((IEnumerable)replay.XPathEvaluate("//events/item"))
+					.Cast<XElement>()
+					.Last(x => x.Attribute("type")?.Value != "EndLog")
+					.Descendants()
+					.First(x => x.Name == "StateChange")
+					.Descendants()
+					.First(x => x.Name == "time")
+					.Descendants()
+					.First(x => x.Name == "time")
+					.Value);
 
-					var isPal = ((IEnumerable)replay.XPathEvaluate("//snapshots/item/config/device/palTiming"))
-						.Cast<XElement>()
-						.Any(x => x.Value.ToString() == "true");
+				var seconds = ConvertTimestamp(lengthTimestamp);
 
-					if (isPal)
-					{
-						result.Region = RegionType.Pal;
-					}
+				result.Frames = (int)Math.Ceiling(seconds * (result.Region == RegionType.Pal ? 50.1589758045661 : 59.9227510135505));
 
-					var lengthTimestamp = long.Parse(((IEnumerable)replay.XPathEvaluate("//events/item"))
-						.Cast<XElement>()
-						.Last(x => x.Attribute("type")?.Value != "EndLog")
-						.Descendants()
-						.First(x => x.Name == "StateChange")
-						.Descendants()
-						.First(x => x.Name == "time")
-						.Descendants()
-						.First(x => x.Name == "time")
-						.Value);
+				var system = ((IEnumerable)replay.XPathEvaluate("//snapshots/item/config/config/children/item/children/item"))
+					.Cast<XElement>()
+					.First(x => x.Descendants().Any(d => d.Name == "name" && d.Value == "type"))
+					.Descendants()
+					.First(x => x.Name == "data")
+					.Value
+					.ToLower();
 
-					var seconds = ConvertTimestamp(lengthTimestamp);
-
-					result.Frames = (int)Math.Ceiling(seconds * (result.Region == RegionType.Pal ? 50.1589758045661 : 59.9227510135505));
-
-					var system = ((IEnumerable)replay.XPathEvaluate("//snapshots/item/config/config/children/item/children/item"))
-						.Cast<XElement>()
-						.First(x => x.Descendants().Any(d => d.Name == "name" && d.Value == "type"))
-						.Descendants()
-						.First(x => x.Name == "data")
-						.Value
-						.ToLower();
-
-					if (system.StartsWith("svi"))
-					{
-						result.SystemCode = SystemCodes.Svi;
-					}
-					else if (system.StartsWith("coleco"))
-					{
-						result.SystemCode = SystemCodes.Coleco;
-					}
+				if (system.StartsWith("svi"))
+				{
+					result.SystemCode = SystemCodes.Svi;
+				}
+				else if (system.StartsWith("coleco"))
+				{
+					result.SystemCode = SystemCodes.Coleco;
 				}
 			}
 
