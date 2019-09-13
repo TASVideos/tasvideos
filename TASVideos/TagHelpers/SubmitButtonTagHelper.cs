ccbe98433d7a656ebdc7559e8635c3a1cad5f906
adelikat <adelikat@tasvideos.org>
2019-09-12 19:29:02 -0500

    make a submit button tag helper that bakes in double click prevention, use that on submissions/submit page instead of one-off code

---
+++ TASVideos/TagHelpers/SubmitButtonTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers
+{
+	public class SubmitButtonTagHelper : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			var guid = Guid.NewGuid();
+			output.TagName = "button";
+			output.Attributes.Add("type", "submit");
+			output.Attributes.Add("data-submit-id", guid);
+			output.PostElement.AppendHtml($@"
+<script>
+	document.querySelector('[data-submit-id=""{guid}""]').onclick = function () {{
+		var btn = this;
+		setTimeout(function () {{ btn.disabled = true }}, 0);
+		setTimeout(function () {{ btn.disabled = false }}, 750);
+	}}
+</script>");
+		}
+	}
+}
