bc9754eb678485cc939cd8cd1b5570bd3e3518e5
adelikat <adelikat@tasvideos.org>
2019-09-16 19:32:36 -0500

    move more logic to topic watcher

--- TASVideos/Pages/Profile/WatchedTopics.cshtml.cs
+++ TASVideos/Pages/Profile/WatchedTopics.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using TASVideos.Data;
 using TASVideos.Data.Entity.Forum;
 using TASVideos.Pages.Profile.Models;
+using TASVideos.Services;
 
 namespace TASVideos.Pages.Profile
 {
@@ -16,10 +17,12 @@ namespace TASVideos.Pages.Profile
 	public class WatchedTopicsModel : BasePageModel
 	{
 		private readonly ApplicationDbContext _db;
+		private readonly ITopicWatcher _topicWatcher;
 
-		public WatchedTopicsModel(ApplicationDbContext db)
+		public WatchedTopicsModel(ApplicationDbContext db, ITopicWatcher topicWatcher)
 		{
 			_db = db;
+			_topicWatcher = topicWatcher;
 		}
 
 		public IEnumerable<WatchedTopicEntry> Watches { get; set; } = new List<WatchedTopicEntry>();
@@ -43,24 +46,7 @@ namespace TASVideos.Pages.Profile
 
 		public async Task<IActionResult> OnPostStopWatching(int topicId)
 		{
-			try
-			{
-				var userId = User.GetUserId();
-				var watch = await _db.ForumTopicWatches
-					.SingleOrDefaultAsync(tw => tw.UserId == userId && tw.ForumTopicId == topicId);
-				_db.ForumTopicWatches.Remove(watch);
-				await _db.SaveChangesAsync();
-			}
-			catch (DbUpdateConcurrencyException)
-			{
-				// Do nothing
-				// 1) if a watch is already removed, we are done
-				// 2) if a watch was updated (for instance, someone posted in the topic),
-				//		there isn't much we can do other than reload the page anyway with an error
-				//		An error would only be modestly helpful anyway, and wouldn't save clicks
-				//		However, this would be an nice to have one day
-			}
-
+			await _topicWatcher.UnwatchTopic(topicId, User.GetUserId());
 			return RedirectToPage("WatchedTopics");
 		}
 	}
