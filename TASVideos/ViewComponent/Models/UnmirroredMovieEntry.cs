a4fa2d1fa44586926e4202398ecda37644c54871
adelikat <adelikat@tasvideos.org>
2019-09-21 17:14:40 -0500

    unmirroredmovies module

---
+++ TASVideos/ViewComponent/Models/UnmirroredMovieEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TASVideos.ViewComponents
+{
+	public class UnmirroredMovieEntry
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public IEnumerable<string> EncodePaths { get; set; } = new List<string>();
+	}
+}
--- TASVideos/ViewComponent/UnmirroredMovies.cs
+++ TASVideos/ViewComponent/UnmirroredMovies.cs
@@ -1,14 +1,74 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 using TASVideos.Data.Entity;
 
 namespace TASVideos.ViewComponents
 {
 	public class UnmirroredMovies : ViewComponent
 	{
+		private readonly ApplicationDbContext _db;
+
+		public UnmirroredMovies(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
 		public async Task<IViewComponentResult> InvokeAsync(WikiPage pageData, string pp)
 		{
-			var model = new object();
+			var query = _db.Publications.AsQueryable();
+				
+
+			if (ParamHelper.HasParam(pp, "obs"))
+			{
+				query = query.Where(p => p.ObsoletedById.HasValue);
+			}
+			else if (ParamHelper.HasParam(pp, "current"))
+			{
+				query = query.Where(p => !p.ObsoletedById.HasValue);
+			}
+
+			if (ParamHelper.HasParam(pp, "allunstreamed"))
+			{
+				query = query.Where(p => p.OnlineWatchingUrl == null);
+			}
+			else if (ParamHelper.HasParam(pp, "unstreamed"))
+			{
+				query = query.Where(p => p.OnlineWatchingUrl == null && p.MirrorSiteUrl == null);
+			}
+			else if (ParamHelper.HasParam(pp, "streamed"))
+			{
+				query = query.Where(p => p.OnlineWatchingUrl != null && p.MirrorSiteUrl == null);
+			}
+			else if (ParamHelper.HasParam(pp, "noyoutube"))
+			{
+				query = query.Where(p => p.OnlineWatchingUrl == null || !p.OnlineWatchingUrl.Contains("youtube"));
+			}
+			else if (ParamHelper.HasParam(pp, "playlist"))
+			{
+				query = query.Where(p => p.OnlineWatchingUrl != null && p.OnlineWatchingUrl.Contains("view_play_list"));
+			}
+			else
+			{
+				query = query.Where(p => p.MirrorSiteUrl == null);
+			}
+
+			var model = await query
+				.OrderBy(p => p.SystemId)
+				.Select(p => new UnmirroredMovieEntry
+				{
+					Id = p.Id,
+					Title = p.Title,
+					EncodePaths = p.Files
+						.Where(f => f.Type == FileType.Torrent)
+						.Select(f => f.Path)
+						.ToList()
+				})
+				.ToListAsync();
+				
 
 			return View(model);
 		}
