3eedb100c5be4c1d39e999f2616cd0b163dc87a2
adelikat <adelikat@tasvideos.org>
2019-09-22 10:28:56 -0500

    public history service - progress

---
+++ TASVideos/Services/Dtos/PublicationHistoryNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TASVideos.Services
+{
+	public class PublicationHistoryGroup
+	{
+		public int GameId { get; set; }
+
+		public IEnumerable<PublicationHistoryNode> Branches { get; set; } = new List<PublicationHistoryNode>();
+	}
+
+	public class PublicationHistoryNode
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Branch { get; set; }
+
+		public PublicationHistoryNode Obsoletes { get; set; }
+	}
+}
--- TASVideos/Services/PublicationHistory.cs
+++ TASVideos/Services/PublicationHistory.cs
@@ -1,9 +1,20 @@
-﻿using TASVideos.Data;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
 
 namespace TASVideos.Services.PublicationChain
 {
 	public interface IPublicationHistory
 	{
+		/// <summary>
+		/// Returns the publication history for a game,
+		/// grouped by non-obsolete publications as the parent node
+		/// </summary>
+		Task<PublicationHistoryGroup> ForGame(int gameId);
 	}
 
 	public class PublicationHistory : IPublicationHistory
@@ -17,6 +28,50 @@ namespace TASVideos.Services.PublicationChain
 		{
 			_db = db;
 			_cache = cache;
+		}
+
+		public async Task<PublicationHistoryGroup> ForGame(int gameId)
+		{
+			var game = await _db.Games
+				.SingleOrDefaultAsync(g => g.Id == gameId);
+
+			if (game == null)
+			{
+				return null;
+			}
+
+			var parents = new List<PublicationHistoryNode>();
+
+			var publications = await _db.Publications
+				.Where(p => p.GameId == gameId)
+				.Select(p => new
+				{
+					p.Id,
+					p.Title,
+					p.Branch,
+					p.ObsoletedById
+				})
+				.ToListAsync();
+
+			var parentNodes = publications
+				.Where(p => !p.ObsoletedById.HasValue)
+				.ToList();
+
+			foreach (var node in parentNodes)
+			{
+				parents.Add(new PublicationHistoryNode
+				{
+					Id = node.Id,
+					Title = node.Title,
+					Branch = node.Branch
+				});
+			}
+
+			return new PublicationHistoryGroup
+			{
+				GameId = gameId,
+				Branches = parents
+			};
 		}
 	}
 }
