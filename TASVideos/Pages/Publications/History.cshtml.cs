dd2706eac9a07fc03253dcd6ddf1f8dbca635201
adelikat <adelikat@tasvideos.org>
2019-09-22 14:16:45 -0500

    Publications/History page - plumbing

---
+++ TASVideos/Pages/Publications/History.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+
+using AutoMapper.QueryableExtensions;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
+using TASVideos.Pages.Publications.Models;
+using TASVideos.Services;
+
+namespace TASVideos.Pages.Publications
+{
+	[AllowAnonymous]
+	public class HistoryModel : BasePageModel
+	{
+		private readonly ApplicationDbContext _db;
+
+		public HistoryModel(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		[FromRoute]
+		public int Id { get; set; }
+
+		public async Task<IActionResult> OnGet()
+		{
+			var publication = await _db.Publications
+				.ProjectTo<PublicationDisplayModel>()
+				.SingleOrDefaultAsync(p => p.Id == Id);
+
+			if (publication == null)
+			{
+				return NotFound();
+			}
+
+			return Page();
+		}
+	}
+}
