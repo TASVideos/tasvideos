2855c55a3aa4a973756ad4ff434965c7da0b0b56
adelikat <adelikat@tasvideos.org>
2019-09-22 09:06:14 -0500

    publication history - plumbing

---
+++ TASVideos/Services/PublicationHistory.cs
@@ -0,0 +1,22 @@
+﻿using TASVideos.Data;
+
+namespace TASVideos.Services.PublicationChain
+{
+	public interface IPublicationHistory
+	{
+	}
+
+	public class PublicationHistory : IPublicationHistory
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly ICacheService _cache;
+
+		public PublicationHistory(
+			ApplicationDbContext db,
+			ICacheService cache)
+		{
+			_db = db;
+			_cache = cache;
+		}
+	}
+}
