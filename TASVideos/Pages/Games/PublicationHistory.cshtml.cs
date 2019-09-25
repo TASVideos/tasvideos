b65c430a41e351f80d2ae8dec59ef69e49960fa0
adelikat <adelikat@tasvideos.org>
2019-09-24 19:22:04 -0500

    move history page to Games/PublicationHistory

---
+++ TASVideos/Pages/Games/PublicationHistory.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using TASVideos.Data;
+using TASVideos.Data.Entity.Game;
+using TASVideos.Services;
+using TASVideos.Services.PublicationChain;
+
+namespace TASVideos.Pages.Games
+{
+	[AllowAnonymous]
+	public class PublicationHistoryModel : BasePageModel
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly IPublicationHistory _history;
+
+		public PublicationHistoryModel(
+			ApplicationDbContext db,
+			IPublicationHistory history)
+		{
+			_history = history;
+			_db = db;
+		}
+
+		[FromRoute]
+		public int Id { get; set; }
+
+		public PublicationHistoryGroup History { get; set; }
+		public Game Game { get; set; }
+
+		public async Task<IActionResult> OnGet()
+		{
+			Game = await _db.Games.SingleOrDefaultAsync(p => p.Id == Id);
+
+			if (Game == null)
+			{
+				return NotFound();
+			}
+
+			History = await _history.ForGame(Id);
+
+			return Page();
+		}
+	}
+}
