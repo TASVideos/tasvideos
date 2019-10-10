9654fdd224b2c0749d0eba3c0bdcd44e83fae52b
adelikat <adelikat@tasvideos.org>
2019-10-09 20:48:43 -0500

    address nullability in some view models

--- TASVideos/Models/PrivateMessageModel.cs
+++ TASVideos/Models/PrivateMessageModel.cs
@@ -4,15 +4,15 @@ namespace TASVideos.Models
 {
 	public class PrivateMessageModel
 	{
-		public string Subject { get; set; }
+		public string? Subject { get; set; }
 		public DateTime SentOn { get; set; }
-		public string Text { get; set; }
-		public string RenderedText { get; set; }
+		public string Text { get; set; } = "";
+		public string RenderedText { get; set; } = "";
 		public int FromUserId { get; set; }
-		public string FromUserName { get; set; }
+		public string FromUserName { get; set; } = "";
 
 		public int ToUserId { get; set; }
-		public string ToUserName { get; set; }
+		public string ToUserName { get; set; } = "";
 
 		public bool CanReply { get; set; }
 
