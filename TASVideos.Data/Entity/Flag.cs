24bfc530d92b478de0c5a2e40de5ef5177646319
adelikat <adelikat@tasvideos.org>
2019-10-11 15:20:51 -0500

    make some database columns not nullable

--- TASVideos.Data/Entity/Flag.cs
+++ TASVideos.Data/Entity/Flag.cs
@@ -5,7 +5,10 @@ namespace TASVideos.Data.Entity
 	public class Flag
 	{
 		public int Id { get; set; }
-		public string? Name { get; set; }
+
+		[Required]
+		public string Name { get; set; } = "";
+
 		public string? IconPath { get; set; }
 		public string? LinkPath { get; set; }
 
