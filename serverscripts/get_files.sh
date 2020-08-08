ec332de6a3da14c36d2d7e2d249214eefe97a069
root <root@devtest.tasvideos.org>
2020-08-08 18:55:41 +0000

    Updated get_files, now gets files.  Also unzips media files to directory.

--- serverscripts/get_files.sh
+++ serverscripts/get_files.sh
@@ -4,9 +4,12 @@
 source ~/tempconfigfile
 
 # LFTP to the $MAIN_SITE, grabbing $MEDIA_FILE and $MYSQL_FILE and putting them in $DESTINATION_PATH with their names.
-lftp --env-password $MAIN_SITE -e "pget -O $DESTINATION_PATH $MEDIA_FILE -O $DESTINATION_PATH $MYSQL_FILE; bye"
+lftp --env-password $MAIN_SITE -p $MAIN_SITE_PORT -e "set ftp:use-feat false; set ssl-allow false; user $LFTP_USERNAME $LFTP_PASSWORD; pget -O $DESTINATION_PATH $MEDIA_FILE $MYSQL_FILE; bye"
 
 # Import the SQL file.
 
 # Put the media files in the appropriate place.
+unzip -u $DESTINATION_PATH/$MEDIA_FILE -d $MEDIA_DIRECTORY
 
+rm $DESTINATION_PATH/$MEDIA_FILE
+# rm $DESTINATION_PATH/$MYSQL_FILE
