ac3675d4c1000bd0ed784d6941d14bb42688a21b
Adam Michaud <invariel@gmail.com>
2020-08-09 12:55:44 -0400

    Updating start.sh
    Re-committing.

--- serverscripts/start.sh
+++ serverscripts/start.sh
@@ -2,5 +2,5 @@
 source ~/homedir
 
 echo "starting dot net" >> $HOME_DIR/cronlog.txt
-dotnet run --project "$HOME_DIR/tasvideos/TASVideos" --urls "http://127.0.0.1:5000" --environment "Demo"
+dotnet run --project "$HOME_DIR/tasvideos/TASVideos" --urls "http://127.0.0.1:5000" --environment "Demo" --StartupStrategy Minimal
 
