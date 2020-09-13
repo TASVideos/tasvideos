3dfe7dd110d768899e7cd68c5311aefeeee2ef0a
adelikat <adelikat@tasvideos.org>
2020-09-13 11:48:21 -0500

    Subs-List - support legacy status and year tokens

--- TASVideos/Extensions/ServiceCollectionExtensions.cs
+++ TASVideos/Extensions/ServiceCollectionExtensions.cs
@@ -140,6 +140,7 @@ namespace TASVideos.Extensions
 						"/",
 						model => model.Filters.Add(new SetPageViewBagAttribute()));
 					options.Conventions.AddPageRoute("/Games/Index", "{id:int}G");
+					options.Conventions.AddPageRoute("/Submissions/Index", "Subs-{query}");
 					options.Conventions.AddPageRoute("/Submissions/Index", "Subs-List");
 					options.Conventions.AddPageRoute("/Submissions/View", "{id:int}S");
 					options.Conventions.AddPageRoute("/Publications/Index", "Movies-{query}");
---
+++ TASVideos/Pages/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TASVideos.Pages
+{
+	public static class Extensions
+	{
+		public static ICollection<string> ToTokens(this string? routeQuery)
+		{
+			if (string.IsNullOrWhiteSpace(routeQuery))
+			{
+				return new List<string>();
+			}
+
+			return routeQuery
+				.Split(new[] { "-" }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(s => s.Trim(' '))
+				.Where(s => !string.IsNullOrWhiteSpace(s))
+				.Select(s => s.ToLower())
+				.ToList();
+		}
+	}
+}
