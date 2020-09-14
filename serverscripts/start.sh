08931f71b1bcea8a622f2a28d0c2b8865966a374
Adam Michaud <invariel@gmail.com>
2020-09-14 13:11:17 -0400

    Adding error redirection to the start scripts.

--- serverscripts/start.sh
+++ serverscripts/start.sh
@@ -2,5 +2,5 @@
 source ~/homedir
 
 echo "starting dot net" >> $HOME_DIR/cronlog.txt
-dotnet run --project "$HOME_DIR/tasvideos/TASVideos" --urls "http://0.0.0.0:5000" --environment "Demo" --StartupStrategy Minimal
+dotnet run --project "$HOME_DIR/tasvideos/TASVideos" --urls "http://0.0.0.0:5000" --environment "Demo" --StartupStrategy Minimal 2> ~/errorlog.txt
 
