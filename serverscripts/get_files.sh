4cf8190f881d73f2fff0b2307e3a48ad443ee89e
Adam Michaud <invariel@gmail.com>
2020-10-11 12:57:20 -0400

    Adding torrent file information to get_files.

--- serverscripts/get_files.sh
+++ serverscripts/get_files.sh
@@ -6,7 +6,7 @@ source ~/tempconfigfile
 DATE=`date`
 echo \[$DATE\] Downloading files. >> $DESTINATION_PATH/get_files.log
 # LFTP to the $MAIN_SITE, grabbing $MEDIA_FILE and $MYSQL_FILE and putting them in $DESTINATION_PATH with their names.
-lftp --env-password $MAIN_SITE -p $MAIN_SITE_PORT -e "set ftp:use-feat false; set ssl-allow false; user $LFTP_USERNAME $LFTP_PASSWORD; pget -O $DESTINATION_PATH $MEDIA_FILE $MYSQL_FILE; bye"
+lftp --env-password $MAIN_SITE -p $MAIN_SITE_PORT -e "set ftp:use-feat false; set ssl-allow false; user $LFTP_USERNAME $LFTP_PASSWORD; pget -O $DESTINATION_PATH $MEDIA_FILE $MYSQL_FILE $TORRENT_FILE; bye"
 
 DATE=`date`
 echo \[$DATE\] Importing SQL file. >> $DESTINATION_PATH/get_files.log
@@ -20,8 +20,14 @@ echo \[$DATE\] Updating media directory. >> $DESTINATION_PATH/get_files.log
 # Put the media files in the appropriate place.
 unzip -o -j $DESTINATION_PATH/$MEDIA_FILE -d $MEDIA_DIRECTORY
 
+DATE=`date`
+echo \[$DATE\] Updating torrents directory. >> $DESTINATION_PATH/get_files.log
+# Put the torrent files in the appropriate place.
+unzip -o -j $DESTINATION_PATH/$TORRENT_FILE -d $TORRENT_DIRECTORY
+
 rm $DESTINATION_PATH/$MEDIA_FILE
 rm $DESTINATION_PATH/$UNGZ_MYSQL_FILE
+rm $DESTINATION_PATH/$TORRENT_FILE
 
 DATE=`date`
 echo \[$DATE\] Operation finished. >> $DESTINATION_PATH/get_files.log
