cddc101e3b4b024060b44abf979cd33f81b4f1d1
adelikat <adelikat@tasvideos.org>
2020-10-19 16:45:36 -0500

    implement MoviesGameList module, fixes #187

---
+++ TASVideos/ViewComponent/Models/MoviesGameListModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TASVideos.ViewComponents
+{
+	public class MoviesGameListModel
+	{
+		public int? SystemId { get; set; }
+		public string? SystemCode { get; set; }
+
+		public ICollection<GameEntry> Games { get; set; } = new List<GameEntry>();
+		public class GameEntry
+		{
+			public int Id { get; set; }
+			public string Name { get; set; } = "";
+			public ICollection<int> PublicationIds { get; set; } = new List<int>();
+		}
+	}
+}
