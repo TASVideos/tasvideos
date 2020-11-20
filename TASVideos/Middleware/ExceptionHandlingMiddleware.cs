af3c1543218f74833544077847930277659e9d9a
adelikat <adelikat@tasvideos.org>
2020-11-20 12:28:10 -0600

    reorg middleware classes

--- TASVideos/Middleware/ExceptionHandlingMiddleware.cs
+++ TASVideos/Middleware/ExceptionHandlingMiddleware.cs
@@ -10,7 +10,7 @@ using Newtonsoft.Json;
 
 using TASVideos.Extensions;
 
-namespace TASVideos
+namespace TASVideos.Middleware
 {
 	public class ErrorHandlingMiddleware
 	{
--- TASVideos/Middleware/RobotHandlingMiddleware.cs
+++ TASVideos/Middleware/RobotHandlingMiddleware.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
-namespace TASVideos
+namespace TASVideos.Middleware
 {
 	public class RobotHandlingMiddleware
 	{
