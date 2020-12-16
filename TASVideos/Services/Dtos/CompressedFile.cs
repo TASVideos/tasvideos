67600c833b1caf780b619d2984f743edce9f0b6f
adelikat <adelikat@tasvideos.org>
2020-12-16 17:26:09 -0600

    simplify FileService and use a record type

--- TASVideos/Services/Dtos/CompressedFile.cs
+++ TASVideos/Services/Dtos/CompressedFile.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using TASVideos.Data.Entity;
+﻿using TASVideos.Data.Entity;
 
 namespace TASVideos.Services
 {
-	public class CompressedFile
-	{
-		public string FileName { get; init; } = "";
-		public int OriginalSize { get; init; }
-		public int CompressedSize { get; set; }
-		public Compression Type { get; set; }
-
-		public byte[] Data { get; set; } = Array.Empty<byte>();
-	}
+	public record CompressedFile(
+		int OriginalSize,
+		int CompressedSize,
+		Compression Type,
+		byte[] Data);
 }
