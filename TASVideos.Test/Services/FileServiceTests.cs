0030c5ad31bdbb97eeffd965e7307e0bb05b4ce1
adelikat <adelikat@tasvideos.org>
2020-12-19 14:13:52 -0600

    add some more fileservice unit tests

--- TASVideos.Test/Services/FileServiceTests.cs
+++ TASVideos.Test/Services/FileServiceTests.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.IO.Compression;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TASVideos.Data.Entity;
 using TASVideos.Services;
 using TASVideos.Test.MovieParsers;
 
@@ -14,7 +12,7 @@ namespace TASVideos.Test.Services
 	[TestClass]
 	public class FileServiceTests : BaseParserTests
 	{
-		readonly FileService _fileService = new FileService();
+		readonly FileService _fileService = new();
 
 		public override string ResourcesPath { get; } = "TASVideos.Test.MovieParsers.Bk2SampleFiles.";
 		
@@ -41,6 +39,38 @@ namespace TASVideos.Test.Services
 			Assert.AreEqual(1, resultZipArchive.Entries.Count);
 			var entry = resultZipArchive.Entries.Single();
 			Assert.AreEqual(newName, entry.Name);
+		}
+		
+		[TestMethod]
+		public async Task Compress_ReturnsGzipIfSmaller()
+		{
+			// Arrange
+			var bytes = Enumerable.Repeat(0, 100).Select(i => (byte)i).ToArray();
+			
+			// Act
+			var result = await _fileService.Compress(bytes);
+			
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(bytes.Length, result.OriginalSize);
+			Assert.IsTrue(result.CompressedSize < result.OriginalSize);
+			Assert.AreEqual(Compression.Gzip, result.Type);
+		}
+
+		[TestMethod]
+		public async Task Compress_ReturnsUncompressedIfNotSmaller()
+		{
+			// Arrange
+			var bytes = new byte[] { 31, 139, 8, 0, 0, 0, 0, 0, 0, 10 };
+
+			// Act
+			var result = await _fileService.Compress(bytes);
+			
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(bytes.Length, result.OriginalSize);
+			Assert.IsTrue(result.CompressedSize >= result.OriginalSize);
+			Assert.AreEqual(Compression.None, result.Type);
 		}
 	}
 }
