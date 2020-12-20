de8f68f668442ac3927bff1831e79800d65dd46e
adelikat <adelikat@tasvideos.org>
2020-12-19 18:29:54 -0600

    mark some methods static

--- TASVideos.Test/MovieParsers/BaseParserTests.cs
+++ TASVideos.Test/MovieParsers/BaseParserTests.cs
@@ -24,25 +24,25 @@ namespace TASVideos.Test.MovieParsers
 			return stream;
 		}
 
-		protected void AssertNoWarnings(IParseResult result)
+		protected static void AssertNoWarnings(IParseResult result)
 		{
 			Assert.IsNotNull(result.Warnings);
 			Assert.AreEqual(0, result.Warnings.Count());
 		}
 
-		protected void AssertNoErrors(IParseResult result)
+		protected static void AssertNoErrors(IParseResult result)
 		{
 			Assert.IsNotNull(result.Errors);
 			Assert.AreEqual(0, result.Errors.Count());
 		}
 
-		protected void AssertNoWarningsOrErrors(IParseResult result)
+		protected static void AssertNoWarningsOrErrors(IParseResult result)
 		{
 			AssertNoWarnings(result);
 			AssertNoErrors(result);
 		}
 
-		protected bool FrameRatesAreEqual(double expected, double actual)
+		protected static bool FrameRatesAreEqual(double expected, double actual)
 		{
 			return Math.Abs(expected - actual) < 0.0001;
 		}
