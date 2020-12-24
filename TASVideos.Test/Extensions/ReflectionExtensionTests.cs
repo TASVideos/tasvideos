70d151695a620821b199c035f8a852e00f9dc800
adelikat <adelikat@tasvideos.org>
2020-12-24 12:29:55 -0600

    unit tests for ReflectionExtensions

---
+++ TASVideos.Test/Extensions/ReflectionExtensionTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TASVideos.Extensions;
+
+namespace TASVideos.Test.Extensions
+{
+	[TestClass]
+	public class ReflectionExtensionTests
+	{
+		[TestMethod]
+		public void Description_NullReturnsEmpty()
+		{
+			TestEnum? e = null;
+			var actual = e.Description();
+			Assert.AreEqual("", actual);
+		}
+		
+		[TestMethod]
+		public void Description_ReturnsDescription()
+		{
+			TestEnum e = TestEnum.Zero;
+			var actual = e.Description();
+			Assert.AreEqual("00", actual);
+		}
+		
+		[TestMethod]
+		public void Description_DisplayReturnsDisplayDescription()
+		{
+			TestEnum e = TestEnum.One;
+			var actual = e.Description();
+			Assert.AreEqual("01", actual);
+		}
+		
+		[TestMethod]
+		public void Description_Display_NoAttributes_ReturnsName()
+		{
+			TestEnum e = TestEnum.Two;
+			var actual = e.Description();
+			Assert.AreEqual("", actual);
+		}
+		
+		[TestMethod]
+		public void EnumDisplayName_NullReturnsEmpty()
+		{
+			TestEnum? e = null;
+			var actual = e.EnumDisplayName();
+			Assert.AreEqual("", actual);
+		}
+		
+		[TestMethod]
+		public void EnumDisplayName_DisplayReturnDisplay()
+		{
+			TestEnum e = TestEnum.One;
+			var actual = e.EnumDisplayName();
+			Assert.AreEqual("The One", actual);
+		}
+		
+		[TestMethod]
+		public void EnumDisplayName_NoAttribute_ReturnsToString()
+		{
+			TestEnum e = TestEnum.Two;
+			var actual = e.EnumDisplayName();
+			Assert.AreEqual("Two", actual);
+		}
+	}
+}
