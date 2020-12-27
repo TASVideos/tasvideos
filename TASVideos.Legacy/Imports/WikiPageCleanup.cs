a951ff6018e01f7ec8b8bed3679bfbb9e976cd03
adelikat <adelikat@tasvideos.org>
2020-12-27 11:57:13 -0600

    Submission Importer - cleanup some shenaigans.  They are still shenagans, but less error prone at least

--- TASVideos.Legacy/Imports/WikiPageCleanup.cs
+++ TASVideos.Legacy/Imports/WikiPageCleanup.cs
@@ -43,6 +43,16 @@ namespace TASVideos.Legacy.Imports
 			context.SaveChanges();
 		}
 
+		private static string ReplaceCssWithImages(string markup, string cssPath, string imagesPath)
+		{
+			if (markup.Contains(cssPath))
+			{
+				return markup.Replace(cssPath, imagesPath);
+			}
+
+			return markup;
+		}
+
 		private static WikiPage? MarkupShenanigans(WikiPage page, IEnumerable<UserDto> users)
 		{
 			string markup = page.Markup;
@@ -69,16 +79,16 @@ namespace TASVideos.Legacy.Imports
 				markup = markup.Replace("[module:GoogleFlavor]", "");
 			}
 
-			if (markup.Contains("=css/fastest-completion.png")) markup = markup.Replace("=css/fastest-completion.png", "=images/fastest-completion.png");
-			if (markup.Contains("=css/vaulttier.png")) markup = markup.Replace("=css/vaulttier.png", "=images/vaulttier.png");
-			if (markup.Contains("=css/moontier.png")) markup = markup.Replace("=css/moontier.png", "=images/moontier.png");
-			if (markup.Contains("=css/favourite.png")) markup = markup.Replace("=css/favourite.png", "=images/startier.png");
-			if (markup.Contains("=/css/vaulttier.png")) markup = markup.Replace("=/css/vaulttier.png", "=images/vaulttier.png");
-			if (markup.Contains("=/css/moontier.png")) markup = markup.Replace("=/css/moontier.png", "=images/moontier.png");
-			if (markup.Contains("=/css/favourite.png")) markup = markup.Replace("=/css/favourite.png", "=images/startier.png");
-			if (markup.Contains("=/css/newbierec.gif")) markup = markup.Replace("=/css/newbierec.gif", "=images/newbierec.gif");
-			if (markup.Contains("=/css/bolt.png")) markup = markup.Replace("=/css/bolt.png", "=images/notable.png");
-			if (markup.Contains("=/css/verified.png")) markup = markup.Replace("=/css/verified.png", "=images/verified.png");
+			markup = ReplaceCssWithImages(markup, "=css/fastest-completion.png", "=images/fastest-completion.png");
+			markup = ReplaceCssWithImages(markup, "=css/vaulttier.png", "=images/vaulttier.png");
+			markup = ReplaceCssWithImages(markup, "=css/moontier.png", "=images/moontier.png");
+			markup = ReplaceCssWithImages(markup, "=css/favourite.png", "=images/startier.png");
+			markup = ReplaceCssWithImages(markup, "=/css/vaulttier.png", "=images/vaulttier.png");
+			markup = ReplaceCssWithImages(markup, "=/css/moontier.png", "=images/moontier.png");
+			markup = ReplaceCssWithImages(markup, "=/css/favourite.png", "=images/startier.png");
+			markup = ReplaceCssWithImages(markup, "=/css/newbierec.gif", "=images/newbierec.gif");
+			markup = ReplaceCssWithImages(markup, "=/css/bolt.png", "=images/notable.png");
+			markup = ReplaceCssWithImages(markup, "=/css/verified.png", "=images/verified.png");
 
 			// These are automatic now
 			markup = Regex.Replace(markup, "\\[module:gameheader\\]", "", RegexOptions.IgnoreCase);
@@ -99,9 +109,22 @@ namespace TASVideos.Legacy.Imports
 			}
 
 			// Common markup mistakes
-			if (markup.Contains(" [!]")) markup = markup.Replace(" [!]", " [[!]]"); // Non-escaped Rom names, shenanigans to avoid turning proper markup: [[!]] into [[[!]]]
-			if (markup.Contains(")[!]")) markup = markup.Replace(")[!]", "[[!]]"); // Non-escaped Rom names
-			if (markup.Contains("[''''!'''']")) markup = markup.Replace("[''''!'''']", "[[!]]");
+			// Non-escaped Rom names, shenanigans to avoid turning proper markup: [[!]] into [[[!]]]
+			if (markup.Contains(" [!]"))
+			{
+				markup = markup.Replace(" [!]", " [[!]]");
+			}
+
+			// Non-escaped Rom names
+			if (markup.Contains(")[!]"))
+			{
+				markup = markup.Replace(")[!]", "[[!]]");
+			}
+
+			if (markup.Contains("[''''!'''']"))
+			{
+				markup = markup.Replace("[''''!'''']", "[[!]]");
+			}
 
 			// Fix improperly linked homepages
 			var usersWithPages = users.Where(u => u.HomePage != "").ToList();
