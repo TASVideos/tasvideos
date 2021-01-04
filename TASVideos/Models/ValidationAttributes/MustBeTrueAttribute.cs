8ba70b9481064b7bd9d7bb714d714ffa9fdc2a78
adelikat <adelikat@tasvideos.org>
2021-01-04 15:39:40 -0600

    misc cleanups

--- TASVideos/Models/ValidationAttributes/MustBeTrueAttribute.cs
+++ TASVideos/Models/ValidationAttributes/MustBeTrueAttribute.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace TASVideos.Models.ValidationAttributes
 {
 	// https://forums.asp.net/t/2000494.aspx?How+do+I+require+a+checkbox+to+be+checked+
-	[AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
 	public class MustBeTrueAttribute : ValidationAttribute
 	{
 		public override bool IsValid(object? value)
