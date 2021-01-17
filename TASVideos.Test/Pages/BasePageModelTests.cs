ed6cbf79e5b092f7575cf57f7349d4625fedf0df
adelikat <adelikat@tasvideos.org>
2021-01-17 13:25:31 -0600

    fix redirecting of .html urls to the correct wiki page, also add a unit test to cover this scenario, add a few more RenderModel tests as well

---
+++ TASVideos.Test/Pages/BasePageModelTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+
+namespace TASVideos.Test.Pages
+{
+	public class BasePageModelTests
+	{
+		public PageContext TestPageContext()
+		{
+			var httpContext = new DefaultHttpContext();
+			var modelState = new ModelStateDictionary();
+			var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+			var modelMetadataProvider = new EmptyModelMetadataProvider();
+			var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+			return new PageContext(actionContext)
+			{
+				ViewData = viewData
+			};
+		}
+	}
+}
---
+++ TASVideos.Test/Pages/Wiki/RenderModelTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.Wiki;
+using TASVideos.Services;
+
+namespace TASVideos.Test.Pages.Wiki
+{
+	[TestClass]
+	public class RenderModelTests : BasePageModelTests
+	{
+		private Mock<IWikiPages> _mockWikiPages = null!;
+		private TestDbContext _db = null!;
+		private RenderModel _model = null!;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_mockWikiPages = new Mock<IWikiPages>();
+			_db = TestDbContext.Create();
+			_model = new RenderModel(_mockWikiPages.Object, _db)
+			{
+				PageContext = TestPageContext()
+			};
+		}
+
+		[TestMethod]
+		[DataRow(null)]
+		[DataRow("")]
+		[DataRow(" ")]
+		public async Task Render_NullUrl_Redirects(string url)
+		{
+			var result = await _model.OnGet(url);
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(RedirectToPageResult));
+			var redirect = (RedirectToPageResult)result;
+			Assert.AreEqual("/Wiki/PageNotFound", redirect.PageName);
+		}
+
+		[TestMethod]
+		public async Task Render_ExistingPage_FindsPage()
+		{
+			const string existingPage = "Test";
+			_mockWikiPages
+				.Setup(m => m.Page(existingPage, null))
+				.ReturnsAsync(new WikiPage { PageName = existingPage });
+
+			var result = await _model.OnGet(existingPage);
+
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(PageResult));
+		}
+
+		[TestMethod]
+		public async Task Render_HtmlExtension_StillFindsPage()
+		{
+			const string existingPage = "Test";
+			_mockWikiPages
+				.Setup(m => m.Page(existingPage, null))
+				.ReturnsAsync(new WikiPage { PageName = existingPage });
+
+			var result = await _model.OnGet(existingPage + ".html");
+
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOfType(result, typeof(PageResult));
+		}
+	}
+}
