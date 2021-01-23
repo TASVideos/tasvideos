63520df3f07f69f6d1d525618d6c44cabbe555cc
adelikat <adelikat@tasvideos.org>
2021-01-23 13:17:35 -0600

    Revert "enable CA2234 and fix violations"
    This reverts commit 113ad1386cffe418cd7936c55eb45de58a23a237.

--- TASVideos/Services/ExternalMediaPublisher/Distributors/DiscordDistributor.cs
+++ TASVideos/Services/ExternalMediaPublisher/Distributors/DiscordDistributor.cs
@@ -195,8 +195,7 @@ namespace TASVideos.Services.ExternalMediaPublisher.Distributors
 			using HttpClient httpClient = _httpClientFactory.CreateClient("Discord");
 			httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bot", _settings.AccessToken);
 
-			var uri = new Uri($"channels/{(post.Type == PostType.Administrative ? _settings.PrivateChannelId : _settings.PublicChannelId)}/messages");
-			var response = await httpClient!.PostAsync(uri, messageContent, _cancellationTokenSource.Token);
+			var response = await httpClient!.PostAsync($"channels/{(post.Type == PostType.Administrative ? _settings.PrivateChannelId : _settings.PublicChannelId)}/messages", messageContent, _cancellationTokenSource.Token);
 
 			if (!response.IsSuccessStatusCode)
 			{
