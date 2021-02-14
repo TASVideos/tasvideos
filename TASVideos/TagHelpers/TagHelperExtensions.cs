TASVideos/tasvideos

commit 51e5c22947131052b6c9843964f1160b3783d519

TagHelper injection prevention (#298)
Build helper methods for all possible escaping contexts in a taghelper, and then use those helper method in every location where we're manipulating string variables in a taghelper.

It's certainly true that certain instances of interpolation can be staticly known to be safe, but that's a hard and unscalable problem to answer in general; you need to think to every variable, where it came from, what could have touched or edited it, etc.

Here, I forego escaping on things that come from inline string constants like `foo ? "up" : "down"`, as well as things where the resultant string value couldn't possibly be bad at the type level; `int`, `Guid`, `double`, etc.  But just about everything else gets the boot.

TASVideos/TagHelpers/TagHelperExtensions.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Encodings.Web;
-
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -28,6 +30,98 @@ namespace TASVideos.TagHelpers
 			var writer = new StringWriter();
 			content.WriteTo(writer, HtmlEncoder.Default);
 			return writer.ToString();
+		}
+
+		private static readonly Regex ValidAttributeName = new Regex("^[^\t\n\f \\/>\"'=]+$");
+
+		/// <summary>
+		/// Return an HTML attribute <code>name=&quot;value&quot;</code> pair with appropriate escaping.
+		/// </summary>
+		public static string Attr(string name, string value)
+		{
+			if (!ValidAttributeName.IsMatch(name))
+			{
+				throw new ArgumentException($"Attribute name `{name}` contains invalid characters", nameof(name));
+			}
+
+			var sb = new StringBuilder(name.Length + value.Length + 3);
+			sb.Append(name);
+			sb.Append("=\"");
+			foreach (var c in name)
+			{
+				switch (c)
+				{
+					case '<':
+						sb.Append("&lt;");
+						break;
+					case '&':
+						sb.Append("&amp;");
+						break;
+					case '"':
+						sb.Append("&quot;");
+						break;
+					default:
+						sb.Append(c);
+						break;
+				}
+			}
+
+			sb.Append('"');
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Returns raw text HTML escaped suitable for use in a regular element body
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		public static string Text(string text)
+		{
+			var sb = new StringBuilder(text.Length);
+			foreach (var c in text)
+			{
+				switch (c)
+				{
+					case '<':
+						sb.Append("&lt;");
+						break;
+					case '&':
+						sb.Append("&amp;");
+						break;
+					default:
+						sb.Append(c);
+						break;
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		// This is overly restrictive.  If you want to name your js identifiers fÖÖbar, feel free to change it.
+		private static readonly Regex ValidJsIdentifier = new Regex("^[a-zA-Z_$][^[a-zA-Z_$0-9]+$");
+
+		/// <summary>
+		/// Returns a JS identifier suitable for use inside a script tag, after verifying that all characters in it are sane
+		/// </summary>
+		public static string JsIdentifier(string identifier)
+		{
+			if (!ValidJsIdentifier.IsMatch(identifier))
+			{
+				throw new ArgumentException($"Identifier `{identifier}` contains invalid characters", nameof(identifier));
+			}
+
+			return identifier;
+		}
+
+		/// <summary>
+		/// Returns a value serialized to javascript, suitable for inclusion in a script tag.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string JsValue(object? value)
+		{
+			// Newtonsoft by default never escapes `/`; we always escape it to avoid stray </script>s.
+			return Newtonsoft.Json.JsonConvert.SerializeObject(value).Replace("/", "\\/");
 		}
 	}
 }

TASVideos/TagHelpers/BootstrapTagHelpers.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using static TASVideos.TagHelpers.TagHelperExtensions;
 
 namespace TASVideos.TagHelpers
 {
@@ -226,7 +227,7 @@ $@"<button type=""button"" class=""close"" data-dismiss=""alert"" aria-label=""c
 				<button type='button' class='close' data-dismiss='modal'><span aria-hidden='true'>&times;</span></button>
 			</div>
 			<div class='modal-body'>
-				<p>{WarningMessage}</p>
+				<p>{Text(WarningMessage)}</p>
 			</div>
 			<div class='modal-footer'>
 				<form action='{WebUtility.UrlDecode(AspHref)}' method='post'>

TASVideos/TagHelpers/CardLink.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using static TASVideos.TagHelpers.TagHelperExtensions;
 
 namespace TASVideos.TagHelpers
 {
@@ -20,10 +21,10 @@ namespace TASVideos.TagHelpers
 
 			output.Content.AppendHtml($@"
 	<h4 class='card-link-header'>
-		{Header}
+		{Text(Header)}
 	</h4>
 	<span class='card-link-body'>
-		{Body}
+		{Text(Body)}
 	</span>
 	<span class='card-link-arrow fa fa-chevron-right'></span>
 ");

TASVideos/TagHelpers/CollapsableTagHelper.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using static TASVideos.TagHelpers.TagHelperExtensions;
 
 namespace TASVideos.TagHelpers
 {
@@ -19,14 +20,21 @@ namespace TASVideos.TagHelpers
 
 			// TODO: sr and aria tags could use something more informative than BodyId
 			output.Content.AppendHtml($@"
-<a class='collapsed' data-toggle='collapse' href='#{BodyId}' aria-expanded='false' aria-controls='collapse1' role='button'>
-	{content}
-</a>
-<a data-toggle='collapse' class='collapsed btn btn-default btn-xs text-right' href='#{BodyId}' aria-label='Expand/Collapse {BodyId}' aria-expanded='false' role='button'>
-	<i class='fa' aria-hidden='true'></i>
-	<span class='sr-only'>Expand/Collapse {BodyId}</span>
-</a>
-");
+				<a class='collapsed' data-toggle='collapse' {Attr("href", "#" + BodyId)} aria-expanded='false' aria-controls='collapse1' role='button'>
+					{content}
+				</a>
+				<a
+					data-toggle='collapse'
+					class='collapsed btn btn-default btn-xs text-right'
+					{Attr("href", "#" + BodyId)}
+					{Attr("aria-label", $"Expand/Collapse {BodyId}")}
+					aria-expanded='false'
+					role='button'
+				>
+					<i class='fa' aria-hidden='true'></i>
+					<span class='sr-only'>Expand/Collapse {Text(BodyId)}</span>
+				</a>
+			");
 		}
 	}
 

TASVideos/TagHelpers/NavbarTagHelpers.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 using TASVideos.Extensions;
+using static TASVideos.TagHelpers.TagHelperExtensions;
 
 namespace TASVideos.TagHelpers
 {
@@ -52,7 +53,7 @@ namespace TASVideos.TagHelpers
 			}
 
 			output.Content.SetHtmlContent(
-				$"<a href='#' class='nav-link dropdown-toggle' data-toggle='dropdown'>{Name}<span class='caret'></span></a>");
+				$"<a href='#' class='nav-link dropdown-toggle' data-toggle='dropdown'>{Text(Name ?? "")}<span class='caret'></span></a>");
 
 			output.Content.AppendHtml($"<div class='dropdown-menu'>{content}</div>");
 		}

TASVideos/TagHelpers/SortableTableHeadTagHelper.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 using TASVideos.Data;
 using TASVideos.Extensions;
+using static TASVideos.TagHelpers.TagHelperExtensions;
 
 namespace TASVideos.TagHelpers
 {
@@ -69,7 +70,7 @@ namespace TASVideos.TagHelpers
 					}
 
 					output.Content.AppendHtml(
-						$"<a href='{page}?Sort={sortStr}{AdditionalParams()}'>");
+						$"<a {Attr("href", ComputeHref(sortStr))}>");
 					output.Content.AppendHtml(displayName);
 
 					if (isSort)
@@ -90,6 +91,13 @@ namespace TASVideos.TagHelpers
 
 				output.Content.AppendHtml("</th>");
 			}
+		}
+
+		private string ComputeHref(string sortStr)
+		{
+			// TODO: Probably need some URL escaping in here
+			var page = ViewContext.ActionDescriptor.DisplayName;
+			return $"{page}?Sort={sortStr}{AdditionalParams()}";
 		}
 
 		private string AdditionalParams()

TASVideos/TagHelpers/StringListTagHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using static TASVideos.TagHelpers.TagHelperExtensions;
 
 namespace TASVideos.TagHelpers
 {
@@ -37,10 +38,10 @@ namespace TASVideos.TagHelpers
 				output.Content.AppendHtml($@"
 <div class='author-row row mb-1' data-index='{i}'>
 	<div class='col-10'>
-		<input type='text' spellcheck='false' class='form-control' id='{modelId}_{i}_' name='{modelName}' value='{stringList[i]}' />
+		<input type='text' spellcheck='false' class='form-control' {Attr("id", $"{modelId}_{i}_")} {Attr("name", modelName)} {Attr("value", stringList[i])} />
 	</div>
 	<div class='col-2'>
-		<button {(i == 0 ? "id='" + modelId + "-add-btn'" : "")} class='string-list-add-btn btn btn-secondary {(i > 0 ? "d-none" : "")}' type='button'><span class='fa fa-plus-square'></span></button>
+		<button {(i == 0 ? Attr("id", modelId + "-add-btn") : "")} class='string-list-add-btn btn btn-secondary {(i > 0 ? "d-none" : "")}' type='button'><span class='fa fa-plus-square'></span></button>
 		<button onclick='this.parentElement.parentElement.remove()' class='string-list-remove-btn btn btn-danger {(i == 0 ? "d-none" : "")}' type='button'><span class='fa fa-remove'></span></button>
 	</div>
 </div>");
@@ -49,16 +50,16 @@ namespace TASVideos.TagHelpers
 			var uniqueFuncName = "selectList" + context.UniqueId;
 			output.Content.AppendHtml(
 $@"<script>
-	function {uniqueFuncName}() {{
-		var addBtn = document.getElementById('{modelId}-add-btn');
+	function {JsValue(uniqueFuncName)}() {{
+		var addBtn = document.getElementById({JsValue($"{modelId}-add-btn")});
 		addBtn.onclick = function() {{
-			var lastIndex = Math.max.apply(null, document.querySelectorAll('#{parentContainerName} .author-row')
+			var lastIndex = Math.max.apply(null, document.querySelectorAll({JsValue($"#{parentContainerName} .author-row")})
 				.toArray()
 				.map(function(elem) {{
 					return parseInt(elem.getAttribute('data-index'));
 				}}));
 
-			var lastElem = document.querySelector('#{parentContainerName} [data-index=""' + lastIndex + '""]');
+			var lastElem = document.querySelector({JsValue($@"#{parentContainerName} [data-index=""' + lastIndex + '""]")});
 
 			var newIndex = lastIndex + 1;
 			var newElem = lastElem.cloneNode(true);
@@ -73,10 +74,10 @@ $@"<script>
 			var removeBtn = newElem.querySelector('.string-list-remove-btn');
 			removeBtn.classList.remove('d-none');
 
-			document.querySelector('#{parentContainerName} div[class=""string-list-container""]').appendChild(newElem);
+			document.querySelector({JsValue($@"#{parentContainerName} div[class=""string-list-container""]")}).appendChild(newElem);
 		}}
 	}}
-	{uniqueFuncName}();
+	{JsValue(uniqueFuncName)}();
 </script>
 ");
 			output.Content.AppendHtml("</div>");

TASVideos/TagHelpers/TimeZoneConvert.cs
@@ -51,7 +51,7 @@ namespace TASVideos.TagHelpers
 				}
 				catch
 				{
-					// TImeZoneInfo throws an exception if it can not find the timezone
+					// TimeZoneInfo throws an exception if it can not find the timezone
 					// Eat the exception and simply don't convert
 				}
 			}
@@ -61,7 +61,7 @@ namespace TASVideos.TagHelpers
 				: dateTime.ToString(CultureInfo.CurrentCulture);
 			output.TagName = "span";
 			output.TagMode = TagMode.StartTagAndEndTag;
-			output.Content.AppendHtml(dateStr);
+			output.Content.AppendHtml(TagHelperExtensions.Text(dateStr));
 		}
 
 		private void ValidateExpression()

commit 50d09f10215afa38bff1141ff7aa18c0c73998ee

Fix bug in certain tag helpers introduced by 51e5c2294713
Simple typo

TASVideos/TagHelpers/TagHelperExtensions.cs
@@ -47,7 +47,7 @@ namespace TASVideos.TagHelpers
 			var sb = new StringBuilder(name.Length + value.Length + 3);
 			sb.Append(name);
 			sb.Append("=\"");
-			foreach (var c in name)
+			foreach (var c in value)
 			{
 				switch (c)
 				{