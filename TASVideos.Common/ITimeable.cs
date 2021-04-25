053c9f0288457f304c720019500326be8042ae9f
nattthebear <goyuken@gmail.com>
2021-04-25 19:28:41 -0400

    Haha, try and stop me now!
    Probably would be best to not use TimeSpan.ToString("g") at all, since it's not what we want.

--- TASVideos.Common/ITimeable.cs
+++ TASVideos.Common/ITimeable.cs
@@ -18,11 +18,7 @@ namespace TASVideos.Common
 	{
 		public static TimeSpan Time(this ITimeable timeable)
 		{
-			int seconds = (int)(timeable.Frames / timeable.FrameRate);
-			double fractionalSeconds = (timeable.Frames / timeable.FrameRate) - seconds;
-			int milliseconds = (int)(Math.Round(fractionalSeconds, 2) * 1000);
-			var timespan = new TimeSpan(0, 0, 0, seconds, milliseconds);
-			return timespan;
+			return TimeSpan.FromMilliseconds(Math.Round(timeable.Frames / timeable.FrameRate * 100) * 10);
 		}
 	}
 }
