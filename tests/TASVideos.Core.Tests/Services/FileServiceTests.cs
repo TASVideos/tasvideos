9264f13d83c20a09f003bd0fa1cac50eb524f1f6
adelikat <adelikat@tasvideos.org>
2021-05-12 16:12:31 -0500

    fix FileService unit tests

--- tests/TASVideos.Core.Tests/Services/FileServiceTests.cs
+++ tests/TASVideos.Core.Tests/Services/FileServiceTests.cs
@@ -1,76 +1,86 @@
-﻿////using System.IO;
-////using System.IO.Compression;
-////using System.Linq;
-////using System.Threading.Tasks;
-////using Microsoft.VisualStudio.TestTools.UnitTesting;
-////using TASVideos.Core.Services;
-////using TASVideos.Data.Entity;
-////using TASVideos.Test.MovieParsers;
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TASVideos.Core.Services;
+using TASVideos.Data.Entity;
 
-////namespace TASVideos.Core.Tests.Services
-////{
-////	[TestClass]
-////	public class FileServiceTests : BaseParserTests
-////	{
-////		private readonly FileService _fileService = new ();
+namespace TASVideos.Core.Tests.Services
+{
+	[TestClass]
+	public class FileServiceTests
+	{
+		private readonly FileService _fileService = new ();
 
-////		public override string ResourcesPath { get; } = "TASVideos.Test.MovieParsers.Bk2SampleFiles.";
+		private Stream Embedded(string name)
+		{
+			var stream = Assembly.GetAssembly(typeof(FileServiceTests))?.GetManifestResourceStream("TASVideos.Core.Tests.Services.TestFiles." + name);
+			if (stream == null)
+			{
+				throw new InvalidOperationException($"Unable to find embedded resource {name}");
+			}
 
-////		[TestMethod]
-////		public async Task CopyZip_BasicTest()
-////		{
-////			// Arrange
-////			const string newName = "1M";
-////			var stream = Embedded("2Frames.zip");
-////			await using var ms = new MemoryStream();
-////			await stream.CopyToAsync(ms);
-////			var bytes = ms.ToArray();
+			return stream;
+		}
 
-////			// Act
-////			var result = await _fileService.CopyZip(bytes, newName);
+		[TestMethod]
+		public async Task CopyZip_BasicTest()
+		{
+			// Arrange
+			const string newName = "1M";
+			var stream = Embedded("2Frames.zip");
+			await using var ms = new MemoryStream();
+			await stream.CopyToAsync(ms);
+			var bytes = ms.ToArray();
 
-////			// Assert
-////			Assert.IsNotNull(result);
-////			Assert.IsTrue(result.Any());
+			// Act
+			var result = await _fileService.CopyZip(bytes, newName);
 
-////			await using var resultStream = new MemoryStream(result);
-////			using var resultZipArchive = new ZipArchive(resultStream, ZipArchiveMode.Read);
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.IsTrue(result.Any());
 
-////			Assert.AreEqual(1, resultZipArchive.Entries.Count);
-////			var entry = resultZipArchive.Entries.Single();
-////			Assert.AreEqual(newName, entry.Name);
-////		}
+			await using var resultStream = new MemoryStream(result);
+			using var resultZipArchive = new ZipArchive(resultStream, ZipArchiveMode.Read);
 
-////		[TestMethod]
-////		public async Task Compress_ReturnsGzipIfSmaller()
-////		{
-////			// Arrange
-////			var bytes = Enumerable.Repeat(0, 100).Select(i => (byte)i).ToArray();
+			Assert.AreEqual(1, resultZipArchive.Entries.Count);
+			var entry = resultZipArchive.Entries.Single();
+			Assert.AreEqual(newName, entry.Name);
+		}
 
-////			// Act
-////			var result = await _fileService.Compress(bytes);
+		[TestMethod]
+		public async Task Compress_ReturnsGzipIfSmaller()
+		{
+			// Arrange
+			var bytes = Enumerable.Repeat(0, 100).Select(i => (byte)i).ToArray();
 
-////			// Assert
-////			Assert.IsNotNull(result);
-////			Assert.AreEqual(bytes.Length, result.OriginalSize);
-////			Assert.IsTrue(result.CompressedSize < result.OriginalSize);
-////			Assert.AreEqual(Compression.Gzip, result.Type);
-////		}
+			// Act
+			var result = await _fileService.Compress(bytes);
 
-////		[TestMethod]
-////		public async Task Compress_ReturnsUncompressedIfNotSmaller()
-////		{
-////			// Arrange
-////			var bytes = new byte[] { 31, 139, 8, 0, 0, 0, 0, 0, 0, 10 };
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(bytes.Length, result.OriginalSize);
+			Assert.IsTrue(result.CompressedSize < result.OriginalSize);
+			Assert.AreEqual(Compression.Gzip, result.Type);
+		}
 
-////			// Act
-////			var result = await _fileService.Compress(bytes);
+		[TestMethod]
+		public async Task Compress_ReturnsUncompressedIfNotSmaller()
+		{
+			// Arrange
+			var bytes = new byte[] { 31, 139, 8, 0, 0, 0, 0, 0, 0, 10 };
 
-////			// Assert
-////			Assert.IsNotNull(result);
-////			Assert.AreEqual(bytes.Length, result.OriginalSize);
-////			Assert.IsTrue(result.CompressedSize >= result.OriginalSize);
-////			Assert.AreEqual(Compression.None, result.Type);
-////		}
-////	}
-////}
+			// Act
+			var result = await _fileService.Compress(bytes);
+
+			// Assert
+			Assert.IsNotNull(result);
+			Assert.AreEqual(bytes.Length, result.OriginalSize);
+			Assert.IsTrue(result.CompressedSize >= result.OriginalSize);
+			Assert.AreEqual(Compression.None, result.Type);
+		}
+	}
+}
