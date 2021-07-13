b09e8a3409634a62face5cca870cd0dd3c4eb34c
Noxxa <woutvanpoppel@gmail.com>
2021-07-13 02:41:21 +0200

    playerpointstable module (#313)

---
+++ TASVideos/ViewComponents/Models/PlayerPointsModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TASVideos.ViewComponents
+{
+	public class PlayerPointsModel
+	{
+		[Display(Name = "Pos")]
+		public int Position { get; set; } = 0;
+
+		[Display(Name = "PlayerID")]
+		public int ID { get; init; } = 0;
+
+		[Display(Name = "Player")]
+		public string Player { get; init; } = "";
+
+		[Display(Name = "Points")]
+		public double Points { get; set; } = 0.0;
+	}
+}
---
+++ TASVideos/ViewComponents/PlayerPointsTable.cs
@@ -0,0 +1,53 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TASVideos.Core.Services;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.WikiEngine;
+
+namespace TASVideos.ViewComponents
+{
+	[WikiModule(WikiModules.PlayerPointsTable)]
+	public class PlayerPointsTable : ViewComponent
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly IPointsService _pointsService;
+
+		public PlayerPointsTable(ApplicationDbContext db, IPointsService pointsService)
+		{
+			_db = db;
+			_pointsService = pointsService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int? count)
+		{
+			var showCount = count ?? 50;
+
+			var players = await _db.Users
+				.ThatArePublishedAuthors()
+				.Select(u => new PlayerPointsModel
+				{
+					ID = u.Id,
+					Player = u.UserName
+				})
+				.ToListAsync();
+
+			foreach (var user in players)
+			{
+				user.Points = await _pointsService.PlayerPoints(user.ID);
+			}
+
+			var sortedPlayers = players.OrderByDescending(u => u.Points).Take(showCount);
+
+			int counter = 0;
+			foreach (var user in sortedPlayers)
+			{
+				user.Position = ++counter;
+			}
+
+			return View(sortedPlayers);
+		}
+	}
+}
