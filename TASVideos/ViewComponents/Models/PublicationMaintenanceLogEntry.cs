3cd90c015f879d6d85a438315e2412beb6f2815a
adelikat <adelikat@tasvideos.org>
2021-07-19 14:56:25 -0500

    implement MovieMaintLog wiki module

---
+++ TASVideos/ViewComponents/Models/PublicationMaintenanceLogEntry.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace TASVideos.ViewComponents
+{
+	public record PublicationMaintenanceLogEntry(string Log, string UserName, DateTime Timestamp);
+	public record ParentPublicationMaintenanceEntry(int Id, string Title);
+}
