f1da16df2453447a951215520d8f379504e06090
adelikat <adelikat@tasvideos.org>
2021-07-20 12:26:49 -0500

    Publication Tier edit - log to pub maintenance logs, fix external media message, add status message, handle concurrency

---
+++ TASVideos.Core/Services/PublicationMaintenanceLogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Core.Services
+{
+	public interface IPublicationMaintenanceLogger
+	{
+		Task Log(int publicationId, int userId, string log);
+	}
+
+	internal class PublicationMaintenanceLogger : IPublicationMaintenanceLogger
+	{
+		private readonly ApplicationDbContext _db;
+
+		public PublicationMaintenanceLogger(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task Log(int publicationId, int userId, string log)
+		{
+			_db.PublicationMaintenanceLogs.Add(new PublicationMaintenanceLog
+			{
+				TimeStamp = DateTime.UtcNow,
+				PublicationId = publicationId,
+				UserId = userId,
+				Log = log
+			});
+			await _db.SaveChangesAsync();
+		}
+	}
+}
