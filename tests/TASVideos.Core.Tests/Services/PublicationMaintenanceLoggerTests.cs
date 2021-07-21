db65ddf3c3cfb94f27e71b999498298704649dd9
adelikat <adelikat@tasvideos.org>
2021-07-20 19:07:34 -0500

    Publication edit - wire up general edits to publication maintenance

--- tests/TASVideos.Core.Tests/Services/PublicationMaintenanceLoggerTests.cs
+++ tests/TASVideos.Core.Tests/Services/PublicationMaintenanceLoggerTests.cs
@@ -37,5 +37,27 @@ namespace TASVideos.Core.Tests.Services
 			Assert.AreEqual(userId, log.UserId);
 			Assert.AreEqual(logMessage, log.Log);
 		}
+
+		[TestMethod]
+		public async Task Log_Multiple_BasicSuccess()
+		{
+			const int publicationId = 1;
+			const int userId = 2;
+			const string message1 = "Test1";
+			const string message2 = "Test2";
+			_db.Publications.Add(new Publication { Id = publicationId });
+			_db.Users.Add(new User { Id = userId });
+			await _db.SaveChangesAsync();
+
+			await _publicationMaintenanceLogger.Log(publicationId, userId, new[] { message1, message2 });
+			var logs = _db.PublicationMaintenanceLogs.ToList();
+			Assert.AreEqual(2, logs.Count);
+			Assert.IsTrue(logs.Any(l => l.PublicationId == publicationId
+				&& l.UserId == userId
+				&& l.Log == message1));
+			Assert.IsTrue(logs.Any(l => l.PublicationId == publicationId
+				&& l.UserId == userId
+				&& l.Log == message2));
+		}
 	}
 }
