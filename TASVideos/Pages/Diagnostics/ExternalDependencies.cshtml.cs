1d5f400b5ec90a21c99011e2c0207e64863e8353
adelikat <adelikat@tasvideos.org>
2021-07-22 19:51:21 -0500

    diagnostics page to show external dependencies, including the ability to get youtube api access tokens

---
+++ TASVideos/Pages/Diagnostics/ExternalDependencies.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TASVideos.Core.Services.Youtube;
+using TASVideos.Core.Settings;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Pages.Diagnostics
+{
+	[RequirePermission(PermissionTo.SeeDiagnostics)]
+	public class ExternalDependenciesModel : PageModel
+	{
+		private readonly IGoogleAuthService _googleAuthService;
+		private readonly AppSettings _settings;
+
+		public ExternalDependenciesModel(IGoogleAuthService googleAuthService, AppSettings settings)
+		{
+			_googleAuthService = googleAuthService;
+			_settings = settings;
+		}
+
+		public ExternalDependenciesViewModel Statuses { get; set; } = new ();
+
+		public async Task OnGet()
+		{
+
+			Statuses.YoutubeEnabled = _googleAuthService.IsEnabled();
+			if (Statuses.YoutubeEnabled)
+			{
+				try
+				{
+					Statuses.YoutubeAccessSuccessful = !string.IsNullOrWhiteSpace(await _googleAuthService.GetAccessToken());
+				}
+				catch
+				{
+					// Do nothing;
+				}
+			}
+
+			Statuses.IrcEnabled = _settings.Irc.IsEnabled();
+			Statuses.SecureIrcEnabled = _settings.Irc.IsSecureChannelEnabled();
+			Statuses.DiscordEnabled = _settings.Discord.IsEnabled();
+			Statuses.DiscordPrivateChannelEnabled = _settings.Discord.IsPrivateChannelEnabled();
+			Statuses.TwitterEnabled = _settings.Twitter.IsEnabled();
+		}
+
+		public class ExternalDependenciesViewModel
+		{
+			public bool YoutubeEnabled { get; set; }
+			public bool? YoutubeAccessSuccessful { get; set; }
+			public bool IrcEnabled { get; set; }
+			public bool SecureIrcEnabled { get; set; }
+			public bool DiscordEnabled { get; set; }
+			public bool DiscordPrivateChannelEnabled { get; set; }
+			public bool TwitterEnabled { get; set; }
+		}
+	}
+}
