8a128c7594af015a4cfa00c48de6e73d8167473b
adelikat <adelikat@tasvideos.org>
2021-07-24 10:34:29 -0500

    better error handling when google api auth fails

--- TASVideos.Core/Services/Youtube/GoogleAuthService.cs
+++ TASVideos.Core/Services/Youtube/GoogleAuthService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using TASVideos.Core.Services.Youtube.Dtos;
 using TASVideos.Core.HttpClientExtensions;
 using TASVideos.Core.Settings;
@@ -19,16 +20,19 @@ namespace TASVideos.Core.Services.Youtube
 		private readonly HttpClient _client;
 		private readonly ICacheService _cache;
 		private readonly AppSettings _settings;
+		private readonly ILogger<GoogleAuthService> _logger;
 
 		public GoogleAuthService(
 			IHttpClientFactory httpClientFactory,
 			ICacheService cache,
-			AppSettings settings)
+			AppSettings settings,
+			ILogger<GoogleAuthService> logger)
 		{
 			_client = httpClientFactory.CreateClient(HttpClients.GoogleAuth)
 				?? throw new InvalidOperationException($"Unable to initalize {HttpClients.GoogleAuth} client");
 			_cache = cache;
 			_settings = settings;
+			_logger = logger;
 		}
 
 		public bool IsEnabled()
@@ -53,7 +57,14 @@ namespace TASVideos.Core.Services.Youtube
 			}.ToStringContent();
 
 			var response = await _client.PostAsync("token", body);
-			response.EnsureSuccessStatusCode();
+			
+			if (!response.IsSuccessStatusCode)
+			{
+				var errorResponse = await response.Content.ReadAsStringAsync();
+				_logger.LogError("Unable to authorize google apis: " + errorResponse);
+				return "";
+			}
+
 			var tokenResponse = await response.ReadAsync<AccessTokenResponse>();
 
 			// Subtract a bit of time to ensure it does not expire between the time of accessing and using it
