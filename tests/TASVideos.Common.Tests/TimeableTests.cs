19c8b83022006bd4bacf467dae66945ef03ae9a1
Masterjun3 <masterjun3@gmail.com>
2021-09-02 17:11:26 +0200

    change display of movie times to compacted version
    it's now [h:]mm:ss.ff if below a day, otherwise d:hh:mm:ss.ff

--- tests/TASVideos.Common.Tests/TimeableTests.cs
+++ tests/TASVideos.Common.Tests/TimeableTests.cs
@@ -6,21 +6,20 @@ namespace TASVideos.Common.Tests
 	public class TimeableTests
 	{
 		[TestMethod]
-		[DataRow(60.1, 421, "0:00:07")]
-		[DataRow(75, 2346, "0:00:31.28")]
+		[DataRow(60.1, 421, "00:07.00")]
+		[DataRow(75, 2346, "00:31.28")]
 		[DataRow(60, 412521, "1:54:35.35")]
-		[DataRow(58.643, 5213, "0:01:28.89")]
-		[DataRow(62.5, 11221, "0:02:59.54")]
-		[DataRow(50, 6666, "0:02:13.32")]
+		[DataRow(58.643, 5213, "01:28.89")]
+		[DataRow(62.5, 11221, "02:59.54")]
+		[DataRow(50, 6666, "02:13.32")]
 		[DataRow(60.09, 325523, "1:30:17.26")]
-		[DataRow(60, 16997, "0:04:43.28")]
-		[DataRow(60, 0, "0:00:00")]
-		[DataRow(0, 1, "10675199:2:48:05.4775807")]
+		[DataRow(60, 16997, "04:43.28")]
+		[DataRow(60, 0, "00:00.00")]
+		[DataRow(0, 1, "10675199:02:48:05.47")]
 		public void TimeableTimespanTest(double frameRate, int frames, string expected)
 		{
 			var actual = new Timeable { FrameRate = frameRate, Frames = frames }
-				.Time()
-				.ToString("g");
+				.Time().ToStringWithOptionalDaysAndHours();
 			Assert.AreEqual(expected, actual);
 		}
 	}
