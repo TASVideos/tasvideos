4cf9f5143b3c43b82acc50d42577d62307408645
Noxxa <woutvanpoppel@gmail.com>
2021-09-05 23:56:08 +0200

    [Moviechangelog] Implementation of moviechangelog module (tasvideos.org/NewMovies.html)

---
+++ TASVideos/ViewComponents/Models/MovieHistoryModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.ViewComponents
+{
+	public class MovieHistoryModel
+	{
+		public ICollection<MovieHistoryEntry> MovieHistory { get; init; } = new List<MovieHistoryEntry>();
+
+		public class MovieHistoryEntry
+		{
+			public DateTime Date { get; init; }
+			public ICollection<PublicationEntry> Pubs { get; init; } = new List<PublicationEntry>();
+		}
+
+		public class PublicationEntry
+		{
+			public int Id { get; init; }
+			public string Name { get; init; } = "";
+			public bool IsNewGame { get; init; }
+			public bool IsNewBranch { get; init; }
+		}
+	}
+}
