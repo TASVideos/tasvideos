a41737318a9b5f1942712f400f8288e0ce03d9e2
Noxxa <woutvanpoppel@gmail.com>
2021-09-12 18:49:33 +0200

    MovieStatistics module (#412)
    * [MovieStatistics] The ginormous beast that is the MovieStatistics module
    
    * [MovieStatistics] Resolve nullability warnings, use dictionary initializer

---
+++ TASVideos/ViewComponents/Models/MovieGeneralStatisticsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using TASVideos.Common;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.ViewComponents
+{
+	public class MovieGeneralStatisticsModel
+	{
+		public int PublishedMovieCount { get; init; }
+		public int TotalMovieCount { get; init; }
+		public int SubmissionCount { get; init; }
+		public int AverageRerecordCount { get; init; }
+	}
+}
