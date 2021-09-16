49fc5bd1b986a4215fdc0f36ce16e84bcecf1f72
Masterjun3 <masterjun3@gmail.com>
2021-09-16 02:28:42 +0200

    rename width to w and height to h to match old site

--- TASVideos/ViewComponents/Youtube.cs
+++ TASVideos/ViewComponents/Youtube.cs
@@ -6,7 +6,7 @@ namespace TASVideos.ViewComponents
 	[WikiModule(WikiModules.Youtube)]
 	public class Youtube : ViewComponent
 	{
-		public IViewComponentResult Invoke(string v, int? loop, bool hidelink, bool flashblock, int? width, int? height, string? align, int? start)
+		public IViewComponentResult Invoke(string v, int? loop, bool hidelink, bool flashblock, int? w, int? h, string? align, int? start)
 		{
 			var model = new YoutubeModel
 			{
@@ -16,14 +16,14 @@ namespace TASVideos.ViewComponents
 				FlashBlock = flashblock,
 			};
 
-			if (width.HasValue)
+			if (w.HasValue)
 			{
-				model.Width = width.Value;
+				model.Width = w.Value;
 			}
 
-			if (height.HasValue)
+			if (h.HasValue)
 			{
-				model.Height = height.Value;
+				model.Height = h.Value;
 			}
 
 			if (!string.IsNullOrWhiteSpace(align)
