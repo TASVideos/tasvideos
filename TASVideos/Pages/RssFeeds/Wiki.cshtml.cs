69e9650fc7e1980125b63c93e43af6c1acb85caa
adelikat <adelikat@tasvideos.org>
2021-09-24 12:55:25 -0500

    Wiki rss feed

---
+++ TASVideos/Pages/RssFeeds/Wiki.cshtml.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TASVideos.Core.Settings;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.RssFeeds.Models;
+
+namespace TASVideos.Pages.RssFeeds
+{
+	public class WikiModel : PageModel
+	{
+		private readonly ApplicationDbContext _db;
+
+		public WikiModel(ApplicationDbContext db, AppSettings settings)
+		{
+			_db = db;
+			BaseUrl = settings.BaseUrl;
+		}
+
+		public List<RssWiki> WikiEdits { get; set; } = new ();
+		public string BaseUrl { get; set; }
+
+		public async Task<IActionResult> OnGet()
+		{
+			WikiEdits = await _db.WikiPages
+				.ByMostRecent()
+				.Select(wp => new RssWiki
+				{
+					RevisionMessage = wp.RevisionMessage ?? "",
+					PageName = wp.PageName,
+					PubDate = wp.LastUpdateTimestamp,
+					IsNew = wp.Revision == 1,
+					Author = wp.CreateUserName ?? ""
+				})
+				.Take(10)
+				.ToListAsync();
+			PageResult pageResult = Page();
+			pageResult.ContentType = "application/rss+xml; charset=utf-8";
+			return pageResult;
+		}
+	}
+}
