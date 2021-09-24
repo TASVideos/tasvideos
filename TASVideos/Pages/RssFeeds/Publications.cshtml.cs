0e6cf7f39b69b665480f61fd26f5493409d7ccd1
adelikat <adelikat@tasvideos.org>
2021-09-23 21:53:12 -0500

    Implement publications and submissions rss feed

---
+++ TASVideos/Pages/RssFeeds/Publications.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TASVideos.Core.Settings;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.RssFeeds.Models;
+
+namespace TASVideos.Pages.RssFeeds
+{
+	public class PublicationsModel : PageModel
+	{
+		private readonly ApplicationDbContext _db;
+
+		public PublicationsModel(
+			ApplicationDbContext db,
+			AppSettings settings)
+		{
+			_db = db;
+			BaseUrl = settings.BaseUrl;
+		}
+
+		public List<RssPublication> Publications { get; set; } = new ();
+		public string BaseUrl { get; set; }
+		public async Task<IActionResult> OnGet()
+		{
+			var minTimestamp = DateTime.Now.AddDays(-60);
+			Publications = await _db.Publications
+				.ByMostRecent()
+				.Where(p => p.CreateTimestamp >= minTimestamp)
+				.Select(p => new RssPublication
+				{
+					Id = p.Id,
+					MovieFileSize = p.MovieFile.Length,
+					CreateTimestamp = p.CreateTimestamp,
+					Title = p.Title,
+					Wiki = p.WikiContent!,
+					TagNames = p.PublicationTags.Select(pt => pt.Tag!.DisplayName).ToList(),
+					Files = p.Files.Select(pf => new RssPublication.File
+					{
+						Path = pf.Path,
+						Type = pf.Type
+					}).ToList(),
+					StreamingUrls = p.PublicationUrls
+						.Where(pu => pu.Type == PublicationUrlType.Streaming)
+						.Where(pu => pu.Url != null)
+						.Select(pu => pu.Url!)
+						.ToList(),
+					Ratings = p.PublicationRatings
+						.Select(pr => new RssPublication.Rating
+						{
+							Value = pr.Value,
+							Type = pr.Type
+						})
+						.ToList()
+				})
+				.ToListAsync();
+
+			PageResult pageResult = Page();
+			pageResult.ContentType = "application/rss+xml; charset=utf-8";
+			return pageResult;
+		}
+	}
+}
