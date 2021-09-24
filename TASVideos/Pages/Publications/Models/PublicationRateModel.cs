301173daf22396216c65c7a03e1102caa6ad18dc
adelikat <adelikat@tasvideos.org>
2021-09-24 15:28:54 -0500

    publications ratings - server side enforce the rating range, fixes #465

--- TASVideos/Pages/Publications/Models/PublicationRateModel.cs
+++ TASVideos/Pages/Publications/Models/PublicationRateModel.cs
@@ -7,9 +7,11 @@ namespace TASVideos.Pages.Publications.Models
 		public string Title { get; set; } = "";
 
 		[Display(Name = "Tech Rating")]
+		[Range(0, 10.0)]
 		public double? TechRating { get; set; }
 
 		[Display(Name = "Entertainment Rating")]
+		[Range(0, 10.0)]
 		public double? EntertainmentRating { get; set; }
 	}
 }
