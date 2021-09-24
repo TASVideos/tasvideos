582ac083752d2b36ae29a473c5f103f6c116b4e6
adelikat <adelikat@tasvideos.org>
2021-09-24 14:44:52 -0500

    add news.rss feed

---
+++ TASVideos/Pages/RssFeeds/Models/RssNews.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TASVideos.Pages.RssFeeds.Models
+{
+	public class RssNews
+	{
+		public int PostId { get; init; }
+		public DateTime PubDate { get; set; }
+		public string Subject { get; init; } = "";
+		public string Text { get; init; } = "";
+		public bool EnableHtml { get; set; }
+		public bool EnableBbCode { get; set; }
+	}
+}
