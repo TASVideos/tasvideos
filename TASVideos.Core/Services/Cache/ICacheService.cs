8ce7608bbb268d19e0d0f808f0679aad9fbf1702
adelikat <adelikat@tasvideos.org>
2021-09-25 19:04:13 -0500

    break some classes into separate files

---
+++ TASVideos.Core/Services/Cache/ICacheService.cs
@@ -0,0 +1,9 @@
+﻿namespace TASVideos.Core.Services
+{
+	public interface ICacheService
+	{
+		bool TryGetValue<T>(string key, out T value);
+		void Set(string key, object? data, int? cacheTime = null);
+		void Remove(string key);
+	}
+}
---
+++ TASVideos.Core/Services/Cache/NoCacheService.cs
@@ -0,0 +1,19 @@
+﻿namespace TASVideos.Core.Services
+{
+	public class NoCacheService : ICacheService
+	{
+		public bool TryGetValue<T>(string key, out T value)
+		{
+			value = default!;
+			return false;
+		}
+
+		public void Set(string key, object? data, int? cacheTime)
+		{
+		}
+
+		public void Remove(string key)
+		{
+		}
+	}
+}
