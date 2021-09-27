f58868025411c9d7e1dce00861e16d0cdcbcd575
adelikat <adelikat@tasvideos.org>
2021-09-26 20:22:17 -0500

    fbm parser, minus savestate anchored movie support

---
+++ TASVideos.Parsers/Parsers/Fbm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.MovieParsers.Parsers
+{
+	[FileExtension("fbm")]
+	internal class Fbm : ParserBase, IParser
+	{
+		public override string FileExtension => "fbm";
+
+		public async Task<IParseResult> Parse(Stream file)
+		{
+			var result = new ParseResult
+			{
+				Region = RegionType.Ntsc,
+				FileExtension = FileExtension,
+				SystemCode = SystemCodes.Arcade
+			};
+
+			using var br = new BinaryReader(file);
+			var header = new string(br.ReadChars(4));
+			if (header != "FB1 ")
+			{
+				return new ErrorResult("Invalid file format, does not seem to be a .fbm");
+			}
+
+			br.ReadByte(); // Version number
+			var nextHeader = new string(br.ReadChars(4));
+			if (nextHeader == "FS1 ")
+			{
+				return new ErrorResult("Savestate movies not supported yet!");
+			}
+
+			if (nextHeader != "FR1 ")
+			{
+				return new ErrorResult("Input data not found");
+			}
+
+			br.ReadBytes(4); // Size of frame data chunk in bytes (not including the chunk identifier)
+			result.Frames = br.ReadInt32();
+			result.RerecordCount = br.ReadInt32();
+
+			return await Task.FromResult(result);
+		}
+	}
+}
---
+++ tests/TASVideos.MovieParsers.Tests/FbmTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TASVideos.MovieParsers.Parsers;
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.MovieParsers.Tests
+{
+	[TestClass]
+	[TestCategory("FbmParsers")]
+	public class FbmTests : BaseParserTests
+	{
+		private readonly Fbm _fbmParser;
+
+		public override string ResourcesPath { get; } = "TASVideos.MovieParsers.Tests.FbmSampleFiles.";
+
+		public FbmTests()
+		{
+			_fbmParser = new Fbm();
+		}
+
+		[TestMethod]
+		public async Task InvalidHeader()
+		{
+			var result = await _fbmParser.Parse(Embedded("wrongmarker.fbm"));
+			Assert.IsFalse(result.Success);
+			AssertNoWarnings(result);
+			Assert.IsNotNull(result.Errors);
+			Assert.AreEqual(1, result.Errors.Count());
+		}
+
+		[TestMethod]
+		public async Task NoInputSection()
+		{
+			var result = await _fbmParser.Parse(Embedded("missinginput.fbm"));
+			Assert.IsFalse(result.Success);
+			AssertNoWarnings(result);
+			Assert.IsNotNull(result.Errors);
+			Assert.AreEqual(1, result.Errors.Count());
+		}
+
+		[TestMethod]
+		public async Task Rerecords()
+		{
+			var result = await _fbmParser.Parse(Embedded("basictest.fbm"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+			Assert.AreEqual(12298, result.RerecordCount);
+		}
+
+		[TestMethod]
+		public async Task Frames()
+		{
+			var result = await _fbmParser.Parse(Embedded("basictest.fbm"));
+			Assert.IsTrue(result.Success);
+			AssertNoWarningsOrErrors(result);
+			Assert.AreEqual(49064, result.Frames);
+		}
+	}
+}
