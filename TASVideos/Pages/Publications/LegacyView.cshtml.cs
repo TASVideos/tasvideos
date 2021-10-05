afcf8243418e50ee0cfebcb98b9f5362265a7c58
nattthebear <goyuken@gmail.com>
2021-10-05 19:26:49 -0400

    Add some redirects for old {id}S.html and {id}M.html links

---
+++ TASVideos/Pages/Publications/LegacyView.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TASVideos.Pages.Publications
+{
+	// Handles legacy {id}M.html links
+	[AllowAnonymous]
+	public class LegacyViewPageModel : PageModel
+	{
+		[FromRoute]
+		public int Id { get; set; }
+
+		public IActionResult OnGet()
+		{
+			return RedirectPermanent($"{Id}M");
+		}
+	}
+}
