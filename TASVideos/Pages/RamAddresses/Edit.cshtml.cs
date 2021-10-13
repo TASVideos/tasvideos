24b41a6ac63705a80fe27d6b67f0cb48112f2652
adelikat <adelikat@tasvideos.org>
2021-10-12 19:55:25 -0500

    ability to add/edit/delete ram addresses

--- TASVideos/Pages/RamAddresses/Edit.cshtml.cs
+++ TASVideos/Pages/RamAddresses/Edit.cshtml.cs
@@ -1,54 +1,91 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TASVideos.Data;
 using TASVideos.Data.Entity;
 using TASVideos.Data.Entity.Game;
+using TASVideos.Pages.RamAddresses.Models;
 
 namespace TASVideos.Pages.RamAddresses
 {
 	[RequirePermission(PermissionTo.EditRamAddresses)]
-	public class EditModel : PageModel
+	public class EditModel : AddressBasePageModel
 	{
 		private readonly ApplicationDbContext _db;
+		private readonly IMapper _mapper;
 
-		public EditModel(ApplicationDbContext db)
+		public EditModel(ApplicationDbContext db, IMapper mapper)
+			: base(db)
 		{
 			_db = db;
+			_mapper = mapper;
 		}
 
 		[FromRoute]
 		public int Id { get; set; }
 
-		public string GameName { get; set; } = "";
-		public string SystemCode { get; set; } = "";
-
-		public IEnumerable<GameRamAddress> Addresses { get; set; } = new List<GameRamAddress>();
-
 		public async Task<IActionResult> OnGet()
 		{
-			var game = await _db.Games
-				.Include(g => g.System)
-				.Where(g => g.Id == Id)
+			Address = await _mapper.ProjectTo<AddressEditModel>(_db.GameRamAddresses
+				.Include(r => r.System)
+				.Include(r => r.Game)
+				.Where(r => r.Id == Id))
 				.SingleOrDefaultAsync();
 
-			if (game is null)
+			if (Address is null)
 			{
 				return NotFound();
 			}
 
-			GameName = game.DisplayName;
-			SystemCode = game.System!.Code;
-
-			Addresses = await _db.GameRamAddresses
-				.Include(r => r.GameRamAddressDomain)
-				.Where(r => r.GameId == Id)
-				.ToListAsync();
+			await PopulateDropdowns(Address.SystemId);
 
 			return Page();
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				await PopulateDropdowns(Address.SystemId);
+				return Page();
+			}
+
+			var address = await _db.GameRamAddresses
+				.SingleOrDefaultAsync(a => a.Id == Id);
+
+			if (address == null)
+			{
+				return NotFound();
+			}
+
+			await ValidateDomain();
+
+			address.GameRamAddressDomainId = Address.GameRamAddressDomainId;
+			address.Address = Address.Address;
+			address.Type = Address.Type ?? RamAddressType.Byte;
+			address.Signed = Address.Signed ?? RamAddressSigned.Signed;
+			address.Endian = Address.Endian ?? RamAddressEndian.Big;
+			address.Description = Address.Description;
+
+			await ConcurrentSave(_db, "Address successfully updated", "Unable to modify address");
+			return RedirectToList(Address.GameId);
+		}
+
+		public async Task<IActionResult> OnPostDelete()
+		{
+			var address = await _db.GameRamAddresses
+				.SingleOrDefaultAsync(a => a.Id == Id);
+
+			if (address == null)
+			{
+				return NotFound();
+			}
+
+			_db.GameRamAddresses.Remove(address);
+			await ConcurrentSave(_db, $"Address {address.Address} successfully deleted.", "Unable to delete address.");
+			return RedirectToList(address.GameId!.Value);
 		}
 	}
 }
