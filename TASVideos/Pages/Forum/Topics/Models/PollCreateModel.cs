4652e58b1d072427d62cbde5bc3c914942860db5
adelikat <adelikat@tasvideos.org>
2021-10-15 17:48:40 -0500

    Poll Add/Edit - allow setting the multiselect flag

--- TASVideos/Pages/Forum/Topics/Models/PollCreateModel.cs
+++ TASVideos/Pages/Forum/Topics/Models/PollCreateModel.cs
@@ -13,6 +13,9 @@ namespace TASVideos.Pages.Forum.Topics.Models
 		[Display(Name = "Days to Run for", Description = "0 or empty for a never-ending poll")]
 		public int? DaysOpen { get; set; }
 
+		[Display(Name = "Allow Multiple Selections")]
+		public bool MultiSelect { get; set; }
+
 		[Display(Name = "Options")]
 		public IList<string> PollOptions { get; set; } = new List<string> { "", "" };
 
