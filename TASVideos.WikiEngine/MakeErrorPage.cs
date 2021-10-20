0fb36c217984679ad2e63d6a1e5e386fc18cfa3f
nattthebear <goyuken@gmail.com>
2021-10-20 12:50:54 -0400

    More intelligible error display on wiki syntax error
    Closes #574.
    This was painful to write; we need to abstract the AST away from the view layer, and maybe create a stronger view layer.  Or maybe not.

--- TASVideos.WikiEngine/MakeErrorPage.cs
+++ TASVideos.WikiEngine/MakeErrorPage.cs
@@ -13,6 +13,18 @@ namespace TASVideos.WikiEngine
 		/// <returns>Human-readable document showing the markup and the error.</returns>
 		public static List<INode> MakeErrorPage(string content, NewParser.SyntaxException e)
 		{
+			INode ClassedText(int at, string content, string? clazz = null)
+			{
+				var ret = new Element(at, "span");
+				if (clazz != null)
+					ret.Attributes.Add("class", clazz);
+				ret.Children.Add(new Text(at, content));
+				return ret;
+			}
+
+			var lines = content.Split('\n');
+			var linePadding = (lines.Length + 1).ToString().Length;
+
 			var ret = new List<INode>();
 
 			var head = new Element(0, "h1", new[] { new Text(0, "Syntax Error") });
@@ -22,14 +34,34 @@ namespace TASVideos.WikiEngine
 			{
 				Attributes = { ["class"] = "error-code" }
 			};
-			var i = e.TextLocation;
-			elt.Children.Add(new Text(0, content.Substring(0, i)));
-			var marker = new Element(i, "span", new[] { new Element(i, "span", new[] { new Text(i, e.Message) }) })
+
+			var lineNo = 1;
+			var charAt = 0;
+
+			foreach (var line in lines)
 			{
-				Attributes = { ["class"] = "error-marker" }
-			};
-			elt.Children.Add(marker);
-			elt.Children.Add(new Text(i, content.Substring(i)));
+				elt.Children.Add(ClassedText(charAt, $"{lineNo.ToString().PadLeft(linePadding)}. ", "info"));
+				if (e.TextLocation >= charAt && e.TextLocation <= charAt + line.Length)
+				{
+					elt.Children.Insert(0, ClassedText(0, $"Syntax Error on line {lineNo}\n", "info"));
+					var column = e.TextLocation - charAt;
+					elt.Children.Add(ClassedText(charAt, line.Substring(0, column)));
+					var marker = new Element(e.TextLocation, "span", new[] { ClassedText(e.TextLocation, e.Message) })
+					{
+						Attributes = { ["class"] = "error-marker" }
+					};
+					elt.Children.Add(marker);
+					elt.Children.Add(ClassedText(charAt, line.Substring(column) + '\n'));
+				}
+				else
+				{
+					elt.Children.Add(ClassedText(charAt, line + '\n'));
+				}
+
+				charAt += line.Length + 1;
+				lineNo++;
+			}
+
 			ret.Add(elt);
 
 			return ret;
