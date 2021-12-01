9524fd873924763299f6b8aabdf992fbb7ee8ae5
adelikat <adelikat@tasvideos.org>
2021-11-30 20:04:51 -0600

    Diagnostics - log all the levels

--- TASVideos/Pages/Diagnostics/Index.cshtml.cs
+++ TASVideos/Pages/Diagnostics/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 using TASVideos.Data.Entity;
 
 namespace TASVideos.Pages.Diagnostics
@@ -6,12 +7,26 @@ namespace TASVideos.Pages.Diagnostics
 	[RequirePermission(PermissionTo.SeeDiagnostics)]
 	public class IndexModel : BasePageModel
 	{
+		private readonly ILogger<IndexModel> _logger;
+
+		public IndexModel(ILogger<IndexModel> logger)
+		{
+			_logger = logger;
+		}
+
 		public void OnGet()
 		{
 		}
 
 		public void OnPostMake500()
 		{
+			_logger.LogTrace("This is a trace log.");
+			_logger.LogDebug("This is a debug log.");
+			_logger.LogInformation("This in an information log.");
+			_logger.LogWarning("This is a warning log.");
+			_logger.LogError("This is an error log.");
+			_logger.LogCritical("This is a critical log.");
+
 			throw new Exception("Testing 500 exceptions from Diagnostics page.");
 		}
 	}
