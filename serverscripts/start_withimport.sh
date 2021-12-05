c8d62db01c9fcf4cd0051995a8a99fc5da7018c4
adelikat <adelikat@tasvideos.org>
2021-12-05 17:20:34 -0600

    remove another import shell script

--- serverscripts/start_withimport.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ~/homedir
-
-echo "starting dot net with import" >> $HOME_DIR/cronlog.txt
-dotnet run --project "$HOME_DIR/tasvideos/TASVideos" --urls "http://127.0.0.1:5000" --environment "$ENV" --StartupStrategy Import 2> ~/errorlog.txt
