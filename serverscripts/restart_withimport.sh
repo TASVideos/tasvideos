6a510a344bd78968826fcca139d7dc71fac00b3e
adelikat <adelikat@tasvideos.org>
2021-12-05 10:26:54 -0600

    remove import shell scripts that use no longer existing import startup

--- serverscripts/restart_withimport.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ~/homedir
-
-echo "restart_withimport script started" >> $HOME_DIR/cronlog.txt
-kill $(ps aux | grep 'TASVideos.dll' | awk '{print $2}')
-$HOME_DIR/tasvideos/serverscripts/pull.sh >> $HOME_DIR/cronlog.txt
-$HOME_DIR/tasvideos/serverscripts/start_withimport.sh
-
--- serverscripts/screen_restart_withimport.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ~/homedir
-
-echo "starting restart with import" >> $HOME_DIR/cronlog.txt
-date >> $HOME_DIR/cronlog.txt
-screen -X -S tasvideos quit
-screen -dmS tasvideos $HOME_DIR/tasvideos/serverscripts/restart_withimport.sh
-echo "done with restart with import script" >> $HOME_DIR/cronlog.txt
-date >> $HOME_DIR/cronlog.txt
