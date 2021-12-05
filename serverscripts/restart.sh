c3d8f083aaf351956a4c704d34bceda65c3b713e
adelikat <adelikat@tasvideos.org>
2021-12-05 17:49:56 -0600

    remove some more server scripts, we don't do screen stuff and manual restart, and use the service config instead

--- serverscripts/restart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source ~/homedir
-
-echo "restart script started" >> $HOME_DIR/cronlog.txt
-kill $(ps aux | grep 'TASVideos.dll' | awk '{print $2}')
-$HOME_DIR/tasvideos/serverscripts/pull.sh >> $HOME_DIR/cronlog.txt
-$HOME_DIR/tasvideos/serverscripts/start.sh
--- serverscripts/screen_restart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ~/homedir
-
-echo "starting restart script" >> $HOME_DIR/cronlog.txt
-date >> $HOME_DIR/cronlog.txt
-screen -X -S tasvideos quit
-screen -dmS tasvideos $HOME_DIR/tasvideos/serverscripts/restart.sh
-echo "done with restart script" >> $HOME_DIR/cronlog.txt
-date >> $HOME_DIR/cronlog.txt
