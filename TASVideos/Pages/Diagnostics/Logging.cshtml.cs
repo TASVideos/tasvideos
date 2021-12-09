20482576b6d15869fb5306fe2003759a287b202e
adelikat <adelikat@tasvideos.org>
2021-12-08 18:35:32 -0600

    permission restrict Diagnostics/Logging

--- TASVideos/Pages/Diagnostics/Logging.cshtml.cs
+++ TASVideos/Pages/Diagnostics/Logging.cshtml.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TASVideos.Data.Entity;
 
 namespace TASVideos.Pages.Diagnostics
 {
+	[RequirePermission(PermissionTo.SeeDiagnostics)]
 	public class LoggingModel : BasePageModel
 	{
 		private readonly ILogger<ILogger> _logger;
