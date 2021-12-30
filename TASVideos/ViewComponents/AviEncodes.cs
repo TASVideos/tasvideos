7fb75898b1d68f9410c65fdffb5834ea5e9fd6ec
adelikat <adelikat@tasvideos.org>
2021-12-30 16:55:45 -0600

    remove logic for AviEncodes, still todo - remove this wiki module one day

--- TASVideos/ViewComponents/AviEncodes.cs
+++ TASVideos/ViewComponents/AviEncodes.cs
@@ -1,8 +1,5 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TASVideos.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using TASVideos.WikiEngine;
 
 namespace TASVideos.ViewComponents
@@ -10,21 +7,9 @@ namespace TASVideos.ViewComponents
 	[WikiModule(WikiModules.AviEncodes)]
 	public class AviEncodes : ViewComponent
 	{
-		private readonly ApplicationDbContext _db;
-
-		public AviEncodes(ApplicationDbContext db)
+		public IViewComponentResult Invoke()
 		{
-			_db = db;
-		}
-
-		public async Task<IViewComponentResult> InvokeAsync()
-		{
-			var encodes = await _db.PublicationFiles
-				.Where(pf => pf.Path.EndsWith(".avi.torrent"))
-				.Select(pf => new AviEncodeResultModel(pf.PublicationId, pf.Publication!.Title, pf.Path))
-				.ToListAsync();
-
-			return View(encodes);
+			return new ContentViewComponentResult($"{WikiModules.AviEncodes} wiki module no longer supported");
 		}
 	}
 }
