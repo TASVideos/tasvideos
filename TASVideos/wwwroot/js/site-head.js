8192adf5079b4a3ed59dcebbf9c8f53bf5169de6
TiKevin83 <38826675+TiKevin83@users.noreply.github.com>
2022-01-02 10:03:58 -0500

    Fix unstyled content flickering on page load with blocking script (#719)

---
+++ TASVideos/wwwroot/js/site-head.js
@@ -0,0 +1,57 @@
+﻿function forceDarkMode() {
+	let DarkModeStylesheet = document.getElementById("style-dark");
+	if (!DarkModeStylesheet) {
+		removeAutoDarkMode();
+
+		var newElement = document.createElement('link');
+		newElement.rel = "stylesheet";
+		newElement.id = "style-dark";
+		newElement.href = "/css/darkmode.css";
+		document.head.appendChild(newElement);
+
+		localStorage.setItem("style-dark", "true");
+	}
+}
+
+function forceLightMode() {
+	removeForcedDarkMode();
+	removeAutoDarkMode();
+
+	localStorage.setItem("style-dark", "false");
+}
+
+function autoDarkMode() {
+	let initialDarkModeStylesheet = document.getElementById("style-dark-initial");
+	if (!initialDarkModeStylesheet) {
+		removeForcedDarkMode();
+
+		var newElement = document.createElement('link');
+		newElement.rel = "stylesheet";
+		newElement.id = "style-dark-initial";
+		newElement.href = "/css/darkmode-initial.css";
+		document.head.appendChild(newElement);
+
+		localStorage.removeItem("style-dark");
+	}
+}
+
+function removeForcedDarkMode() {
+	let DarkModeStylesheet = document.getElementById("style-dark");
+	if (DarkModeStylesheet) {
+		DarkModeStylesheet.parentElement.removeChild(DarkModeStylesheet);
+	}
+}
+
+function removeAutoDarkMode() {
+	let initialDarkModeStylesheet = document.getElementById("style-dark-initial");
+	if (initialDarkModeStylesheet) {
+		initialDarkModeStylesheet.parentElement.removeChild(initialDarkModeStylesheet);
+	}
+}
+
+if (localStorage.getItem("style-dark") !== null) {
+	removeAutoDarkMode();
+	if (localStorage.getItem("style-dark") === "true") {
+		forceDarkMode();
+	}
+}
--- TASVideos/wwwroot/js/site.js
+++ TASVideos/wwwroot/js/site.js
@@ -16,64 +16,6 @@ function scrollToTop() {
 window.addEventListener("scroll", showHideScrollToTop);
 document.getElementById("button-scrolltop").addEventListener("click", scrollToTop);
 
-function forceDarkMode() {
-	let DarkModeStylesheet = document.getElementById("style-dark");
-	if (!DarkModeStylesheet) {
-		removeAutoDarkMode();
-
-		var newElement = document.createElement('link');
-		newElement.rel = "stylesheet";
-		newElement.id = "style-dark";
-		newElement.href = "/css/darkmode.css";
-		document.head.appendChild(newElement);
-
-		localStorage.setItem("style-dark", "true");
-	}
-}
-
-function forceLightMode() {
-	removeForcedDarkMode();
-	removeAutoDarkMode();
-
-	localStorage.setItem("style-dark", "false");
-}
-
-function autoDarkMode() {
-	let initialDarkModeStylesheet = document.getElementById("style-dark-initial");
-	if (!initialDarkModeStylesheet) {
-		removeForcedDarkMode();
-
-		var newElement = document.createElement('link');
-		newElement.rel = "stylesheet";
-		newElement.id = "style-dark-initial";
-		newElement.href = "/css/darkmode-initial.css";
-		document.head.appendChild(newElement);
-
-		localStorage.removeItem("style-dark");
-	}
-}
-
-function removeForcedDarkMode() {
-	let DarkModeStylesheet = document.getElementById("style-dark");
-	if (DarkModeStylesheet) {
-		DarkModeStylesheet.parentElement.removeChild(DarkModeStylesheet);
-	}
-}
-
-function removeAutoDarkMode () {
-	let initialDarkModeStylesheet = document.getElementById("style-dark-initial");
-	if (initialDarkModeStylesheet) {
-		initialDarkModeStylesheet.parentElement.removeChild(initialDarkModeStylesheet);
-	}
-}
-
-if (localStorage.getItem("style-dark") !== null) {
-	removeAutoDarkMode();
-	if (localStorage.getItem("style-dark") === "true") {
-		forceDarkMode();
-	}
-}
-
 if (location.hash) {
 	let expandButton = document.querySelector(`[href='${location.hash}']`);
 	if (expandButton) {
