045727a723a8091c7d82c6897e8e64b3fc7fd739
adelikat <adelikat@tasvideos.org>
2022-01-02 11:31:05 -0600

    You add to AllowedUserNameCharacters.  This fixes some really weird behavior.  Also fix email setting

--- TASVideos/Pages/Profile/Settings.cshtml.cs
+++ TASVideos/Pages/Profile/Settings.cshtml.cs
@@ -84,7 +84,7 @@ namespace TASVideos.Pages.Profile
 			var user = await _userManager.GetUserAsync(User);
 
 			var email = user.Email;
-			if (string.Equals(Settings.Email, email, StringComparison.CurrentCultureIgnoreCase))
+			if (!string.Equals(Settings.Email, email, StringComparison.CurrentCultureIgnoreCase))
 			{
 				var setEmailResult = await _userManager.SetEmailAsync(user, Settings.Email);
 
