e8fd81993c8de5cb78cf560d337ab3ebb92a994d
TiKevin83 <38826675+TiKevin83@users.noreply.github.com>
2022-01-04 18:19:45 -0500

    Add movie ID to front of forum movie title helper (#783)
    * Add movie ID to front of forum movie title helper
    
    * respond to PR comment

--- TASVideos.Core/Services/ForumWriterHelper.cs
+++ TASVideos.Core/Services/ForumWriterHelper.cs
@@ -14,7 +14,12 @@ namespace TASVideos.Core.Services
 			_db = db;
 		}
 
-		public async Task<string?> GetMovieTitle(int id) => (await _db.Publications.FirstOrDefaultAsync(p => p.Id == id))?.Title;
+		public async Task<string?> GetMovieTitle(int id)
+		{
+			var publication = await _db.Publications.FirstOrDefaultAsync(p => p.Id == id);
+			return publication is not null ? $"[{publication.Id}] {publication.Title}" : null;
+		}
+
 		public async Task<string?> GetSubmissionTitle(int id) => (await _db.Submissions.FirstOrDefaultAsync(s => s.Id == id))?.Title;
 	}
 }
