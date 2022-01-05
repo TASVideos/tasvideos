17bd2042298395e31a772a3d5f6d58e5d7d53c2e
adelikat <adelikat@tasvideos.org>
2022-01-05 09:07:04 -0600

    make minimum hours before judgement 72, was 1 for testing and we forgot to put it back

--- TASVideos.Common/Constants.cs
+++ TASVideos.Common/Constants.cs
@@ -31,7 +31,7 @@
 		public const int VestedPostCount = 3; // Minimum number of posts to become an experienced forum user
 
 		// TODO: Change this back to 72, but it helps for testing to be low
-		public const int MinimumHoursBeforeJudgment = 1; // Minimum number of hours before a judge can set a submission to accepted/rejected
+		public const int MinimumHoursBeforeJudgment = 72; // Minimum number of hours before a judge can set a submission to accepted/rejected
 
 		public const string TASVideoAgent = "TASVideoAgent";
 		public const int TASVideoAgentId = 505;
