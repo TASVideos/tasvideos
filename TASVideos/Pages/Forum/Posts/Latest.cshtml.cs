664bde5fbbf9121179f92128e9be9f0838c83625
Masterjun3 <masterjun3@gmail.com>
2022-01-06 04:42:28 +0100

    first working version of a page showing the latest posts

---
+++ TASVideos/Pages/Forum/Posts/Latest.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Core;
+using TASVideos.Core.Services;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.Data.Entity.Forum;
+using TASVideos.Pages.Forum.Posts.Models;
+
+namespace TASVideos.Pages.Forum.Posts
+{
+	[AllowAnonymous]
+	public class LatestModel : BasePageModel
+	{
+		private readonly ApplicationDbContext _db;
+		private readonly IAwards _awards;
+
+		public LatestModel(
+			ApplicationDbContext db,
+			IAwards awards)
+		{
+			_db = db;
+			_awards = awards;
+		}
+
+		[FromQuery]
+		public PagingModel Search { get; set; } = new ();
+
+		public PageOf<LatestPostsModel> Posts { get; set; } = PageOf<LatestPostsModel>.Empty();
+
+		public async Task OnGet()
+		{
+			var allowRestricted = User.Has(PermissionTo.SeeRestrictedForums);
+			var numberOfPosts = 100;
+			Posts = await _db.ForumPosts
+				.ExcludeRestricted(allowRestricted)
+				.Select(p => new LatestPostsModel
+				{
+					Id = p.Id,
+					CreateTimestamp = p.CreateTimestamp,
+					LastUpdateTimestamp = p.LastUpdateTimestamp,
+					EnableBbCode = p.EnableBbCode,
+					EnableHtml = p.EnableHtml,
+					Text = p.Text,
+					Subject = p.Subject,
+					TopicId = p.TopicId ?? 0,
+					TopicTitle = p.Topic!.Title,
+					ForumId = p.Topic.ForumId,
+					ForumName = p.Topic!.Forum!.Name,
+					PosterId = p.PosterId,
+					PosterName = p.Poster!.UserName,
+					PosterRoles = p.Poster.UserRoles
+						.Where(ur => !ur.Role!.IsDefault)
+						.Select(ur => ur.Role!.Name)
+						.ToList(),
+					PosterLocation = p.Poster.From,
+					Signature = p.Poster.Signature,
+					PosterAvatar = p.Poster.Avatar,
+					PosterJoined = p.Poster.CreateTimestamp,
+					PosterPostCount = p.Poster.Posts.Count,
+					PosterMoodUrlBase = p.Poster.MoodAvatarUrlBase,
+					PosterMood = p.PosterMood,
+					PosterPronouns = p.Poster.PreferredPronouns
+				})
+				.OrderByDescending(p => p.CreateTimestamp)
+				.Take(numberOfPosts)
+				.PageOf(Search);
+
+			foreach (var post in Posts)
+			{
+				post.Awards = await _awards.ForUser(post.PosterId);
+			}
+		}
+	}
+}
