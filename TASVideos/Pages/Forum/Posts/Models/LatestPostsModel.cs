db2637964c36b00ab94c426f0bfd72dadb19b094
Masterjun <masterjun3@gmail.com>
2022-01-07 04:17:12 +0100

    first working version of a page showing the latest posts (#830)

---
+++ TASVideos/Pages/Forum/Posts/Models/LatestPostsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TASVideos.Pages.Forum.Posts.Models
+{
+	public class LatestPostsModel : ForumPostEntry
+	{
+		public string TopicTitle { get; set; } = "";
+		public int ForumId { get; set; }
+		public string ForumName { get; set; } = "";
+	}
+}
