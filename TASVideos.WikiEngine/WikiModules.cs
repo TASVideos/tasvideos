76ea81ead93ee31cce56858954587ad6f2cb5843
adelikat <adelikat@tasvideos.org>
2022-01-09 19:21:24 -0600

    rename wiki module nogamename to just nogame

--- TASVideos.WikiEngine/WikiModules.cs
+++ TASVideos.WikiEngine/WikiModules.cs
@@ -32,7 +32,7 @@ namespace TASVideos.WikiEngine
 		public const string MoviesList = "movieslist";
 		public const string MovieStatistics = "moviestatistics";
 		public const string MovieMaintenanceLog = "moviemaintlog";
-		public const string NoGameName = "nogamename";
+		public const string NoGameName = "nogame";
 		public const string NoRom = "norom";
 		public const string PlatformAuthorList = "platformtaserlists";
 		public const string PlatformFramerates = "platformframerates";
