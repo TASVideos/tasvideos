45c52c6e3b07990d619445dd7a7abe0808b9523b
adelikat <adelikat@tasvideos.org>
2022-01-11 19:55:58 -0600

    replace some annoying author list generating logic with a taghelper that does the work

---
+++ TASVideos/TagHelpers/AuthorListTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using TASVideos.Extensions;
+
+namespace TASVideos.TagHelpers
+{
+	public class AuthorListTagHelper : TagHelper
+	{
+		public IEnumerable<string>? Authors { get; set; } = new List<string>();
+		public string? AdditionalAuthors { get; set; }
+
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "span";
+			output.Content.SetHtmlContent(GetAuthorString());
+		}
+
+		private string GetAuthorString()
+		{
+			var authors = Authors ?? Enumerable.Empty<string>();
+			return string.Join(", ", authors.Concat((AdditionalAuthors ?? "").SplitWithEmpty(","))).LastCommaToAmpersand();
+		}
+	}
+}
