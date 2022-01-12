32d2a9fe5def287e08d3a98c3f686e4ac1841516
Masterjun <masterjun3@gmail.com>
2022-01-12 16:54:24 +0100

    Add "Show more" taghelper and use it for Author Notes in submission topics (#901)
    * implement showmore taghelper and use for submission topics
    
    * add hide button and scroll accordingly
    
    * account for dynamic height change e.g. image embeds

---
+++ TASVideos/TagHelpers/ShowMoreTagHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers
+{
+	public class ShowMoreTagHelper : TagHelper
+	{
+		public string MaxHeight { get; set; } = "none";
+		public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+		{
+			var content = (await output.GetChildContentAsync()).GetContent();
+			output.TagName = "div";
+			output.Content.SetHtmlContent($@"<div style=""overflow-y: scroll; max-height: {MaxHeight};"" id=""{context.UniqueId}"">{content}</div>");
+			output.Content.AppendHtml($@"<div class=""p-2 text-center d-none border border-primary rounded"" id=""show-{context.UniqueId}""><a href=""#"" onclick=""return false;""><h4 class=""m-0""><i class=""fa fa-chevron-down""></i> Show more</h4></a></div>");
+			output.Content.AppendHtml($@"<div class=""p-2 text-center d-none border border-primary rounded"" id=""hide-{context.UniqueId}""><a href=""#"" onclick=""return false;""><h4 class=""m-0""><i class=""fa fa-chevron-up""></i> Hide</h4></a></div>");
+			output.Content.AppendHtml($@"
+<script>
+	{{
+		let content = document.getElementById(""{context.UniqueId}"");
+		if (content.scrollHeight > content.clientHeight)
+			{{
+				let show = document.getElementById('show-{context.UniqueId}');
+				let hide = document.getElementById('hide-{context.UniqueId}');
+				let height = content.style.maxHeight;
+				let clHeight = content.clientHeight;
+				content.style.overflowY = 'hidden'
+				show.classList.remove('d-none');
+				show.onclick = function()
+				{{
+					content.style.overflowY = null;
+					content.style.maxHeight = null;
+					show.classList.add('d-none');
+					hide.classList.remove('d-none');
+				}}
+				hide.onclick = function()
+				{{
+					window.scrollBy({{top: clHeight - content.scrollHeight, left: 0, behavior: 'instant'}});
+					content.style.overflowY = 'hidden';
+					content.style.maxHeight = height;
+					hide.classList.add('d-none');
+					show.classList.remove('d-none');
+				}}
+			}}
+	}}
+</script>");
+		}
+	}
+}
