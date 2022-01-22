f6d910a52667c3e9a0c0e4ef0c815fc54dc0ca02
nattthebear <goyuken@gmail.com>
2022-01-22 15:05:22 -0500

    Round timespans to nearest hundredth for display (#1036)
    * round timespans to nearest hundreth for display
    
    * Fix better

--- TASVideos.Common/Extensions/TimeExtensions.cs
+++ TASVideos.Common/Extensions/TimeExtensions.cs
@@ -4,26 +4,6 @@ namespace TASVideos.Extensions
 {
 	public static class TimeExtensions
 	{
-		public static string ToCondensedString(this TimeSpan timeSpan)
-		{
-			if (timeSpan.Days > 0)
-			{
-				return timeSpan.ToString("d\\.hh\\:mm\\:ss\\.ff");
-			}
-
-			if (timeSpan.Hours > 0)
-			{
-				return timeSpan.ToString("h\\:mm\\:ss\\.ff");
-			}
-
-			if (timeSpan.Minutes > 0)
-			{
-				return timeSpan.ToString("m\\:ss\\.ff");
-			}
-
-			return timeSpan.ToString("s\\.ff");
-		}
-
 		public static int DaysAgo(this DateTime startDate)
 		{
 			TimeSpan elapsed = DateTime.UtcNow.Subtract(startDate);
