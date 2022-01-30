d977965288a563533aece21d5e6cf14c57799a25
adelikat <adelikat@tasvideos.org>
2022-01-30 11:53:32 -0600

    re-introduce points service tests, InMemoryDatabase is fixed in .net 6 yay

--- tests/TASVideos.Core.Tests/Services/PointsServiceTests.cs
+++ tests/TASVideos.Core.Tests/Services/PointsServiceTests.cs
@@ -1,77 +1,68 @@
-﻿/*
-// As of v5.0.10, These tests error on a query that the in memory database can not project, but a real database provider can
-// Try these out on a newer in memory nuget package and see if the bug is fixed
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TASVideos.Core.Services;
+﻿using TASVideos.Core.Services;
 using TASVideos.Data;
 using TASVideos.Data.Entity;
-using TASVideos.Tests.Base;
 
-namespace TASVideos.Test.Services
+namespace TASVideos.Test.Services;
+
+[TestClass]
+public class PointsServiceTests
 {
-	[TestClass]
-	public class PointsServiceTests
+	private readonly IPointsService _pointsService;
+	private readonly ApplicationDbContext _db;
+	private static User Player => new () { UserName = "Player" };
+
+	public PointsServiceTests()
 	{
-		private readonly IPointsService _pointsService;
-		private readonly ApplicationDbContext _db;
-		private static User Player => new () { UserName = "Player" };
+		_db = TestDbContext.Create();
+		_pointsService = new PointsService(_db, new NoCacheService());
+	}
 
-		public PointsServiceTests()
+	[TestMethod]
+	public async Task PlayerPoints_NoUser_Returns0()
+	{
+		var actual = await _pointsService.PlayerPoints(int.MinValue);
+		Assert.AreEqual(0, actual);
+	}
+
+	[TestMethod]
+	public async Task PlayerPoints_UserWithNoMovies_Returns0()
+	{
+		_db.Users.Add(Player);
+		await _db.SaveChangesAsync();
+		var user = _db.Users.Single();
+		var actual = await _pointsService.PlayerPoints(user.Id);
+		Assert.AreEqual(0, actual);
+	}
+
+	[TestMethod]
+	public async Task PlayerPoints_NoRatings_MinimumPointsReturned()
+	{
+		int numMovies = 2;
+
+		_db.Users.Add(Player);
+		var publicationClass = new PublicationClass { Weight = 1, Name = "Test" };
+		_db.PublicationClasses.Add(publicationClass);
+		for (int i = 0; i < numMovies; i++)
 		{
-			_db = TestDbContext.Create();
-			_pointsService = new PointsService(_db, new NoCacheService());
+			_db.Publications.Add(new Publication { PublicationClass = publicationClass });
 		}
 
-		[TestMethod]
-		public async Task PlayerPoints_NoUser_Returns0()
-		{
-			var actual = await _pointsService.PlayerPoints(int.MinValue);
-			Assert.AreEqual(0, actual);
-		}
-
-		[TestMethod]
-		public async Task PlayerPoints_UserWithNoMovies_Returns0()
-		{
-			_db.Users.Add(Player);
-			await _db.SaveChangesAsync();
-			var user = _db.Users.Single();
-			var actual = await _pointsService.PlayerPoints(user.Id);
-			Assert.AreEqual(0, actual);
-		}
-
-		[TestMethod]
-		public async Task PlayerPoints_NoRatings_MinimumPointsReturned()
-		{
-			int numMovies = 2;
-
-			_db.Users.Add(Player);
-			var publicationClass = new PublicationClass { Weight = 1, Name = "Test" };
-			_db.PublicationClasses.Add(publicationClass);
-			for (int i = 0; i < numMovies; i++)
+		await _db.SaveChangesAsync();
+		var user = _db.Users.Single();
+		var pa = _db.Publications
+			.ToList()
+			.Select(p => new PublicationAuthor
 			{
-				_db.Publications.Add(new Publication { PublicationClass = publicationClass });
-			}
+				PublicationId = p.Id,
+				UserId = user.Id
+			})
+			.ToList();
 
-			await _db.SaveChangesAsync();
-			var user = _db.Users.Single();
-			var pa = _db.Publications
-				.ToList()
-				.Select(p => new PublicationAuthor
-				{
-					PublicationId = p.Id,
-					UserId = user.Id
-				})
-				.ToList();
+		_db.PublicationAuthors.AddRange(pa);
+		await _db.SaveChangesAsync();
 
-			_db.PublicationAuthors.AddRange(pa);
-			await _db.SaveChangesAsync();
-
-			var actual = await _pointsService.PlayerPoints(user.Id);
-			int expected = numMovies * PlayerPointConstants.MinimumPlayerPointsForPublication;
-			Assert.AreEqual(expected, actual);
-		}
+		var actual = await _pointsService.PlayerPoints(user.Id);
+		int expected = numMovies * PlayerPointConstants.MinimumPlayerPointsForPublication;
+		Assert.AreEqual(expected, actual);
 	}
 }
-*/
