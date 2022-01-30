91d0420279c6e6aae219b55af4b110b324ffa35f
adelikat <adelikat@tasvideos.org>
2022-01-29 22:38:52 -0600

    implicit usings on api project, doesn't really gain much except less boilerplate in the global usings file

--- TASVideos.Api/GlobalUsings.cs
+++ TASVideos.Api/GlobalUsings.cs
@@ -1,8 +1,4 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using System.Threading.Tasks;
-global using Microsoft.AspNetCore.Authorization;
+﻿global using Microsoft.AspNetCore.Authorization;
 global using Microsoft.AspNetCore.Mvc;
 global using TASVideos.Core.Services;
 global using TASVideos.Extensions;
