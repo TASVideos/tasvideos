f9f1ed3aaeadad5ef10b3f59d46c5664a9a967ed
adelikat <adelikat@tasvideos.org>
2022-01-29 22:55:33 -0600

    implicit usings in parsers project

--- TASVideos.Parsers/GlobalUsings.cs
+++ TASVideos.Parsers/GlobalUsings.cs
@@ -1,6 +1,1 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.IO;
-global using System.IO.Compression;
-global using System.Linq;
-global using System.Threading.Tasks;
+﻿global using System.IO.Compression;
