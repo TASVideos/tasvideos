182d58ce1cfba560770fcd7c217fb1fe89a619e5
adelikat <adelikat@tasvideos.org>
2022-01-30 12:16:03 -0600

    use top-level statements for WikiEngine.Snapshots

--- tests/TASVideos.WikiEngine.Snapshots/Program.cs
+++ tests/TASVideos.WikiEngine.Snapshots/Program.cs
@@ -2,9 +2,10 @@
 using Newtonsoft.Json;
 using TASVideos.Data;
 using TASVideos.Data.Entity;
+using TASVideos.WikiEngine;
 using TASVideos.WikiEngine.AST;
 
-namespace TASVideos.WikiEngine.Snapshots;
+//namespace TASVideos.WikiEngine.Snapshots;
 
 /*
 	1. Clone https://github.com/nattthebear/TASVideosWikiSnaps.git to somewhere
@@ -19,119 +20,114 @@ namespace TASVideos.WikiEngine.Snapshots;
 		--filter [string]:  Only process pages whose title contains [string].
 			Used to quickly observe changes to a single page during development.  Before committing to the snapshot repo, all other pages should be run.
  */
-public class Program
+
+if (!File.Exists(".params.json"))
 {
-	internal class Options
+	Console.WriteLine(".params.json not found");
+	return -1;
+}
+
+var settings = JsonConvert.DeserializeObject<Options>(File.ReadAllText(".params.json"));
+if (string.IsNullOrWhiteSpace(settings?.ConnectionString) || string.IsNullOrWhiteSpace(settings.OutDir))
+{
+	Console.WriteLine("Need to set values in .params.json");
+	return -1;
+}
+
+
+var serviceProvider = new ServiceCollection()
+	.AddTasvideosData(settings.ConnectionString)
+	.BuildServiceProvider();
+
+var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+
+var wantUpdate = args.Any(s => s == "--update");
+var force = args.Any(s => s == "--force");
+string? filter = null;
+{
+	var index = Array.IndexOf(args, "--filter");
+	if (index >= 0 && index < args.Length - 1)
 	{
-		public string? ConnectionString { get; init; }
-		public string? OutDir { get; init; }
+		filter = args[index + 1];
+	}
+}
+
+var query = context.WikiPages
+	.ThatAreNotDeleted()
+	.WithNoChildren();
+if (filter != null)
+{
+	query = query.Where(wp => wp.PageName.Contains(filter));
+}
+
+var toProcess = query
+	.Select(wp => new
+	{
+		wp.PageName,
+		wp.Revision
+	})
+	.ToList();
+Console.WriteLine($"Found {toProcess.Count} current page revisions.");
+
+var progress = 0;
+Console.Write("{0,8}", progress);
+
+foreach (var wp in toProcess)
+{
+	Console.Write(new string('\b', 8));
+	Console.Write("{0,8}", progress++);
+
+	var directory = Path.Combine(settings.OutDir, wp.PageName);
+	new DirectoryInfo(directory).Create();
+	var path = Path.Combine(directory, "content");
+
+	var existingRevision = -1;
+	if (File.Exists(path))
+	{
+		using var tr = new StreamReader(path);
+		var result = int.TryParse(tr.ReadLine(), out int parsedRevision);
+		if (result)
+		{
+			existingRevision = parsedRevision;
+		}
 	}
 
-	public static int Main(string[] args)
+	var revision = wantUpdate ? wp.Revision : existingRevision != -1 ? existingRevision : wp.Revision;
+
+	if (existingRevision == revision && !force)
+		continue;
+
+	var markup = context.WikiPages
+		.Where(p => p.Revision == revision && p.PageName == wp.PageName)
+		.Select(p => p.Markup)
+		.Single();
+
+	List<INode> nodes;
+
+	try
 	{
-		if (!File.Exists(".params.json"))
-		{
-			Console.WriteLine(".params.json not found");
-			return -1;
-		}
-
-		var settings = JsonConvert.DeserializeObject<Options>(File.ReadAllText(".params.json"));
-		if (string.IsNullOrWhiteSpace(settings?.ConnectionString) || string.IsNullOrWhiteSpace(settings.OutDir))
-		{
-			Console.WriteLine("Need to set values in .params.json");
-			return -1;
-		}
-
-
-		var serviceProvider = new ServiceCollection()
-			.AddTasvideosData(settings.ConnectionString)
-			.BuildServiceProvider();
-
-		var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
-
-		var wantUpdate = args.Any(s => s == "--update");
-		var force = args.Any(s => s == "--force");
-		string? filter = null;
-		{
-			var index = Array.IndexOf(args, "--filter");
-			if (index >= 0 && index < args.Length - 1)
-			{
-				filter = args[index + 1];
-			}
-		}
-
-		var query = context.WikiPages
-			.ThatAreNotDeleted()
-			.WithNoChildren();
-		if (filter != null)
-		{
-			query = query.Where(wp => wp.PageName.Contains(filter));
-		}
-
-		var toProcess = query
-			.Select(wp => new
-			{
-				wp.PageName,
-				wp.Revision
-			})
-			.ToList();
-		Console.WriteLine($"Found {toProcess.Count} current page revisions.");
-
-		var progress = 0;
-		Console.Write("{0,8}", progress);
-
-		foreach (var wp in toProcess)
-		{
-			Console.Write(new string('\b', 8));
-			Console.Write("{0,8}", progress++);
-
-			var directory = Path.Combine(settings.OutDir, wp.PageName);
-			new DirectoryInfo(directory).Create();
-			var path = Path.Combine(directory, "content");
-
-			var existingRevision = -1;
-			if (File.Exists(path))
-			{
-				using var tr = new StreamReader(path);
-				var result = int.TryParse(tr.ReadLine(), out int parsedRevision);
-				if (result)
-				{
-					existingRevision = parsedRevision;
-				}
-			}
-
-			var revision = wantUpdate ? wp.Revision : existingRevision != -1 ? existingRevision : wp.Revision;
-
-			if (existingRevision == revision && !force)
-				continue;
-
-			var markup = context.WikiPages
-				.Where(p => p.Revision == revision && p.PageName == wp.PageName)
-				.Select(p => p.Markup)
-				.Single();
-
-			List<INode> nodes;
-
-			try
-			{
-				nodes = NewParser.Parse(markup);
-			}
-			catch (NewParser.SyntaxException e)
-			{
-				nodes = Builtins.MakeErrorPage(markup, e);
-			}
-
-			using var tw = new StreamWriter(path);
-			tw.WriteLine(revision);
-			foreach (var node in nodes)
-			{
-				node.DumpContentDescriptive(tw, "");
-			}
-		}
-
-		Console.Write(new string('\b', 8));
-		Console.Write("{0,8}", progress++);
-		Console.WriteLine();
-		return 0;
+		nodes = NewParser.Parse(markup);
 	}
+	catch (NewParser.SyntaxException e)
+	{
+		nodes = Builtins.MakeErrorPage(markup, e);
+	}
+
+	using var tw = new StreamWriter(path);
+	tw.WriteLine(revision);
+	foreach (var node in nodes)
+	{
+		node.DumpContentDescriptive(tw, "");
+	}
+}
+
+Console.Write(new string('\b', 8));
+Console.Write("{0,8}", ++progress);
+Console.WriteLine();
+return 0;
+
+internal class Options
+{
+	public string? ConnectionString { get; init; }
+	public string? OutDir { get; init; }
 }
