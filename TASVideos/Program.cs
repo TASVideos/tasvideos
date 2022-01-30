2442aab326599efc662b6ce26787f66ef31a4857
adelikat <adelikat@tasvideos.org>
2022-01-30 12:09:01 -0600

    razor pages project - use top-level statements instead of program class

--- TASVideos/Program.cs
+++ TASVideos/Program.cs
@@ -1,51 +1,44 @@
 ﻿using Microsoft.AspNetCore;
 using Serilog;
+using TASVideos;
 using TASVideos.Core.Data;
 
-namespace TASVideos;
+var host = BuildWebHost(args);
 
-public class Program
+using (var scope = host.Services.CreateScope())
 {
-	public static async Task Main(string[] args)
-	{
-		var host = BuildWebHost(args);
+	var env = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+	var configuration = new ConfigurationBuilder()
+		.AddJsonFile("appsettings.json")
+		.AddJsonFile($"appsettings.{env.EnvironmentName ?? "Development"}.json", true)
+		.Build();
+	Log.Logger = new LoggerConfiguration()
+		.ReadFrom.Configuration(configuration)
+		.CreateLogger();
 
-		using (var scope = host.Services.CreateScope())
+	var services = scope.ServiceProvider;
+
+	await DbInitializer.InitializeDatabase(services);
+}
+
+await host.RunAsync();
+
+static IWebHost BuildWebHost(string[] args)
+{
+	var config = new ConfigurationBuilder()
+		.AddCommandLine(args)
+		.Build();
+
+	return WebHost.CreateDefaultBuilder(args)
+		.UseConfiguration(config)
+		.UseStartup<Startup>()
+		.UseSerilog()
+		.ConfigureAppConfiguration((_, builder) =>
 		{
-			var env = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
-			var configuration = new ConfigurationBuilder()
-				.AddJsonFile("appsettings.json")
-				.AddJsonFile($"appsettings.{env.EnvironmentName ?? "Development"}.json", true)
-				.Build();
-			Log.Logger = new LoggerConfiguration()
-				.ReadFrom.Configuration(configuration)
-				.CreateLogger();
-
-			var services = scope.ServiceProvider;
-
-			await DbInitializer.InitializeDatabase(services);
-		}
-
-		await host.RunAsync();
-	}
-
-	public static IWebHost BuildWebHost(string[] args)
-	{
-		var config = new ConfigurationBuilder()
-			.AddCommandLine(args)
-			.Build();
-
-		return WebHost.CreateDefaultBuilder(args)
-			.UseConfiguration(config)
-			.UseStartup<Startup>()
-			.UseSerilog()
-			.ConfigureAppConfiguration((hostContext, builder) =>
-			{
-					// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
-					// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
-					// Manually specify the secret id (matching the csproj) here as a workaround.
-					builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
-			})
-			.Build();
-	}
+				// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
+				// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
+				// Manually specify the secret id (matching the csproj) here as a workaround.
+				builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
+		})
+		.Build();
 }
