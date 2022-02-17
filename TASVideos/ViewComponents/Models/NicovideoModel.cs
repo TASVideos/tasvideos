1eaadf3c25a4cc409eee840522a9b6bd89774d30
Masterjun3 <masterjun3@gmail.com>
2022-02-17 21:45:44 +0100

    add nicovideo module

---
+++ TASVideos/ViewComponents/Models/NicovideoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TASVideos.ViewComponents;
+
+public class NicovideoModel
+{
+	public string Code { get; set; } = "";
+	public int Width { get; set; } = 728;
+	public int Height { get; set; } = 410;
+	public string Align { get; set; } = "";
+	public bool HideLink { get; set; }
+}
---
+++ TASVideos/ViewComponents/Nicovideo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TASVideos.WikiEngine;
+
+namespace TASVideos.ViewComponents;
+
+[WikiModule(WikiModules.Nicovideo)]
+public class Nicovideo : ViewComponent
+{
+	public IViewComponentResult Invoke(string v, bool hideLink, int? w, int? h, string? align)
+	{
+		var model = new NicovideoModel
+		{
+			Code = v,
+			HideLink = hideLink,
+		};
+
+		if (w.HasValue)
+		{
+			model.Width = w.Value;
+		}
+
+		if (h.HasValue)
+		{
+			model.Height = h.Value;
+		}
+
+		if (!string.IsNullOrWhiteSpace(align)
+			&& (align.ToLower() == "left" || align.ToLower() == "right"))
+		{
+			model.Align = align.ToLower();
+		}
+
+		return View(model);
+	}
+}
