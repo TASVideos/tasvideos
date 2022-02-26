c007a28d2196e1f5b2c60e1c7d0ea3573d60eeaf
adelikat <adelikat@tasvideos.org>
2022-02-26 10:45:01 -0600

    move mood avatar generation logic to the Avatar class, hopefully we can consolidate other similar logic into one place

--- TASVideos.Core/Dtos/AvatarUrls.cs
+++ TASVideos.Core/Dtos/AvatarUrls.cs
@@ -1,7 +1,16 @@
-﻿namespace TASVideos.Core.Services;
+﻿using TASVideos.Data.Entity.Forum;
+
+namespace TASVideos.Core.Services;
 
 public record AvatarUrls(string? Avatar, string? MoodBase)
 {
 	public bool HasMoods => !string.IsNullOrWhiteSpace(MoodBase);
 	public bool HasAvatar => !HasMoods && !string.IsNullOrWhiteSpace(Avatar);
+
+	public string ToMoodUrl(ForumPostMood mood)
+	{
+		return MoodBase == null
+			? ""
+			: MoodBase.Replace("$", ((int)mood).ToString());
+	}
 }
