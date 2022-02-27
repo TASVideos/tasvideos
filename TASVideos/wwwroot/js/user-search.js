602b55640e89a2bfacdece28fafa70fc5accd806
adelikat <adelikat@tasvideos.org>
2022-02-27 09:31:58 -0600

    combine user search logic on Users/List and Create PM, and put in separate .js file

---
+++ TASVideos/wwwroot/js/user-search.js
@@ -0,0 +1,36 @@
+﻿function searchUsers(searchBoxElemId) {
+	const searchBoxElem = document.getElementById(searchBoxElemId);
+	const formElem = searchBoxElem.closest("form");
+	const submitBtnElem = formElem.querySelector("button[type='Submit']");
+	searchBoxElem.onkeyup = function () {
+		const searchVal = searchBoxElem.value.trim();
+		const dataListId = `search-username-list-${searchVal}`;
+		if (searchVal.length > 2) {
+			const existingList = document.getElementById(dataListId);
+			if (existingList) {
+				searchBoxElem.setAttribute("list", dataListId);
+				return;
+			}
+
+			window.fetch(`/Users/List/Search/?partial=${searchVal}`)
+				.then(r => r.json())
+				.then(data => {
+					const newSearchList = document.createElement("datalist");
+					newSearchList.id = dataListId;
+					for (const i in data) {
+						if (data.hasOwnProperty(i)) {
+							const option = document.createElement("option");
+							option.innerHTML = data[i];
+							newSearchList.appendChild(option);
+						}
+					}
+
+					formElem.appendChild(newSearchList);
+					searchBoxElem.setAttribute("list", dataListId);
+					submitBtnElem.removeAttribute("disabled");
+				});
+		} else {
+			submitBtnElem.setAttribute("disabled", "disabled");
+		}
+	}
+}
