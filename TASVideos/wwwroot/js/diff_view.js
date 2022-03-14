b7ed7e8c108d0e7958825261133469907d3bcc0d
nattthebear <goyuken@gmail.com>
2022-03-14 17:42:34 -0400

    Add context size to diff viewer (#1151)
    * wip of contextsize on diff engine
    
    * add basic expand
    
    * connect up
    
    * less ugly

--- TASVideos/wwwroot/js/diff_view.js
+++ TASVideos/wwwroot/js/diff_view.js
@@ -1,10 +1,80 @@
-function renderDiff(from, to, el, inline) {
+function renderDiff(from, to, destEl, inline, contextSize) {
 	const dmp = new diff_match_patch();
 	dmp.Diff_Timeout = 0;
 	dmp.Diff_EditCost = 20;
 
 	const d = dmp.diff_main(from.text, to.text);
 	dmp.diff_cleanupEfficiency(d);
+
+	const allLines = [];
+
+	{
+		let leftLineNumber = 1;
+		let rightLineNumber = 1;
+		let spans = [];
+		function flush() {
+			allLines.push({
+				leftLineNumber,
+				rightLineNumber,
+				spans,
+				important: spans.some(s => s.type),
+				distance: 99999999,
+			});
+		}
+		for (const { 0: type, 1: text } of d) {
+			const textParts = text.split("\n");
+			for (let i = 0; i < textParts.length; i++) {
+				spans.push({ type, text: textParts[i] });
+				if (i < textParts.length - 1) {
+					flush();
+					spans = [];
+					if (type <= 0) { leftLineNumber++; }
+					if (type >= 0) { rightLineNumber++; }
+				}
+			}
+		}
+		if (spans.length) {
+			flush();
+		}
+	}
+
+	{
+		for (let i = 0; i < allLines.length; i++) {
+			const line = allLines[i];
+			if (line.important) {
+				line.distance = 0;
+			} else if (i > 0) {
+				line.distance = allLines[i - 1].distance + 1;
+			}
+		}
+		for (let i = allLines.length - 1; i >= 0; i--) {
+			const line = allLines[i];
+			if (!line.important && i < allLines.length - 1) {
+				line.distance = Math.min(line.distance, allLines[i + 1].distance + 1);
+			}
+		}
+	}
+
+	const condensedLines = [];
+
+	{
+		let condensedSet = [];
+		function flush() {
+			if (condensedSet.length) {
+				condensedLines.push(condensedSet);
+				condensedSet = [];
+			}
+		}
+		for (const line of allLines) {
+			if (line.distance <= contextSize) {
+				flush();
+				condensedLines.push(line);
+			} else {
+				condensedSet.push(line);
+			}
+		}
+		flush();
+	}
 
 	function h(tag, ...args) {
 		const e = document.createElement(tag);
@@ -39,107 +109,85 @@ function renderDiff(from, to, el, inline) {
 		);
 	}
 
-	class LineCounter {
-		number = 0;
-		renderedNumber = 0;
-		start() {
-			if (this.number === 0) {
-				this.increment();
+	{
+		let leftNumber;
+		let rightNumber;
+
+		function pushLine(line, classSuffix) {
+			function pushSpans(filter) {
+				const spans = line.spans.filter(filter)
+					.map(({ type, text }) => h("span", { class: type > 0 ? "add" : type < 0 ? "delete" : "keep" }, text));
+				if (spans.length) {
+					results.push(h("div", { class: "line" + classSuffix }, spans));
+				} else {
+					results.push(h("div"));
+				}
 			}
-		}
-		increment() { this.number++; }
-		render() {
-			if (this.renderedNumber !== this.number) {
-				this.renderedNumber = this.number;
-				return h("div", { class: "linenumber" }, this.number);
+
+			if (leftNumber !== line.leftLineNumber) {
+				results.push(h("div", { class: "linenumber" + classSuffix }, leftNumber = line.leftLineNumber));
 			} else {
-				return h("div");
+				results.push(h("div"));
+			}
+			if (!inline) {
+				pushSpans(span => span.type <= 0);
+			}
+			if (rightNumber !== line.rightLineNumber) {
+				results.push(h("div", { class: "linenumber" + classSuffix }, rightNumber = line.rightLineNumber));
+			} else {
+				results.push(h("div"));
+			}
+			pushSpans(span => inline || span.type >= 0);
+		}
+
+		for (let i = 0; i < condensedLines.length; i++) {
+			const lineOrSet = condensedLines[i];
+	
+			if (Array.isArray(lineOrSet)) {
+				results.push(h("button", { class: "expander", type: "button" }, "⋯"));
+				results.push(h("button", { class: "contracter top", type: "button" }, "▼"));
+				leftNumber = undefined;
+				rightNumber = undefined;
+				for (const line of lineOrSet) {
+					pushLine(line, " expanded");
+				}
+				results.push(h("button", { class: "contracter bottom", type: "button" }, "▲"))
+				leftNumber = undefined;
+				rightNumber = undefined;
+			} else {
+				pushLine(lineOrSet, "");
 			}
 		}
 	}
 
-	if (inline) {
-		let oldl = new LineCounter;
-		let newl = new LineCounter;
-		const line = [];
-		function flush() {
-			results.push(oldl.render(), newl.render(), h("div", { class: "line" }, line));
-			line.length = 0;
-		}
-
-		for (const { 0: type, 1: text } of d) {
-			const clazz = type > 0 ? "add" : type < 0 ? "delete" : "keep";
-			const parts = text.split("\n");
-			if (type <= 0) { oldl.start(); }
-			if (type >= 0) { newl.start(); }
-			let first = true;
-			for (const part of parts) {
-				if (first) {
-					first = false;
-				} else {
-					line.push("\n");
-					flush();
-					if (type <= 0) { oldl.increment(); }
-					if (type >= 0) { newl.increment(); }
+	destEl.innerHTML = "";
+	destEl.appendChild(h("div", { class: inline ? "diff inline" : "diff sidebyside" }, results));
+	destEl.children[0].addEventListener("click", (event) => {
+		const { target } = event;
+		const { classList } = target;
+		if (classList.contains("expander")) {
+			target.style.display = "none";
+			let element = target;
+			while (!element.classList.contains("bottom")) {
+				element = element.nextElementSibling;
+				element.style.display = "unset";
+			}
+		} else if (classList.contains("contracter")) {
+			target.style.display = "";
+			if (classList.contains("top")) {
+				target.previousElementSibling.style.display = "";
+				let element = target;
+				while (!element.classList.contains("bottom")) {
+					element = element.nextElementSibling;
+					element.style.display = "";
 				}
-				line.push(h("span", { class: clazz }, part));
-			}
-		}
-		if (line.length) {
-			flush();
-		}
-	} else {
-		class SideBySideLine {
-			counter = new LineCounter;
-			line = [];
-			text(text, clazz) {
-				this.counter.start();
-				this.line.push(h("span", { class: clazz }, text));
-			}
-			flush() {
-				if (this.line.length) {
-					const ret = [
-						this.counter.render(),
-						h("div", { class: "line" }, this.line),
-					];
-					this.line.length = 0;
-					return ret;
-				} else {
-					return [
-						h("div"),
-						h("div"),
-					];
+			} else {
+				let element = target;
+				while (!element.classList.contains("expander")) {
+					element = element.previousElementSibling;
+					element.style.display = "";
 				}
 			}
 		}
-		const left = new SideBySideLine;
-		const right = new SideBySideLine;
-		
-		function flush() {
-			results.push(...left.flush(), ...right.flush());
-		}
-
-		for (const { 0: type, 1: text } of d) {
-			const clazz = type > 0 ? "add" : type < 0 ? "delete" : "keep";
-			const parts = text.split("\n");
-			let first = true;
-			for (const part of parts) {
-				if (first) {
-					first = false;
-				} else {
-					flush();
-					if (type <= 0) { left.counter.increment(); }
-					if (type >= 0) { right.counter.increment(); }
-				}
-				if (type <= 0) { left.text(part, clazz); }
-				if (type >= 0) { right.text(part, clazz); }
-			}
-		}
-		if (left.line.length || right.line.length) {
-			flush();
-		}
-	}
-
-	el.innerHTML = "";
-	el.appendChild(h("div", { class: inline ? "diff inline" : "diff sidebyside" }, results));
+	}, { passive: true });
 }
