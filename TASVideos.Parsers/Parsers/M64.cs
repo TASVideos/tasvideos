94fd1beb2f2ec7b5222283e0753d928abd4e811b
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-03-15 22:38:39 -0700

    m64 parser

---
+++ TASVideos.Parsers/Parsers/M64.cs
@@ -0,0 +1,56 @@
+﻿using TASVideos.MovieParsers.Extensions;
+using TASVideos.MovieParsers.Result;
+
+namespace TASVideos.MovieParsers.Parsers;
+
+[FileExtension("m64")]
+internal class M64 : ParserBase, IParser
+{
+	public override string FileExtension => "m64";
+
+	public async Task<IParseResult> Parse(Stream file)
+	{
+		var result = new ParseResult
+		{
+			Region = RegionType.Ntsc,
+			FileExtension = FileExtension,
+			SystemCode = SystemCodes.N64
+		};
+
+		using var br = new BinaryReader(file);
+		var header = new string(br.ReadChars(4));
+		if (header != "M64\u001a")
+		{
+			return new ErrorResult("Invalid file format, does not seem to be a .m64");
+		}
+
+		br.ReadUInt32(); // Version id
+		br.ReadInt32(); // Movie uid and unix time of recording
+		result.Frames = (int)br.ReadUInt32(); // Vertical interrupts
+		result.RerecordCount = (int)br.ReadUInt32();
+		var fps = br.ReadByte(); // Vertical interrupts per second
+		if (fps == 50)
+		{
+			result.Region = RegionType.Pal;
+		}
+
+		br.ReadByte(); // Number of controllers
+		br.ReadUInt16(); // Reserved
+		br.ReadUInt32(); // Number of input samples
+		var type = br.ReadByte();
+		if (type.Bit(0))
+		{
+			result.StartType = MovieStartType.Savestate;
+		}
+		else if (type.Bit(1))
+		{
+			result.StartType = MovieStartType.PowerOn;
+		}
+		else if (type.Bit(2))
+		{
+			result.StartType = MovieStartType.Sram;
+		}
+
+		return await Task.FromResult(result);
+	}
+}
---
+++ tests/TASVideos.MovieParsers.Tests/M64Tests.cs
@@ -0,0 +1,105 @@
+﻿namespace TASVideos.MovieParsers.Tests;
+
+[TestClass]
+[TestCategory("M64Parsers")]
+public class M64Tests : BaseParserTests
+{
+	private readonly M64 _m64Parser;
+
+	public override string ResourcesPath { get; } = "TASVideos.MovieParsers.Tests.M64SampleFiles.";
+
+	public M64Tests()
+	{
+		_m64Parser = new M64();
+	}
+
+	[TestMethod]
+	public async Task InvalidHeader()
+	{
+		var result = await _m64Parser.Parse(Embedded("wrongheader.m64"));
+		Assert.IsFalse(result.Success);
+		AssertNoWarnings(result);
+		Assert.IsNotNull(result.Errors);
+		Assert.AreEqual(1, result.Errors.Count());
+	}
+
+	[TestMethod]
+	public async Task ValidHeader()
+	{
+		var result = await _m64Parser.Parse(Embedded("2frames.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+	}
+
+	[TestMethod]
+	public async Task System()
+	{
+		var result = await _m64Parser.Parse(Embedded("2frames.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(SystemCodes.N64, result.SystemCode);
+	}
+
+	[TestMethod]
+	public async Task RerecordCount()
+	{
+		var result = await _m64Parser.Parse(Embedded("2frames.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(1, result.RerecordCount);
+	}
+
+	[TestMethod]
+	public async Task Ntsc()
+	{
+		var result = await _m64Parser.Parse(Embedded("2frames.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(RegionType.Ntsc, result.Region);
+	}
+
+	[TestMethod]
+	public async Task Pal()
+	{
+		var result = await _m64Parser.Parse(Embedded("pal.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(RegionType.Pal, result.Region);
+	}
+
+	[TestMethod]
+	public async Task Length()
+	{
+		var result = await _m64Parser.Parse(Embedded("2frames.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(2, result.Frames);
+	}
+
+	[TestMethod]
+	public async Task PowerOn()
+	{
+		var result = await _m64Parser.Parse(Embedded("2frames.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(MovieStartType.PowerOn, result.StartType);
+	}
+
+	[TestMethod]
+	public async Task Sram()
+	{
+		var result = await _m64Parser.Parse(Embedded("sram.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(MovieStartType.Sram, result.StartType);
+	}
+
+	[TestMethod]
+	public async Task Savestate()
+	{
+		var result = await _m64Parser.Parse(Embedded("savestate.m64"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(MovieStartType.Savestate, result.StartType);
+	}
+}
