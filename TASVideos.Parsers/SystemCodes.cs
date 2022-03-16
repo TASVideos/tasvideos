9448a4feef10459c1e429b767bd45d1f79795c22
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2022-03-16 16:02:21 -0700

    m64 parser (#1156)

--- TASVideos.Parsers/SystemCodes.cs
+++ TASVideos.Parsers/SystemCodes.cs
@@ -24,6 +24,7 @@ internal class SystemCodes
 	public const string Lynx = "lynx";
 	public const string Msx = "msx";
 	public const string N3ds = "3ds";
+	public const string N64 = "N64";
 	public const string Ngb = "ngb";
 	public const string Nes = "nes";
 	public const string Odyssey2 = "o2";
