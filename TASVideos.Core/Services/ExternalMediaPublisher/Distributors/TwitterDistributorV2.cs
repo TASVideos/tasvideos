3e99dea693413202e3f0ed2dbe320cf9e8022373
adelikat <adelikat@tasvideos.org>
2022-03-26 12:48:34 -0500

    Twitter v2 - correctly populate the tweet with the title or body depending on postgroup, include the link and account for tweet limitations.  Also a few cleanups

--- TASVideos.Core/Services/ExternalMediaPublisher/Distributors/TwitterDistributorV2.cs
+++ TASVideos.Core/Services/ExternalMediaPublisher/Distributors/TwitterDistributorV2.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using MimeKit;
 using TASVideos.Core.HttpClientExtensions;
 using TASVideos.Core.Services.Cache;
 using TASVideos.Core.Settings;
@@ -23,7 +25,8 @@ public class TwitterDistributorV2 : IPostDistributor
 	private const int RefreshTokenDuration = 2 * 60 * 60 - 30;	// Two hours minus thirty seconds in seconds.  How long the retrieved access token will last.
 
 	public TwitterDistributorV2 (
-		RedisCacheService redisCache,		// Intentionally using Redis Cache here, if we need to turn Redis off, come up with a new solution.  -- Invariel, March 2022.
+		// Intentionally using Redis Cache here, if we need to turn Redis off, come up with a new solution.  -- Invariel, March 2022.
+		RedisCacheService redisCache,
 		AppSettings appSettings,
 		IHttpClientFactory httpClientFactory,
 		ILogger<TwitterDistributorV2> logger)
@@ -52,7 +55,7 @@ public class TwitterDistributorV2 : IPostDistributor
 
 		var tweetData = new
 		{
-			text = post.Body
+			text = GenerateTwitterMessage(post)
 		};
 
 		var response = await _twitterClient.PostAsync("", tweetData.ToStringContent());
@@ -80,23 +83,23 @@ public class TwitterDistributorV2 : IPostDistributor
 	{
 		var keys = _redisCacheService.GetAll<string>(new List<string>
 		{
-			TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_KEY,
-			TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_TIME_KEY
+			TwitterDistributorConstants.RefreshToken,
+			TwitterDistributorConstants.RefreshTokenTime
 		});
 
-		if (!keys.ContainsKey(TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_KEY)
-			|| string.IsNullOrWhiteSpace(keys[TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_KEY]))
+		if (!keys.ContainsKey(TwitterDistributorConstants.RefreshToken)
+			|| string.IsNullOrWhiteSpace(keys[TwitterDistributorConstants.RefreshToken]))
 		{
-			_logger.LogError("Unable to initialize twitter, missing value {token}", TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_KEY);
+			_logger.LogError("Unable to initialize twitter, missing value {token}", TwitterDistributorConstants.RefreshToken);
 			return;
 		}
 
-		_refreshToken = keys[TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_KEY];
+		_refreshToken = keys[TwitterDistributorConstants.RefreshToken];
 		_nextRefreshTime = DateTime.UtcNow.AddDays(-1);
-		if (keys.ContainsKey(TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_TIME_KEY)
-			&& string.IsNullOrWhiteSpace(keys[TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_TIME_KEY]))
+		if (keys.ContainsKey(TwitterDistributorConstants.RefreshTokenTime)
+			&& string.IsNullOrWhiteSpace(keys[TwitterDistributorConstants.RefreshTokenTime]))
 		{
-			var result = DateTime.TryParse(keys[TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_TIME_KEY], out var time);
+			var result = DateTime.TryParse(keys[TwitterDistributorConstants.RefreshTokenTime], out var time);
 			if (result)
 			{
 				_nextRefreshTime = time;
@@ -106,8 +109,8 @@ public class TwitterDistributorV2 : IPostDistributor
 
 	public void CacheValues()
 	{
-		_redisCacheService.Set(TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_KEY, _refreshToken);
-		_redisCacheService.Set(TwitterDistributorConstants.TWITTER_REFRESH_TOKEN_TIME_KEY, _nextRefreshTime.ToString());
+		_redisCacheService.Set(TwitterDistributorConstants.RefreshToken, _refreshToken);
+		_redisCacheService.Set(TwitterDistributorConstants.RefreshTokenTime, _nextRefreshTime.ToString());
 	}
 
 	public async Task RequestTokensFromTwitter()
@@ -130,23 +133,45 @@ public class TwitterDistributorV2 : IPostDistributor
 			var responseData = JsonSerializer.Deserialize<TwitterRefreshTokenResponse>(await response.Content.ReadAsStringAsync());
 
 			_accessToken = responseData!.AccessToken;
-			_refreshToken = responseData!.RefreshToken;
+			_refreshToken = responseData.RefreshToken;
 			_nextRefreshTime = DateTime.UtcNow.AddSeconds(RefreshTokenDuration);
 			CacheValues();
 		}
+	}
+
+	private static string GenerateTwitterMessage(IPostable post)
+	{
+		var hasLink = !string.IsNullOrWhiteSpace(post.Link);
+		
+		var body = post.Group switch
+		{
+			PostGroups.Submission => post.Title,
+			PostGroups.Publication => post.Title,
+			_ => post.Body
+		};
+
+		body = body.Cap(280 - (hasLink ? 25 : 0)) ?? ""; // Twitter always makes links 23, make it 25 just in case
+		
+		if (hasLink)
+		{
+			body = $"{body}\n{post.Link}";
+		}
+
+		return body;
 	}
 }
 
 public class TwitterDistributorConstants
 {
-	public static string TWITTER_REFRESH_TOKEN_KEY = "TwitterRefreshToken";
-	public static string TWITTER_REFRESH_TOKEN_TIME_KEY = "TwitterRefreshTokenTime";
+	public static string RefreshToken = "TwitterRefreshToken";
+	public static string RefreshTokenTime = "TwitterRefreshTokenTime";
 }
 
 public class TwitterRefreshTokenResponse
 {
 	[JsonPropertyName("access_token")]
 	public string AccessToken { get; set; } = "";
+
 	[JsonPropertyName("refresh_token")]
 	public string RefreshToken { get; set; } = "";
 }
