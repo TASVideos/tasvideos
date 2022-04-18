2c1cafd0658c5e1677a0a531378209e692804395
adelikat <adelikat@tasvideos.org>
2022-04-17 20:16:22 -0500

    Change Email Workflow (#1241)
    * progress
    
    * sire things up
    
    * link to change email
    
    * profile navbar
    
    * email confirmed icon
    
    * redirect to Profile/Settings and remove code from cache
    
    * fix a thing
    
    * set email confirmed to true
    
    * be logged in to receive change email confirmation, and use ChangeEmailAsync method
    
    * passing userid is unecessary if user is required to be logged in

---
+++ TASVideos/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Core.Services;
+
+namespace TASVideos.Pages.Account;
+
+[Authorize]
+public class ConfirmEmailChangeModel : BasePageModel
+{
+	private readonly UserManager _userManager;
+	private readonly IUserMaintenanceLogger _userMaintenanceLogger;
+	private readonly ICacheService _cache;
+
+	public ConfirmEmailChangeModel(
+		UserManager userManager,
+		IUserMaintenanceLogger userMaintenanceLogger,
+		ICacheService cache)
+	{
+		_userManager = userManager;
+		_userMaintenanceLogger = userMaintenanceLogger;
+		_cache = cache;
+	}
+
+	public async Task<IActionResult> OnGet(string? code)
+	{
+		if (string.IsNullOrWhiteSpace(code))
+		{
+			return AccessDenied();
+		}
+
+		var user = await _userManager.GetUserAsync(User);
+
+		var cacheResult = _cache.TryGetValue(code, out string newEmail);
+		if (!cacheResult)
+		{
+			return BadRequest("Unrecognized or expired code.");
+		}
+
+		var result = await _userManager.ChangeEmailAsync(user, newEmail, code);
+		if (!result.Succeeded)
+		{
+			return RedirectToPage("/Error");
+		}
+
+		_cache.Remove(code);
+		await _userMaintenanceLogger.Log(user.Id, $"User changed email from {IpAddress}");
+		return RedirectToPage("/Profile/Settings");
+	}
+}
