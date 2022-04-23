7960b5bfcc43b509dc285a93ccbce4e46d734ec6
adelikat <adelikat@tasvideos.org>
2022-04-23 11:33:37 -0500

    Simplify model

--- TASVideos/Pages/Forum/Posts/Models/PostsSinceLastVisitModel.cs
+++ TASVideos/Pages/Forum/Posts/Models/PostsSinceLastVisitModel.cs
@@ -51,9 +51,18 @@ public class ForumPostEntry
 	}
 }
 
-public class PostsSinceLastVisitModel : ForumPostEntry
+public class PostsSinceLastVisitModel
 {
+	public int Id { get; set; }
+	public int TopicId { get; set; }
 	public string TopicTitle { get; set; } = "";
 	public int ForumId { get; set; }
 	public string ForumName { get; set; } = "";
+
+	[Sortable]
+	public DateTime CreateTimestamp { get; set; }
+
+	public string Text { get; set; } = "";
+
+	public string PosterName { get; set; } = "";
 }
--- TASVideos/Pages/Forum/Posts/New.cshtml.cs
+++ TASVideos/Pages/Forum/Posts/New.cshtml.cs
@@ -15,16 +15,11 @@ public class NewModel : BasePageModel
 {
 	private readonly ApplicationDbContext _db;
 	private readonly UserManager _userManager;
-	private readonly IAwards _awards;
 
-	public NewModel(
-		ApplicationDbContext db,
-		UserManager userManager,
-		IAwards awards)
+	public NewModel(ApplicationDbContext db, UserManager userManager)
 	{
 		_db = db;
 		_userManager = userManager;
-		_awards = awards;
 	}
 
 	[FromQuery]
@@ -52,37 +47,14 @@ public class NewModel : BasePageModel
 			{
 				Id = p.Id,
 				CreateTimestamp = p.CreateTimestamp,
-				LastUpdateTimestamp = p.LastUpdateTimestamp,
-				EnableBbCode = p.EnableBbCode,
-				EnableHtml = p.EnableHtml,
 				Text = p.Text,
-				Subject = p.Subject,
 				TopicId = p.TopicId ?? 0,
 				TopicTitle = p.Topic!.Title,
 				ForumId = p.Topic.ForumId,
 				ForumName = p.Topic!.Forum!.Name,
-				PosterId = p.PosterId,
-				PosterName = p.Poster!.UserName,
-				PosterRoles = p.Poster.UserRoles
-					.Where(ur => !ur.Role!.IsDefault)
-					.Where(ur => ur.Role!.Name != "Published Author")
-					.Select(ur => ur.Role!.Name)
-					.ToList(),
-				PosterLocation = p.Poster.From,
-				Signature = p.Poster.Signature,
-				PosterAvatar = p.Poster.Avatar,
-				PosterJoined = p.Poster.CreateTimestamp,
-				PosterPostCount = p.Poster.Posts.Count,
-				PosterMoodUrlBase = p.Poster.MoodAvatarUrlBase,
-				PosterMood = p.PosterMood,
-				PosterPronouns = p.Poster.PreferredPronouns
+				PosterName = p.Poster!.UserName
 			})
 			.OrderByDescending(p => p.CreateTimestamp)
 			.PageOf(Search);
-
-		foreach (var post in Posts)
-		{
-			post.Awards = await _awards.ForUser(post.PosterId);
-		}
 	}
 }
