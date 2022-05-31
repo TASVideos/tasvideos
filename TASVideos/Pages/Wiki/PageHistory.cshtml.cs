38111d6c78b03c6b794bd944362008d9b4da09a3
adelikat <adelikat@tasvideos.org>
2022-05-31 09:30:29 -0500

    Remove Wiki/Diff and support functionality in Wiki/PageHistory (#1311)
    * rework PageHistory to be able to load in a diff state
    
    * support latest diff from PageHistory page
    
    * Link to PageHistory instead of Diff and remove page
    
    * cleanup
    
    * update Robots.txt to disallow PageHistory

--- TASVideos/Pages/Wiki/PageHistory.cshtml.cs
+++ TASVideos/Pages/Wiki/PageHistory.cshtml.cs
@@ -20,7 +20,18 @@ public class PageHistoryModel : BasePageModel
 	[FromQuery]
 	public string? Path { get; set; }
 
+	[FromQuery]
+	public int? FromRevision { get; set; }
+
+	[FromQuery]
+	public int? ToRevision { get; set; }
+
 	public WikiHistoryModel History { get; set; } = new();
+
+	public WikiDiffModel Diff { get; set; } = new();
+
+	[FromQuery]
+	public bool? Latest { get; set; }
 
 	public async Task OnGet()
 	{
@@ -42,5 +53,62 @@ public class PageHistoryModel : BasePageModel
 				})
 				.ToListAsync()
 		};
+
+		if (Latest == true)
+		{
+			var (from, to) = await GetLatestRevisions(Path);
+			FromRevision = from;
+			ToRevision = to;
+		}
+
+		if (FromRevision.HasValue && ToRevision.HasValue)
+		{
+			var diff = await GetPageDiff(Path, FromRevision.Value, ToRevision.Value);
+			if (diff is not null)
+			{
+				Diff = diff;
+			}
+		}
+	}
+
+	private async Task<WikiDiffModel?> GetPageDiff(string pageName, int fromRevision, int toRevision)
+	{
+		var revisions = await _wikiPages.Query
+			.ForPage(pageName)
+			.Where(wp => wp.Revision == fromRevision
+				|| wp.Revision == toRevision)
+			.ToListAsync();
+
+		if (revisions.Count != (fromRevision == toRevision ? 1 : 2))
+		{
+			return null;
+		}
+
+		return new WikiDiffModel
+		{
+			LeftMarkup = revisions.Single(wp => wp.Revision == fromRevision).Markup,
+			RightMarkup = revisions.Single(wp => wp.Revision == toRevision).Markup
+		};
+	}
+
+	private async Task<(int? from, int? to)> GetLatestRevisions(string pageName)
+	{
+		var revisions = await _wikiPages.Query
+			.ForPage(pageName)
+			.ThatAreNotDeleted()
+			.OrderByDescending(wp => wp.Revision)
+			.Select(wp => wp.Revision)
+			.Take(2)
+			.ToListAsync();
+
+		if (!revisions.Any())
+		{
+			return (null, null);
+		}
+
+		// If count is 1, it must be a new page with no history, so compare against nothing
+		return revisions.Count == 1
+			? (1, 1)
+			: (revisions[1], revisions[0]);
 	}
 }
