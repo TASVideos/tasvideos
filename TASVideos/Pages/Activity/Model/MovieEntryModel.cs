e37110ffd4a0ab719c1c5da763a41bafa5ec3c6d
feos <vadosnaprimer@users.noreply.github.com>
2022-06-01 16:10:54 +0300

    show id, date, and class in publication activity (#1315)

--- TASVideos/Pages/Activity/Model/MovieEntryModel.cs
+++ TASVideos/Pages/Activity/Model/MovieEntryModel.cs
@@ -3,5 +3,7 @@
 public class MovieEntryModel
 {
 	public int Id { get; set; }
+	public DateTime CreateTimestamp { get; set; }
 	public string Title { get; set; } = "";
+	public string Class { get; set; } = "";
 }
