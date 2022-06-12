92b8089fe14232d306979e37b339abc2fa4b63e1
adelikat <adelikat@tasvideos.org>
2022-06-12 13:59:39 -0500

    properly fix role deletion with the needed database migration and remove now unnecessary code

---
+++ TASVideos.Data/Migrations/20220612185556_RoleLinkId.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASVideos.Data.Migrations
+{
+    public partial class RoleLinkId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_role_links_roles_role_id",
+                table: "role_links");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "role_id",
+                table: "role_links",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_role_links_roles_role_id",
+                table: "role_links",
+                column: "role_id",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_role_links_roles_role_id",
+                table: "role_links");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "role_id",
+                table: "role_links",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_role_links_roles_role_id",
+                table: "role_links",
+                column: "role_id",
+                principalTable: "roles",
+                principalColumn: "id");
+        }
+    }
+}
