aff0ac10a0ec58a639f1d2d94b6b0b5165ffe24c
adelikat <adelikat@tasvideos.org>
2022-07-03 12:25:28 -0500

    stylecop nags for auto-generated code ugh

--- TASVideos.Data/Migrations/20220703163741_AddGameGroupDescription.cs
+++ TASVideos.Data/Migrations/20220703163741_AddGameGroupDescription.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
-{
-    public partial class AddGameGroupDescription : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "description",
-                table: "game_groups",
-                type: "citext",
-                maxLength: 2000,
-                nullable: true);
-        }
+namespace TASVideos.Data.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "description",
-                table: "game_groups");
-        }
-    }
+public partial class AddGameGroupDescription : Migration
+{
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddColumn<string>(
+			name: "description",
+			table: "game_groups",
+			type: "citext",
+			maxLength: 2000,
+			nullable: true);
+	}
+
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "description",
+			table: "game_groups");
+	}
 }
