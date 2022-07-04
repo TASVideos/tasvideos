af07348a808d1d9c8e89bb4024fef5125c5f705d
adelikat <adelikat@tasvideos.org>
2022-07-04 11:00:11 -0500

    db migration

---
+++ TASVideos.Data/Migrations/20220704155137_RemoveUnnecessaryGameFields.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASVideos.Data.Migrations;
+
+public partial class RemoveUnnecessaryGameFields : Migration
+{
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "good_name",
+			table: "games");
+
+		migrationBuilder.DropColumn(
+			name: "search_key",
+			table: "games");
+	}
+
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddColumn<string>(
+			name: "good_name",
+			table: "games",
+			type: "citext",
+			maxLength: 250,
+			nullable: false,
+			defaultValue: "");
+
+		migrationBuilder.AddColumn<string>(
+			name: "search_key",
+			table: "games",
+			type: "citext",
+			maxLength: 64,
+			nullable: true);
+	}
+}
