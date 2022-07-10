173e412b146c8dbfaf38fda98d9b66cc67c2eae5
Masterjun3 <masterjun3@gmail.com>
2022-07-10 17:33:42 +0200

    route /p/1234 links to a separate Legacy model, so that generated links don't use the legacy format

---
+++ TASVideos/Pages/Forum/Legacy/Post.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.Core.Services;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Pages.Forum.Legacy;
+
+// Handles legacy forum links to viewTopic.php
+[AllowAnonymous]
+public class PostModel : BaseForumModel
+{
+	[FromRoute]
+	public int? Id { get; set; }
+
+	public IActionResult OnGet()
+	{
+		return BasePageRedirect("/Forum/Posts/Index", new { Id });
+	}
+}
