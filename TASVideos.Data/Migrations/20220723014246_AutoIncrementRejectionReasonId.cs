93d89e887c276d347bd8a001175906172c250f95
adelikat <adelikat@tasvideos.org>
2022-07-23 15:28:53 -0500

    cleanup migration file

--- TASVideos.Data/Migrations/20220723014246_AutoIncrementRejectionReasonId.cs
+++ TASVideos.Data/Migrations/20220723014246_AutoIncrementRejectionReasonId.cs
@@ -3,32 +3,31 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
-{
-    public partial class AutoIncrementRejectionReasonId : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<int>(
-                name: "id",
-                table: "submission_rejection_reasons",
-                type: "integer",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "integer")
-                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
-        }
+namespace TASVideos.Data.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<int>(
-                name: "id",
-                table: "submission_rejection_reasons",
-                type: "integer",
-                nullable: false,
-                oldClrType: typeof(int),
-                oldType: "integer")
-                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
-        }
-    }
+public partial class AutoIncrementRejectionReasonId : Migration
+{
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AlterColumn<int>(
+				name: "id",
+				table: "submission_rejection_reasons",
+				type: "integer",
+				nullable: false,
+				oldClrType: typeof(int),
+				oldType: "integer")
+			.Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+	}
+
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AlterColumn<int>(
+				name: "id",
+				table: "submission_rejection_reasons",
+				type: "integer",
+				nullable: false,
+				oldClrType: typeof(int),
+				oldType: "integer")
+			.OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+	}
 }
