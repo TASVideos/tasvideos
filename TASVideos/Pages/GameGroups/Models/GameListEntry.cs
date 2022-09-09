9ddf907253f7039c3c44c94585d81b0f2c64644e
Masterjun3 <masterjun3@gmail.com>
2022-09-09 21:05:47 +0200

    add System column for GameGroup pages

--- TASVideos/Pages/GameGroups/Models/GameListEntry.cs
+++ TASVideos/Pages/GameGroups/Models/GameListEntry.cs
@@ -4,7 +4,7 @@ public class GameListEntry
 {
 	public int Id { get; set; }
 	public string Name { get; set; } = "";
-	public string SystemCode { get; set; } = "";
+	public IEnumerable<string> Systems { get; set; } = new List<string>();
 	public int PublicationCount { get; set; }
 	public int SubmissionsCount { get; set; }
 	public string? GameResourcesPage { get; set; }
