291a7e990d7a47441ade8112725c589f676c3def
Masterjun3 <masterjun3@gmail.com>
2022-10-15 01:26:00 +0200

    clear cache on publication and unpublication

--- TASVideos.Core/Services/TASVideoAgent.cs
+++ TASVideos.Core/Services/TASVideoAgent.cs
@@ -118,6 +118,9 @@ internal class TASVideoAgent : ITASVideoAgent
 				PosterMood = ForumPostMood.Happy
 			});
 			await _db.SaveChangesAsync();
+
+			_forumService.ClearLatestPostCache();
+			_forumService.ClearTopicActivityCache();
 		}
 	}
 
@@ -153,6 +156,9 @@ internal class TASVideoAgent : ITASVideoAgent
 				PosterMood = ForumPostMood.Puzzled
 			});
 			await _db.SaveChangesAsync();
+
+			_forumService.ClearLatestPostCache();
+			_forumService.ClearTopicActivityCache();
 		}
 	}
 
