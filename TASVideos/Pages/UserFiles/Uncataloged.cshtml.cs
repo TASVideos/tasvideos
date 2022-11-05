f649456951046cd6f37e4a9a9c69fa0163c5922e
adelikat <adelikat@tasvideos.org>
2022-11-05 16:18:46 -0500

    add a userfiles page for uncataloged movies, allows users to find userfiles not associated with a game much easier, and resolves #509

---
+++ TASVideos/Pages/UserFiles/Uncataloged.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.UserFiles.Models;
+
+namespace TASVideos.Pages.UserFiles;
+
+[AllowAnonymous]
+public class Uncataloged : BasePageModel
+{
+	private readonly ApplicationDbContext _db;
+
+	public Uncataloged(ApplicationDbContext db)
+	{
+		_db = db;
+	}
+
+	public IReadOnlyCollection<UncatalogedViewModel> Files { get; set; } = new List<UncatalogedViewModel>();
+
+	[FromRoute]
+	public int Id { get; set; }
+
+	public async Task<IActionResult> OnGet()
+	{
+		Files = await _db.UserFiles
+			.Where(uf => !uf.Hidden)
+			.Select(uf => new UncatalogedViewModel
+			{
+				Id = uf.Id,
+				FileName = uf.FileName,
+				SystemCode = uf.System != null ? uf.System.Code : null,
+				UploadTimestamp = uf.UploadTimestamp,
+				Author = uf.Author!.UserName
+			})
+			.ToListAsync();
+
+		return Page();
+	}
+}
