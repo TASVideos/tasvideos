70ead7f5f6239da916456d9643e053c4566bf672
adelikat <adelikat@tasvideos.org>
2022-12-24 13:14:25 -0600

    backup-text.js -> tabs instead of spaces

--- TASVideos/wwwroot/js/backup-text.js
+++ TASVideos/wwwroot/js/backup-text.js
@@ -5,33 +5,33 @@
 // id: "backup-restore-button" | button to restore data
 
 function convertSecondsToRelativeTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(seconds / (60 * 60));
-    const days = Math.floor(seconds / (60 * 60 * 24));
-    if (seconds < 5) {
-        return "a few seconds ago";
-    }
-    else if (seconds < 60) {
-        return `${seconds} seconds ago`;
-    }
-    else if (minutes < 2) {
-        return "1 minute ago";
-    }
-    else if (minutes < 60) {
-        return `${minutes} minutes ago`;
-    }
-    else if (hours < 2) {
-        return "1 hour ago";
-    }
-    else if (hours < 24) {
-        return `${hours} hours ago`;
-    }
-    else if (days < 2) {
-        return "1 day ago";
-    }
-    else {
-        return `${days} days ago`;
-    }
+	const minutes = Math.floor(seconds / 60);
+	const hours = Math.floor(seconds / (60 * 60));
+	const days = Math.floor(seconds / (60 * 60 * 24));
+	if (seconds < 5) {
+		return "a few seconds ago";
+	}
+	else if (seconds < 60) {
+		return `${seconds} seconds ago`;
+	}
+	else if (minutes < 2) {
+		return "1 minute ago";
+	}
+	else if (minutes < 60) {
+		return `${minutes} minutes ago`;
+	}
+	else if (hours < 2) {
+		return "1 hour ago";
+	}
+	else if (hours < 24) {
+		return `${hours} hours ago`;
+	}
+	else if (days < 2) {
+		return "1 day ago";
+	}
+	else {
+		return `${days} days ago`;
+	}
 }
 
 const textarea = document.querySelector('textarea.backup-content');
@@ -40,33 +40,33 @@ localStorage.removeItem(backupKey + '-restore');
 
 let backupData = localStorage.getItem(backupKey);
 if (backupData) {
-    let backupObject = JSON.parse(backupData);
-    document.getElementById('backup-time').innerText = convertSecondsToRelativeTime(Math.floor(Date.now() / 1000) - backupObject.date);
-    localStorage.setItem(backupKey + '-restore', backupData);
-    document.getElementById('backup-restore').classList.remove('d-none');
+	let backupObject = JSON.parse(backupData);
+	document.getElementById('backup-time').innerText = convertSecondsToRelativeTime(Math.floor(Date.now() / 1000) - backupObject.date);
+	localStorage.setItem(backupKey + '-restore', backupData);
+	document.getElementById('backup-restore').classList.remove('d-none');
 }
 
 function backupContent() {
-    if (textarea.value) {
-        let backupObject = JSON.stringify({ content: textarea.value, date: Math.floor(Date.now() / 1000) });
-        localStorage.setItem(backupKey, backupObject)
-    }
+	if (textarea.value) {
+		let backupObject = JSON.stringify({ content: textarea.value, date: Math.floor(Date.now() / 1000) });
+		localStorage.setItem(backupKey, backupObject)
+	}
 }
 setInterval(backupContent, 5000);
 
 const restoreButton = document.getElementById('backup-restore-button');
 function restoreContent() {
-    let backupObject = JSON.parse(localStorage.getItem(backupKey + '-restore'));
-    if (backupObject && !textarea.value) {
-        textarea.value = backupObject.content;
-        backupContent();
-    }
+	let backupObject = JSON.parse(localStorage.getItem(backupKey + '-restore'));
+	if (backupObject && !textarea.value) {
+		textarea.value = backupObject.content;
+		backupContent();
+	}
 }
 restoreButton.onclick = restoreContent;
 
 const form = document.querySelector('form.backup-form');
 function deleteBackup() {
-    localStorage.removeItem(backupKey);
-    localStorage.removeItem(backupKey + '-restore');
+	localStorage.removeItem(backupKey);
+	localStorage.removeItem(backupKey + '-restore');
 }
 form.onsubmit = deleteBackup;
