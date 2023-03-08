f2edc39b658df491d5d2f5996a316c8a506bbe28
Masterjun3 <masterjun3@gmail.com>
2023-03-08 23:28:20 +0100

    only set trim result if we have non-whitespace to trim, other cases are handled in other binders

--- TASVideos/Models/TrimStringModelBinder.cs
+++ TASVideos/Models/TrimStringModelBinder.cs
@@ -9,16 +9,12 @@ public class TrimStringModelBinder : IModelBinder
 	public Task BindModelAsync(ModelBindingContext bindingContext)
 	{
 		string? value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).FirstValue;
-		if (bindingContext.ModelMetadata.ConvertEmptyStringToNull && string.IsNullOrWhiteSpace(value))
-		{
-			value = null;
-		}
-		else if (value is not null)
+		if (!string.IsNullOrWhiteSpace(value))
 		{
 			value = value.Trim();
+			bindingContext.Result = ModelBindingResult.Success(value);
 		}
 
-		bindingContext.Result = ModelBindingResult.Success(value);
 		return Task.CompletedTask;
 	}
 }
