cfd78c48740f2012144218a605130a61836009de
Masterjun <masterjun3@gmail.com>
2023-04-06 04:58:35 +0200

    use disabled attribute instead of dynamic html to change stylesheets on the fly (#1540)
    this allows weboptimizer to properly assign the hash of the stylesheet so that caches are automatically busted once we change styles

--- TASVideos/wwwroot/js/site-head.js
+++ TASVideos/wwwroot/js/site-head.js
@@ -1,16 +1,8 @@
 ﻿function forceDarkMode() {
-	const darkModeStylesheet = document.getElementById("style-dark");
-	if (!darkModeStylesheet) {
-		removeAutoDarkMode();
+	removeAutoDarkMode();
+	document.getElementById("style-dark").disabled = false;
 
-		const newElement = document.createElement("link");
-		newElement.rel = "stylesheet";
-		newElement.id = "style-dark";
-		newElement.href = "/css/darkmode.css";
-		document.head.appendChild(newElement);
-
-		localStorage.setItem("style-dark", "true");
-	}
+	localStorage.setItem("style-dark", "true");
 }
 
 function forceLightMode() {
@@ -21,32 +13,18 @@ function forceLightMode() {
 }
 
 function autoDarkMode() {
-	const initialDarkModeStylesheet = document.getElementById("style-dark-initial");
-	if (!initialDarkModeStylesheet) {
-		removeForcedDarkMode();
+	removeForcedDarkMode();
+	document.getElementById("style-dark-initial").disabled = false;
 
-		const newElement = document.createElement("link");
-		newElement.rel = "stylesheet";
-		newElement.id = "style-dark-initial";
-		newElement.href = "/css/darkmode-initial.css";
-		document.head.appendChild(newElement);
-
-		localStorage.removeItem("style-dark");
-	}
+	localStorage.removeItem("style-dark");
 }
 
 function removeForcedDarkMode() {
-	const darkModeStylesheet = document.getElementById("style-dark");
-	if (darkModeStylesheet) {
-		darkModeStylesheet.parentElement.removeChild(darkModeStylesheet);
-	}
+	document.getElementById("style-dark").disabled = true;
 }
 
 function removeAutoDarkMode() {
-	const initialDarkModeStylesheet = document.getElementById("style-dark-initial");
-	if (initialDarkModeStylesheet) {
-		initialDarkModeStylesheet.parentElement.removeChild(initialDarkModeStylesheet);
-	}
+	document.getElementById("style-dark-initial").disabled = true;
 }
 
 if (localStorage.getItem("style-dark") !== null) {
