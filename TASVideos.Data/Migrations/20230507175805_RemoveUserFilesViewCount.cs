6a7976f11075e888726cfc3481ad01473638f956
adelikat <adelikat@tasvideos.org>
2023-05-07 12:58:42 -0500

    db migration to remove userfiles.views

---
+++ TASVideos.Data/Migrations/20230507175805_RemoveUserFilesViewCount.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASVideos.Data.Migrations
+{
+    public partial class RemoveUserFilesViewCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "views",
+                table: "user_files");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "views",
+                table: "user_files",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
