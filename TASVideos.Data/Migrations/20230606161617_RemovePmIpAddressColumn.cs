1949a998c971cd843ad8b6c67270dc6434b357fc
adelikat <adelikat@tasvideos.org>
2023-06-06 11:22:50 -0500

    stylecop, I will never remember this the first time

--- TASVideos.Data/Migrations/20230606161617_RemovePmIpAddressColumn.cs
+++ TASVideos.Data/Migrations/20230606161617_RemovePmIpAddressColumn.cs
@@ -2,25 +2,24 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
-{
-    public partial class RemovePmIpAddressColumn : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "ip_address",
-                table: "private_messages");
-        }
+namespace TASVideos.Data.Migrations;
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "ip_address",
-                table: "private_messages",
-                type: "citext",
-                maxLength: 50,
-                nullable: true);
-        }
-    }
+public partial class RemovePmIpAddressColumn : Migration
+{
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "ip_address",
+			table: "private_messages");
+	}
+
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddColumn<string>(
+			name: "ip_address",
+			table: "private_messages",
+			type: "citext",
+			maxLength: 50,
+			nullable: true);
+	}
 }
