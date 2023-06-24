5de7d0c6abbc160ebc3840fcd7c21ee67c6ec850
adelikat <adelikat@tasvideos.org>
2023-06-24 13:16:57 -0500

    Account/ResetPassword - make password fields more clear, fixes #1530

--- TASVideos/Pages/Account/ResetPassword.cshtml.cs
+++ TASVideos/Pages/Account/ResetPassword.cshtml.cs
@@ -22,13 +22,14 @@ public class ResetPasswordModel : BasePageModel
 
 	[BindProperty]
 	[Required]
+	[Display(Name = "New password")]
 	[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 12)]
 	[DataType(DataType.Password)]
 	public string Password { get; set; } = "";
 
 	[BindProperty]
 	[DataType(DataType.Password)]
-	[Display(Name = "Confirm password")]
+	[Display(Name = "Confirm new password")]
 	[Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
 	public string ConfirmPassword { get; set; } = "";
 
