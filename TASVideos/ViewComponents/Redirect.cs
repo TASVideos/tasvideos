e77a9092ec45a8f9e8cae12384d8dd3ab50e7fbb
Randomno <emailrandomno@gmail.com>
2023-07-02 14:45:31 +0100

    redirect

---
+++ TASVideos/ViewComponents/Redirect.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using TASVideos.WikiEngine;
+
+namespace TASVideos.ViewComponents;
+
+[WikiModule(WikiModules.Redirect)]
+public class Redirect : ViewComponent
+{
+	public IViewComponentResult Invoke(string page)
+	{
+		HttpContext context = ViewContext.HttpContext;
+
+		context.Response.Redirect(page);
+
+		return Content("");
+	}
+}
