c4b1fc56d932440c50adceee336c5e66b2c83628
RetroEdit <30182911+RetroEdit@users.noreply.github.com>
2023-07-10 14:50:00 -0500

    parsers: add GBAHawk .gbmv support (#1601)
    (.gbmv is currently just renamed .bk2)
    
    add GBAHawk parser testcase
    
    Co-authored-by: RetroEdit <retroeditcode@gmail.com>

---
+++ tests/TASVideos.MovieParsers.Tests/GbmvParserTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TASVideos.MovieParsers.Tests;
+
+[TestClass]
+[TestCategory("GbmvParsers")]
+public class GbmvParserTests : BaseParserTests
+{
+	private readonly Gbmv _gbmvParser;
+	public override string ResourcesPath { get; } = "TASVideos.MovieParsers.Tests.GbmvSampleFiles.";
+
+	public GbmvParserTests()
+	{
+		_gbmvParser = new Gbmv();
+	}
+
+	[TestMethod]
+	public async Task GBAHawk_NewHeaderValues()
+	{
+		var result = await _gbmvParser.Parse(Embedded("GBAHawk_NewHeaderValues.gbmv"), EmbeddedLength("GBAHawk_NewHeaderValues.gbmv"));
+		Assert.AreEqual(true, result.Success);
+		AssertNoWarningsOrErrors(result);
+	}
+}
