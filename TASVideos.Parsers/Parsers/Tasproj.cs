c5fd546e6984441c30bc99411f8584e04f4ed9d5
adelikat <adelikat@tasvideos.org>
2023-07-19 08:07:54 -0500

    fix tasproj similarly to gbmv

--- TASVideos.Parsers/Parsers/Tasproj.cs
+++ TASVideos.Parsers/Parsers/Tasproj.cs
@@ -3,6 +3,8 @@
 [FileExtension("tasproj")]
 internal class Tasproj : Bk2
 {
+	public override string FileExtension => "tasproj";
+
 	protected override string[] InvalidArchiveEntries => new[]
 	{
 		"greenzone"
