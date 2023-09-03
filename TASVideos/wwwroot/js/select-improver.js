12d20674cfa65a160ca6c6d230d6b2453a394ec1
adelikat <adelikat@tasvideos.org>
2023-09-03 11:06:34 -0500

    select-improver - remove another set of redundant parenthesis

--- TASVideos/wwwroot/js/select-improver.js
+++ TASVideos/wwwroot/js/select-improver.js
@@ -31,8 +31,7 @@
 }
 
 function engageSelectImprover(multiSelectId, maxHeight = '250px') {
-	{
-		let initialHtmlToAdd = `
+	let initialHtmlToAdd = `
 <div id='${multiSelectId}_div' class='d-none border bg-body rounded-2 onclick-focusinput' style='cursor: text;'>
 	<div id='${multiSelectId}_buttons' class='onclick-focusinput px-2 pt-2 pb-1'>
 		<span class='text-body-tertiary onclick-focusinput px-1'>No selection</span>
@@ -42,106 +41,105 @@ function engageSelectImprover(multiSelectId, maxHeight = '250px') {
 	<div id='${multiSelectId}_list' class='list-group mt-1 overflow-auto border' style='max-height: ${maxHeight};'></div>
 </div>
 `;
-		let multiSelect = document.getElementById(multiSelectId);
-		multiSelect.classList.add('d-none');
-		let div = document.getElementById(multiSelectId + '_div');
-		div?.remove();
-		multiSelect.insertAdjacentHTML('afterend', initialHtmlToAdd);
-		div = document.getElementById(multiSelectId + '_div');
-		let list = document.getElementById(multiSelectId + '_list');
-		let buttons = document.getElementById(multiSelectId + '_buttons');
-		div.classList.remove('d-none');
-		let input = document.getElementById(multiSelectId + '_input');
-		let anyNotSelected = false;
-		for (var option of multiSelect.options) {
-			let entry = document.createElement('div');
-			entry.classList.add('list-group-item', 'list-group-item-action', 'px-1');
-			let label = document.createElement('label');
-			label.classList.add('form-check-label', 'stretched-link');
-			let checkbox = document.createElement('input');
-			checkbox.type = 'checkbox';
-			checkbox.classList.add('form-check-input', 'ms-1', 'me-2');
-			if (option.selected) {
-				checkbox.checked = true;
-			}
-
-			checkbox.dataset.value = option.value;
-			checkbox.addEventListener('change', (e) => {
-				toggleSelectOption(multiSelect, buttons, list, e.currentTarget.dataset.value);
-			});
-
-			label.appendChild(checkbox);
-			label.append(option.text);
-			entry.appendChild(label);
-			list.appendChild(entry);
-
-			let button = document.createElement('button');
-			button.type = 'button';
-			button.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-1', 'me-1');
-			button.dataset.value = option.value;
-			if (option.selected) {
-				buttons.querySelector('span').classList.add('d-none');
-			} else {
-				button.classList.add('d-none');
-			}
-
-			button.addEventListener('click', (e) => {
-				toggleSelectOption(multiSelect, buttons, list, e.currentTarget.dataset.value);
-			});
-
-			let buttonSpanText = document.createElement('span');
-			buttonSpanText.innerText = option.text;
-			button.appendChild(buttonSpanText);
-			let buttonSpanX = document.createElement('span');
-			buttonSpanX.innerText = '✕';
-			buttonSpanX.classList.add('ps-1');
-			button.appendChild(buttonSpanX);
-
-			buttons.appendChild(button);
-
-			if (!option.selected) {
-				anyNotSelected = true;
-			}
+	let multiSelect = document.getElementById(multiSelectId);
+	multiSelect.classList.add('d-none');
+	let div = document.getElementById(multiSelectId + '_div');
+	div?.remove();
+	multiSelect.insertAdjacentHTML('afterend', initialHtmlToAdd);
+	div = document.getElementById(multiSelectId + '_div');
+	let list = document.getElementById(multiSelectId + '_list');
+	let buttons = document.getElementById(multiSelectId + '_buttons');
+	div.classList.remove('d-none');
+	let input = document.getElementById(multiSelectId + '_input');
+	let anyNotSelected = false;
+	for (var option of multiSelect.options) {
+		let entry = document.createElement('div');
+		entry.classList.add('list-group-item', 'list-group-item-action', 'px-1');
+		let label = document.createElement('label');
+		label.classList.add('form-check-label', 'stretched-link');
+		let checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		checkbox.classList.add('form-check-input', 'ms-1', 'me-2');
+		if (option.selected) {
+			checkbox.checked = true;
 		}
 
-		let toggleAllIcon = document.createElement('i');
-		toggleAllIcon.classList.add('fa', 'fa-sm');
-		toggleAllIcon.classList.add(anyNotSelected ? 'fa-plus' : 'fa-minus');
-		buttons.querySelector('a').appendChild(toggleAllIcon);
-		buttons.querySelector('a').title = anyNotSelected ? 'Select All' : 'Deselect All';
-		buttons.querySelector('a').addEventListener('click', () => {
-			const notSelected = [...multiSelect.options].filter(o => !o.selected);
-			if (notSelected.length) {
-				for (let o of notSelected) {
-					toggleSelectOption(multiSelect, buttons, list, o.value, false);
-				}
-			} else {
-				for (let o of multiSelect.options) {
-					toggleSelectOption(multiSelect, buttons, list, o.value, false);
-				}
-			}
-			multiSelect.dispatchEvent(new Event('change')); // somewhat hacky way to support external event listeners
+		checkbox.dataset.value = option.value;
+		checkbox.addEventListener('change', (e) => {
+			toggleSelectOption(multiSelect, buttons, list, e.currentTarget.dataset.value);
 		});
-		div.addEventListener('click', (e) => {
-			if (e.target.classList.contains('onclick-focusinput')) {
-				input.classList.remove('d-none');
-				input.focus();
-			}
+
+		label.appendChild(checkbox);
+		label.append(option.text);
+		entry.appendChild(label);
+		list.appendChild(entry);
+
+		let button = document.createElement('button');
+		button.type = 'button';
+		button.classList.add('btn', 'btn-primary', 'btn-sm', 'mb-1', 'me-1');
+		button.dataset.value = option.value;
+		if (option.selected) {
+			buttons.querySelector('span').classList.add('d-none');
+		} else {
+			button.classList.add('d-none');
+		}
+
+		button.addEventListener('click', (e) => {
+			toggleSelectOption(multiSelect, buttons, list, e.currentTarget.dataset.value);
 		});
-		input.addEventListener('input', () => {
-			const searchValue = input.value.toLowerCase();
-			for (let entry of list.querySelectorAll('label')) {
-				if (entry.innerText.toLowerCase().includes(searchValue)) {
-					entry.parentNode.classList.remove('d-none');
-				} else {
-					entry.parentNode.classList.add('d-none');
-				}
-			}
-		});
-		input.addEventListener('focusout', () => {
-			if (!input.value) {
-				input.classList.add('d-none');
-			}
-		});
+
+		let buttonSpanText = document.createElement('span');
+		buttonSpanText.innerText = option.text;
+		button.appendChild(buttonSpanText);
+		let buttonSpanX = document.createElement('span');
+		buttonSpanX.innerText = '✕';
+		buttonSpanX.classList.add('ps-1');
+		button.appendChild(buttonSpanX);
+
+		buttons.appendChild(button);
+
+		if (!option.selected) {
+			anyNotSelected = true;
+		}
 	}
+
+	let toggleAllIcon = document.createElement('i');
+	toggleAllIcon.classList.add('fa', 'fa-sm');
+	toggleAllIcon.classList.add(anyNotSelected ? 'fa-plus' : 'fa-minus');
+	buttons.querySelector('a').appendChild(toggleAllIcon);
+	buttons.querySelector('a').title = anyNotSelected ? 'Select All' : 'Deselect All';
+	buttons.querySelector('a').addEventListener('click', () => {
+		const notSelected = [...multiSelect.options].filter(o => !o.selected);
+		if (notSelected.length) {
+			for (let o of notSelected) {
+				toggleSelectOption(multiSelect, buttons, list, o.value, false);
+			}
+		} else {
+			for (let o of multiSelect.options) {
+				toggleSelectOption(multiSelect, buttons, list, o.value, false);
+			}
+		}
+		multiSelect.dispatchEvent(new Event('change')); // somewhat hacky way to support external event listeners
+	});
+	div.addEventListener('click', (e) => {
+		if (e.target.classList.contains('onclick-focusinput')) {
+			input.classList.remove('d-none');
+			input.focus();
+		}
+	});
+	input.addEventListener('input', () => {
+		const searchValue = input.value.toLowerCase();
+		for (let entry of list.querySelectorAll('label')) {
+			if (entry.innerText.toLowerCase().includes(searchValue)) {
+				entry.parentNode.classList.remove('d-none');
+			} else {
+				entry.parentNode.classList.add('d-none');
+			}
+		}
+	});
+	input.addEventListener('focusout', () => {
+		if (!input.value) {
+			input.classList.add('d-none');
+		}
+	});
 }
