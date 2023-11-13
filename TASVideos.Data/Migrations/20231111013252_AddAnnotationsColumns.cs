b5ba2b6582bea190fa2a99c7b8ede31776cf1ffa
adelikat <adelikat@tasvideos.org>
2023-11-13 11:35:18 -0600

    oops, only show annotations on submission if there are any, also cleanup styleocp nags

--- TASVideos.Data/Migrations/20231111013252_AddAnnotationsColumns.cs
+++ TASVideos.Data/Migrations/20231111013252_AddAnnotationsColumns.cs
@@ -4,32 +4,32 @@
 
 namespace TASVideos.Data.Migrations
 {
-    public partial class AddAnnotationsColumns : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "annotations",
-                table: "user_files",
-                type: "citext",
-                nullable: true);
+	public partial class AddAnnotationsColumns : Migration
+	{
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AddColumn<string>(
+				name: "annotations",
+				table: "user_files",
+				type: "citext",
+				nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "annotations",
-                table: "submissions",
-                type: "citext",
-                nullable: true);
-        }
+			migrationBuilder.AddColumn<string>(
+				name: "annotations",
+				table: "submissions",
+				type: "citext",
+				nullable: true);
+		}
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "annotations",
-                table: "user_files");
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				name: "annotations",
+				table: "user_files");
 
-            migrationBuilder.DropColumn(
-                name: "annotations",
-                table: "submissions");
-        }
-    }
+			migrationBuilder.DropColumn(
+				name: "annotations",
+				table: "submissions");
+		}
+	}
 }
