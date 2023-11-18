cc8c38d0fd6fc5217ac9feda76b144ee879e1b48
adelikat <adelikat@tasvideos.org>
2023-11-18 08:32:36 -0600

    remove unused .net framework library, fix a few stylecop nags

--- tests/TASVideos.Core.Tests/GlobalUsings.cs
+++ tests/TASVideos.Core.Tests/GlobalUsings.cs
@@ -1,4 +1,4 @@
 ﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using NSubstitute;
-global using TASVideos.Tests.Base;
 global using TASVideos.Core.Services;
+global using TASVideos.Tests.Base;
