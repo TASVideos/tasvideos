6389bf3f98a66dbc48f82fead18bcd6ec945d50c
adelikat <AndrewJDelikat@gmail.com>
2024-02-17 12:13:29 -0600

    Revert "Revert "migration after .NET 8 update (#1772)""
    This reverts commit 6f9672d7b1b083069d21ebcc0738bd01f023b5d9.

---
+++ TASVideos.Data/Migrations/20240207204032_DotNet8Update.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASVideos.Data.Migrations;
+
+/// <inheritdoc />
+public partial class DotNet8Update : Migration
+{
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropForeignKey(
+			name: "fk_forum_topics_submissions_submission_id1",
+			table: "forum_topics");
+
+		migrationBuilder.AddForeignKey(
+			name: "fk_forum_topics_submissions_submission_id",
+			table: "forum_topics",
+			column: "submission_id",
+			principalTable: "submissions",
+			principalColumn: "id");
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropForeignKey(
+			name: "fk_forum_topics_submissions_submission_id",
+			table: "forum_topics");
+
+		migrationBuilder.AddForeignKey(
+			name: "fk_forum_topics_submissions_submission_id1",
+			table: "forum_topics",
+			column: "submission_id",
+			principalTable: "submissions",
+			principalColumn: "id");
+	}
+}
