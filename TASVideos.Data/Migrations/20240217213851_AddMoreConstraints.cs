2c35a47751f8e06a19cf1db819682e809aad263c
adelikat <AndrewJDelikat@gmail.com>
2024-02-18 14:32:56 -0600

    stylecop nags

--- TASVideos.Data/Migrations/20240217213851_AddMoreConstraints.cs
+++ TASVideos.Data/Migrations/20240217213851_AddMoreConstraints.cs
@@ -2,57 +2,56 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
+namespace TASVideos.Data.Migrations;
+
+/// <inheritdoc />
+public partial class AddMoreConstraints : Migration
 {
-    /// <inheritdoc />
-    public partial class AddMoreConstraints : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "normalized_email",
-                table: "users",
-                type: "citext",
-                maxLength: 100,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "citext",
-                oldNullable: true);
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AlterColumn<string>(
+			name: "normalized_email",
+			table: "users",
+			type: "citext",
+			maxLength: 100,
+			nullable: false,
+			defaultValue: "",
+			oldClrType: typeof(string),
+			oldType: "citext",
+			oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "email",
-                table: "users",
-                type: "citext",
-                maxLength: 100,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "citext",
-                oldNullable: true);
-        }
+		migrationBuilder.AlterColumn<string>(
+			name: "email",
+			table: "users",
+			type: "citext",
+			maxLength: 100,
+			nullable: false,
+			defaultValue: "",
+			oldClrType: typeof(string),
+			oldType: "citext",
+			oldNullable: true);
+	}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "normalized_email",
-                table: "users",
-                type: "citext",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "citext",
-                oldMaxLength: 100);
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AlterColumn<string>(
+			name: "normalized_email",
+			table: "users",
+			type: "citext",
+			nullable: true,
+			oldClrType: typeof(string),
+			oldType: "citext",
+			oldMaxLength: 100);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "email",
-                table: "users",
-                type: "citext",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "citext",
-                oldMaxLength: 100);
-        }
-    }
+		migrationBuilder.AlterColumn<string>(
+			name: "email",
+			table: "users",
+			type: "citext",
+			nullable: true,
+			oldClrType: typeof(string),
+			oldType: "citext",
+			oldMaxLength: 100);
+	}
 }
