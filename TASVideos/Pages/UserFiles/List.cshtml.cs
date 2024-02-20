45e68086f7a380f87676949776e990c5faf4f4e4
Masterjun3 <masterjun3@gmail.com>
2024-02-20 22:04:42 +0100

    add userfiles list page

---
+++ TASVideos/Pages/UserFiles/List.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TASVideos.Core;
+using TASVideos.Data;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.UserFiles.Models;
+
+namespace TASVideos.Pages.UserFiles;
+
+public class ListModel : PageModel
+{
+	private readonly ApplicationDbContext _db;
+	public ListModel(ApplicationDbContext db)
+	{
+		_db = db;
+	}
+
+	[FromQuery]
+	public UserFileListRequest Search { get; set; } = new();
+
+	public PageOf<UserFileListModel> UserFiles { get; set; } = PageOf<UserFileListModel>.Empty();
+	public async Task OnGet()
+	{
+		UserFiles = await _db.UserFiles
+			.ThatArePublic()
+			.ByRecentlyUploaded()
+			.Select(uf => new UserFileListModel
+			{
+				Id = uf.Id,
+				Title = uf.Title,
+				FileName = uf.FileName,
+				Author = uf.Author!.UserName,
+				GameId = uf.GameId,
+				GameName = uf.Game != null ? uf.Game.DisplayName : "",
+				Frames = uf.Frames,
+				Rerecords = uf.Rerecords,
+				CommentCount = uf.Comments.Count,
+				UploadTimestamp = uf.UploadTimestamp,
+			})
+			.SortedPageOf(Search);
+	}
+}
---
+++ TASVideos/Pages/UserFiles/Models/UserFileListModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TASVideos.Core;
+
+namespace TASVideos.Pages.UserFiles.Models;
+
+public class UserFileListModel
+{
+	[TableIgnore]
+	public long Id { get; set; }
+
+	public string Title { get; set; } = "";
+
+	[TableIgnore]
+	public string FileName { get; set; } = "";
+
+	[Sortable]
+	public string Author { get; set; } = "";
+
+	[TableIgnore]
+	public int? GameId { get; set; }
+
+	[Sortable]
+	[Display(Name = "Game")]
+	public string GameName { get; set; } = "";
+
+	[Sortable]
+	public int Frames { get; set; }
+
+	[Sortable]
+	public int Rerecords { get; set; }
+
+	[Sortable]
+	[Display(Name = "Comments")]
+	public int CommentCount { get; set; }
+
+	[Sortable]
+	[Display(Name = "Uploaded")]
+	public DateTime UploadTimestamp { get; set; }
+}
