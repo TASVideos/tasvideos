d51b5ddbadc37e4185344b744d470b7cf0ebdd75
Masterjun <masterjun3@gmail.com>
2024-02-22 01:21:28 +0100

    Add Userfiles List Page (#1779)
    * add userfiles list page
    
    * use proper column name as default

---
+++ TASVideos/Pages/UserFiles/Models/UserFileListRequest.cs
@@ -0,0 +1,13 @@
+﻿using TASVideos.Core;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.Pages.UserFiles.Models;
+
+public class UserFileListRequest : PagingModel
+{
+	public UserFileListRequest()
+	{
+		PageSize = 50;
+		Sort = $"-{nameof(UserFileListModel.UploadTimestamp)}";
+	}
+}
