a7fc323059ca5832c9318fc35a79380b32e5b1c1
adelikat <AndrewJDelikat@gmail.com>
2024-02-21 18:23:42 -0600

    cleanup migration stylecop nags, I will never remember to do that the first time

--- TASVideos.Data/Migrations/20240219153649_RemovePublicationBranch.cs
+++ TASVideos.Data/Migrations/20240219153649_RemovePublicationBranch.cs
@@ -2,28 +2,27 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
-{
-    /// <inheritdoc />
-    public partial class RemovePublicationBranch : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "branch",
-                table: "publications");
-        }
+namespace TASVideos.Data.Migrations;
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "branch",
-                table: "publications",
-                type: "citext",
-                maxLength: 50,
-                nullable: true);
-        }
-    }
+/// <inheritdoc />
+public partial class RemovePublicationBranch : Migration
+{
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "branch",
+			table: "publications");
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.AddColumn<string>(
+			name: "branch",
+			table: "publications",
+			type: "citext",
+			maxLength: 50,
+			nullable: true);
+	}
 }
