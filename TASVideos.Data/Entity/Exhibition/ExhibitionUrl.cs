946d7b1e0487d29a2c2b7cca2970d0212debbdc4
Masterjun3 <masterjun3@gmail.com>
2024-02-22 21:03:54 +0100

    wip

--- TASVideos.Data/Entity/Exhibition/ExhibitionUrl.cs
+++ TASVideos.Data/Entity/Exhibition/ExhibitionUrl.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TASVideos.Data.Entity.Exhibition;
+﻿namespace TASVideos.Data.Entity.Exhibition;
 public enum ExhibitionUrlType { Streaming, External }
 public class ExhibitionUrl
 {
