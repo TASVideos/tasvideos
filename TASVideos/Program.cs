9ec510e9ef897f03653dc0e383a058c8983169c9
Masterjun3 <masterjun3@gmail.com>
2024-03-05 19:51:06 +0100

    move all of Program into one method

--- TASVideos/Program.cs
+++ TASVideos/Program.cs
@@ -9,7 +9,22 @@ public class Program
 {
 	public static async Task Main(string[] args)
 	{
-		var host = BuildWebHost(args);
+		var config = new ConfigurationBuilder()
+			.AddCommandLine(args)
+			.Build();
+
+		var host = WebHost.CreateDefaultBuilder(args)
+			.UseConfiguration(config)
+			.UseStartup<Startup>()
+			.UseSerilog()
+			.ConfigureAppConfiguration((hostContext, builder) =>
+			{
+				// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
+				// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
+				// Manually specify the secret id (matching the csproj) here as a workaround.
+				builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
+			})
+			.Build();
 
 		using (var scope = host.Services.CreateScope())
 		{
@@ -28,25 +43,5 @@ public class Program
 		}
 
 		await host.RunAsync();
-	}
-
-	public static IWebHost BuildWebHost(string[] args)
-	{
-		var config = new ConfigurationBuilder()
-			.AddCommandLine(args)
-			.Build();
-
-		return WebHost.CreateDefaultBuilder(args)
-			.UseConfiguration(config)
-			.UseStartup<Startup>()
-			.UseSerilog()
-			.ConfigureAppConfiguration((hostContext, builder) =>
-			{
-				// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
-				// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
-				// Manually specify the secret id (matching the csproj) here as a workaround.
-				builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
-			})
-			.Build();
 	}
 }
