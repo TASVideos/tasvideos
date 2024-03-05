c2e237cce69ad294c0be745fe8b87f1c78003486
Masterjun3 <masterjun3@gmail.com>
2024-03-05 20:17:04 +0100

    cut+paste from startup to program

--- TASVideos/Program.cs
+++ TASVideos/Program.cs
@@ -7,7 +7,41 @@ using TASVideos.Core.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//.UseStartup<Startup>()
+/*
+AppSettings Settings => Configuration.Get<AppSettings>()!
+
+// Mvc Project Services
+services
+	.AddAppSettings(Configuration)
+	.AddRequestLocalization()
+	.AddCookieConfiguration()
+	.AddGzipCompression(Settings)
+	.AddSwagger(Settings)
+	.AddTextModules();
+
+// Internal Libraries
+string dbConnection = Settings.UseSampleDatabase
+	? Settings.ConnectionStrings.PostgresSampleDataConnection
+	: Settings.ConnectionStrings.PostgresConnection;
+
+services
+	.AddTasvideosData(Environment.IsDevelopment(), dbConnection)
+	.AddTasvideosCore<WikiToTextRenderer>(Environment.IsDevelopment(), Settings)
+	.AddMovieParser();
+
+// 3rd Party
+services
+	.AddMvcWithOptions(Environment)
+	.AddIdentity(Environment)
+	.AddReCaptcha(Configuration.GetSection("ReCaptcha"));
+
+services.AddWebOptimizer(pipeline =>
+{
+	pipeline.AddScssBundle("/css/bootstrap.css", "/css/bootstrap.scss");
+	pipeline.AddScssBundle("/css/site.css", "/css/site.scss");
+	pipeline.AddScssBundle("/css/forum.css", "/css/forum.scss");
+});
+*/
 
 builder.WebHost.UseSerilog();
 
@@ -17,6 +51,27 @@ builder.WebHost.UseSerilog();
 builder.Configuration.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
 
 var app = builder.Build();
+
+/*
+app
+	.UseRobots()
+	.UseMiddleware<Middleware.HtmlRedirectionMiddleware>()
+	.UseRequestLocalization()
+	.UseExceptionHandlers(env)
+	.UseGzipCompression(Settings)
+	.UseWebOptimizer()
+	.UseStaticFilesWithExtensionMapping()
+	.UseAuthentication()
+	.UseMiddleware<CustomLocalizationMiddleware>()
+	.UseSwaggerUi(Environment)
+	.UseLogging()
+	.UseMvcWithOptions(env);
+
+if (env.IsDevelopment())
+{
+	app.UseHttpsRedirection();
+}
+*/
 
 using (var scope = app.Services.CreateScope())
 {
