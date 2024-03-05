939cd3c530650408443c1fedbce46ce719b408e6
Masterjun3 <masterjun3@gmail.com>
2024-03-05 19:52:48 +0100

    convert to top level Program

--- TASVideos/Program.cs
+++ TASVideos/Program.cs
@@ -1,47 +1,40 @@
 ﻿using Microsoft.AspNetCore;
 using Serilog;
+using TASVideos;
 using TASVideos.Core.Data;
 
-namespace TASVideos;
-
 // Do not remove this class and use top-level design without ensuring EF migrations can still run
-public class Program
-{
-	public static async Task Main(string[] args)
+var config = new ConfigurationBuilder()
+	.AddCommandLine(args)
+	.Build();
+
+var host = WebHost.CreateDefaultBuilder(args)
+	.UseConfiguration(config)
+	.UseStartup<Startup>()
+	.UseSerilog()
+	.ConfigureAppConfiguration((hostContext, builder) =>
 	{
-		var config = new ConfigurationBuilder()
-			.AddCommandLine(args)
-			.Build();
+		// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
+		// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
+		// Manually specify the secret id (matching the csproj) here as a workaround.
+		builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
+	})
+	.Build();
 
-		var host = WebHost.CreateDefaultBuilder(args)
-			.UseConfiguration(config)
-			.UseStartup<Startup>()
-			.UseSerilog()
-			.ConfigureAppConfiguration((hostContext, builder) =>
-			{
-				// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
-				// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
-				// Manually specify the secret id (matching the csproj) here as a workaround.
-				builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
-			})
-			.Build();
+using (var scope = host.Services.CreateScope())
+{
+	var env = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+	var configuration = new ConfigurationBuilder()
+		.AddJsonFile("appsettings.json")
+		.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+		.Build();
+	Log.Logger = new LoggerConfiguration()
+		.ReadFrom.Configuration(configuration)
+		.CreateLogger();
 
-		using (var scope = host.Services.CreateScope())
-		{
-			var env = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
-			var configuration = new ConfigurationBuilder()
-				.AddJsonFile("appsettings.json")
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-				.Build();
-			Log.Logger = new LoggerConfiguration()
-				.ReadFrom.Configuration(configuration)
-				.CreateLogger();
+	var services = scope.ServiceProvider;
 
-			var services = scope.ServiceProvider;
-
-			await DbInitializer.InitializeDatabase(services);
-		}
-
-		await host.RunAsync();
-	}
+	await DbInitializer.InitializeDatabase(services);
 }
+
+await host.RunAsync();
