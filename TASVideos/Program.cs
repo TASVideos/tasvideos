62b657244e9d6aafd3a1dcf9bf5bc93b2e716b9e
Masterjun <masterjun3@gmail.com>
2024-03-06 02:58:18 +0100

    Use WebApplication Builder pattern instead of Startup (#1786)
    * move all of Program into one method
    
    * convert to top level Program
    
    * use webapplication instead of webhost
    
    * readd serilog and user secrets
    
    * cut+paste from startup to program
    
    * readd services
    
    * rename settings variable
    
    * readd app pipeline
    
    * move logger configuration outside the scope
    
    * remove comment, EF migrations work as normal

--- TASVideos/Program.cs
+++ TASVideos/Program.cs
@@ -1,52 +1,85 @@
-﻿using Microsoft.AspNetCore;
+﻿using AspNetCore.ReCaptcha;
 using Serilog;
+using TASVideos.Core;
 using TASVideos.Core.Data;
+using TASVideos.Core.Settings;
+using TASVideos.Data;
+using TASVideos.Middleware;
+using TASVideos.Services;
 
-namespace TASVideos;
+var builder = WebApplication.CreateBuilder(args);
 
-// Do not remove this class and use top-level design without ensuring EF migrations can still run
-public class Program
+AppSettings settings = builder.Configuration.Get<AppSettings>()!;
+
+// Mvc Project Services
+builder.Services.AddAppSettings(builder.Configuration);
+builder.Services.AddRequestLocalization();
+builder.Services.AddCookieConfiguration();
+builder.Services.AddGzipCompression(settings);
+builder.Services.AddSwagger(settings);
+builder.Services.AddTextModules();
+
+// Internal Libraries
+string dbConnection = settings.UseSampleDatabase
+	? settings.ConnectionStrings.PostgresSampleDataConnection
+	: settings.ConnectionStrings.PostgresConnection;
+
+builder.Services.AddTasvideosData(builder.Environment.IsDevelopment(), dbConnection);
+builder.Services.AddTasvideosCore<WikiToTextRenderer>(builder.Environment.IsDevelopment(), settings);
+builder.Services.AddMovieParser();
+
+// 3rd Party
+builder.Services.AddMvcWithOptions(builder.Environment);
+builder.Services.AddIdentity(builder.Environment);
+builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
+
+builder.Services.AddWebOptimizer(pipeline =>
 {
-	public static async Task Main(string[] args)
-	{
-		var host = BuildWebHost(args);
+	pipeline.AddScssBundle("/css/bootstrap.css", "/css/bootstrap.scss");
+	pipeline.AddScssBundle("/css/site.css", "/css/site.scss");
+	pipeline.AddScssBundle("/css/forum.css", "/css/forum.scss");
+});
 
-		using (var scope = host.Services.CreateScope())
-		{
-			var env = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
-			var configuration = new ConfigurationBuilder()
-				.AddJsonFile("appsettings.json")
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
-				.Build();
-			Log.Logger = new LoggerConfiguration()
-				.ReadFrom.Configuration(configuration)
-				.CreateLogger();
+builder.WebHost.UseSerilog();
 
-			var services = scope.ServiceProvider;
+// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
+// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.Configuration.AddUserSecrets<T>() will not work.
+// Manually specify the secret id (matching the csproj) here as a workaround.
+builder.Configuration.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
 
-			await DbInitializer.InitializeDatabase(services);
-		}
+var app = builder.Build();
 
-		await host.RunAsync();
-	}
+app.UseRobots();
+app.UseMiddleware<HtmlRedirectionMiddleware>();
+app.UseRequestLocalization();
+app.UseExceptionHandlers(app.Environment);
+app.UseGzipCompression(settings);
+app.UseWebOptimizer();
+app.UseStaticFilesWithExtensionMapping();
+app.UseAuthentication();
+app.UseMiddleware<CustomLocalizationMiddleware>();
+app.UseSwaggerUi(app.Environment);
+app.UseLogging();
+app.UseMvcWithOptions(app.Environment);
 
-	public static IWebHost BuildWebHost(string[] args)
-	{
-		var config = new ConfigurationBuilder()
-			.AddCommandLine(args)
-			.Build();
-
-		return WebHost.CreateDefaultBuilder(args)
-			.UseConfiguration(config)
-			.UseStartup<Startup>()
-			.UseSerilog()
-			.ConfigureAppConfiguration((hostContext, builder) =>
-			{
-				// We use <GenerateAssemblyInfo>false</GenerateAssemblyInfo> to support GitVersionTask.
-				// This also suppresses the creation of [assembly: UserSecretsId("...")], so builder.AddUserSecrets<T>() will not work.
-				// Manually specify the secret id (matching the csproj) here as a workaround.
-				builder.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
-			})
-			.Build();
-	}
+if (app.Environment.IsDevelopment())
+{
+	app.UseHttpsRedirection();
 }
+
+var configuration = new ConfigurationBuilder()
+	.AddJsonFile("appsettings.json")
+	.AddJsonFile($"appsettings.{app.Environment.EnvironmentName}.json", true)
+	.Build();
+Log.Logger = new LoggerConfiguration()
+	.ReadFrom.Configuration(configuration)
+	.CreateLogger();
+
+using (var scope = app.Services.CreateScope())
+{
+	var services = scope.ServiceProvider;
+
+	await DbInitializer.InitializeDatabase(services);
+}
+
+await app.RunAsync();
