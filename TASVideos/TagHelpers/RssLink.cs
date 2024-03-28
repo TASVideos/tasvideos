3b1cfa33463f0e9b114fddc4a4513b6bc5d721cc
adelikat <AndrewJDelikat@gmail.com>
2024-03-28 16:27:46 -0500

    simplify Rss feed generation

---
+++ TASVideos/TagHelpers/RssLink.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+// A work-around for the fact that tools do not understand we are building RSS and not HTML, this makes errors and warnings go away
+public class RssLink : TagHelper
+{
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "link";
+	}
+}
