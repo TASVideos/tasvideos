04bef734fd79a2ce34b7419b3d385bcc375eb834
adelikat <AndrewJDelikat@gmail.com>
2024-04-01 20:28:13 -0500

    Simplify MovieMaintenanceLog module

--- TASVideos/WikiModules/MovieMaintenanceLog.cshtml.cs
+++ TASVideos/WikiModules/MovieMaintenanceLog.cshtml.cs
@@ -6,42 +6,23 @@ namespace TASVideos.WikiModules;
 [WikiModule(ModuleNames.MovieMaintenanceLog)]
 public class MovieMaintenanceLog(ApplicationDbContext db) : WikiViewComponent
 {
-	public IEnumerable<IGrouping<ParentPublicationMaintenanceEntry, PublicationMaintenanceLogEntry>> Global { get; set; } = [];
-	public IEnumerable<PublicationMaintenanceLogEntry> Default { get; set; } = [];
-	public bool DefaultMode { get; set; }
+	public IEnumerable<IGrouping<ParentLogEntry, LogEntry>> Logs { get; set; } = [];
+	public bool IsSinglePub { get; set; }
+	public int Next { get; set; }
 	public async Task<IViewComponentResult> InvokeAsync()
 	{
-		if (int.TryParse(Request.Query["id"], out int publicationId))
-		{
-			var publicationTitle = await db.Publications
-				.Where(p => p.Id == publicationId)
-				.Select(p => p.Title)
-				.SingleOrDefaultAsync();
-
-			if (publicationTitle is null)
-			{
-				return new ContentViewComponentResult($"Publication #{publicationId} not found.");
-			}
-
-			ViewData["pubTitle"] = publicationTitle;
-			ViewData["pubId"] = publicationId;
-
-			Default = await db.PublicationMaintenanceLogs
-				.Where(l => l.PublicationId == publicationId)
-				.Select(l => new PublicationMaintenanceLogEntry(l.Log, l.User!.UserName, l.TimeStamp))
-				.ToListAsync();
-			DefaultMode = true;
-			return View();
-		}
+		IsSinglePub = int.TryParse(Request.Query["id"], out int publicationId);
 
 		const int pageSize = 50;
 		int.TryParse(Request.Query["begin"], out int begin);
-		ViewData["next"] = begin + pageSize;
+		Next = begin + pageSize;
 
-		IEnumerable<IGrouping<ParentPublicationMaintenanceEntry, PublicationMaintenanceLogEntry>> globalEntries = (await db.PublicationMaintenanceLogs
-			.OrderByDescending(l => l.TimeStamp)
-			.Skip(begin)
-			.Take(pageSize)
+		var query = db.PublicationMaintenanceLogs.OrderByDescending(l => l.TimeStamp).AsQueryable();
+		query = IsSinglePub
+			? query.Where(p => p.PublicationId == publicationId)
+			: query.Skip(begin).Take(pageSize);
+
+		Logs = (await query
 			.Select(l => new
 			{
 				l.PublicationId,
@@ -52,12 +33,11 @@ public class MovieMaintenanceLog(ApplicationDbContext db) : WikiViewComponent
 			})
 			.ToListAsync())
 			.GroupBy(
-				gkey => new ParentPublicationMaintenanceEntry(gkey.PublicationId, gkey.PublicationTitle),
-				gvalue => new PublicationMaintenanceLogEntry(gvalue.Log, gvalue.UserName, gvalue.TimeStamp));
-		Global = globalEntries;
+				gkey => new ParentLogEntry(gkey.PublicationId, gkey.PublicationTitle),
+				gvalue => new LogEntry(gvalue.Log, gvalue.UserName, gvalue.TimeStamp));
 		return View();
 	}
 
-	public record PublicationMaintenanceLogEntry(string Log, string UserName, DateTime Timestamp);
-	public record ParentPublicationMaintenanceEntry(int Id, string Title);
+	public record LogEntry(string Log, string UserName, DateTime Timestamp);
+	public record ParentLogEntry(int Id, string Title);
 }
