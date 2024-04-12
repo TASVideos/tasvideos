c7a6429f6668d1a926d8a61bb05be2c0f6cd746e
adelikat <AndrewJDelikat@gmail.com>
2024-04-12 09:15:49 -0500

    add paging to media posts wiki module, and remove support for no longer needed parameters

--- TASVideos/WikiModules/MediaPosts.cshtml.cs
+++ TASVideos/WikiModules/MediaPosts.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using TASVideos.Core.Services.ExternalMediaPublisher;
+﻿using TASVideos.Core;
+using TASVideos.Core.Services.ExternalMediaPublisher;
 using TASVideos.WikiEngine;
 
 namespace TASVideos.WikiModules;
@@ -6,22 +7,18 @@ namespace TASVideos.WikiModules;
 [WikiModule(ModuleNames.MediaPosts)]
 public class MediaPosts(ApplicationDbContext db) : WikiViewComponent
 {
-	public List<MediaPost> Posts { get; set; } = [];
+	public PageOf<MediaPost> Posts { get; set; } = PageOf<MediaPost>.Empty();
 
 	public async Task<IViewComponentResult> InvokeAsync(int? days, int? limit)
 	{
-		var startDate = DateTime.UtcNow.AddDays(-(days ?? 7));
-
 		var canSeeRestricted = UserClaimsPrincipal.Has(PermissionTo.SeeRestrictedForums);
 
 		Posts = await db.MediaPosts
-			.Since(startDate)
 			.Where(m => canSeeRestricted || m.Type != PostType.Critical.ToString())
 			.Where(m => canSeeRestricted || m.Type != PostType.Administrative.ToString())
 			.Where(m => canSeeRestricted || m.Type != PostType.Log.ToString())
 			.ByMostRecent()
-			.Take(limit ?? 50)
-			.ToListAsync();
+			.PageOf(GetPaging());
 
 		return View();
 	}
