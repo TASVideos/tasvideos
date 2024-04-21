d6888b25350258440af0166beb5844dcd8aea209
adelikat <AndrewJDelikat@gmail.com>
2024-04-21 18:03:31 -0500

    Cleanup

--- TASVideos.Api/WebApplicationExtensions.cs
+++ TASVideos.Api/WebApplicationExtensions.cs
@@ -5,39 +5,35 @@ using Microsoft.Extensions.Hosting;
 namespace TASVideos.Api;
 public static class WebApplicationExtensions
 {
-	public static WebApplication UseTasvideosApiEndpoints(this WebApplication app, IHostEnvironment env)
+	public static IApplicationBuilder UseTasvideosApiEndpoints(this WebApplication app, IHostEnvironment env)
 	{
-		app.MapPublications()
+		return app.MapPublications()
 			.MapSubmissions()
 			.MapGames()
 			.MapSystems()
 			.MapUsers()
 			.MapTags()
-			.MapClasses();
-
-		UseSwaggerUi(app, env);
-
-		app.UseExceptionHandler(exceptionHandlerApp =>
-		{
-			exceptionHandlerApp.Run(async httpContext =>
+			.MapClasses()
+			.UseSwaggerUi(env)
+			.UseExceptionHandler(exceptionHandlerApp =>
 			{
-				if (httpContext.Request.Path.ToString().StartsWith("/api"))
+				exceptionHandlerApp.Run(async httpContext =>
 				{
-					var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
-					if (pds == null
-						|| !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
+					if (httpContext.Request.Path.ToString().StartsWith("/api"))
 					{
-						// Fallback behavior
-						await httpContext.Response.WriteAsync("Fallback: An error occurred.");
+						var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
+						if (pds == null
+							|| !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
+						{
+							// Fallback behavior
+							await httpContext.Response.WriteAsync("Fallback: An error occurred.");
+						}
 					}
-				}
+				});
 			});
-		});
-
-		return app;
 	}
 
-	private static void UseSwaggerUi(this WebApplication app, IHostEnvironment env)
+	private static IApplicationBuilder UseSwaggerUi(this IApplicationBuilder app, IHostEnvironment env)
 	{
 		// Append environment to app name when in non-production environments
 		var appName = "TASVideos";
@@ -46,14 +42,12 @@ public static class WebApplicationExtensions
 			appName += $" ({env.EnvironmentName})";
 		}
 
-		// Enable middleware to serve generated Swagger as a JSON endpoint.
-		app.UseSwagger();
-
-		// Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-		app.UseSwaggerUI(c =>
-		{
-			c.SwaggerEndpoint("/swagger/v1/swagger.json", appName);
-			c.RoutePrefix = "api";
-		});
+		return app
+			.UseSwagger()
+			.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", appName);
+				c.RoutePrefix = "api";
+			});
 	}
 }
