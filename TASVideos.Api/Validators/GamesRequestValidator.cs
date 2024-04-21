f28d6623a3b07d4dddd295d9119f60913cce8201
adelikat <AndrewJDelikat@gmail.com>
2024-04-21 11:24:30 -0500

    Refactor ApiRequestValidator logic to not be duplicated over and over

--- TASVideos.Api/Validators/GamesRequestValidator.cs
+++ TASVideos.Api/Validators/GamesRequestValidator.cs
@@ -1,14 +1,10 @@
 ﻿namespace TASVideos.Api.Validators;
+
 public class GamesRequestValidator : AbstractValidator<GamesRequest>
 {
 	public GamesRequestValidator()
 	{
 		RuleFor(g => g.Systems).MaximumLength(200);
-
-		// TODO: how to use ApiRequestValidator so these do not have to be copy/pasted?
-		RuleFor(a => a.PageSize).Must(ps => ps is null or >= 1 and <= ApiConstants.MaxPageSize).WithMessage("Invalid page size");
-		RuleFor(a => a.CurrentPage).Must(c => c is null or >= 1).WithMessage("Invalid current page");
-		RuleFor(a => a.Sort).MaximumLength(200).Must(sort => sort.IsValidSort(typeof(GamesResponse)));
-		RuleFor(a => a.Fields).MaximumLength(200);
+		this.ValidateApiRequest(typeof(GamesResponse));
 	}
 }
--- TASVideos.Api/Validators/PublicationsRequestValidator.cs
+++ TASVideos.Api/Validators/PublicationsRequestValidator.cs
@@ -5,11 +5,6 @@ public class PublicationsRequestValidator : AbstractValidator<PublicationsReques
 	{
 		RuleFor(p => p.StartYear).Must(st => st is null or >= 2000).WithMessage("Year must be 2000 or greater");
 		RuleFor(p => p.EndYear).Must(st => st is null or >= 2000).WithMessage("Year must be 2000 or greater");
-
-		// TODO: how to use ApiRequestValidator so these do not have to be copy/pasted?
-		RuleFor(a => a.PageSize).Must(ps => ps is null or >= 1 and <= ApiConstants.MaxPageSize).WithMessage("Invalid page size");
-		RuleFor(a => a.CurrentPage).Must(c => c is null or >= 1).WithMessage("Invalid current page");
-		RuleFor(a => a.Sort).MaximumLength(200).Must(sort => sort.IsValidSort(typeof(PublicationsResponse)));
-		RuleFor(a => a.Fields).MaximumLength(200);
+		this.ValidateApiRequest(typeof(PublicationsResponse));
 	}
 }
