a574668b68984ae5924969fe6a77e813c1b20048
adelikat <AndrewJDelikat@gmail.com>
2024-04-22 11:41:20 -0500

    Cleanup

--- TASVideos.Api/Requests/PublicationsRequest.cs
+++ TASVideos.Api/Requests/PublicationsRequest.cs
@@ -3,40 +3,40 @@
 internal class PublicationsRequest : ApiRequest, IPublicationTokens
 {
 	[SwaggerParameter("The system codes to filter by")]
-	public string? Systems { get; set; }
+	public string? Systems { get; init; }
 
 	[SwaggerParameter("The publication class names to filter by")]
-	public string? ClassNames { get; set; }
+	public string? ClassNames { get; init; }
 
 	[SwaggerParameter("The start year to filter by")]
-	public int? StartYear { get; set; }
+	public int? StartYear { get; init; }
 
 	[SwaggerParameter("The end year to filter by")]
-	public int? EndYear { get; set; }
+	public int? EndYear { get; init; }
 
 	[SwaggerParameter("The genre names to filter by")]
-	public string? GenreNames { get; set; }
+	public string? GenreNames { get; init; }
 
 	[SwaggerParameter("The names of the publication tags to filter by")]
-	public string? TagNames { get; set; }
+	public string? TagNames { get; init; }
 
 	[SwaggerParameter("The names of the publication flags to filter by")]
-	public string? FlagNames { get; set; }
+	public string? FlagNames { get; init; }
 
 	[SwaggerParameter("The ids of the authors to filter by")]
-	public string? AuthorIds { get; set; }
+	public string? AuthorIds { get; init; }
 
 	[SwaggerParameter("Indicates whether or not to return obsoleted publications")]
-	public bool? ShowObsoleted { get; set; }
+	public bool? ShowObsoleted { get; init; }
 
 	[SwaggerParameter("Indicates whether or not to only return obsoleted publications")]
-	public bool? OnlyObsoleted { get; set; }
+	public bool? OnlyObsoleted { get; init; }
 
 	[SwaggerParameter("The ids of the games to filter by")]
-	public string? GameIds { get; set; }
+	public string? GameIds { get; init; }
 
 	[SwaggerParameter("The ids of the game groups to filter by")]
-	public string? GameGroupIds { get; set; }
+	public string? GameGroupIds { get; init; }
 
 	bool IPublicationTokens.ShowObsoleted => ShowObsoleted ?? false;
 	bool IPublicationTokens.OnlyObsoleted => OnlyObsoleted ?? false;
--- TASVideos.Api/Requests/SubmissionsRequest.cs
+++ TASVideos.Api/Requests/SubmissionsRequest.cs
@@ -3,25 +3,25 @@
 internal class SubmissionsRequest : ApiRequest, ISubmissionFilter
 {
 	[SwaggerParameter("The statuses to filter by")]
-	public string? Statuses { get; set; }
+	public string? Statuses { get; init; }
 
 	[SwaggerParameter("The author/submitter name to filter by")]
-	public string? User { get; set; }
+	public string? User { get; init; }
 
 	[SwaggerParameter("The start year to filter by")]
-	public int? StartYear { get; set; }
+	public int? StartYear { get; init; }
 
 	[SwaggerParameter("The end year to filter by")]
-	public int? EndYear { get; set; }
+	public int? EndYear { get; init; }
 
 	[SwaggerParameter("The system codes to filter by")]
-	public string? Systems { get; set; }
+	public string? Systems { get; init; }
 
 	[SwaggerParameter("The ids of the games to filter by")]
-	public string? Games { get; set; }
+	public string? Games { get; init; }
 
 	[SwaggerParameter("The start type of the movie. 0 = Power On, 1 = Sram, 2 = Savestate")]
-	public int? StartType { get; set; }
+	public int? StartType { get; init; }
 
 	ICollection<int> ISubmissionFilter.Years => StartYear.YearRange(EndYear).ToList();
 
