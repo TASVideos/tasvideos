f63e41adbb941e5882b28d83b2d9666070f2dcbc
adelikat <AndrewJDelikat@gmail.com>
2024-04-28 21:43:10 -0500

    Simplify AwardsEditor/CreateCategory by having bound proeprites instead of a ViewModel

--- TASVideos/Pages/AwardsEditor/CreateCategory.cshtml.cs
+++ TASVideos/Pages/AwardsEditor/CreateCategory.cshtml.cs
@@ -6,12 +6,32 @@ namespace TASVideos.Pages.AwardsEditor;
 [RequirePermission(PermissionTo.CreateAwards)]
 public class CreateCategoryModel(IAwards awards, IMediaFileUploader mediaFileUploader) : BasePageModel
 {
-	public List<SelectListItem> AvailableAwardTypes { get; set; } = Enum
+	public List<SelectListItem> AvailableAwardTypes => Enum
 		.GetValues<AwardType>()
 		.ToDropDown();
 
 	[BindProperty]
-	public CreateAwardCategoryModel AwardCategory { get; set; } = new();
+	public AwardType Type { get; init; }
+
+	[BindProperty]
+	[StringLength(25)]
+	public string ShortName { get; init; } = "";
+
+	[BindProperty]
+	[StringLength(50)]
+	public string Description { get; init; } = "";
+
+	[BindProperty]
+	[Required]
+	public IFormFile? BaseImage { get; init; }
+
+	[BindProperty]
+	[Required]
+	public IFormFile? BaseImage2X { get; init; }
+
+	[BindProperty]
+	[Required]
+	public IFormFile? BaseImage4X { get; init; }
 
 	public async Task<IActionResult> OnPost()
 	{
@@ -20,48 +40,22 @@ public class CreateCategoryModel(IAwards awards, IMediaFileUploader mediaFileUpl
 			return Page();
 		}
 
-		if (!AwardCategory.BaseImage!.FileName.EndsWith(".png"))
+		if (!BaseImage!.FileName.EndsWith(".png"))
 		{
-			ModelState.AddModelError($"{nameof(AwardCategory)}.{nameof(AwardCategory.BaseImage)}", "Invalid image type, must be .png");
+			ModelState.AddModelError($"{nameof(BaseImage)}", "Invalid image type, must be .png");
 			return Page();
 		}
 
 		await mediaFileUploader.UploadAwardImage(
-			AwardCategory.BaseImage!,
-			AwardCategory.BaseImage2X!,
-			AwardCategory.BaseImage4X!,
-			AwardCategory.ShortName);
+			BaseImage!, BaseImage2X!, BaseImage4X!, ShortName);
 
 		var result = await awards.AddAwardCategory(
-			AwardCategory.Type,
-			AwardCategory.ShortName,
-			AwardCategory.Description);
-
+			Type, ShortName, Description);
 		if (!result)
 		{
 			ModelState.AddModelError("", "An award with this short name and description already exist");
 		}
 
 		return BasePageRedirect("Index");
-	}
-
-	public class CreateAwardCategoryModel
-	{
-		public AwardType Type { get; init; }
-
-		[StringLength(25)]
-		public string ShortName { get; init; } = "";
-
-		[StringLength(50)]
-		public string Description { get; init; } = "";
-
-		[Required]
-		public IFormFile? BaseImage { get; init; }
-
-		[Required]
-		public IFormFile? BaseImage2X { get; init; }
-
-		[Required]
-		public IFormFile? BaseImage4X { get; init; }
 	}
 }
