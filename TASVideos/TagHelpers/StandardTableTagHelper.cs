b6eb5c1d096cf577f71e5dd8e14143aca08c71a8
adelikat <AndrewJDelikat@gmail.com>
2024-05-11 12:53:31 -0500

    create a standard-table tag helper that sets all the bootstrap classes we want and make all tables use this. In some cases this might trade looks for consistency but I choose consistency, in most places it made things better.  If we want to style things differently we can now do it in one place

---
+++ TASVideos/TagHelpers/StandardTableTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class StandardTableTagHelper : TagHelper
+{
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "table";
+		output.AddCssClass("table");
+		output.AddCssClass("table-sm");
+		output.AddCssClass("table-bordered");
+		output.AddCssClass("table-striped");
+		output.AddCssClass("table-responsive");
+	}
+}
