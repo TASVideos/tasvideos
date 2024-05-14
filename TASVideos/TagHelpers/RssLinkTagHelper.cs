298afb01b77fb734058ce6ae5233eadd803b55d4
adelikat <AndrewJDelikat@gmail.com>
2024-05-14 08:54:29 -0500

    fix a couple of naming inconsistencies in TagHelper calsses

--- TASVideos/TagHelpers/RssLinkTagHelper.cs
+++ TASVideos/TagHelpers/RssLinkTagHelper.cs
@@ -3,7 +3,7 @@
 namespace TASVideos.TagHelpers;
 
 // A work-around for the fact that tools do not understand we are building RSS and not HTML, this makes errors and warnings go away
-public class RssLink : TagHelper
+public class RssLinkTagHelper : TagHelper
 {
 	public override void Process(TagHelperContext context, TagHelperOutput output)
 	{
