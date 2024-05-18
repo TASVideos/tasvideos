2fd2f67f1aae38a21f4501f0559708e389fc8e73
adelikat <AndrewJDelikat@gmail.com>
2024-05-18 16:21:55 -0500

    UI cleanups for Tag/Flag pages, introducing some new tag/taghelpers and use those helpers on some other pages

---
+++ TASVideos/TagHelpers/AddButtonTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class AddButtonTagHelper(IHtmlGenerator generator) : AnchorTagHelper(generator)
+{
+	public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "a";
+		await base.ProcessAsync(context, output);
+		output.AddCssClass("btn");
+		output.AddCssClass("btn-primary");
+		var content = (await output.GetChildContentAsync()).GetContent();
+		if (string.IsNullOrWhiteSpace(content))
+		{
+			output.Content.AppendHtml("<i class=\"fa fa-plus\"></i> Add");
+		}
+	}
+}
