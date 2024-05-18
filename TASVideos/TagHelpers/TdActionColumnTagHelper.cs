a530e9016fcea916485d03adffa627c5157ed295
adelikat <AndrewJDelikat@gmail.com>
2024-05-18 10:41:36 -0500

    cleanup action columns on tables using better css, and create a tag helper to make it easier to use, and wire it up to various tables, also consistently use btn-sm on these action buttons

---
+++ TASVideos/TagHelpers/TdActionColumnTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class TdActionColumnTagHelper : TagHelper
+{
+	public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "td";
+		var content = (await output.GetChildContentAsync()).GetContent();
+		output.Content.SetHtmlContent(
+			$"""
+			<div class='action-column'>
+				{content}
+			</div>
+			""");
+	}
+}
