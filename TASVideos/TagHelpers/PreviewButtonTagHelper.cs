2e92ba8ad02861d9ba6679d028fc8c8faf9c25f8
adelikat <AndrewJDelikat@gmail.com>
2024-05-18 12:04:44 -0500

    preview-button tag helper

---
+++ TASVideos/TagHelpers/PreviewButtonTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class PreviewButtonTagHelper : TagHelper
+{
+	public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "button";
+		output.Attributes.Add("type", "button");
+		output.Attributes.Add("id", "preview-button");
+		output.AddCssClass("btn");
+		output.AddCssClass("btn-secondary");
+
+		var content = (await output.GetChildContentAsync()).GetContent();
+		if (string.IsNullOrWhiteSpace(content))
+		{
+			output.Content.AppendHtml("<i class=\"fa fa-eye\"></i> Preview");
+		}
+	}
+}
