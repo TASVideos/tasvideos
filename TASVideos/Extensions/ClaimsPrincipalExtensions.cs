23aa39349dab7b999799b6d34fcfe0f7a906c4d9
adelikat <AndrewJDelikat@gmail.com>
2024-05-26 10:09:12 -0500

    Simplify some code with an extension method

---
+++ TASVideos/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace TASVideos;
+
+public static class ClaimsPrincipalExtensions
+{
+	public static bool CanEditWiki(this ClaimsPrincipal user, string pageName)
+		=> WikiHelper.UserCanEditWikiPage(pageName, user.Name(), user.Permissions());
+}
