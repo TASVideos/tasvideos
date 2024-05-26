6d4566ebc149f4685f920af7ac7b81f9fd0a1eb6
adelikat <AndrewJDelikat@gmail.com>
2024-05-26 17:40:54 -0500

    back-button tag helper

---
+++ TASVideos/TagHelpers/BackButtonTagHelper.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class BackButtonTagHelper(IHtmlGenerator generator, IHttpContextAccessor httpContextAccessor) : AnchorTagHelper(generator)
+{
+	public string? NameOverride { get; set; }
+
+	public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "a";
+
+		var returnUrl = GetReturnUrl();
+		if (!string.IsNullOrWhiteSpace(returnUrl))
+		{
+			output.Attributes.SetAttribute("href", returnUrl);
+			Page = null;
+			Action = null;
+			Area = null;
+			PageHandler = null;
+			Fragment = null;
+			RouteValues = new Dictionary<string, string>();
+		}
+
+		await base.ProcessAsync(context, output);
+		output.AddCssClass("btn");
+		output.AddCssClass("btn-secondary");
+
+		var content = (await output.GetChildContentAsync()).GetContent();
+		if (string.IsNullOrWhiteSpace(content))
+		{
+			var name = string.IsNullOrEmpty(NameOverride) ? "Back" : NameOverride;
+			output.Content.AppendHtml($"<i class=\"fa fa-arrow-left\"></i> {name}");
+		}
+	}
+
+	private string? GetReturnUrl() => httpContextAccessor.HttpContext?.Request.ReturnUrl();
+}
