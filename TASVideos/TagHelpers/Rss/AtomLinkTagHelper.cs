eca96f2a3e1e7a40dd7197a67d4d0cf7d55a17bf
adelikat <AndrewJDelikat@gmail.com>
2024-05-28 12:04:36 -0500

    Rss - fix rss names, add some taghelpers for some common stuff, move rss taghelpers to a folder

---
+++ TASVideos/TagHelpers/Rss/AtomLinkTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+[HtmlTargetElement("atom-link", TagStructure = TagStructure.WithoutEndTag)]
+public class AtomLinkTagHelper : TagHelper
+{
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "atom:link";
+		output.Attributes.Add("rel", "self");
+		output.Attributes.Add("type", "application/rss+xml");
+		output.Attributes.Add("xmlns", "atom");
+	}
+}
---
+++ TASVideos/TagHelpers/Rss/RssTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+// A work-around for the fact that tools do not understand we are building RSS and not HTML, this makes errors and warnings go away
+public class RssTagHelper : TagHelper
+{
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "rss";
+		output.Attributes.Add("version", "2.0");
+		output.Attributes.Add("xmlns:atom", "http://www.w3.org/2005/Atom");
+		output.Attributes.Add("xmlns:media", "http://search.yahoo.com/mrss/");
+	}
+}
