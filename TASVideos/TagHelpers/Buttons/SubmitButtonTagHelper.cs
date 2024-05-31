934a92d404ac16cb43a0d1fcd3dacf92b57ea25c
adelikat <AndrewJDelikat@gmail.com>
2024-05-31 11:33:00 -0500

    do double-click prevent logic automatically and without the submit-button tag helper setting it up, fixes the look of the user/list search bar, and keeps it from being a bunch of inline scripts for every button

--- TASVideos/TagHelpers/Buttons/SubmitButtonTagHelper.cs
+++ TASVideos/TagHelpers/Buttons/SubmitButtonTagHelper.cs
@@ -8,21 +8,8 @@ public class SubmitButtonTagHelper : TagHelper
 
 	public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
 	{
-		var guid = Guid.NewGuid();
 		output.TagName = "button";
 		output.Attributes.Add("type", "submit");
-		output.Attributes.Add("data-submit-id", guid);
-		output.PostElement.AppendHtml(
-			$$"""
-				<script>
-					document.querySelector('[data-submit-id="{{guid}}"]').onclick = function () {
-						let btn = this;
-						setTimeout(function () { btn.disabled = true }, 0);
-						setTimeout(function () { btn.disabled = false }, 750);
-					}
-				</script>
-				""");
-
 		output.AddCssClass("btn");
 
 		output.AddCssClass(string.IsNullOrEmpty(BtnClassOverride)
---
+++ TASVideos/wwwroot/js/prevent-double-submit.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", findSubmitButtons);
+
+function findSubmitButtons() {
+	Array.from(document.querySelectorAll('button[type="submit"]')).forEach(btn => {
+		btn.onclick = function() {
+			let btn = this;
+			setTimeout(function () { btn.disabled = true }, 0);
+			setTimeout(function () { btn.disabled = false }, 750);
+		}
+	});
+}
