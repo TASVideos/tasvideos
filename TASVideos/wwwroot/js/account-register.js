57f15a5b0854caa93797cc414226504d410a5fd1
adelikat <AndrewJDelikat@gmail.com>
2024-06-06 13:05:39 -0500

    move some js out of a view into a js file

---
+++ TASVideos/wwwroot/js/account-register.js
@@ -0,0 +1,6 @@
+﻿// Autofill timezone if not set
+if (!document.querySelector("#TimeZone option:checked").value) {
+	const timezoneOffset = 0 - new Date().getTimezoneOffset();
+	let timezone = document.querySelector(`[data-offset="${timezoneOffset}"]`);
+	timezone.setAttribute('selected', 'selected');
+}
