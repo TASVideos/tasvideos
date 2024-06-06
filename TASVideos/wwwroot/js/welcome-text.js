aa0b222165f62a271be10e544e09b5ffb006b94b
adelikat <AndrewJDelikat@gmail.com>
2024-06-06 13:02:15 -0500

    move some js out of a view into a js file

---
+++ TASVideos/wwwroot/js/welcome-text.js
@@ -0,0 +1,11 @@
+﻿let welcomeHeader = document.getElementById("welcome-header");
+let dismissBtn = document.getElementById("welcome-header-dismiss");
+const dismiss = localStorage.getItem("DismissWelcomeHeader");
+if (dismiss !== "true") {
+	welcomeHeader.classList.remove("d-none");
+}
+
+dismissBtn.onclick = function () {
+	localStorage.setItem("DismissWelcomeHeader", true);
+	welcomeHeader.classList.add("d-none");
+}
