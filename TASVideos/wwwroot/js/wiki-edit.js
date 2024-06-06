dc9c131d3b5df911593ae666a02a14772b956054
adelikat <AndrewJDelikat@gmail.com>
2024-06-06 12:44:09 -0500

    move some js out of a view into a js file

---
+++ TASVideos/wwwroot/js/wiki-edit.js
@@ -0,0 +1,5 @@
+﻿window.addEventListener("load", function() {
+	const rightMarkupElem = document.querySelector('[data-id=wiki-markup]');
+	rightMarkupElem.addEventListener("change", generateDiff);
+	rightMarkupElem.addEventListener("keyup", generateDiff);
+});
