8e5e0a1452fe5316c4a17e9fc13bd08b46c48a95
adelikat <AndrewJDelikat@gmail.com>
2024-06-17 07:43:43 -0500

    add youtube to csp

--- TASVideos/Extensions/ApplicationBuilderExtensions.cs
+++ TASVideos/Extensions/ApplicationBuilderExtensions.cs
@@ -65,7 +65,7 @@ public static class ApplicationBuilderExtensions
 			context.Response.Headers.XContentTypeOptions = "nosniff";
 			context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
 			context.Response.Headers.XPoweredBy = "";
-			context.Response.Headers.ContentSecurityPolicy = "upgrade-insecure-requests; script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com ";
+			context.Response.Headers.ContentSecurityPolicy = "upgrade-insecure-requests; script-src 'self' https://code.jquery.com https://cdn.jsdelivr.net https://cdnjs.cloudflare.com https://www.youtube.com/iframe_api";
 			await next();
 		});
 
