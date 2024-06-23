0f2f0188e83808c66a455d2d74b31d9eb33b4836
adelikat <AndrewJDelikat@gmail.com>
2024-06-23 10:28:42 -0500

    forum-textarea tag helper

---
+++ TASVideos/TagHelpers/ForumTextareaTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class ForumTextareaTagHelper(IHtmlGenerator generator) : TextAreaTagHelper(generator)
+{
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "textarea";
+		output.Attributes.Add("placeholder", "Enter your [b]bbcode[/b] here...");
+		output.Attributes.Add("data-id", "forum-edit");
+		base.Process(context, output);
+	}
+}
