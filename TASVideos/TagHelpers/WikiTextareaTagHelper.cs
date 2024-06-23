8e2fb84c2b8b51224a577ece9ae189301091065a
adelikat <AndrewJDelikat@gmail.com>
2024-06-23 10:16:35 -0500

    wiki-textarea tag helper that adds the data id and palceholder attributes automatically

---
+++ TASVideos/TagHelpers/WikiTextareaTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TASVideos.TagHelpers;
+
+public class WikiTextareaTagHelper(IHtmlGenerator generator) : TextAreaTagHelper(generator)
+{
+	public override void Process(TagHelperContext context, TagHelperOutput output)
+	{
+		output.TagName = "textarea";
+		output.Attributes.Add("placeholder", "Enter your __wiki markup__ here...");
+		output.Attributes.Add("data-id", "wiki-edit");
+		base.Process(context, output);
+	}
+}
