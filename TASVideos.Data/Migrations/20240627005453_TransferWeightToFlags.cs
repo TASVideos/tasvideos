2afccf92b0002ddc1119b3aac631c535daefcfb0
adelikat <AndrewJDelikat@gmail.com>
2024-06-27 08:53:24 -0500

    cleanup migration formatting

--- TASVideos.Data/Migrations/20240627005453_TransferWeightToFlags.cs
+++ TASVideos.Data/Migrations/20240627005453_TransferWeightToFlags.cs
@@ -2,39 +2,38 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
+namespace TASVideos.Data.Migrations;
+
+/// <inheritdoc />
+public partial class TransferWeightToFlags : Migration
 {
-    /// <inheritdoc />
-    public partial class TransferWeightToFlags : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "weight",
-                table: "publication_classes");
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "weight",
+			table: "publication_classes");
 
-            migrationBuilder.AddColumn<double>(
-                name: "weight",
-                table: "flags",
-                type: "double precision",
-                nullable: false,
-                defaultValue: 0.0);
-        }
+		migrationBuilder.AddColumn<double>(
+			name: "weight",
+			table: "flags",
+			type: "double precision",
+			nullable: false,
+			defaultValue: 0.0);
+	}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "weight",
-                table: "flags");
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "weight",
+			table: "flags");
 
-            migrationBuilder.AddColumn<double>(
-                name: "weight",
-                table: "publication_classes",
-                type: "double precision",
-                nullable: false,
-                defaultValue: 0.0);
-        }
-    }
+		migrationBuilder.AddColumn<double>(
+			name: "weight",
+			table: "publication_classes",
+			type: "double precision",
+			nullable: false,
+			defaultValue: 0.0);
+	}
 }
