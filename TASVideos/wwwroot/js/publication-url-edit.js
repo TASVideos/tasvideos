23bcb1a138efaa553a5adbde045e5fdb8d0f11fe
adelikat <AndrewJDelikat@gmail.com>
2024-06-29 12:58:33 -0500

    publication url edit -> automatically pick Mirror for the user, if putting an archive.org link, resolves #1893

---
+++ TASVideos/wwwroot/js/publication-url-edit.js
@@ -0,0 +1,9 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+	const urlBox = document.querySelector('[data-id="url"]');
+	urlBox.addEventListener('input', () => {
+		if (urlBox.value && urlBox.value.includes('archive.org')) {
+			const typeDropdown = document.querySelector('[data-id="url-types"]');
+			typeDropdown.value = '1';
+		}
+	});
+});
