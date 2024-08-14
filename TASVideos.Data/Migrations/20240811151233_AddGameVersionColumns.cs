840fd13b03f5c184f1939a2226b870ca8edbcd68
adelikat <AndrewJDelikat@gmail.com>
2024-08-14 09:30:14 -0500

    stylcop on latest migration

--- TASVideos.Data/Migrations/20240811151233_AddGameVersionColumns.cs
+++ TASVideos.Data/Migrations/20240811151233_AddGameVersionColumns.cs
@@ -4,37 +4,37 @@
 
 namespace TASVideos.Data.Migrations
 {
-    /// <inheritdoc />
-    public partial class AddGameVersionColumns : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "notes",
-                table: "game_versions",
-                type: "citext",
-                maxLength: 1000,
-                nullable: true);
+	/// <inheritdoc />
+	public partial class AddGameVersionColumns : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AddColumn<string>(
+				name: "notes",
+				table: "game_versions",
+				type: "citext",
+				maxLength: 1000,
+				nullable: true);
 
-            migrationBuilder.AddColumn<string>(
-                name: "source_db",
-                table: "game_versions",
-                type: "citext",
-                maxLength: 50,
-                nullable: true);
-        }
+			migrationBuilder.AddColumn<string>(
+				name: "source_db",
+				table: "game_versions",
+				type: "citext",
+				maxLength: 50,
+				nullable: true);
+		}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "notes",
-                table: "game_versions");
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.DropColumn(
+				name: "notes",
+				table: "game_versions");
 
-            migrationBuilder.DropColumn(
-                name: "source_db",
-                table: "game_versions");
-        }
-    }
+			migrationBuilder.DropColumn(
+				name: "source_db",
+				table: "game_versions");
+		}
+	}
 }
