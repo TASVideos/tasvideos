7a67b966afcd9bad5859e1d472e68931e5d90828
adelikat <AndrewJDelikat@gmail.com>
2024-08-14 09:30:42 -0500

    ugh

--- TASVideos.Data/Migrations/20240811151233_AddGameVersionColumns.cs
+++ TASVideos.Data/Migrations/20240811151233_AddGameVersionColumns.cs
@@ -2,39 +2,38 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
+namespace TASVideos.Data.Migrations;
+
+/// <inheritdoc />
+public partial class AddGameVersionColumns : Migration
 {
 	/// <inheritdoc />
-	public partial class AddGameVersionColumns : Migration
+	protected override void Up(MigrationBuilder migrationBuilder)
 	{
-		/// <inheritdoc />
-		protected override void Up(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.AddColumn<string>(
-				name: "notes",
-				table: "game_versions",
-				type: "citext",
-				maxLength: 1000,
-				nullable: true);
+		migrationBuilder.AddColumn<string>(
+			name: "notes",
+			table: "game_versions",
+			type: "citext",
+			maxLength: 1000,
+			nullable: true);
 
-			migrationBuilder.AddColumn<string>(
-				name: "source_db",
-				table: "game_versions",
-				type: "citext",
-				maxLength: 50,
-				nullable: true);
-		}
+		migrationBuilder.AddColumn<string>(
+			name: "source_db",
+			table: "game_versions",
+			type: "citext",
+			maxLength: 50,
+			nullable: true);
+	}
 
-		/// <inheritdoc />
-		protected override void Down(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropColumn(
-				name: "notes",
-				table: "game_versions");
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "notes",
+			table: "game_versions");
 
-			migrationBuilder.DropColumn(
-				name: "source_db",
-				table: "game_versions");
-		}
+		migrationBuilder.DropColumn(
+			name: "source_db",
+			table: "game_versions");
 	}
 }
