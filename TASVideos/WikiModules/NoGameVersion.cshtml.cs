0e6b06bafc3bb8fdf6ddddeb35e5ae646ada0f95
adelikat <AndrewJDelikat@gmail.com>
2024-08-26 15:31:30 -0500

    NoGameVersion wiki module - refactor to be game version based, and show both missing hashes, and only having 1 hash

--- TASVideos/WikiModules/NoGameVersion.cshtml.cs
+++ TASVideos/WikiModules/NoGameVersion.cshtml.cs
@@ -5,15 +5,23 @@ namespace TASVideos.WikiModules;
 [WikiModule(ModuleNames.NoGameVersion)]
 public class NoGameVersion(ApplicationDbContext db) : WikiViewComponent
 {
-	public List<NoGame.Entry> Publications { get; set; } = [];
+	public List<NoHashEntry> GameVersions { get; set; } = [];
 	public List<NoGame.Entry> Submissions { get; set; } = [];
 
 	public async Task<IViewComponentResult> InvokeAsync()
 	{
-		Publications = await db.Publications
-			.Where(p => p.GameVersionId == -1)
-			.OrderBy(p => p.Id)
-			.Select(p => new NoGame.Entry(p.Id, p.Title))
+		GameVersions = await db.GameVersions
+			.Where(v => v.Sha1 == null || v.Md5 == null)
+			.Select(v => new NoHashEntry(
+				v.Id,
+				v.Name,
+				v.GameId,
+				v.Game!.DisplayName,
+				v.Sha1,
+				v.Md5,
+				v.System!.Code,
+				v.Publications.Select(p => new NoGame.Entry(p.Id, p.Title)).ToList(),
+				v.Submissions.Select(s => new NoGame.Entry(s.Id, s.Title)).ToList()))
 			.ToListAsync();
 		Submissions = await db.Submissions
 			.Where(s => s.GameVersionId == null || s.GameVersionId < 1)
@@ -24,4 +32,15 @@ public class NoGameVersion(ApplicationDbContext db) : WikiViewComponent
 
 		return View();
 	}
+
+	public record NoHashEntry(
+		int GameVersionId,
+		string GameVersionName,
+		int GameId,
+		string GameName,
+		string? Sha1,
+		string? Md5,
+		string SystemCode,
+		List<NoGame.Entry> Pubs,
+		List<NoGame.Entry> Subs);
 }
