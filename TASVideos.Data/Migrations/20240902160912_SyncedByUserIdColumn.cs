c12fd5c627c8782fedc69053f3c57f33465488ff
adelikat <AndrewJDelikat@gmail.com>
2024-09-02 12:19:30 -0500

    stylecope nags in latest migration file

--- TASVideos.Data/Migrations/20240902160912_SyncedByUserIdColumn.cs
+++ TASVideos.Data/Migrations/20240902160912_SyncedByUserIdColumn.cs
@@ -2,58 +2,57 @@
 
 #nullable disable
 
-namespace TASVideos.Data.Migrations
+namespace TASVideos.Data.Migrations;
+
+/// <inheritdoc />
+public partial class SyncedByUserIdColumn : Migration
 {
-    /// <inheritdoc />
-    public partial class SyncedByUserIdColumn : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "synced_by",
-                table: "submissions");
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropColumn(
+			name: "synced_by",
+			table: "submissions");
 
-            migrationBuilder.AddColumn<int>(
-                name: "synced_by_user_id",
-                table: "submissions",
-                type: "integer",
-                nullable: true);
+		migrationBuilder.AddColumn<int>(
+			name: "synced_by_user_id",
+			table: "submissions",
+			type: "integer",
+			nullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "ix_submissions_synced_by_user_id",
-                table: "submissions",
-                column: "synced_by_user_id");
+		migrationBuilder.CreateIndex(
+			name: "ix_submissions_synced_by_user_id",
+			table: "submissions",
+			column: "synced_by_user_id");
 
-            migrationBuilder.AddForeignKey(
-                name: "fk_submissions_users_synced_by_user_id",
-                table: "submissions",
-                column: "synced_by_user_id",
-                principalTable: "users",
-                principalColumn: "id");
-        }
+		migrationBuilder.AddForeignKey(
+			name: "fk_submissions_users_synced_by_user_id",
+			table: "submissions",
+			column: "synced_by_user_id",
+			principalTable: "users",
+			principalColumn: "id");
+	}
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "fk_submissions_users_synced_by_user_id",
-                table: "submissions");
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropForeignKey(
+			name: "fk_submissions_users_synced_by_user_id",
+			table: "submissions");
 
-            migrationBuilder.DropIndex(
-                name: "ix_submissions_synced_by_user_id",
-                table: "submissions");
+		migrationBuilder.DropIndex(
+			name: "ix_submissions_synced_by_user_id",
+			table: "submissions");
 
-            migrationBuilder.DropColumn(
-                name: "synced_by_user_id",
-                table: "submissions");
+		migrationBuilder.DropColumn(
+			name: "synced_by_user_id",
+			table: "submissions");
 
-            migrationBuilder.AddColumn<string>(
-                name: "synced_by",
-                table: "submissions",
-                type: "citext",
-                maxLength: 50,
-                nullable: true);
-        }
-    }
+		migrationBuilder.AddColumn<string>(
+			name: "synced_by",
+			table: "submissions",
+			type: "citext",
+			maxLength: 50,
+			nullable: true);
+	}
 }
