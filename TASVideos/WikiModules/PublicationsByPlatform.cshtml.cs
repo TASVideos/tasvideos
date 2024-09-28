dbc6836424c79ee9c2084938f11fa62e60ba0ba3
James Groom <OSSYoshiRulz+git@gmail.com>
2024-09-29 00:27:44 +1000

    Add `[module:PublicationsByPlatform]` (#1952)

---
+++ TASVideos/WikiModules/PublicationsByPlatform.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using TASVideos.WikiEngine;
+
+namespace TASVideos.WikiModules;
+
+[WikiModule(ModuleNames.PublicationsByPlatform)]
+public class PublicationsByPlatform(IGameSystemService platforms, IClassService classes) : WikiViewComponent
+{
+	public IReadOnlyList<(string DisplayName, string Code)> Platforms { get; private set; } = null!;
+
+	public IReadOnlyCollection<PublicationClass> PubClasses { get; set; } = null!;
+
+	public async Task<IViewComponentResult> InvokeAsync(IList<string> groupings)
+	{
+		var extant = (await platforms.GetAll()).ToList();
+		List<IReadOnlyList<SystemsResponse>> rows = [];
+		void ProcessGroup(string groupStr)
+		{
+			List<SystemsResponse> row = [];
+			foreach (var idStr in groupStr.Split('-'))
+			{
+				var found = extant.FirstOrDefault(sys => sys.Code.Equals(idStr, StringComparison.OrdinalIgnoreCase));
+				if (found is null)
+				{
+					// ignore, TODO log?
+					return;
+				}
+
+				extant.Remove(found);
+				row.Add(found);
+			}
+
+			rows.Add(row);
+		}
+
+		foreach (var groupStr in groupings)
+		{
+			ProcessGroup(groupStr);
+		}
+
+		Platforms = extant.Select(static sys => (sys.DisplayName, sys.Code))
+			.Concat(rows.Select(static row => (
+				DisplayName: string.Join(" / ", row.Select(static sys => sys.DisplayName)),
+				Code: string.Join("-", row.Select(static sys => sys.Code))
+			)))
+			.OrderBy(static tuple => tuple.DisplayName)
+			.ToArray();
+		PubClasses = await classes.GetAll();
+		return View();
+	}
+}
