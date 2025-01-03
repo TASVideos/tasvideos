bc0718b1f1234ffd3f635dc1b4a3458fe5629108
Masterjun3 <masterjun3@gmail.com>
2025-01-03 23:06:53 +0100

    in dev environments, override the file provider to use a fallback image for certain paths

---
+++ TASVideos/Services/DevFallbackFileProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace TASVideos.Services;
+
+public class DevFallbackFileProvider(IFileProvider originalProvider) : IFileProvider
+{
+	public IDirectoryContents GetDirectoryContents(string subpath)
+	{
+		return originalProvider.GetDirectoryContents(subpath);
+	}
+
+	public IFileInfo GetFileInfo(string subpath)
+	{
+		IFileInfo fileInfo = originalProvider.GetFileInfo(subpath);
+		if (!fileInfo.Exists && subpath.StartsWith("/media/"))
+		{
+			return originalProvider.GetFileInfo("/images/tasvideos_rss.png");
+		}
+
+		return fileInfo;
+	}
+
+	public IChangeToken Watch(string filter)
+	{
+		return originalProvider.Watch(filter);
+	}
+}
