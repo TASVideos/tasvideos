888107433ea61bed09901746c8a4592e06b51ee9
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-02-18 05:51:32 +1000

    Add a setup for building first-party Analyzers
    and implemented the first Analyzer for the first custom style rule:
    TVO1000 "Do not use `#if false`"

---
+++ analyzers/TASVideos.Analyzers.Style/NoCommentingWithPreprocAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+
+namespace TASVideos.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class NoCommentingWithPreprocAnalyzer : DiagnosticAnalyzer
+{
+	private static readonly DiagnosticDescriptor DiagNoCommentingWithPreproc = new(
+		id: "TVO1000",
+		title: "Do not use `#if false`",
+		messageFormat: "Do not use `#if false`",
+		category: "Usage",
+		defaultSeverity: DiagnosticSeverity.Warning,
+		isEnabledByDefault: true);
+
+	public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(DiagNoCommentingWithPreproc);
+
+	public override void Initialize(AnalysisContext context)
+	{
+		context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+		context.EnableConcurrentExecution();
+		context.RegisterSyntaxNodeAction(
+			snac =>
+			{
+				var idts = (IfDirectiveTriviaSyntax)snac.Node;
+				if (idts.Condition.ToString() is "false")
+				{
+					snac.ReportDiagnostic(Diagnostic.Create(DiagNoCommentingWithPreproc, idts.GetLocation()));
+				}
+			},
+			SyntaxKind.IfDirectiveTrivia);
+	}
+}
