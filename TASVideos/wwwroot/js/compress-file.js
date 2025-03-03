8765da20b41b6b4e0376ac466ea7cda050dbb30b
Masterjun3 <masterjun3@gmail.com>
2025-03-03 19:22:36 +0100

    show nozip reminder and hide file size limit

--- TASVideos/wwwroot/js/compress-file.js
+++ TASVideos/wwwroot/js/compress-file.js
@@ -1,17 +1,28 @@
 ﻿document.addEventListener("DOMContentLoaded", registerFileInputs);
 
 function registerFileInputs() {
+	const noZipReminder = document.querySelector('[data-id="nozip-reminder"]');
 	Array.from(document.querySelectorAll('[data-id="movie-file"]')).forEach(elem => {
 		elem.addEventListener("input", async () => {
-			await compressInput(elem);
+			await compressInput(elem, noZipReminder);
 		})
 	});
 }
 
-async function compressInput(fileInput) {
+async function compressInput(fileInput, noZipReminder) {
 	const inputFile = fileInput.files[0];
 	if (!inputFile) {
 		return;
+	}
+
+	if (noZipReminder) {
+		const ext = (s => s.substring(s.lastIndexOf('.') + 1))(inputFile.name);
+		if (ext.toLowerCase() === "zip") {
+			noZipReminder.classList.remove('d-none');
+		}
+		else {
+			noZipReminder.classList.add('d-none');
+		}
 	}
 
 	const inputBytes = await inputFile.arrayBuffer();
