4301b7971fd55f85a1e6cc184594e4452a2e95e6
Spikestuff <stamaadam@gmail.com>
2025-03-08 12:33:34 +1100

    Gameboy... Oops.

--- TASVideos/wwwroot/awards/automation/Step 0 (Replacement).bat
+++ TASVideos/wwwroot/awards/automation/Step 0 (Replacement).bat
@@ -6,7 +6,7 @@
 @echo This is the script that can automate *almost all* the award podiums, so it doesn't have to be adjusted manually.
 @echo What this script doesn't do is place the year at the end, so you have to adjust for it.
 
-@echo This does not automate the following: TAS Homebrew, TAS GameBoy, TAS Nintendo 3D, Both Exotic, TAS of, TASer of, Rookie of.
+@echo This does not automate the following: TAS GameBoy, TAS Homebrew, TAS Nintendo 3D, Both Exotic, TAS of, TASer of, Rookie of.
 @echo "TAS of" is quick to adjust for as you just need to just copy paste the grey plate backing.
 @echo "Exotic" all you need to do is just copy paste the lower quarter to replace.
 
