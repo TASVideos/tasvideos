6f44905ab636fdae6c0f493572b7a67f5e4279fd
adelikat <AndrewJDelikat@gmail.com>
2025-03-16 10:41:23 -0500

    add unit tests for Users/Ratings page model, includes helper method for testing user permissions

---
+++ tests/TASVideos.Test/Pages/Users/RatingsModelTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TASVideos.Core;
+using TASVideos.Core.Services;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.Users;
+using TASVideos.Tests.Base;
+using static TASVideos.RazorPages.Tests.RazorTestHelpers;
+
+namespace TASVideos.RazorPages.Tests.Pages.Users;
+
+[TestClass]
+public class RatingModelTests : TestDbBase
+{
+	private readonly IRatingService _ratingService;
+	private readonly RatingsModel _page;
+
+	public RatingModelTests()
+	{
+		_ratingService = Substitute.For<IRatingService>();
+		_page = new RatingsModel(_ratingService);
+	}
+
+	[TestMethod]
+	public async Task UserDoesNotExist_ReturnsNotFound()
+	{
+		const string userName = "DoesNotExist";
+		_page.UserName = userName;
+		_ratingService.GetUserRatings(userName, _page.Search).Returns(null as UserRatings);
+
+		var result = await _page.OnGet();
+
+		Assert.IsNotNull(result);
+		Assert.IsInstanceOfType<NotFoundResult>(result);
+	}
+
+	[TestMethod]
+	public async Task UserExists_PrivateRatings_ViewerDoesNotHavePermission_ReturnsNotFound()
+	{
+		const string userName = "RaterExists";
+		SetRatingResult(userName);
+		AddAuthenticatedUser(_page, "Viewer", []);
+
+		var result = await _page.OnGet();
+
+		Assert.IsNotNull(result);
+		Assert.IsInstanceOfType<NotFoundResult>(result);
+	}
+
+	[TestMethod]
+	public async Task UserExists_PrivateRatings_ViewerHasPermission_ReturnsRatings()
+	{
+		const string userName = "RaterExists";
+		SetRatingResult(userName);
+		AddAuthenticatedUser(_page, "Viewer", [PermissionTo.SeePrivateRatings]);
+
+		var result = await _page.OnGet();
+		Assert.IsNotNull(result);
+		Assert.IsInstanceOfType<PageResult>(result);
+		Assert.AreEqual(1, _page.Ratings.Ratings.Count());
+	}
+
+	[TestMethod]
+	public async Task UserExists_PrivateRatings_ViewerIsUserAndWithoutPermission_ReturnsRatings()
+	{
+		const string userName = "RaterExists";
+		SetRatingResult(userName);
+		AddAuthenticatedUser(_page, userName, []);
+
+		var result = await _page.OnGet();
+
+		Assert.IsNotNull(result);
+		Assert.IsInstanceOfType<PageResult>(result);
+		Assert.AreEqual(1, _page.Ratings.Ratings.Count());
+	}
+
+	private void SetRatingResult(string userName)
+	{
+		_page.UserName = userName;
+		_ratingService.GetUserRatings(userName, _page.Search, true).Returns(new UserRatings
+		{
+			Ratings = new PageOf<UserRatings.Rating, RatingRequest>([new()], _page.Search)
+		});
+		_ratingService.GetUserRatings(userName, _page.Search, false).Returns(null as UserRatings);
+	}
+}
---
+++ tests/TASVideos.Test/RazorTestHelpers.cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using TASVideos.Data.Entity;
+
+namespace TASVideos.RazorPages.Tests;
+
+public static class RazorTestHelpers
+{
+	public static void AddAuthenticatedUser(PageModel page, string userName, IEnumerable<PermissionTo> permissions)
+	{
+		var identity = new GenericIdentity(userName);
+		foreach (var p in permissions)
+		{
+			identity.AddClaim(new Claim(CustomClaimTypes.Permission, ((int)p).ToString()));
+		}
+
+		var principle = new ClaimsPrincipal(identity);
+
+		var httpContext = new DefaultHttpContext
+		{
+			User = principle
+		};
+
+		var modelState = new ModelStateDictionary();
+		var modelMetadataProvider = new EmptyModelMetadataProvider();
+		var viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+		var actionContext = new ActionContext(httpContext, new RouteData(), new PageActionDescriptor(), modelState);
+		page.PageContext = new PageContext(actionContext)
+		{
+			ViewData = viewData
+		};
+	}
+}
