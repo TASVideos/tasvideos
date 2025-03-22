7edbe46770be226803ad70e6550e364fe67367f3
Masterjun <masterjun3@gmail.com>
2025-03-22 17:21:00 +0100

    Add Rater statistics and Top leaderboard (#2108)
    * add summary section to ratings showing comparisons with other raters and a top 30 rater list
    
    * add tests for rating summary and percentage extension
    
    * remove unnecessary usings that somehow appeared
    
    * handle edge case where user has not rated any movies and thus does not appear in any lists
    
    * fix bug where permission alert is shown even though the users ratings are public
    
    * assign summary before returning for private ratings

--- tests/TASVideos.Common.Tests/Extensions/NumberExtensions.cs
+++ tests/TASVideos.Common.Tests/Extensions/NumberExtensions.cs
@@ -47,4 +47,18 @@ public class NumberExtensions
 		var actual = byteCount.ToSizeString();
 		Assert.AreEqual(expected, actual);
 	}
+
+	[TestMethod]
+	[DataRow(0, 0, "0%")]
+	[DataRow(1, 0, "100%")]
+	[DataRow(double.NaN, 0, "n/a")]
+	[DataRow(double.PositiveInfinity, 0, "n/a")]
+	[DataRow(double.NegativeInfinity, 0, "n/a")]
+	[DataRow(0.4567, 1, "45.6%")]
+	[DataRow(0.99999, 1, "99.9%")]
+	public void ToPercentage(double ratio, int decimalPlaces, string expected)
+	{
+		var actual = ratio.ToPercentage(decimalPlaces);
+		Assert.AreEqual(expected, actual);
+	}
 }
