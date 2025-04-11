1191dc7335177a5b65e5b12e9dd19ca757e664b6
Masterjun3 <masterjun3@gmail.com>
2025-04-11 23:33:56 +0200

    show canonical urls on wiki pages, and allow wrong casing to lead to the correct casing wiki page
    also add a javascript that will read out the canonical url and automatically replace it in the browser

--- TASVideos/Pages/Wiki/Render.cshtml.cs
+++ TASVideos/Pages/Wiki/Render.cshtml.cs
@@ -24,7 +24,7 @@ public class RenderModel(IWikiPages wikiPages, ApplicationDbContext db, ILogger<
 			}
 		}
 
-		if (!WikiHelper.IsValidWikiPageName(url))
+		if (!WikiHelper.IsValidWikiPageName(url, validateLoosely: true)) // allow wrong url casing to navigate to the wiki page, we will show a proper canonical url in the html, and rewrite the browser url with javascript
 		{
 			// Support legacy links like [adelikat] that should have been [user:adelikat]
 			if (await wikiPages.Exists(LinkConstants.HomePages + url))
@@ -46,6 +46,7 @@ public class RenderModel(IWikiPages wikiPages, ApplicationDbContext db, ILogger<
 			WikiPage = wikiPage;
 			ViewData.SetWikiPage(WikiPage);
 			ViewData.SetTitle(WikiPage.PageName);
+			ViewData.SetCanonicalUrl($"{Request.Scheme}://{Request.Host}{Request.PathBase}/{WikiPage.PageName}{Request.QueryString}");
 			return Page();
 		}
 
