83cb3653e15ed47c91d654983ccc3aee9b1ac396
adelikat <AndrewJDelikat@gmail.com>
2025-05-08 16:10:06 -0500

    .3ct parser (#2139)
    * start .3ct parser.  Just plumbing stuff, no logic yet
    
    * maybe implement cycle count
    
    * set frames to cycles and framerate to match, assume 0.01 time if time is 0
    
    * comment out a unit test, why are these broken as a group, lame.
    
    * make rerecord count 0, fix broken test
    
    * masterjun suggested fix

--- TASVideos.Common/ITimeable.cs
+++ TASVideos.Common/ITimeable.cs
@@ -42,6 +42,11 @@ public static class TimeableExtensions
 			return timeSpan.ToString(@"h\:mm\:ss\.ff");
 		}
 
+		if (timeSpan.TotalSeconds <= 0.01)
+		{
+			return "00:00.01";
+		}
+
 		return timeSpan.ToString(@"mm\:ss\.ff");
 	}
 
---
+++ tests/TASVideos.MovieParsers.Tests/ThreeCtParserTests.cs
@@ -0,0 +1,23 @@
+﻿namespace TASVideos.MovieParsers.Tests;
+
+[TestClass]
+[TestCategory("3CTParsers")]
+public class ThreeCtParserTests : BaseParserTests
+{
+	private readonly ThreeCt _threeCtParser = new();
+	protected override string ResourcesPath => "TASVideos.MovieParsers.Tests.ThreeCtSampleFiles.";
+
+	[TestMethod]
+	public async Task Basic()
+	{
+		var result = await _threeCtParser.Parse(Embedded("basic.3ct"), EmbeddedLength("basic.3ct"));
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(SystemCodes.Nes, result.SystemCode);
+		Assert.AreEqual(RegionType.Ntsc, result.Region);
+		Assert.AreEqual(0, result.RerecordCount);
+		Assert.AreEqual(MovieStartType.PowerOn, result.StartType);
+		Assert.AreEqual("3ct", result.FileExtension);
+		Assert.AreEqual(30, result.CycleCount);
+	}
+}
