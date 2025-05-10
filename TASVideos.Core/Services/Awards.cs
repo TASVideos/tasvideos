f2a32609b1a8fb68fbcd0c2ab2570543da58ff2d
adelikat <AndrewJDelikat@gmail.com>
2025-05-10 11:14:58 -0500

    prefer .ToList() over .ToArray() for consistency, also use some overly fancy language features

--- TASVideos.Core/Services/Awards.cs
+++ TASVideos.Core/Services/Awards.cs
@@ -251,14 +251,11 @@ internal class Awards(ApplicationDbContext db, ICacheService cache) : IAwards
 				g.Key.Description + " of " + g.Key.Year,
 				g.Key.Year,
 				AwardType.Movie,
-				g.Select(gv => new AwardAssignmentPublication(gv.Publication.Id, gv.Publication.Title))
-					.ToList(),
-				g.SelectMany(gv => gv.Users)
-					.Select(u => new AwardAssignmentUser(u.UserId, u.UserName))
-					.ToList()))
+				[.. g.Select(gv => new AwardAssignmentPublication(gv.Publication.Id, gv.Publication.Title))],
+				[.. g.SelectMany(gv => gv.Users).Select(u => new AwardAssignmentUser(u.UserId, u.UserName))]))
 			.ToList();
 
-		var allAwards = userAwards.Concat(publicationAwards).ToArray();
+		var allAwards = userAwards.Concat(publicationAwards).ToList();
 
 		cache.Set(CacheKeys.AwardsCache, allAwards, Durations.OneWeek);
 
