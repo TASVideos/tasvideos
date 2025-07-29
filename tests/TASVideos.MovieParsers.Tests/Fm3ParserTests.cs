cb031bebc20af73981d012718ab7231a365755c9
adelikat <AndrewJDelikat@gmail.com>
2025-07-29 17:00:44 -0500

    fix Fm3ParserTests

--- tests/TASVideos.MovieParsers.Tests/Fm3ParserTests.cs
+++ tests/TASVideos.MovieParsers.Tests/Fm3ParserTests.cs
@@ -10,7 +10,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task Ntsc()
 	{
-		var result = await _fm3Parser.Parse(Embedded("ntsc.fm3"), EmbeddedLength("ntsc.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("ntsc.fm3", out var length), length);
 		Assert.IsTrue(result.Success, "Result is successful");
 		Assert.AreEqual(2, result.Frames, "Frame count should be 2");
 		Assert.AreEqual(RegionType.Ntsc, result.Region);
@@ -23,7 +23,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task Pal()
 	{
-		var result = await _fm3Parser.Parse(Embedded("pal.fm3"), EmbeddedLength("pal.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("pal.fm3", out var length), length);
 		Assert.IsTrue(result.Success, "Result is successful");
 		Assert.AreEqual(2, result.Frames, "Frame count should be 2");
 		Assert.AreEqual(RegionType.Pal, result.Region);
@@ -36,7 +36,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task Fds()
 	{
-		var result = await _fm3Parser.Parse(Embedded("fds.fm3"), EmbeddedLength("fds.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("fds.fm3", out var length), length);
 		Assert.IsTrue(result.Success, "Result is successful");
 		Assert.AreEqual(2, result.Frames, "Frame count should be 2");
 		Assert.AreEqual(RegionType.Ntsc, result.Region);
@@ -49,7 +49,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task Savestate()
 	{
-		var result = await _fm3Parser.Parse(Embedded("savestate.fm3"), EmbeddedLength("savestate.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("savestate.fm3", out var length), length);
 		Assert.IsTrue(result.Success, "Result is successful");
 		Assert.AreEqual(2, result.Frames, "Frame count should be 2");
 		Assert.AreEqual(RegionType.Ntsc, result.Region);
@@ -62,7 +62,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task NoRerecords()
 	{
-		var result = await _fm3Parser.Parse(Embedded("norerecords.fm3"), EmbeddedLength("norerecords.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("norerecords.fm3", out var length), length);
 		Assert.IsTrue(result.Success);
 		Assert.AreEqual(0, result.RerecordCount, "Rerecord count is assumed to be 0");
 		Assert.AreEqual(1, result.Warnings.Count());
@@ -72,7 +72,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task Binary()
 	{
-		var result = await _fm3Parser.Parse(Embedded("binary.fm3"), EmbeddedLength("binary.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("binary.fm3", out var length), length);
 		Assert.IsTrue(result.Success, "Result is successful");
 		Assert.AreEqual(2, result.Frames, "Frame count should be 2");
 		AssertNoWarningsOrErrors(result);
@@ -81,7 +81,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task InvalidVersion()
 	{
-		var result = await _fm3Parser.Parse(Embedded("invalidversion.fm3"), EmbeddedLength("invalidversion.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("invalidversion.fm3", out var length), length);
 		Assert.IsFalse(result.Success);
 		AssertNoWarnings(result);
 		Assert.AreEqual(1, result.Errors.Count());
@@ -91,7 +91,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task NoRomFilename()
 	{
-		var result = await _fm3Parser.Parse(Embedded("noromfilename.fm3"), EmbeddedLength("noromfilename.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("noromfilename.fm3", out var length), length);
 		Assert.IsFalse(result.Success);
 		AssertNoWarnings(result);
 		Assert.AreEqual(1, result.Errors.Count());
@@ -101,7 +101,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task NoRomChecksum()
 	{
-		var result = await _fm3Parser.Parse(Embedded("noromchecksum.fm3"), EmbeddedLength("noromchecksum.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("noromchecksum.fm3", out var length), length);
 		Assert.IsFalse(result.Success);
 		AssertNoWarnings(result);
 		Assert.AreEqual(1, result.Errors.Count());
@@ -111,7 +111,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task NoGuid()
 	{
-		var result = await _fm3Parser.Parse(Embedded("noguid.fm3"), EmbeddedLength("noguid.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("noguid.fm3", out var length), length);
 		Assert.IsFalse(result.Success);
 		AssertNoWarnings(result);
 		Assert.AreEqual(1, result.Errors.Count());
@@ -121,7 +121,7 @@ public class Fm3ParserTests : BaseParserTests
 	[TestMethod]
 	public async Task Hash()
 	{
-		var result = await _fm3Parser.Parse(Embedded("hash.fm3"), EmbeddedLength("hash.fm3"));
+		var result = await _fm3Parser.Parse(Embedded("hash.fm3", out var length), length);
 		Assert.IsTrue(result.Success);
 		Assert.AreEqual(1, result.Hashes.Count);
 		Assert.AreEqual(HashType.Md5, result.Hashes.First().Key);
