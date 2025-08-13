656bc51fd6be6983880f28bddaf9bbd37d89c008
adelikat <AndrewJDelikat@gmail.com>
2025-08-13 09:17:50 -0500

    unit tests for UserFiles/Uncataloged

---
+++ tests/TASVideos.RazorPages.Tests/Pages/UserFiles/UncatalogedModelTests.cs
@@ -0,0 +1,157 @@
+﻿using TASVideos.Data.Entity;
+using TASVideos.Data.Entity.Game;
+using TASVideos.Pages.UserFiles;
+using TASVideos.Tests.Base;
+
+namespace TASVideos.RazorPages.Tests.Pages.UserFiles;
+
+[TestClass]
+public class UncatalogedModelTests : TestDbBase
+{
+	private readonly Uncataloged _page;
+
+	public UncatalogedModelTests()
+	{
+		_page = new Uncataloged(_db);
+	}
+
+	[TestMethod]
+	public async Task OnGet_LoadsPublicUncatalogedFiles()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+		var system = new GameSystem { Id = 1, Code = "NES", DisplayName = "Nintendo Entertainment System" };
+		_db.GameSystems.Add(system);
+
+		var uncatalogedFile = new UserFile
+		{
+			Id = 1,
+			FileName = "uncataloged.bk2",
+			Title = "Uncataloged File",
+			Author = user,
+			Hidden = false,
+			SystemId = system.Id,
+			GameId = null,
+			UploadTimestamp = DateTime.UtcNow.AddDays(-1)
+		};
+
+		var game = new Game { Id = 1, DisplayName = "Test Game" };
+		_db.Games.Add(game);
+
+		var catalogedFile = new UserFile
+		{
+			Id = 2,
+			FileName = "cataloged.bk2",
+			Title = "Cataloged File",
+			Author = user,
+			Hidden = false,
+			SystemId = system.Id,
+			GameId = game.Id,
+			UploadTimestamp = DateTime.UtcNow.AddDays(-2)
+		};
+
+		_db.UserFiles.AddRange(uncatalogedFile, catalogedFile);
+		await _db.SaveChangesAsync();
+
+		await _page.OnGet();
+
+		Assert.AreEqual(1, _page.Files.Count);
+		var file = _page.Files.First();
+		Assert.AreEqual(1, file.Id);
+		Assert.AreEqual("uncataloged.bk2", file.FileName);
+		Assert.AreEqual("NES", file.SystemCode);
+		Assert.AreEqual("TestUser", file.Author);
+	}
+
+	[TestMethod]
+	public async Task OnGet_ExcludesHiddenFiles()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+		var system = new GameSystem { Id = 1, Code = "NES", DisplayName = "Nintendo Entertainment System" };
+		_db.GameSystems.Add(system);
+
+		var publicFile = new UserFile
+		{
+			Id = 1,
+			FileName = "public.bk2",
+			Title = "Public File",
+			Author = user,
+			Hidden = false,
+			SystemId = system.Id,
+			GameId = null,
+			UploadTimestamp = DateTime.UtcNow
+		};
+
+		var hiddenFile = new UserFile
+		{
+			Id = 2,
+			FileName = "hidden.bk2",
+			Title = "Hidden File",
+			Author = user,
+			Hidden = true,
+			SystemId = system.Id,
+			GameId = null,
+			UploadTimestamp = DateTime.UtcNow
+		};
+
+		_db.UserFiles.AddRange(publicFile, hiddenFile);
+		await _db.SaveChangesAsync();
+
+		await _page.OnGet();
+
+		Assert.AreEqual(1, _page.Files.Count);
+		Assert.AreEqual("public.bk2", _page.Files.First().FileName);
+	}
+
+	[TestMethod]
+	public async Task OnGet_HandlesFilesWithoutSystem()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+
+		var fileWithoutSystem = new UserFile
+		{
+			Id = 1,
+			FileName = "NoSystem.lua",
+			Title = "File Without System",
+			Author = user,
+			SystemId = null,
+			GameId = null,
+		};
+
+		_db.UserFiles.Add(fileWithoutSystem);
+		await _db.SaveChangesAsync();
+
+		await _page.OnGet();
+
+		Assert.AreEqual(1, _page.Files.Count);
+		var file = _page.Files.First();
+		Assert.AreEqual("NoSystem.lua", file.FileName);
+		Assert.IsNull(file.SystemCode);
+	}
+
+	[TestMethod]
+	public async Task OnGet_ReturnsEmptyListWhenNoUncatalogedFiles()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+
+		var game = new Game { Id = 1, DisplayName = "Test Game" };
+		_db.Games.Add(game);
+
+		var catalogedFile = new UserFile
+		{
+			Id = 1,
+			FileName = "cataloged.bk2",
+			Title = "Cataloged File",
+			Author = user,
+			Hidden = false,
+			GameId = game.Id,
+			UploadTimestamp = DateTime.UtcNow
+		};
+
+		_db.UserFiles.Add(catalogedFile);
+		await _db.SaveChangesAsync();
+
+		await _page.OnGet();
+
+		Assert.AreEqual(0, _page.Files.Count);
+	}
+}
