6cf5a8ec81bab3fff190908896126800caed202d
adelikat <AndrewJDelikat@gmail.com>
2025-08-17 21:00:05 -0500

    unit tests for Forum/Posts/Unansered

---
+++ tests/TASVideos.RazorPages.Tests/Pages/Forum/Posts/UnansweredModelTests.cs
@@ -0,0 +1,222 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using TASVideos.Core;
+using TASVideos.Data.Entity;
+using TASVideos.Pages.Forum.Posts;
+using static TASVideos.RazorPages.Tests.RazorTestHelpers;
+
+namespace TASVideos.RazorPages.Tests.Pages.Forum.Posts;
+
+[TestClass]
+public class UnansweredModelTests : BasePageModelTests
+{
+	private readonly UnansweredModel _model;
+
+	public UnansweredModelTests()
+	{
+		_model = new UnansweredModel(_db)
+		{
+			PageContext = TestPageContext()
+		};
+	}
+
+	[TestMethod]
+	public async Task OnGet_NoTopics_ReturnsEmptyPagedResult()
+	{
+		await _model.OnGet();
+
+		Assert.AreEqual(0, _model.Posts.Count());
+		Assert.AreEqual(0, _model.Posts.RowCount);
+	}
+
+	[TestMethod]
+	public async Task OnGet_TopicsWithOnePost_ReturnsUnansweredTopics()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+		var topic = _db.AddTopic(user).Entity;
+		topic.Title = "Unanswered Topic";
+		topic.Forum!.Name = "Test Forum";
+
+		var post = _db.CreatePostForTopic(topic, user).Entity;
+		post.CreateTimestamp = DateTime.UtcNow.AddDays(-1);
+
+		await _db.SaveChangesAsync();
+
+		await _model.OnGet();
+
+		Assert.AreEqual(1, _model.Posts.Count());
+		var unansweredPost = _model.Posts.First();
+		Assert.AreEqual(topic.ForumId, unansweredPost.ForumId);
+		Assert.AreEqual("Test Forum", unansweredPost.ForumName);
+		Assert.AreEqual(topic.Id, unansweredPost.TopicId);
+		Assert.AreEqual("Unanswered Topic", unansweredPost.TopicName);
+		Assert.AreEqual(user.Id, unansweredPost.AuthorId);
+		Assert.AreEqual("TestUser", unansweredPost.AuthorName);
+	}
+
+	[TestMethod]
+	public async Task OnGet_TopicsWithMultiplePosts_ExcludesAnsweredTopics()
+	{
+		var user1 = _db.AddUser("User1").Entity;
+		var user2 = _db.AddUser("User2").Entity;
+
+		var unansweredTopic = _db.AddTopic(user1).Entity;
+		unansweredTopic.Title = "Unanswered Topic";
+		_ = _db.CreatePostForTopic(unansweredTopic, user1).Entity;
+
+		var answeredTopic = _db.AddTopic(user1).Entity;
+		answeredTopic.Title = "Answered Topic";
+		_ = _db.CreatePostForTopic(answeredTopic, user1).Entity;
+		_ = _db.CreatePostForTopic(answeredTopic, user2).Entity;
+
+		await _db.SaveChangesAsync();
+
+		await _model.OnGet();
+
+		Assert.AreEqual(1, _model.Posts.Count());
+		var result = _model.Posts.First();
+		Assert.AreEqual("Unanswered Topic", result.TopicName);
+	}
+
+	[TestMethod]
+	public async Task OnGet_TopicsOrderedByCreateTimestamp_ReturnsNewestFirst()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+
+		var olderTopic = _db.AddTopic(user).Entity;
+		olderTopic.Title = "Older Topic";
+		olderTopic.CreateTimestamp = DateTime.UtcNow.AddDays(-2);
+		_ = _db.CreatePostForTopic(olderTopic, user).Entity;
+
+		var newerTopic = _db.AddTopic(user).Entity;
+		newerTopic.Title = "Newer Topic";
+		newerTopic.CreateTimestamp = DateTime.UtcNow.AddDays(-1);
+		_ = _db.CreatePostForTopic(newerTopic, user).Entity;
+
+		await _db.SaveChangesAsync();
+
+		await _model.OnGet();
+
+		Assert.AreEqual(2, _model.Posts.Count());
+		var posts = _model.Posts.ToList();
+		Assert.AreEqual("Newer Topic", posts[0].TopicName);
+		Assert.AreEqual("Older Topic", posts[1].TopicName);
+		Assert.IsTrue(posts[0].PostDate > posts[1].PostDate);
+	}
+
+	[TestMethod]
+	public async Task OnGet_RestrictedTopics_WithoutPermission_ExcludesRestrictedTopics()
+	{
+		var user = _db.AddUserWithRole("RegularUser").Entity;
+		var restrictedForum = _db.AddForum("Restricted Forum", true).Entity;
+		var publicForum = _db.AddForum("Public Forum", false).Entity;
+
+		var restrictedTopic = _db.AddTopic(user).Entity;
+		restrictedTopic.Forum = restrictedForum;
+		restrictedTopic.Title = "Restricted Topic";
+		_ = _db.CreatePostForTopic(restrictedTopic, user).Entity;
+
+		var publicTopic = _db.AddTopic(user).Entity;
+		publicTopic.Forum = publicForum;
+		publicTopic.Title = "Public Topic";
+		_ = _db.CreatePostForTopic(publicTopic, user).Entity;
+
+		await _db.SaveChangesAsync();
+
+		AddAuthenticatedUser(_model, user, []);
+
+		await _model.OnGet();
+
+		Assert.AreEqual(1, _model.Posts.Count());
+		var result = _model.Posts.First();
+		Assert.AreEqual("Public Topic", result.TopicName);
+	}
+
+	[TestMethod]
+	public async Task OnGet_RestrictedTopics_WithPermission_IncludesRestrictedTopics()
+	{
+		var user = _db.AddUserWithRole("AdminUser").Entity;
+		var restrictedForum = _db.AddForum("Restricted Forum", true).Entity;
+		var publicForum = _db.AddForum("Public Forum", false).Entity;
+
+		var restrictedTopic = _db.AddTopic(user).Entity;
+		restrictedTopic.Forum = restrictedForum;
+		restrictedTopic.Title = "Restricted Topic";
+		_ = _db.CreatePostForTopic(restrictedTopic, user).Entity;
+
+		var publicTopic = _db.AddTopic(user).Entity;
+		publicTopic.Forum = publicForum;
+		publicTopic.Title = "Public Topic";
+		_ = _db.CreatePostForTopic(publicTopic, user).Entity;
+
+		await _db.SaveChangesAsync();
+
+		AddAuthenticatedUser(_model, user, [PermissionTo.SeeRestrictedForums]);
+
+		await _model.OnGet();
+
+		Assert.AreEqual(2, _model.Posts.Count());
+		var posts = _model.Posts.ToList();
+		Assert.IsTrue(posts.Any(p => p.TopicName == "Restricted Topic"));
+		Assert.IsTrue(posts.Any(p => p.TopicName == "Public Topic"));
+	}
+
+	[TestMethod]
+	public async Task OnGet_MultipleUnansweredTopics_MapsAllFieldsCorrectly()
+	{
+		var author = _db.AddUser("TestAuthor").Entity;
+		var topic = _db.AddTopic(author).Entity;
+		topic.Title = "Test Topic Title";
+		topic.Forum!.Name = "Test Forum Name";
+		var testTimestamp = DateTime.UtcNow.AddDays(-1);
+		topic.CreateTimestamp = testTimestamp;
+
+		_ = _db.CreatePostForTopic(topic, author).Entity;
+
+		await _db.SaveChangesAsync();
+
+		await _model.OnGet();
+
+		Assert.AreEqual(1, _model.Posts.Count());
+		var unansweredPost = _model.Posts.First();
+
+		Assert.AreEqual(topic.ForumId, unansweredPost.ForumId);
+		Assert.AreEqual("Test Forum Name", unansweredPost.ForumName);
+		Assert.AreEqual(topic.Id, unansweredPost.TopicId);
+		Assert.AreEqual("Test Topic Title", unansweredPost.TopicName);
+		Assert.AreEqual(author.Id, unansweredPost.AuthorId);
+		Assert.AreEqual("TestAuthor", unansweredPost.AuthorName);
+		Assert.IsTrue(Math.Abs((testTimestamp - unansweredPost.PostDate).TotalMilliseconds) < 1000, "Timestamps should be within 1 second");
+	}
+
+	[TestMethod]
+	public async Task OnGet_WithPagingModel_UsesCorrectPagination()
+	{
+		var user = _db.AddUser("TestUser").Entity;
+
+		for (int i = 0; i < 30; i++)
+		{
+			var topic = _db.AddTopic(user).Entity;
+			topic.Title = $"Topic {i}";
+			topic.CreateTimestamp = DateTime.UtcNow.AddDays(-1).AddMinutes(i);
+			_ = _db.CreatePostForTopic(topic, user).Entity;
+		}
+
+		await _db.SaveChangesAsync();
+
+		_model.Search = new PagingModel { CurrentPage = 2, PageSize = 10 };
+
+		await _model.OnGet();
+
+		Assert.AreEqual(10, _model.Posts.Count());
+		Assert.AreEqual(30, _model.Posts.RowCount);
+		Assert.AreEqual(2, _model.Posts.Request.CurrentPage);
+	}
+
+	[TestMethod]
+	public void UnansweredModel_HasAllowAnonymousAttribute()
+	{
+		var type = typeof(UnansweredModel);
+		var attributes = type.GetCustomAttributes(typeof(AllowAnonymousAttribute), false);
+		Assert.AreEqual(1, attributes.Length);
+	}
+}
