4e5808128a9b159b96ed9699ff60123959e933bf
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-08-18 23:56:28 +1000

    Add unit tests for link tag helpers

---
+++ tests/TASVideos.RazorPages.Tests/LinkTagHelperTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Immutable;
+
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+using TASVideos.TagHelpers;
+
+namespace TASVideos.RazorPages.Tests;
+
+[TestClass]
+public sealed class LinkTagHelperTests
+{
+	private static TagHelperContext GetHelperContext()
+		=> new(
+			[],
+			ImmutableDictionary<object, object>.Empty,
+			Guid.NewGuid().ToString("N"));
+
+	private static TagHelperOutput GetOutputObj(string contentsUnencoded)
+	{
+		TagHelperOutput output = new(string.Empty, [], (_, _) => Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+		output.Content.SetContent(contentsUnencoded);
+		return output;
+	}
+
+	[DataRow(123, "some game", """<a href="/123G">some game</a>""")]
+	[TestMethod]
+	public void TestGameLinkHelper(int id, string label, string expected)
+	{
+		GameLinkTagHelper gameLinkHelper = new() { Id = id };
+		var output = GetOutputObj(contentsUnencoded: label);
+		gameLinkHelper.Process(GetHelperContext(), output);
+		Assert.AreEqual(expected, output.GetString());
+	}
+
+	[DataRow("YoshiRulz", "unused", """<a href="/Users/Profile/YoshiRulz">YoshiRulz</a>""")]
+	[TestMethod]
+	public void TestProfileLinkHelper(string username, string label, string expected)
+	{
+		var generator = TestableHtmlGenerator.Create(out var viewCtx, "Users/Profile/{Username}");
+		ProfileLinkTagHelper profileLinkHelper = new(generator) { Username = username, ViewContext = viewCtx };
+		var output = GetOutputObj(contentsUnencoded: label);
+		profileLinkHelper.Process(GetHelperContext(), output);
+		Assert.AreEqual(expected, output.GetString());
+	}
+
+	[DataRow(1234, "some movie", """<a href="/1234M">some movie</a>""")]
+	[TestMethod]
+	public void TestPubLinkHelper(int id, string label, string expected)
+	{
+		PubLinkTagHelper pubLinkHelper = new() { Id = id };
+		var output = GetOutputObj(contentsUnencoded: label);
+		pubLinkHelper.Process(GetHelperContext(), output);
+		Assert.AreEqual(expected, output.GetString());
+	}
+
+	[DataRow(1234, "some movie", """<a href="/1234S">some movie</a>""")]
+	[TestMethod]
+	public void TestSubLinkHelper(int id, string label, string expected)
+	{
+		SubLinkTagHelper subLinkHelper = new() { Id = id };
+		var output = GetOutputObj(contentsUnencoded: label);
+		subLinkHelper.Process(GetHelperContext(), output);
+		Assert.AreEqual(expected, output.GetString());
+	}
+
+	[DataRow("WelcomeToTASVideos", "unused", """<a href="/WelcomeToTASVideos">WelcomeToTASVideos</a>""")]
+	[TestMethod]
+	public void TestWikiLinkHelper(string wikiPageName, string label, string expected)
+	{
+		WikiLinkTagHelper wikiLinkHelper = new() { PageName = wikiPageName };
+		var output = GetOutputObj(contentsUnencoded: label);
+		wikiLinkHelper.Process(GetHelperContext(), output);
+		Assert.AreEqual(expected, output.GetString());
+	}
+}
