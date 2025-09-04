c68804dd297c352057de6f84ae5f4fdbd259faf2
adelikat <AndrewJDelikat@gmail.com>
2025-09-03 19:28:17 -0500

    add a test for WikiLinkTagHelper, as a POC of how to set up tests for TagHelpers

---
+++ tests/TASVideos.RazorPages.Tests/TagHelpers/WikiLinkTagHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using TASVideos.TagHelpers;
+
+namespace TASVideos.RazorPages.Tests.TagHelpers;
+
+[TestClass]
+public class WikiLinkTagHelperTests
+{
+	[TestMethod]
+	public void WikiLinkTagHelper_Process_RendersCorrectHtml()
+	{
+		var tagHelper = new WikiLinkTagHelper { PageName = "GameResources/NES/SuperMarioBros" };
+		var context = new TagHelperContext(
+			allAttributes: [],
+			items: new Dictionary<object, object>(),
+			uniqueId: "test");
+		var output = new TagHelperOutput(
+			tagName: "wiki-link",
+			attributes: [],
+			getChildContentAsync: (_, _) =>
+				Task.FromResult<TagHelperContent>(new DefaultTagHelperContent()));
+
+		tagHelper.Process(context, output);
+
+		var htmlString = GetHtmlString(output);
+		Assert.AreEqual("<a href=\"/GameResources/NES/SuperMarioBros\">GameResources/NES/SuperMarioBros</a>", htmlString);
+	}
+
+	private static string GetHtmlString(TagHelperOutput output)
+	{
+		using var writer = new StringWriter();
+		output.WriteTo(writer, NullHtmlEncoder.Default);
+		return writer.ToString();
+	}
+}
