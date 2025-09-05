0496b20161781b4269cb6bed2115e15d477557ba
Sestain <sestain3850@gmail.com>
2025-09-05 11:37:35 +0300

    updated P2m2.cs

--- TASVideos.Parsers/Parsers/P2m2.cs
+++ TASVideos.Parsers/Parsers/P2m2.cs
@@ -16,22 +16,17 @@ internal class P2m2 : Parser, IParser
 		using var br = new BinaryReader(file);
 		br.ReadBytes(1); // Recording File Version
 		var header = new string(br.ReadChars(5));
-		if (!header.StartsWith("PCSX2"))
+		if (header != "PCSX2")
 		{
 			return InvalidFormat();
 		}
 
 		br.ReadBytes(2); // "-v"
-		br.ReadBytes(8); // PCSX2 Version Used
-		br.ReadBytes(35);
-		br.ReadBytes(32); // Author
-		br.ReadBytes(223);
-		br.ReadBytes(32); // Associated Game Name or ISO Filename
-		br.ReadBytes(223);
-		result.Frames = br.ReadUInt16();
-		br.ReadBytes(2);
-		result.RerecordCount = br.ReadUInt16();
-		br.ReadBytes(2);
+		br.ReadBytes(43); // PCSX2 Version Used
+		br.ReadBytes(255); // Author
+		br.ReadBytes(255); // Associated Game Name or ISO Filename
+		result.Frames = br.ReadInt32();
+		result.RerecordCount = br.ReadInt32();
 		var startsFromSavestate = br.ReadByte() > 0;
 		if (startsFromSavestate)
 		{
