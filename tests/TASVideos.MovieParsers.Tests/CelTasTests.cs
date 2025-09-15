655d3be049adbe4537d0bab919f8a66d476d823f
InputEvelution <theprogamerwithaj@gmail.com>
2025-09-15 21:55:06 +0930

    Fixed build errors due to refactors

--- tests/TASVideos.MovieParsers.Tests/CelTasTests.cs
+++ tests/TASVideos.MovieParsers.Tests/CelTasTests.cs
@@ -14,17 +14,16 @@ public class CelTasTests : BaseParserTests
 	[DataRow("improperfiletime.celtas", DisplayName = "Improper FileTime header creates error")]
 	public async Task Errors(string filename)
 	{
-		var result = await _celtasParser.Parse(Embedded(filename), EmbeddedLength(filename));
-		Assert.AreEqual(false, result.Success);
+		var result = await _celtasParser.Parse(Embedded(filename, out var length), length);
+		Assert.IsFalse(result.Success);
 		AssertNoWarnings(result);
-		Assert.IsNotNull(result.Errors);
 		Assert.AreEqual(1, result.Errors.Count());
 	}
 
 	[TestMethod]
 	public async Task ValidHeader()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas"), EmbeddedLength("2465ms.celtas"));
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 	}
@@ -32,7 +31,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task System()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas"), EmbeddedLength("2465ms.celtas"));
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(SystemCodes.Celeste, result.SystemCode);
@@ -41,7 +40,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task RerecordCount()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas"), EmbeddedLength("2465ms.celtas"));
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(12, result.RerecordCount);
@@ -50,7 +49,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task Length()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas"), EmbeddedLength("2465ms.celtas"));
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(2465, result.Frames);
