d987e126f7cf1128990f8ccda1e71dcc0fc2fd8a
InputEvelution <theprogamerwithaj@gmail.com>
2025-10-04 00:51:59 +0200

    Add CelesteTAS movie file parser (#2212)
    * Add parser for CelesteTAS
    
    * Remove TODO comments
    
    * Fixed build errors due to refactors
    
    * Change .celtas to just be renamed .tas file
    
    * Add support for undocumented TotalRecordCount

---
+++ TASVideos.Parsers/Parsers/CelTas.cs
@@ -0,0 +1,125 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using SharpCompress.Common;
+
+namespace TASVideos.MovieParsers.Parsers;
+
+[FileExtension("celtas")]
+internal class CelTas : Parser, IParser
+{
+	private const int FrameRate = 60;
+	private const int IGTFrameRate = 1000;
+
+	private const string FileTimeHeader = "FileTime: ";
+	private const string TotalRerecordCountHeader = "TotalRecordCount: ";
+	private const string RerecordCountHeader = "RecordCount: ";
+
+	public async Task<IParseResult> Parse(Stream file, long length)
+	{
+		var result = new SuccessResult(FileExtension)
+		{
+			Region = RegionType.Ntsc,
+			SystemCode = SystemCodes.Celeste
+		};
+
+		var fileTimeFound = 0;
+		var totalRecordCountUsed = false;
+
+		using var reader = new StreamReader(file);
+		while (await reader.ReadLineAsync() is { } s)
+		{
+			if (s.StartsWith(FileTimeHeader))
+			{
+				fileTimeFound = 1;
+				if (string.IsNullOrWhiteSpace(s))
+				{
+					return Error("No FileTime duration found");
+				}
+
+				var split = s.Split([':', '.', ' ', '(']);
+				if (split.Length > 5) // This should always output at least 6 strings
+				{
+					var i = split.Length - 2;
+					var test = int.TryParse(split[i], out int milliseconds);
+					if (test)
+					{
+						result.Frames = milliseconds;
+						i--;
+					}
+
+					test = int.TryParse(split[i], out int seconds);
+					if (test)
+					{
+						result.Frames += seconds * 1000;
+						i--;
+					}
+
+					test = int.TryParse(split[i], out int minutes);
+					if (test)
+					{
+						result.Frames += minutes * 1000 * 60;
+						i--;
+					}
+
+					if (i > 1)
+					{
+						test = int.TryParse(split[i], out int hours);
+						if (test)
+						{
+							result.Frames += hours * 1000 * 60 * 60;
+						}
+					}
+				}
+				else
+				{
+					return Error("FileTime did not meet expected format");
+				}
+			}
+
+			if (s.StartsWith(TotalRerecordCountHeader))
+			{
+				var split = s.SplitWithEmpty(":");
+				if (split.Length > 1)
+				{
+					var intStr = split.Skip(1).First();
+					var test = int.TryParse(intStr, out int val);
+					if (test)
+					{
+						totalRecordCountUsed = true;
+						result.RerecordCount = val;
+					}
+				}
+			}
+
+			if (s.StartsWith(RerecordCountHeader) && !totalRecordCountUsed)
+			{
+				if (string.IsNullOrWhiteSpace(s))
+				{
+					result.WarnNoRerecords();
+				}
+
+				var split = s.SplitWithEmpty(":");
+				if (split.Length > 1)
+				{
+					var intStr = split.Skip(1).First();
+					var test = int.TryParse(intStr, out int val);
+					if (test)
+					{
+						result.RerecordCount = val;
+					}
+				}
+				else
+				{
+					result.WarnNoRerecords();
+				}
+			}
+		}
+
+		if (fileTimeFound == 0)
+		{
+			return Error("No FileTime found, cannot parse");
+		}
+
+		return result;
+	}
+}
---
+++ tests/TASVideos.MovieParsers.Tests/CelTasTests.cs
@@ -0,0 +1,65 @@
+﻿namespace TASVideos.MovieParsers.Tests;
+
+[TestClass]
+[TestCategory("CelTasParsers")]
+public class CelTasTests : BaseParserTests
+{
+	private readonly CelTas _celtasParser = new();
+
+	protected override string ResourcesPath => "TASVideos.MovieParsers.Tests.CelTasSampleFiles.";
+
+	[TestMethod]
+	[DataRow("missingfiletime.celtas", DisplayName = "Missing FileTime header creates error")]
+	[DataRow("improperfiletime.celtas", DisplayName = "Improper FileTime header creates error")]
+	public async Task Errors(string filename)
+	{
+		var result = await _celtasParser.Parse(Embedded(filename, out var length), length);
+		Assert.IsFalse(result.Success);
+		AssertNoWarnings(result);
+		Assert.AreEqual(1, result.Errors.Count());
+	}
+
+	[TestMethod]
+	public async Task ValidHeader()
+	{
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+	}
+
+	[TestMethod]
+	public async Task System()
+	{
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(SystemCodes.Celeste, result.SystemCode);
+	}
+
+	[TestMethod]
+	public async Task RerecordCount()
+	{
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(12, result.RerecordCount);
+	}
+
+	[TestMethod]
+	public async Task TotalRerecordCount()
+	{
+		var result = await _celtasParser.Parse(Embedded("2465ms_Integrated.celtas", out var length), length);
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(15, result.RerecordCount);
+	}
+
+	[TestMethod]
+	public async Task Length()
+	{
+		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		Assert.IsTrue(result.Success);
+		AssertNoWarningsOrErrors(result);
+		Assert.AreEqual(2465, result.Frames);
+	}
+}
