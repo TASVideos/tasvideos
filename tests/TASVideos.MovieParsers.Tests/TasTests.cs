4d62b70caa025a12b7940955647dc835f7268b56
InputEvelution <theprogamerwithaj@gmail.com>
2025-10-09 01:14:15 +1100

    Change .celtas to .tas

--- tests/TASVideos.MovieParsers.Tests/TasTests.cs
+++ tests/TASVideos.MovieParsers.Tests/TasTests.cs
@@ -1,19 +1,19 @@
 ﻿namespace TASVideos.MovieParsers.Tests;
 
 [TestClass]
-[TestCategory("CelTasParsers")]
-public class CelTasTests : BaseParserTests
+[TestCategory("TasParsers")]
+public class TasTests : BaseParserTests
 {
-	private readonly CelTas _celtasParser = new();
+	private readonly Tas _tasParser = new();
 
-	protected override string ResourcesPath => "TASVideos.MovieParsers.Tests.CelTasSampleFiles.";
+	protected override string ResourcesPath => "TASVideos.MovieParsers.Tests.TasSampleFiles.";
 
 	[TestMethod]
-	[DataRow("missingfiletime.celtas", DisplayName = "Missing FileTime header creates error")]
-	[DataRow("improperfiletime.celtas", DisplayName = "Improper FileTime header creates error")]
+	[DataRow("missingfiletime.tas", DisplayName = "Missing FileTime header creates error")]
+	[DataRow("improperfiletime.tas", DisplayName = "Improper FileTime header creates error")]
 	public async Task Errors(string filename)
 	{
-		var result = await _celtasParser.Parse(Embedded(filename, out var length), length);
+		var result = await _tasParser.Parse(Embedded(filename, out var length), length);
 		Assert.IsFalse(result.Success);
 		AssertNoWarnings(result);
 		Assert.AreEqual(1, result.Errors.Count());
@@ -22,7 +22,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task ValidHeader()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		var result = await _tasParser.Parse(Embedded("2465ms.tas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 	}
@@ -30,7 +30,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task System()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		var result = await _tasParser.Parse(Embedded("2465ms.tas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(SystemCodes.Celeste, result.SystemCode);
@@ -39,7 +39,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task RerecordCount()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		var result = await _tasParser.Parse(Embedded("2465ms.tas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(12, result.RerecordCount);
@@ -48,7 +48,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task TotalRerecordCount()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms_Integrated.celtas", out var length), length);
+		var result = await _tasParser.Parse(Embedded("2465ms_Integrated.tas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(15, result.RerecordCount);
@@ -57,7 +57,7 @@ public class CelTasTests : BaseParserTests
 	[TestMethod]
 	public async Task Length()
 	{
-		var result = await _celtasParser.Parse(Embedded("2465ms.celtas", out var length), length);
+		var result = await _tasParser.Parse(Embedded("2465ms.tas", out var length), length);
 		Assert.IsTrue(result.Success);
 		AssertNoWarningsOrErrors(result);
 		Assert.AreEqual(2465, result.Frames);
