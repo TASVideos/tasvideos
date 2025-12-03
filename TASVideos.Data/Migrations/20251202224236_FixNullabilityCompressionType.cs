d7ffcf8c56893f28a2c55ee0eba0d1db1615a62b
Masterjun <masterjun3@gmail.com>
2025-12-03 15:05:12 +0100

    PublicationFile.CompressionType is nullable, but the db model was out of sync, so this migration appears (#2239)

---
+++ TASVideos.Data/Migrations/20251202224236_FixNullabilityCompressionType.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TASVideos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixNullabilityCompressionType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "compression_type",
+                table: "publication_files",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "compression_type",
+                table: "publication_files",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
