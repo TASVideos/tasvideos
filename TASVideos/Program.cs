a891051663aac6bea032ed9cf130bc4de8b99054
Masterjun3 <masterjun3@gmail.com>
2026-02-21 14:10:34 +0100

    add try catch around main program host in production. this is also suggested by serilog. it allows us to log unexpected failures better

--- TASVideos/Program.cs
+++ TASVideos/Program.cs
@@ -13,80 +13,87 @@ var builder = WebApplication.CreateBuilder(args);
 // Manually specify the secret id (matching the csproj) here.
 builder.Configuration.AddUserSecrets("aspnet-TASVideos-02A8A629-2080-412F-A29C-61E23228B152");
 
-var settings = builder.Configuration.Get<AppSettings>()!;
-
-// Mvc Project Services
-builder.Services
-	.AddAppSettings(builder.Configuration)
-	.AddRequestLocalization()
-	.AddCookieConfiguration()
-	.AddGzipCompression(settings)
-	.AddServices();
-
-// Internal Libraries
-var dbConnection = settings.UseSampleDatabase
-	? settings.ConnectionStrings.PostgresSampleDataConnection
-	: settings.ConnectionStrings.PostgresConnection;
-
-builder.Services
-	.AddTasvideosData(builder.Environment.IsDevelopment(), dbConnection)
-	.AddTasvideosCore<WikiToTextRenderer, WikiToMetaDescriptionRenderer>(builder.Environment.IsDevelopment(), settings)
-	.AddTasvideosMovieParsers()
-	.AddTasvideosApi(settings);
-
-// 3rd Party
-JsEngineSwitcher.AllowCurrentProperty = false;
-builder.Services
-	.AddRazorPages(builder.Environment)
-	.AddIdentity(builder.Environment)
-	.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"))
-	.AddSingleton<IJsEngineSwitcher>(new JsEngineSwitcher([new V8JsEngineFactory()], V8JsEngine.EngineName))
-	.AddWebOptimizer(pipeline =>
-	{
-		pipeline.AddScssBundle("/css/bootstrap.css", "/css/bootstrap.scss");
-		pipeline.AddScssBundle("/css/site.css", "/css/site.scss");
-		pipeline.AddScssBundle("/css/forum.css", "/css/forum.scss");
-		pipeline.AddFiles("text/javascript", "/js/*");
-	})
-	.AddMetrics(settings);
-
-builder.Host.UseSerilog();
-
-var app = builder.Build();
-
-app
-	.UseExceptionHandlers(app.Environment)
-	.UseTasvideosApiEndpoints(builder.Environment)
-	.UseRobots()
-	.UseUserAgentMetrics(settings)
-	.UseMiddleware<HtmlRedirectionMiddleware>()
-	.UseRequestLocalization()
-	.UseGzipCompression(settings)
-	.UseWebOptimizer()
-	.UseStaticFilesWithExtensionMapping(builder.Environment)
-	.UseAuthentication()
-	.UseMiddleware<CustomLocalizationMiddleware>()
-	.UseSerilogRequestLogging()
-	.UseMvcWithOptions(app.Environment, settings);
-
-if (app.Environment.IsDevelopment())
-{
-	app.UseHttpsRedirection();
-}
-
-var configuration = new ConfigurationBuilder()
-	.AddJsonFile("appsettings.json")
-	.AddJsonFile($"appsettings.{app.Environment.EnvironmentName}.json", true)
-	.Build();
 Log.Logger = new LoggerConfiguration()
-	.ReadFrom.Configuration(configuration)
+	.ReadFrom.Configuration(builder.Configuration)
 	.CreateLogger();
 
-using (var scope = app.Services.CreateScope())
+try
 {
-	var services = scope.ServiceProvider;
+	var settings = builder.Configuration.Get<AppSettings>()!;
 
-	await DbInitializer.InitializeDatabase(services);
+	// Mvc Project Services
+	builder.Services
+		.AddAppSettings(builder.Configuration)
+		.AddRequestLocalization()
+		.AddCookieConfiguration()
+		.AddGzipCompression(settings)
+		.AddServices();
+
+	// Internal Libraries
+	var dbConnection = settings.UseSampleDatabase
+		? settings.ConnectionStrings.PostgresSampleDataConnection
+		: settings.ConnectionStrings.PostgresConnection;
+
+	builder.Services
+		.AddTasvideosData(builder.Environment.IsDevelopment(), dbConnection)
+		.AddTasvideosCore<WikiToTextRenderer, WikiToMetaDescriptionRenderer>(builder.Environment.IsDevelopment(), settings)
+		.AddTasvideosMovieParsers()
+		.AddTasvideosApi(settings);
+
+	// 3rd Party
+	JsEngineSwitcher.AllowCurrentProperty = false;
+	builder.Services
+		.AddRazorPages(builder.Environment)
+		.AddIdentity(builder.Environment)
+		.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"))
+		.AddSingleton<IJsEngineSwitcher>(new JsEngineSwitcher([new V8JsEngineFactory()], V8JsEngine.EngineName))
+		.AddWebOptimizer(pipeline =>
+		{
+			pipeline.AddScssBundle("/css/bootstrap.css", "/css/bootstrap.scss");
+			pipeline.AddScssBundle("/css/site.css", "/css/site.scss");
+			pipeline.AddScssBundle("/css/forum.css", "/css/forum.scss");
+			pipeline.AddFiles("text/javascript", "/js/*");
+		})
+		.AddMetrics(settings);
+
+	builder.Host.UseSerilog();
+
+	var app = builder.Build();
+
+	app
+		.UseExceptionHandlers(app.Environment)
+		.UseTasvideosApiEndpoints(builder.Environment)
+		.UseRobots()
+		.UseUserAgentMetrics(settings)
+		.UseMiddleware<HtmlRedirectionMiddleware>()
+		.UseRequestLocalization()
+		.UseGzipCompression(settings)
+		.UseWebOptimizer()
+		.UseStaticFilesWithExtensionMapping(builder.Environment)
+		.UseAuthentication()
+		.UseMiddleware<CustomLocalizationMiddleware>()
+		.UseSerilogRequestLogging()
+		.UseMvcWithOptions(app.Environment, settings);
+
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseHttpsRedirection();
+	}
+
+	using (var scope = app.Services.CreateScope())
+	{
+		var services = scope.ServiceProvider;
+
+		await DbInitializer.InitializeDatabase(services);
+	}
+
+	await app.RunAsync();
 }
-
-await app.RunAsync();
+catch (Exception ex) when (!builder.Environment.IsDevelopment()) // let exceptions propagate in development for better debugging support from IDEs
+{
+	Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+	Log.CloseAndFlush();
+}
