2655251e93339d55d32a094e4d38c515c7d9781b
TheMas3212 <TheMas3212@users.noreply.github.com>
2026-02-22 02:03:38 +1100

    Fix API Pagination (#2265)
    Fix pagination in api by updating default values for Core.IPagable.Offset to match default values from Api.Requests.ApiRequest

--- TASVideos.Core/Paging/IPageable.cs
+++ TASVideos.Core/Paging/IPageable.cs
@@ -20,8 +20,8 @@ public static class PageableExtensions
 {
 	public static int Offset(this IPageable? pageable)
 	{
-		var current = Math.Max(pageable?.CurrentPage ?? 0, 1);
-		var size = Math.Max(pageable?.PageSize ?? 0, 1);
+		var current = Math.Max(pageable?.CurrentPage ?? 1, 1);
+		var size = Math.Max(pageable?.PageSize ?? 100, 1);
 		return (current - 1) * size;
 	}
 }
