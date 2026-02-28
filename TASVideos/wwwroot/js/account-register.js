de66173358ce0df186b52cb8af42adac8181c1d0
Masterjun <masterjun3@gmail.com>
2026-02-28 16:32:39 +0100

    Make EmailConfirmationSent page actually work (#2270)
    * make EmailConfirmationSent page actually work, and make the server actually check if the captcha is valid
    
    * fix tests

--- TASVideos/wwwroot/js/account-register.js
+++ TASVideos/wwwroot/js/account-register.js
@@ -4,7 +4,3 @@ if (!document.querySelector("#TimeZone option:checked").value) {
 	let timezone = document.querySelector(`[data-offset="${timezoneOffset}"]`);
 	timezone.setAttribute('selected', 'selected');
 }
-
-function onSubmit() {
-	document.getElementById("register-form").submit();
-}
