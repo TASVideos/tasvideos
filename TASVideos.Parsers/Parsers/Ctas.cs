64bd05ac315677d5d6a252420ea0a70212f294c5
Ema Alvarado <calvarado.godoy.194@gmail.com>
2026-03-13 13:24:47 -0300

    Adds parser support for CoffeeTools (.ctas) (#2281)
    * Initial support for CoffeeTools TAS files
    
    * Added tests
    
    * Update TASVideos.Parsers/Parsers/Ctas.cs
    
    * Update TASVideos.Parsers/Parsers/Ctas.cs
    
    * Update TASVideos.Parsers/Parsers/Ctas.cs
    
    ---------
    
    Co-authored-by: ikuyo <ema@oneninefour.cl>
    Co-authored-by: adelikat <AndrewJDelikat@gmail.com>

---
+++ TASVideos.Parsers/Parsers/Ctas.cs
@@ -0,0 +1,58 @@
+namespace TASVideos.MovieParsers.Parsers;
+
+[FileExtension("ctas")]
+internal class Ctas : Parser, IParser
+{
+	private const double FrameRate = 60;
+	private const uint Magic = 0x53415443;
+	public async Task<IParseResult> Parse(Stream file, long length)
+	{
+		var result = new SuccessResult(FileExtension)
+		{
+			Region = RegionType.Ntsc,
+			SystemCode = SystemCodes.Windows,
+			FrameRateOverride = FrameRate
+		};
+
+		using var reader = new BinaryReader(file);
+		try
+		{
+			var magic = reader.ReadUInt32();
+			if (magic != Magic)
+			{
+				return InvalidFormat();
+			}
+
+			reader.ReadUInt32();
+			result.Frames = reader.ReadInt32();
+			int rngLen = reader.ReadInt32();
+
+			byte[] buf = new byte[1008];
+
+			reader.Read(buf);
+
+			for (int i = 0; i < result.Frames; i++)
+			{
+				reader.ReadUInt64();
+			}
+
+			for (int i = 0; i < rngLen; i++)
+			{
+				reader.ReadInt32();
+				reader.ReadInt64();
+			}
+		}
+		catch (System.IO.EndOfStreamException)
+		{
+			return InvalidFormat();
+		}
+
+		// check we hit the end of the file
+		if(reader.PeekChar() != -1)
+		{
+			return InvalidFormat();
+		}
+
+		return await Task.FromResult(result);
+	}
+}
