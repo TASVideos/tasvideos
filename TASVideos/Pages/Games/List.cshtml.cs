b7b659cc3daf7c01c466199a478a2142274517fc
Masterjun3 <masterjun3@gmail.com>
2026-05-04 20:30:38 +0200

    make game list sorting a determinstic sorting

--- TASVideos/Pages/Games/List.cshtml.cs
+++ TASVideos/Pages/Games/List.cshtml.cs
@@ -101,7 +101,7 @@ public class ListModel(ApplicationDbContext db) : BasePageModel
 		.SortedPageOf(paging);
 	}
 
-	[PagingDefaults(PageSize = 50, Sort = "-Publications")]
+	[PagingDefaults(PageSize = 50, Sort = "-Publications,Name,Id")]
 	public class GameListRequest : PagingModel
 	{
 		public string? System { get; set; }
