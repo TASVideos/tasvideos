ca2d49f2027dbb64b49873d6824221f5aee517ec
ikuyo <ema@oneninefour.cl>
2026-05-19 22:09:45 -0400

    Update ctas parser with CTAS format version 4

--- TASVideos.Parsers/Parsers/Ctas.cs
+++ TASVideos.Parsers/Parsers/Ctas.cs
@@ -20,27 +20,42 @@ internal class CTas : Parser, IParser
 		try
 		{
 			uint m_magic = reader.ReadUInt32();
-			if(m_magic != Magic) {
+			if(m_magic != Magic)
+			{
 				return InvalidFormat();
 			}
 
-			reader.ReadUInt32();
-			result.Frames = (int)reader.ReadUInt32();
+			uint version = reader.ReadUInt32();
+			uint framecount = reader.ReadUInt32();
 			uint rngLen = reader.ReadUInt32();
+			uint reportedTime = 0;
+
+			if (version >= 4)
+			{
+				result.RerecordCount = (int)reader.ReadUInt32();
+				reportedTime = reader.ReadUInt32();
+				result.Frames = (int)(reportedTime / (1000 / 60));
+			}
 
 			file.Seek(1024, SeekOrigin.Begin);
 
-			for (int i = 0; i < result.Frames; i++)
+			for (int i = 0; i < framecount; i++)
 			{
 				reader.ReadUInt64();
 			}
+
 			for (int i = 0; i < rngLen; i++)
 			{
 				reader.ReadInt32();
 				reader.ReadDouble();
 			}
+
+			if (reportedTime <= 0)
+			{
+				result.Frames = (int)framecount;
+			}
 		}
-		catch (System.IO.EndOfStreamException e)
+		catch (System.IO.EndOfStreamException)
 		{
 			return InvalidFormat();
 		}
