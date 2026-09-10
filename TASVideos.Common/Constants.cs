34aede28319083f73f2959a9e1b2d8d73328f6bb
Masterjun3 <masterjun3@gmail.com>
2026-09-10 22:38:03 +0200

    fix the order of reactions

--- TASVideos.Common/Constants.cs
+++ TASVideos.Common/Constants.cs
@@ -83,7 +83,7 @@ public static class SiteGlobalConstants
 
 	public const string MainIvatarDomain = "seccdn.libravatar.org"; // keep in sync with profile-settings.js
 
-	public static readonly string[] AllowedReactions = ["👍", "👎", "😄", "😕", "❤️", "🎉", "🚀", "👀"];
+	public static readonly string[] AllowedReactions = ["👍", "👎", "😄", "🎉", "😕", "❤️", "🚀", "👀"];
 }
 
 public static class ForumConstants
